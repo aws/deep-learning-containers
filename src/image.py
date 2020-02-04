1298093999cb78d1dfca3f23cd6370b8900c6ba9
SK <saravsak@amazon.com>
2020-02-04 18:10:44 +0000

    Added license file information to all py files

--- src/image.py
+++ src/image.py
@@ -1,7 +1,17 @@
-"""
-This file defines the DockerImage class which contains the attributes and functions for
-a docker image
-"""
+'''
+Copyright 2019-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"). You
+may not use this file except in compliance with the License. A copy of
+the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+or in the "license" file accompanying this file. This file is
+distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ANY KIND, either express or implied. See the License for the specific
+language governing permissions and limitations under the License.
+'''
 from datetime import datetime
 
 from docker import APIClient
