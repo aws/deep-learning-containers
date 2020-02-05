001670cf4527cc4f6fddf7b5daed54a71149f2a5
Ubuntu <ubuntu@ip-172-31-65-42.ec2.internal>
2020-02-05 19:39:32 +0000

    Removed not isatty

--- src/output.py
+++ src/output.py
@@ -82,12 +82,6 @@ class OutputFormatter:
             output_type="list", initial_len=len(futures.items()), interval=0
         ) as output:
             num_iterations = 0
-            #if not isatty:
-            #    output = [""] * len(futures.items())
-                #running_iterator = 0
-                #for image, _ in futures.items():
-                #    output[running_iterator] = f"{image}.........R"
-                #    running_iterator += 1
             self.print_lines(output)
             while True:
                 i = 0
