1d3c4eee0ee5c301b59d2632378115e9dcc5c488
Arjuna Keshavan <arjunake@amazon.com>
2020-02-19 13:56:50 -0800

    xfail for tf inference for now

--- test/sanity/test_pip_check.py
+++ test/sanity/test_pip_check.py
@@ -5,5 +5,9 @@ def test_pip_check(image, docker_client):
     """
     Test to run pip sanity tests
     """
+    if "tensorflow-inference" in image:
+        pytest.xfail(reason='Tensorflow serving api requires tensorflow, but we explicitly do not install'
+                            'tensorflow in serving containers.')
+
     # Add null entrypoint to ensure command exits immediately
     docker_client.containers.run(image, command="pip check", entrypoint='')
