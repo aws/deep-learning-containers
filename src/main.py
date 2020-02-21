c4b3a76a3f78f9bd972d04f7e3c41954a0399d68
SK <saravsak@amazon.com>
2020-02-21 06:49:23 -0800

    Checkpoint

--- src/main.py
+++ src/main.py
@@ -1,22 +1,9 @@
-"""
-Copyright 2019-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License"). You
-may not use this file except in compliance with the License. A copy of
-the License is located at
-
-    http://aws.amazon.com/apache2.0/
-
-or in the "license" file accompanying this file. This file is
-distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-ANY KIND, either express or implied. See the License for the specific
-language governing permissions and limitations under the License.
-"""
-
+from github import GitHubStatusHandler
 import os
+from image_builder import image_builder
 import argparse
-from copy import deepcopy
-import concurrent.futures
+import constants 
+import re
 
 # import utils
 import constants
@@ -27,150 +14,70 @@ from image import DockerImage
 from buildspec import Buildspec
 from output import OutputFormatter
 
-
-# TODO: Abstract away to ImageBuilder class
 if __name__ == "__main__":
-    FORMATTER = OutputFormatter(constants.PADDING)
+
     parser = argparse.ArgumentParser(description="Program to build docker images")
     parser.add_argument("--buildspec", required=True, type=str)
-    parser.add_argument("--frameworks", type=str, default=constants.ALL)
+    parser.add_argument("--framework", required=True,  type=str)
     parser.add_argument("--device_types", type=str, default=constants.ALL)
     parser.add_argument("--image_types", type=str, default=constants.ALL)
     parser.add_argument("--python_versions", type=str, default=constants.ALL)
 
-    ARGS = parser.parse_args()
+    args = parser.parse_args()
 
     # Set necessary environment variables
-    to_build = {'frameworks': constants.FRAMEWORKS,
-                'device_types': constants.DEVICE_TYPES,
+    to_build = {'device_types': constants.DEVICE_TYPES,
                 'image_types': constants.IMAGE_TYPES,
                 'python_versions': constants.PYTHON_VERSIONS}
-    if ARGS.frameworks != constants.ALL:
-        to_build['frameworks'] = constants.FRAMEWORKS.intersection(set(ARGS.frameworks.split(',')))
-    if ARGS.device_types != constants.ALL:
-        to_build['device_types'] = constants.DEVICE_TYPES.intersection(set(ARGS.device_types.split(',')))
-    if ARGS.image_types != constants.ALL:
-        to_build['image_types'] = constants.IMAGE_TYPES.intersection(set(ARGS.image_types.split(',')))
-    if ARGS.python_versions != constants.ALL:
-        to_build['python_versions'] = constants.PYTHON_VERSIONS.intersection(set(ARGS.python_versions.split(',')))
 
-    for framework in to_build['frameworks']:
-        for device_type in to_build['device_types']:
-            for image_type in to_build['image_types']:
-                for python_version in to_build['python_versions']:
-                    env_variable = f"{framework.upper()}_{device_type.upper()}_{image_type.upper()}_{python_version.upper()}"
-                    os.environ[env_variable] = 'true'
+    framework = args.framework
+    device_types = []
+    image_types = []
+    python_versions = []
 
-    BUILDSPEC = Buildspec()
-    BUILDSPEC.load(ARGS.buildspec)
-    IMAGES = []
+    if os.environ.get('BUILD_CONTEXT') == 'PR':
+        # g = GitHubStatusHandler("aws", "deep-learning-containers", "11")
+        files = '\n'.join(g.get_pr_changeset())
 
-    for image in BUILDSPEC["images"].items():
-        ARTIFACTS = deepcopy(BUILDSPEC["context"])
+        dockerfile_match = re.findall("\S+Dockerfile\S+", files) 
+        buildspec_match = re.findall("\S+\/buildspec.yml", files)
+        src_match = re.findall("src\/\S+", files)
 
-        image_name = image[0]
-        image_config = image[1]
+        for dockerfile in dockerfile_match:
+            _, image_type, _, version, python_version, _ = dockerfile.split('/')
+            device_type = dockerfile.split('.')[-1]
+            device_types.append(device_type)
+            image_types.append(image_type)
+            python_versions.append(python_version)
 
-        if image_config.get("version") is not None:
-            if BUILDSPEC["version"] != image_config.get("version"):
-                continue
+        for buildspec in buildspec_match:
+            buildspec_framework, _ = buildspec.split('/')
+            if buildspec_framework == framework:
+                device_types = constants.ALL
+                image_types = constants.ALL
+                python_versions = constants.ALL
 
-        if image_config.get("context") is not None:
-            ARTIFACTS.update(image_config["context"])
+        if len(src_match) != 0:
+            device_types = constants.ALL
+            image_types = constants.ALL
+            python_versions = constants.ALL
 
-        ARTIFACTS.update({"dockerfile": {"source": image_config["docker_file"], "target": "Dockerfile"}})
+    else:        
+        device_types = args.device_types.split(',')
+        image_types = args.image_types.split(',')
+        python_versions = args.python_versions.split(',')
+    
+    if device_types != constants.ALL:
+        to_build['device_types'] = constants.DEVICE_TYPES.intersection(set(device_types))
+    if image_types != constants.ALL:
+        to_build['image_types'] = constants.IMAGE_TYPES.intersection(set(image_types))
+    if python_versions != constants.ALL:
+        to_build['python_versions'] = constants.PYTHON_VERSIONS.intersection(set(python_versions))
+    
+    for device_type in to_build['device_types']:
+        for image_type in to_build['image_types']:
+            for python_version in to_build['python_versions']:
+                env_variable = f"{framework.upper()}_{device_type.upper()}_{image_type.upper()}_{python_version.upper()}"
+                os.environ[env_variable] = 'true'
 
-        context = Context(
-            ARTIFACTS, f"build/{image_name}.tar.gz", image_config["root"]
-        )
-
-        """
-        Override parameters from parent in child.
-        """
-
-        info = {
-            "account_id": str(BUILDSPEC["account_id"]),
-            "region": str(BUILDSPEC["region"]),
-            "framework": str(BUILDSPEC["framework"]),
-            "version": str(BUILDSPEC["version"]),
-            "root": str(image_config["root"]),
-            "name": str(image_name),
-            "device_type": str(image_config["device_type"]),
-            "python_version": str(image_config["python_version"]),
-            "image_type": str(image_config["image_type"]),
-            "image_size_baseline": int(image_config["image_size_baseline"]),
-        }
-
-        image_object = DockerImage(
-            info=info,
-            dockerfile=image_config["docker_file"],
-            repository=image_config["repository"],
-            tag=image_config["tag"],
-            to_build=image_config["build"],
-            context=context,
-        )
-
-        IMAGES.append(image_object)
-
-
-    FORMATTER.banner("DLC")
-    FORMATTER.title("Status")
-
-    THREADS = {}
-
-    # In the context of the ThreadPoolExecutor each instance of image.build submitted 
-    # to it is executed concurrently in a separate thread. 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        for image in IMAGES:
-            THREADS[image.name] = executor.submit(image.build)
-
-        FORMATTER.progress(THREADS)
-
-        FORMATTER.title("Build Logs")
-
-        if not os.path.isdir("logs"):
-            os.makedirs("logs")
-
-        for image in IMAGES:
-            FORMATTER.title(image.name)
-            FORMATTER.table(image.info.items())
-            FORMATTER.separator()
-            FORMATTER.print_lines(image.log)
-            with open(f"logs/{image.name}", "w") as fp:
-                fp.write("/n".join(image.log))
-                image.summary["log"] = f"logs/{image.name}"
-
-        FORMATTER.title("Summary")
-
-        for image in IMAGES:
-            FORMATTER.title(image.name)
-            FORMATTER.table(image.summary.items())
-
-        FORMATTER.title("Errors")
-        ANY_FAIL = False
-        for image in IMAGES:
-            if image.build_status == constants.FAIL:
-                FORMATTER.title(image.name)
-                FORMATTER.print_lines(image.log[-10:])
-                ANY_FAIL = True
-        if ANY_FAIL:
-            raise Exception("Build failed")
-        else:
-            FORMATTER.print("No errors")
-
-        FORMATTER.title("Uploading Metrics")
-        metrics = Metrics(
-            context=constants.BUILD_CONTEXT,
-            region=BUILDSPEC["region"],
-            namespace=constants.METRICS_NAMESPACE,
-        )
-        for image in IMAGES:
-            try:
-                metrics.push_image_metrics(image)
-            except Exception as e:
-                if ANY_FAIL:
-                    raise Exception(f"Build failed.{e}")
-                else:
-                    raise Exception(f"Build passed. {e}")
-
-        FORMATTER.separator()
+    image_builder(args.buildspec)
