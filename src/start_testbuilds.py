04b9a3c4f759b45461fca89bc65aca091f6ed9c5
Arjuna <33526713+arjkesh@users.noreply.github.com>
2020-02-24 17:39:20 -0800

    Add simple testspec and boto3 functions to start test job after build (#10)

---
+++ src/start_testbuilds.py
@@ -0,0 +1,51 @@
+"""
+Copyright 2019-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"). You
+may not use this file except in compliance with the License. A copy of
+the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+or in the "license" file accompanying this file. This file is
+distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ANY KIND, either express or implied. See the License for the specific
+language governing permissions and limitations under the License.
+"""
+import os
+import json
+import logging
+
+import boto3
+
+import constants
+
+
+logger = logging.getLogger(__name__)
+
+
+def run_test_job(commit, codebuild_project):
+    with open(constants.TEST_ENV) as test_env_file:
+        env_overrides = json.load(test_env_file)
+
+    client = boto3.client("codebuild")
+    return client.start_build(
+        projectName=codebuild_project,
+        environmentVariablesOverride=env_overrides,
+        sourceVersion=commit,
+    )
+
+
+def main():
+    build_context = os.getenv("BUILD_CONTEXT")
+    if build_context != "PR":
+        logger.info(f"Not triggering test jobs from boto3, as BUILD_CONTEXT is {build_context}")
+        return
+
+    # Start sanity test job
+    commit = os.getenv("CODEBUILD_RESOLVED_SOURCE_VERSION")
+    run_test_job(commit, "dlc-sanity-test")
+
+
+if __name__ == "__main__":
+    main()
