d9565fd8792f2dcc166ab3a6a41370d46b34d948
Arjuna Keshavan <arjunake@amazon.com>
2020-02-24 22:48:36 -0800

    test

--- test/testrunner.py
+++ test/testrunner.py
@@ -1,11 +1,12 @@
 import os
 
-from src.github import GitHubHandler
+#from src.github import GitHubHandler
 
 if __name__ == "__main__":
     print('*************************')
     print(os.getenv('TEST_TRIGGER'))
     context = "dlc-sanity-test-{}".format(os.getenv("TEST_TRIGGER"))
-    handler = GitHubHandler()
-    handler.set_status(state="success", context=context, description="test")
+    print(context)
+    #handler = GitHubHandler()
+    #handler.set_status(state="success", context=context, description="test")
     print('*************************')
