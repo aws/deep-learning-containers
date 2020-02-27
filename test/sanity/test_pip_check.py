106b2ebdbcd55792b35315510f422767f88e05b2
Arjuna Keshavan <arjunake@amazon.com>
2020-02-27 00:32:14 -0800

    try shell=True

--- test/sanity/test_pip_check.py
+++ test/sanity/test_pip_check.py
@@ -12,4 +12,5 @@ def test_pip_check(image):
                             'tensorflow in serving containers.')
 
     # Add null entrypoint to ensure command exits immediately
-    subprocess.run(["docker", "run", "-it", "--entrypoint=''", image, 'pip', 'check'], capture_output=True, check=True)
+    subprocess.run(["docker", "run", "-it", "--entrypoint=''", image, 'pip', 'check'], shell=True,
+                   capture_output=True, check=True)
