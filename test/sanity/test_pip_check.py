e7757d7cd8f9b3663da19cecdb51bffa37deeed7
Arjuna Keshavan <arjunake@amazon.com>
2020-02-27 00:22:23 -0800

    change entrypoint location

--- test/sanity/test_pip_check.py
+++ test/sanity/test_pip_check.py
@@ -12,4 +12,4 @@ def test_pip_check(image):
                             'tensorflow in serving containers.')
 
     # Add null entrypoint to ensure command exits immediately
-    subprocess.run(f"docker run --entrypoint='' -it {image} pip check", capture_output=True, check=True)
+    subprocess.run(f"docker run -it --entrypoint='' {image} pip check", capture_output=True, check=True)
