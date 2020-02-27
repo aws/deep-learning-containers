74edae3aec7a11c7a8514903778feebed6064e20
Arjuna Keshavan <arjunake@amazon.com>
2020-02-27 01:36:33 -0800

    testing if ec2 instance starts

--- test/conftest.py
+++ test/conftest.py
@@ -34,7 +34,7 @@ def ec2_client():
     return boto3.client('ec2')
 
 
-@pytest.mark.timeout(600)
+@pytest.mark.timeout(300)
 @pytest.fixture(scope="session")
 def start_ec2_instance(ec2_client, ec2_instance_type):
     key = ec2_client.create_key_pair(KeyName="pytest.pem")
@@ -46,18 +46,21 @@ def start_ec2_instance(ec2_client, ec2_instance_type):
         MinCount=1
     )
     instance_id = instances[0].id
+    resp = ec2_client.describe_instances(InstanceIds=[instance_id])
+    running = False
+    while not running:
+        if resp.get('InstanceStatuses', {})[0].get('InstanceState', {}).get('Name') == 'running':
+            return instance_id
 
 
-
-
-@pytest.fixture(scope="session")
-def run_on_ec2_instance(request, start_ec2_instance):
-    ec2 = boto3.client('ec2')
-    return request.config.getoption("--ec2-instance-type")
+# @pytest.fixture(scope="session")
+# def run_on_ec2_instance(request, start_ec2_instance):
+#     ec2 = boto3.client('ec2')
+#     return request.config.getoption("--ec2-instance-type")
 
 
 def pytest_generate_tests(metafunc):
     if "image" in metafunc.fixturenames:
         metafunc.parametrize("image", metafunc.config.getoption("--images"))
-    if "ec2_instance_type" in metafunc.fixturenames:
-        metafunc.parametrize("ec2_instance_type", metafunc.config.getoption("--ec2-instance-types"))
+    # if "ec2_instance_type" in metafunc.fixturenames:
+    #     metafunc.parametrize("ec2_instance_type", metafunc.config.getoption("--ec2-instance-types"))
---
+++ test/sanity/test_ec2_spawn.py
@@ -0,0 +1,5 @@
+import pytest
+
+
+def test_ec2_spawn(start_ec2_instance):
+    print('check if ec2 instance starts')
