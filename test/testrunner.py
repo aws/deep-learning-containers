edd974c327c5e8348fdf750dec8054996d7ed412
Arjuna Keshavan <arjunake@amazon.com>
2020-03-04 20:29:36 -0800

    experiment with test reports

--- test/testrunner.py
+++ test/testrunner.py
@@ -23,7 +23,7 @@ def run_sagemaker_pytest_cmd(image):
     docker_base_name, tag = image.split("/")[1].split(":")
 
     # Assign instance type
-    instance_type = "ml.p3.16xlarge" if "gpu" in tag else "ml.c5.18xlarge"
+    instance_type = "ml.p3.16xlarge" if "gpu" in tag else "ml.c4.8xlarge"
 
     # Get path to test directory
     find_path = docker_base_name.split("-")
@@ -41,15 +41,17 @@ def run_sagemaker_pytest_cmd(image):
             "sagemaker_tests", framework, f"{framework}{tf_major_version}_training"
         )
 
+    test_report = os.path.join(os.getcwd(), f"{tag}.xml")
     mx_pytest_command = f"pytest {integration_path} --region {region} --docker-base-name " \
-                        f"{docker_base_name} --tag {tag} --aws-id {account_id} --instance-type ml.c4.8xlarge"
+                        f"{docker_base_name} --tag {tag} --aws-id {account_id} --instance-type {instance_type} " \
+                        f"--junitxml {test_report}"
 
     context = Context()
     with context.cd(path):
         context.run(f"virtualenv {tag}")
         with context.prefix(f"source {tag}/bin/activate"):
             context.run("pip install -r requirements.txt", warn=True)
-            if framework == "mxnet":
+            if framework == "mxnet" and "cpu" in tag:
                 context.run(mx_pytest_command)
 
 
@@ -71,8 +73,9 @@ def main():
     dlc_images = os.getenv("DLC_IMAGES")
 
     if test_type == "sanity":
+        report = os.path.join(os.getcwd(), 'sanity.xml')
         os.chdir("dlc_tests")
-        sys.exit(pytest.main([test_type]))
+        sys.exit(pytest.main([test_type, f"--junitxml={report}"]))
     elif test_type == "sagemaker":
         run_sagemaker_tests(dlc_images.split(" "))
     else:
