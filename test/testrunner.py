ac6081707e20f6de585766836894b84d349e8737
Arjuna Keshavan <arjunake@amazon.com>
2020-03-05 16:30:59 -0800

    Run all the SM tests!

--- test/testrunner.py
+++ test/testrunner.py
@@ -8,15 +8,13 @@ import pytest
 from invoke.context import Context
 
 
-def run_sagemaker_pytest_cmd(image):
+def generate_sagemaker_pytest_cmd(image):
     """
-    Run pytest in a virtual env for a particular image
+    Parses the image ECR url and returns appropriate pytest command
 
-    Expected to run via multiprocessing
-
-    :param image: ECR url
+    :param image: ECR url of image
+    :return: <tuple> pytest command to be run, path where it should be executed, image tag
     """
-
     region = os.getenv("AWS_REGION", "us-west-2")
     integration_path = os.path.join("integration", "sagemaker")
     account_id = os.getenv("ACCOUNT_ID", image.split(".")[0])
@@ -32,26 +30,50 @@ def run_sagemaker_pytest_cmd(image):
     framework = find_path[1]
     job_type = find_path[2]
     path = os.path.join("sagemaker_tests", framework, job_type)
+    aws_id_arg = "--aws-id"
+    docker_base_arg = "--docker-base-name"
 
-    # NOTE: We are relying on tag structure to get TF major version. If tagging changes, this will break.
-    if framework == "tensorflow" and job_type == "training":
-        tf_major_version = tag.split("-")[-1].split(".")[0]
-        path = os.path.join(
-            "sagemaker_tests", framework, f"{framework}{tf_major_version}_training"
-        )
+    # Conditions for modifying tensorflow SageMaker pytest commands
+    if framework == "tensorflow":
+        if job_type == "training":
+            aws_id_arg = "--account-id"
+
+            # NOTE: We are relying on tag structure to get TF major version. If tagging changes, this will break.
+            tf_major_version = tag.split("-")[-1].split(".")[0]
+            path = os.path.join(
+                "sagemaker_tests", framework, f"{framework}{tf_major_version}_training"
+            )
+        else:
+            aws_id_arg = "--registry"
+            docker_base_arg = "--repo"
 
     test_report = os.path.join(os.getcwd(), f"{tag}.xml")
-    pytest_command = f"pytest {integration_path} --region {region} --docker-base-name " \
-                        f"{docker_base_name} --tag {tag} --aws-id {account_id} --instance-type {instance_type} " \
-                        f"--junitxml {test_report}"
+    return (
+        f"pytest {integration_path} --region {region} {docker_base_arg} "
+        f"{docker_base_name} --tag {tag} {aws_id_arg} {account_id} --instance-type {instance_type} "
+        f"--junitxml {test_report}",
+        path,
+        tag,
+    )
+
+
+def run_sagemaker_pytest_cmd(image):
+    """
+    Run pytest in a virtual env for a particular image
+
+    Expected to run via multiprocessing
+
+    :param image: ECR url
+    """
+
+    pytest_command, path, tag = generate_sagemaker_pytest_cmd(image)
 
     context = Context()
     with context.cd(path):
         context.run(f"virtualenv {tag}")
         with context.prefix(f"source {tag}/bin/activate"):
             context.run("pip install -r requirements.txt", warn=True)
-            if framework != "tensorflow" and "cpu" in tag:
-                context.run(pytest_command)
+            context.run(pytest_command)
 
 
 def run_sagemaker_tests(images):
@@ -59,7 +81,6 @@ def run_sagemaker_tests(images):
     Function to set up multiprocessing for SageMaker tests
 
     :param images:
-    :return:
     """
     pool_number = len(images)
     with Pool(pool_number) as p:
@@ -72,7 +93,7 @@ def main():
     dlc_images = os.getenv("DLC_IMAGES")
 
     if test_type == "sanity":
-        report = os.path.join(os.getcwd(), 'sanity.xml')
+        report = os.path.join(os.getcwd(), "sanity.xml")
         os.chdir("dlc_tests")
         sys.exit(pytest.main([test_type, f"--junitxml={report}"]))
     elif test_type == "sagemaker":
