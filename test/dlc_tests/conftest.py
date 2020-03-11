23e117cb853c1ffd3fea29bff0ba6a85e8712be7
Arjuna Keshavan <arjunake@amazon.com>
2020-03-11 00:25:33 -0700

    Add ecs specific fixtures to new ecs conftest

--- test/dlc_tests/conftest.py
+++ test/dlc_tests/conftest.py
@@ -1,15 +1,10 @@
 import os
-import time
 
 import boto3
 import docker
 import pytest
 
-from test.test_utils import run_subprocess_cmd
-
-
-# Constant to represent AMI Id used to spin up EC2 instances
-UBUNTU_16_BASE_DLAMI = "ami-0e57002aaafd42113"
+from test.test_utils import run_subprocess_cmd, UBUNTU_16_BASE_DLAMI
 
 
 # Immutable constant for framework specific image fixtures
@@ -89,68 +84,6 @@ def ec2_instance(request, ec2_client, ec2_instance_type, ec2_resource):
     waiter = ec2_client.get_waiter("instance_running")
     waiter.wait(InstanceIds=[instance_id])
     return instances[0]
-
-
-@pytest.fixture(scope="session")
-def ecs_cluster(request, ecs_client):
-    """
-
-    :param request:
-    :param ec2_instance:
-    :param ecs_client:
-    :return:
-    """
-    cluster_name = f"{request.node.name}-ecs-cluster"
-    ecs_client.create_cluster(
-        clusterName=cluster_name
-    )
-
-    def delete_ecs_cluster():
-        ecs_client.delete_cluster(cluster=cluster_name)
-
-    request.addfinalizer(delete_ecs_cluster)
-
-    # Wait for max 10 minutes for cluster status to be active
-    timeout = time.time() + 600
-    is_active = False
-    while not is_active:
-        if time.time() > timeout:
-            raise TimeoutError(f"ECS cluster {cluster_name} timed out on creation")
-        response = ecs_client.describe_clusters(clusters=[cluster_name])
-        if response.get('clusters', [{}])[0].get('status') == 'ACTIVE':
-            is_active = True
-
-    return cluster_name
-
-
-@pytest.mark.timeout(300)
-@pytest.fixture(scope="session")
-def ecs_container_instance(request, ecs_cluster, ec2_client, ec2_resource, ec2_instance_type):
-    user_data = f"{ecs_cluster}.txt"
-    with open(user_data, 'w') as user_data_file:
-        user_data_file.write(f"#!/bin/bash\necho ECS_CLUSTER={ecs_cluster} >> /etc/ecs/ecs.config")
-
-    instances = ec2_resource.create_instances(
-        KeyName="pytest.pem",
-        ImageId=UBUNTU_16_BASE_DLAMI,
-        # hard coding for now
-        InstanceType="p2.8xlarge",
-        MaxCount=1,
-        MinCount=1,
-        UserData=f"file://{user_data}",
-        IamInstanceProfile={"Name": "ecsInstanceRole"}
-    )
-    instance_id = instances[0].id
-
-    # Define finalizer to terminate instance after this fixture completes
-    def terminate_ec2_instance():
-        ec2_client.terminate_instances(InstanceIds=[instance_id])
-
-    request.addfinalizer(terminate_ec2_instance)
-
-    waiter = ec2_client.get_waiter("instance_running")
-    waiter.wait(InstanceIds=[instance_id])
-    return instances[0], ecs_cluster
 
 
 @pytest.fixture(scope="session")
---
+++ test/dlc_tests/ecs/conftest.py
@@ -0,0 +1,77 @@
+import time
+
+import pytest
+
+from test.test_utils import UBUNTU_16_BASE_DLAMI
+
+
+@pytest.fixture(scope="session")
+def ecs_cluster(request, ecs_client):
+    """
+    Fixture to handle spin up and tear down of ecs cluster
+
+    :param request:
+    :param ecs_client:
+    :return:
+    """
+    cluster_name = f"{request.node.name}-ecs-cluster"
+    ecs_client.create_cluster(
+        clusterName=cluster_name
+    )
+
+    def delete_ecs_cluster():
+        ecs_client.delete_cluster(cluster=cluster_name)
+
+    request.addfinalizer(delete_ecs_cluster)
+
+    # Wait for max 10 minutes for cluster status to be active
+    timeout = time.time() + 600
+    is_active = False
+    while not is_active:
+        if time.time() > timeout:
+            raise TimeoutError(f"ECS cluster {cluster_name} timed out on creation")
+        response = ecs_client.describe_clusters(clusters=[cluster_name])
+        if response.get('clusters', [{}])[0].get('status') == 'ACTIVE':
+            is_active = True
+
+    return cluster_name
+
+
+@pytest.mark.timeout(300)
+@pytest.fixture(scope="session")
+def ecs_container_instance(request, ecs_cluster, ec2_client, ec2_resource, ec2_instance_type):
+    """
+    Fixture to handle spin up and tear down of ECS container instance
+
+    :param request: pytest request object
+    :param ecs_cluster: ecs cluster fixture
+    :param ec2_client: boto3 ec2 client
+    :param ec2_resource: boto3 ec2 resource
+    :param ec2_instance_type: eventually to be used
+    :return:
+    """
+    user_data = f"{ecs_cluster}.txt"
+    with open(user_data, 'w') as user_data_file:
+        user_data_file.write(f"#!/bin/bash\necho ECS_CLUSTER={ecs_cluster} >> /etc/ecs/ecs.config")
+
+    instances = ec2_resource.create_instances(
+        KeyName="pytest.pem",
+        ImageId=UBUNTU_16_BASE_DLAMI,
+        # hard coding for now
+        InstanceType="p2.8xlarge",
+        MaxCount=1,
+        MinCount=1,
+        UserData=f"file://{user_data}",
+        IamInstanceProfile={"Name": "ecsInstanceRole"}
+    )
+    instance_id = instances[0].id
+
+    # Define finalizer to terminate instance after this fixture completes
+    def terminate_ec2_instance():
+        ec2_client.terminate_instances(InstanceIds=[instance_id])
+
+    request.addfinalizer(terminate_ec2_instance)
+
+    waiter = ec2_client.get_waiter("instance_running")
+    waiter.wait(InstanceIds=[instance_id])
+    return instances[0], ecs_cluster
