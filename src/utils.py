ed4ba090b38aaaeb3ac8aa234c8681b26ba2ec14
satish Kumar Gollaprolu <satishgollaprolu@gmail.com>
2020-03-11 22:07:49 -0700

    Changeset pr (#52)
    * add logic to run builds for file changes under dlc_tests, only add new images build for test
    
    * refactor code for parsing all the files changed from pull requests 
    
    Co-authored-by: Satish Gollaprolu <sgollapr@amazon.com>

--- src/utils.py
+++ src/utils.py
@@ -19,6 +19,28 @@ import json
 import constants
 from github import GitHubHandler
 
+class JobParameters:
+    image_types = []
+    device_types = []
+    py_versions = []
+    @staticmethod
+    def build_for_all_images():
+        JobParameters.image_types = constants.ALL
+        JobParameters.device_types = constants.ALL
+        JobParameters.py_versions = constants.ALL
+    @staticmethod
+    def build_for_all_device_types_py_versions():
+        JobParameters.device_types = constants.ALL
+        JobParameters.py_versions = constants.ALL
+    @staticmethod
+    def do_build_all_images():
+        return (
+            JobParameters.device_types == constants.ALL and
+            JobParameters.image_types == constants.ALL and
+            JobParameters.py_versions == constants.ALL
+        )
+
+
 def get_pr_modified_files(pr_number):
     """
     Fetches all the files modified for a git pull request and return them as a string
@@ -30,16 +52,9 @@ def get_pr_modified_files(pr_number):
     files = "\n".join(files)
     return files
 
-def get_modified_docker_files_info(files, framework, device_types=None, image_types=None, py_versions=None):
-    if py_versions is None:
-        py_versions = []
-    if image_types is None:
-        image_types = []
-    if device_types is None:
-        device_types = []
-    rule = re.findall(r"\S+Dockerfile\S+", files)
+def parse_modified_docker_files_info(files, framework, pattern=""):
+    rule = re.findall(rf"{pattern}", files)
     for dockerfile in rule:
-
         dockerfile = dockerfile.split("/")
         framework_change = dockerfile[0]
 
@@ -50,92 +65,75 @@ def get_modified_docker_files_info(files, framework, device_types=None, image_ty
         image_type = dockerfile[1]
         py_version = dockerfile[4]
         device_type = dockerfile[-1].split(".")[-1]
-        device_types.append(device_type)
-        image_types.append(image_type)
-        py_versions.append(py_version)
-    return  device_types, image_types, py_versions
+        # Use class static variables to avoid passing, returning the varibles from all functions
+        JobParameters.device_types.append(device_type)
+        JobParameters.image_types.append(image_type)
+        JobParameters.py_versions.append(py_version)
 
-def get_modifed_buidspec_yml_info(files, framework, device_types=None, image_types=None, py_versions=None):
-    if py_versions is None:
-        py_versions = []
-    if image_types is None:
-        image_types = []
-    if device_types is None:
-        device_types = []
-    rule = re.findall(r"\S+\/buildspec.yml", files)
-    for buildspec in rule:
-        buildspec_framework = buildspec.split("/")[0]
-        if buildspec_framework == framework:
-            device_types = constants.ALL
-            image_types = constants.ALL
-            py_versions = constants.ALL
-    return device_types, image_types, py_versions
+def parse_modifed_buidspec_yml_info(files, framework, pattern=""):
+    rule = re.findall(rf"{pattern}", files)
+    if not JobParameters.do_build_all_images():
+        for buildspec in rule:
+            buildspec_framework = buildspec.split("/")[0]
+            if buildspec_framework == framework:
+                JobParameters.build_for_all_images()
+
 
 # Rule 3: If any file in the build code changes, build all images
-def get_modifed_src_files_info(files, device_types=None, image_types=None, py_versions=None):
-    if py_versions is None:
-        py_versions = []
-    if image_types is None:
-        image_types = []
-    if device_types is None:
-        device_types = []
-    rule = re.findall(r"src\/\S+", files)
-    if rule:
-        device_types = constants.ALL
-        image_types = constants.ALL
-        py_versions = constants.ALL
-    return device_types, image_types, py_versions
+def parse_modifed_root_files_info(files, pattern=""):
+    rule = re.findall(rf"{pattern}", files)
+    if not JobParameters.do_build_all_images():
+        if rule:
+            JobParameters.build_for_all_images()
 
-def get_modified_test_files_info(files, framework, device_types=None, image_types=None, py_versions=None,
-                                 run_test_types=None):
-    # Rule 1: run  only the tests where the test_files are changed
-    if run_test_types is None:
-        run_test_types = []
-    if py_versions is None:
-        py_versions = []
-    if image_types is None:
-        image_types = []
-    if device_types is None:
-        device_types = []
-    rule = re.findall(r"[\r\n]+test\S+", files)
+def parse_modified_sagemaker_test_files(files, framework, pattern=""):
+    rule = re.findall(rf"{pattern}", files)
     for test_file in rule:
         test_folder = test_file.split("/")[1]
-
         if test_folder == "sagemaker_tests":
             framework_changed = test_file.split("/")[2]
-            job_name = test_file.split("/")[3]
-            if framework_changed != framework:
-                continue
-            run_test_types.append(constants.SAGEMAKER_TESTS)
-            image_types.append(job_name)
-            device_types = constants.ALL
-            py_versions = constants.ALL
+            # The below code looks for file changes in /test/sagemaker_tests/(mxnet|pytorch|tensorflow) directory
+            if framework_changed == framework:
+                job_name = test_file.split("/")[3]
+                # The training folder structure for tensorflow is tensorflow1_training(1.x), tensorflow2_training(2.x)
+                # so we are stripping the tensrflow1 from the name
+                if framework_changed == "tensorflow" and "training" in job_name:
+                    job_name = "training"
+                if job_name in constants.IMAGE_TYPES:
+                    JobParameters.image_types.append(job_name)
+                    JobParameters.build_for_all_device_types_py_versions()
+                else:
+                    JobParameters.build_for_all_images()
+                    break
+            # If file changed is under /test/sagemaker_tests but not in (mxnet|pytorch|tensorflow) dirs
+            elif framework_changed not in constants.FRAMEWORKS:
+                JobParameters.build_for_all_images()
+                break
 
-        elif test_folder == "dlc_tests":
-            framework_changed = test_file.split("/")[3]
-            test_name = test_file.split("/")[2]
-            job_name = test_file.split("/")[4]
-            if framework_changed != framework:
-                continue
-            image_types.append(job_name)
-            if test_name == "ecs":
-                run_test_types.append(constants.ECS_TESTS)
-            elif test_name == "eks":
-                run_test_types.append(constants.EKS_TESTS)
-             # Assumes that changes are made in dlc_tests but not under ecs or eks folders so we run both the tests
-            else:
-                run_test_types.extend([constants.EKS_TESTS, constants.ECS_TESTS])
-            device_types = constants.ALL
-            py_versions = constants.ALL
-
-
-        else:
-            image_types = [constants.ALL]
-            run_test_types = [constants.ALL]
-            device_types = constants.ALL
-            py_versions = constants.ALL
-
-    return device_types, image_types, py_versions, run_test_types
+def parse_modified_dlc_test_files_info(files, framework,  pattern=""):
+    rule = re.findall(rf"{pattern}", files)
+    # JobParameters variables are not set with constants.ALL
+    if not JobParameters.do_build_all_images():
+        for test_file in rule:
+            test_folder = test_file.split("/")[1]
+            if test_folder == "dlc_tests":
+                test_name = test_file.split("/")[2]
+                # The below code looks for file changes in /test/dlc_tests/(ecs|eks|ec2) directory
+                if test_name in ["ecs", "eks", "ec2"]:
+                    framework_changed = test_file.split("/")[3]
+                    if framework_changed == framework:
+                        job_name = test_file.split("/")[4]
+                        if job_name in constants.IMAGE_TYPES:
+                            JobParameters.image_types.append(job_name)
+                            JobParameters.build_for_all_device_types_py_versions()
+                # If file changed is under /test/sagemaker_tests/(ecs|eks|ec2) but not in (mxnet|pytorch|tensorflow) dirs
+                        else:
+                            JobParameters.build_for_all_images()
+                            break
+                # If file changed is under /test/dlc_tests but not in (ecs|eks|ec2) dirs
+                    elif framework_changed not in constants.FRAMEWORKS:
+                        JobParameters.build_for_all_images()
+                        break
 
 
 def pr_build_setup(pr_number, framework):
@@ -153,59 +151,27 @@ def pr_build_setup(pr_number, framework):
     """
     files = get_pr_modified_files(pr_number)
 
-    device_types = []
-    image_types = []
-    py_versions = []
-
     # This below code currently appends the values to device_types, image_types, py_versions for files changed
     # if there are no changes in the files then functions return same lists
     # TODO: use a class to define these lists and use getter setter methods
-    device_types, image_types, py_versions = get_modified_docker_files_info(files, framework, device_types,
-                                                                            image_types, py_versions)
+    parse_modified_docker_files_info(files, framework, pattern="\S+Dockerfile\S+")
 
-    device_types, image_types, py_versions, run_test_types = get_modified_test_files_info(files, framework, device_types,
-                                                                            image_types, py_versions)
+    parse_modified_sagemaker_test_files(files, framework, pattern="\S+sagemaker_tests\/\S+")
 
-    # This below code currently overides the device_types, image_types, py_versions with constants.ALL
-    device_types, image_types, py_versions = get_modifed_buidspec_yml_info(files, framework, device_types,
-                                                                           image_types, py_versions)
+    # The below functions are only run if all JobParameters variables are not set with constants.ALL
+    parse_modified_dlc_test_files_info(files, framework, pattern="\S+dlc_tests\/\S+")
 
-    device_types, image_types, py_versions = get_modifed_src_files_info(files,device_types, image_types,
-                                                                        py_versions)
+    # The below code currently overides the device_types, image_types, py_versions with constants.ALL
+    # when there is a change in any the below files
+    parse_modifed_buidspec_yml_info(files, framework, pattern="\S+\/buildspec.yml")
 
-    return device_types, image_types, py_versions
+    parse_modifed_root_files_info(files, pattern="src\/\S+")
 
+    parse_modifed_root_files_info(files, pattern="(?:test\/(?!(dlc_tests|sagemaker_tests))\S+)")
 
-def pr_test_setup(pr_number):
-    """
-       Identify the PR changeset and set the appropriate environment
-       variables
+    parse_modifed_root_files_info(files, pattern="testspec\.yml")
 
-       Parameters:
-           pr_number: int
-
-       Returns:
-           framwork_changed: [str]
-           image_types: [str] possible values ["training","inference"]
-           py_versions: [str] possible values ["sagemaker","ecs","eks"]
-       """
-    github_handler = GitHubHandler("aws", "deep-learning-containers")
-    files = github_handler.get_pr_files_changed(pr_number)
-    files = "\n".join(files)
-    framework = constants.JOB_FRAMEWORK
-    framework_version = constants.JOB_FRAMEWORK_VERSION
-
-    device_types, image_types, py_versions, run_test_types = get_modified_test_files_info(files, framework)
-
-    rule1 = re.findall(r"testspec.yml", files)
-    if len(rule1) > 0:
-        image_types = [constants.ALL]
-        run_test_types = [constants.ALL]
-
-    image_types = list(set(image_types))
-    run_test_types = list(set(run_test_types))
-
-    return framework, image_types, run_test_types
+    return JobParameters.device_types, JobParameters.image_types, JobParameters.py_versions
 
 
 def build_setup(framework, device_types=None, image_types=None, py_versions=None):
@@ -267,29 +233,8 @@ def set_test_env(images, images_env="DLC_IMAGES", **kwargs):
     test_envs = []
     ecr_urls = []
 
-    if os.environ.get("BUILD_CONTEXT") == "PR":
-        pr_number = os.getenv("CODEBUILD_SOURCE_VERSION")
-        if pr_number is not None:
-            pr_number = int(pr_number.split("/")[-1])
-        else:
-            raise ValueError ("Empty code commit value found for PR build")
-        framework, image_types, run_test_types = pr_test_setup(pr_number)
-        print(f"{run_test_types} should be triggered for {framework} and {image_types} ")
-
     for docker_image in images:
-        docker_image_type = docker_image.repository.split("-")[-1]
-        if (
-            docker_image.build_status == constants.SUCCESS
-            or constants.ALL in image_types
-        ):
-            ecr_urls.append(docker_image.ecr_url)
-            run_test_types = [constants.ALL]
-        elif (
-            framework
-            and len(image_types) > 0
-            and framework in docker_image.repository
-            and docker_image_type in image_types
-        ):
+        if docker_image.build_status == constants.SUCCESS:
             ecr_urls.append(docker_image.ecr_url)
         else:
             print(
@@ -298,11 +243,6 @@ def set_test_env(images, images_env="DLC_IMAGES", **kwargs):
 
     images_arg = " ".join(ecr_urls)
     test_envs.append({"name": images_env, "value": images_arg, "type": "PLAINTEXT"})
-
-    if len(run_test_types) > 0:
-        with open(constants.RUN_TESTS_ENV, "w") as f:
-            for test in run_test_types:
-                f.write(f"{test}\n")
 
     if kwargs:
         for key, value in kwargs.items():
