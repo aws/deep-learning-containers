3b1f70c18cfd6a8d102a5ea7d7437d7bcd4548c1
Arjuna Keshavan <arjunake@amazon.com>
2020-03-11 17:50:13 -0700

    Updates

--- test/dlc_tests/ecs/pytorch/training/test_ecs_pytorch_training.py
+++ test/dlc_tests/ecs/pytorch/training/test_ecs_pytorch_training.py
@@ -1,9 +1,47 @@
-import time
+"""
+ECS tests for PyTorch Training
+"""
 
 
-def test_dummy(pytorch_training, ecs_container_instance):
+def test_ecs_pytorch_training(request, pytorch_training, ecs_container_instance, ecs_client):
     print(pytorch_training)
     print(ecs_container_instance)
 
-    # Sleeping for 300s so I can manually verify the ECS cluster is up with an attached instance
-    time.sleep(300)
+    instance_id, cluster = ecs_container_instance
+
+    family = request.node.name
+
+    container_definitions = [
+        {
+            "command": [
+                "git clone https://github.com/pytorch/examples.git && python examples/mnist/main.py --no-cuda"
+            ],
+            "entryPoint": ["sh", "-c"],
+            "name": "pytorch-training-container",
+            "image": pytorch_training,
+            "memory": 4000,
+            "cpu": 256,
+            "essential": True,
+            "portMappings": [{"containerPort": 80, "protocol": "tcp"}],
+            "logConfiguration": {
+                "logDriver": "awslogs",
+                "options": {
+                    "awslogs-group": "/ecs/pytorch-training-cpu",
+                    "awslogs-region": "us-west-2",
+                    "awslogs-stream-prefix": "mnist",
+                    "awslogs-create-group": "true",
+                },
+            },
+        }
+    ]
+
+    ecs_client.register_task_definition(
+        requiresCompatibilities=["EC2"],
+        containerDefinitions=container_definitions,
+        volumes=[],
+        networkMode="bridge",
+        placementConstraints=[],
+        family=family,
+    )
+
+    task = ecs_client.run_task(cluster=cluster, taskDefinition=family)
