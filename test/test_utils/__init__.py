604848be2106dfebfedf173c10c25550131e528a
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2020-03-12 22:04:43 -0700

    Remove printed error messages for inference request

--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -39,15 +39,12 @@ def request_mxnet_inference_squeezenet(ip_address="127.0.0.1", port="80"):
     """
     run_out = run("curl -O https://s3.amazonaws.com/model-server/inputs/kitten.jpg")
     if run_out.return_code != 0:
-        print('Download failed -')
-        print(run_out.stderr)
+        return False
     run_out = run(f"curl -X POST http://{ip_address}:{port}/predictions/squeezenet -T kitten.jpg")
 
     # The run_out.return_code is not reliable, since sometimes predict request may succeed but the returned result
     # is 404. Hence the extra check.
     if run_out.return_code != 0 or "probability" not in run_out.stdout:
-        print('Test failed because of error in making request -')
-        print(run_out.stderr)
         return False
 
     return True
