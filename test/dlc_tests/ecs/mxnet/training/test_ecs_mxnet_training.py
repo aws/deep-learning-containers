f5d2db2fa3a5bff97971d4d98b1fc487af3fd36e
Sai Parthasarathy Miduthuri <54188298+saimidu@users.noreply.github.com>
2020-03-16 18:53:13 -0700

    feature: Upload tests to unique location on s3 buckets (#61)
    * Upload tests to unique location on s3 buckets
    
    * Modularize code and remove extraneous print statements
    
    * Remove unnecessary fixture, and correct path

--- test/dlc_tests/ecs/mxnet/training/test_ecs_mxnet_training.py
+++ test/dlc_tests/ecs/mxnet/training/test_ecs_mxnet_training.py
@@ -1,2 +1,16 @@
-def test_dummy(mxnet_training):
+import datetime
+
+from invoke import run
+import test.test_utils.ecs as ecs_utils
+
+
+def test_s3_artifact_copy(mxnet_training):
     print(mxnet_training)
+    tag = mxnet_training.split(":")[-1]
+    testname_datetime_suffix = f"mxnet-training-{tag}-{datetime.datetime.now().strftime('%Y%m%d-%H-%M-%S')}"
+    s3_test_artifact_location = ecs_utils.upload_tests_for_ecs(testname_datetime_suffix)
+
+    run_out = run(f"aws s3 ls --recursive {s3_test_artifact_location}/")
+    assert run_out.return_code == 0, "Failed to copy test scripts"
+
+    ecs_utils.delete_uploaded_tests_for_ecs(s3_test_artifact_location)
