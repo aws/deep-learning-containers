2f9f63b2e56f0b682bd90710be57dba152623185
Arjuna <33526713+arjkesh@users.noreply.github.com>
2020-03-17 10:00:14 -0700

    feature: Allow each image to have its own ECS cluster for a given ECS test (#60)

--- test/dlc_tests/ecs/tensorflow/training/test_ecs_tensorflow_training.py
+++ test/dlc_tests/ecs/tensorflow/training/test_ecs_tensorflow_training.py
@@ -8,22 +8,13 @@ from test.test_utils import ECS_AML2_CPU_USWEST2
 
 @pytest.mark.parametrize("ecs_instance_type", ["c5.4xlarge"], indirect=True)
 @pytest.mark.parametrize("ecs_ami", [ECS_AML2_CPU_USWEST2], indirect=True)
-@pytest.mark.parametrize(
-    "ecs_cluster_name",
-    [
-        f"tf-train-mnist-cluster-{os.getenv('TEST_TRIGGER', '')}{datetime.datetime.now().strftime('%Y%m%d-%H-%M-%S')}"
-    ],
-    indirect=True,
-)
-def test_ecs_tf_training_mnist_cpu(request, cpu_only, tensorflow_training, ecs_container_instance, ecs_client):
+def test_ecs_tf_training_mnist_cpu(request, tensorflow_training, cpu_only, ecs_container_instance, ecs_client):
     """
     This is a direct test of our ECS TF training documentation.
 
     Given above parameters, registers a task with family named after this test, runs the task, and waits for
     the task to be stopped before doing teardown operations of instance and cluster.
     """
-
-
     _instance_id, cluster = ecs_container_instance
     # Naming the family after the test name, which is in this format
     family = request.node.name.split("[")[0]
