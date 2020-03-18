72431b623f3dbfa1750b5b7e966f27eaddc6f0c9
Arjuna Keshavan <arjunake@amazon.com>
2020-03-18 15:06:52 -0700

    Introduce py3_only fixture

--- test/dlc_tests/ecs/mxnet/training/test_ecs_mxnet_training.py
+++ test/dlc_tests/ecs/mxnet/training/test_ecs_mxnet_training.py
@@ -51,18 +51,19 @@ def test_ecs_mxnet_training_mnist_gpu(gpu_only, ecs_container_instance, mxnet_tr
 @pytest.mark.parametrize("training_script", [MX_DGL_TRAINING_SCRIPT], indirect=True)
 @pytest.mark.parametrize("ecs_instance_type", ["c4.2xlarge"], indirect=True)
 @pytest.mark.parametrize("ecs_ami", [ECS_AML2_CPU_USWEST2], indirect=True)
-def test_ecs_mxnet_training_dgl_cpu(cpu_only, ecs_container_instance, mxnet_training, training_cmd, ecs_cluster_name):
+def test_ecs_mxnet_training_dgl_cpu(cpu_only, py3_only, ecs_container_instance, mxnet_training, training_cmd,
+                                    ecs_cluster_name):
     """
     CPU DGL test for MXNet Training
 
     Instance Type - c4.2xlarge
 
+    DGL is only supported in py3, hence we have used the "py3_only" fixture to ensure py2 images don't run
+    on this function.
+
     Given above parameters, registers a task with family named after this test, runs the task, and waits for
     the task to be stopped before doing teardown operations of instance and cluster.
     """
-    if "py2" in mxnet_training:
-        pytest.skip(msg=f"DGL is not supported on Python 2. Skipping container {mxnet_training} with py2 in tag.")
-
     instance_id, cluster_arn = ecs_container_instance
 
     ecs_utils.ecs_training_test_executor(ecs_cluster_name, cluster_arn, training_cmd, mxnet_training, instance_id)
@@ -71,18 +72,19 @@ def test_ecs_mxnet_training_dgl_cpu(cpu_only, ecs_container_instance, mxnet_trai
 @pytest.mark.parametrize("training_script", [MX_DGL_TRAINING_SCRIPT], indirect=True)
 @pytest.mark.parametrize("ecs_instance_type", ["p2.xlarge"], indirect=True)
 @pytest.mark.parametrize("ecs_ami", [ECS_AML2_GPU_USWEST2], indirect=True)
-def test_ecs_mxnet_training_dgl_gpu(gpu_only, ecs_container_instance, mxnet_training, training_cmd, ecs_cluster_name):
+def test_ecs_mxnet_training_dgl_gpu(gpu_only, py3_only, ecs_container_instance, mxnet_training, training_cmd,
+                                    ecs_cluster_name):
     """
     GPU DGL test for MXNet Training
 
     Instance Type - p2.xlarge
 
+    DGL is only supported in py3, hence we have used the "py3_only" fixture to ensure py2 images don't run
+    on this function.
+
     Given above parameters, registers a task with family named after this test, runs the task, and waits for
     the task to be stopped before doing teardown operations of instance and cluster.
     """
-    if "py2" in mxnet_training:
-        pytest.skip(msg=f"DGL is not supported on Python 2. Skipping container {mxnet_training} with py2 in tag.")
-
     instance_id, cluster_arn = ecs_container_instance
 
     num_gpus = ec2_utils.get_instance_num_gpus(instance_id)
