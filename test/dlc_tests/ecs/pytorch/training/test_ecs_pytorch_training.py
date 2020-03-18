17a11fdd5aa7351720b53a56dd7387935c326c0a
Arjuna Keshavan <arjunake@amazon.com>
2020-03-18 10:59:25 -0700

    feature: Add TF tests, further simplify ECS training test structure

--- test/dlc_tests/ecs/pytorch/training/test_ecs_pytorch_training.py
+++ test/dlc_tests/ecs/pytorch/training/test_ecs_pytorch_training.py
@@ -7,9 +7,13 @@ from test.test_utils import ecs as ecs_utils
 from test.test_utils import ec2 as ec2_utils
 
 
+PT_MNIST_TRAINING_SCRIPT = os.path.join(os.sep, "test", "bin", "pytorch_tests", "testPyTorch")
+
+
+@pytest.mark.parametrize("training_script", [PT_MNIST_TRAINING_SCRIPT], indirect=True)
 @pytest.mark.parametrize("ecs_instance_type", ["c5.9xlarge"], indirect=True)
 @pytest.mark.parametrize("ecs_ami", [ECS_AML2_CPU_USWEST2], indirect=True)
-def test_ecs_pytorch_training_mnist_cpu(cpu_only, ecs_container_instance, pytorch_training, s3_artifact_copy,
+def test_ecs_pytorch_training_mnist_cpu(cpu_only, ecs_container_instance, pytorch_training, training_cmd,
                                         ecs_cluster_name):
     """
     CPU mnist test for PyTorch Training
@@ -19,20 +23,15 @@ def test_ecs_pytorch_training_mnist_cpu(cpu_only, ecs_container_instance, pytorc
     Given above parameters, registers a task with family named after this test, runs the task, and waits for
     the task to be stopped before doing teardown operations of instance and cluster.
     """
-    s3_test_artifact_location = s3_artifact_copy
-
-    training_cmd = ecs_utils.build_ecs_training_command(
-        s3_test_artifact_location, os.path.join(os.sep, "test", "bin", "pytorch_tests", "testPyTorch")
-    )
-
     instance_id, cluster_arn = ecs_container_instance
 
     ecs_utils.ecs_training_test_executor(ecs_cluster_name, cluster_arn, training_cmd, pytorch_training, instance_id)
 
 
+@pytest.mark.parametrize("training_script", [PT_MNIST_TRAINING_SCRIPT], indirect=True)
 @pytest.mark.parametrize("ecs_instance_type", ["p3.8xlarge"], indirect=True)
 @pytest.mark.parametrize("ecs_ami", [ECS_AML2_GPU_USWEST2], indirect=True)
-def test_ecs_pytorch_training_mnist_gpu(gpu_only, ecs_container_instance, pytorch_training, s3_artifact_copy,
+def test_ecs_pytorch_training_mnist_gpu(gpu_only, ecs_container_instance, pytorch_training, training_cmd,
                                         ecs_cluster_name):
     """
     GPU mnist test for PyTorch Training
@@ -42,12 +41,6 @@ def test_ecs_pytorch_training_mnist_gpu(gpu_only, ecs_container_instance, pytorc
     Given above parameters, registers a task with family named after this test, runs the task, and waits for
     the task to be stopped before doing teardown operations of instance and cluster.
     """
-    s3_test_artifact_location = s3_artifact_copy
-
-    training_cmd = ecs_utils.build_ecs_training_command(
-        s3_test_artifact_location, os.path.join(os.sep, "test", "bin", "pytorch_tests", "testPyTorch")
-    )
-
     instance_id, cluster_arn = ecs_container_instance
 
     num_gpus = ec2_utils.get_instance_num_gpus(instance_id)
