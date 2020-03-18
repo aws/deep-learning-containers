45d7102e5b776cf30c355e597314b18f813d10a1
Arjuna <33526713+arjkesh@users.noreply.github.com>
2020-03-18 15:33:12 -0700

    feature: Add DGL ECS tests, introduce py3_only fixture (#70)
    * feature: Add DGL ECS tests
    * Introduce py3_only fixture

--- test/dlc_tests/ecs/mxnet/training/test_ecs_mxnet_training.py
+++ test/dlc_tests/ecs/mxnet/training/test_ecs_mxnet_training.py
@@ -2,12 +2,13 @@ import os
 
 import pytest
 
-from test.test_utils import ECS_AML2_CPU_USWEST2, ECS_AML2_GPU_USWEST2
+from test.test_utils import ECS_AML2_CPU_USWEST2, ECS_AML2_GPU_USWEST2, CONTAINER_TESTS_PREFIX
 from test.test_utils import ecs as ecs_utils
 from test.test_utils import ec2 as ec2_utils
 
 
-MX_MNIST_TRAINING_SCRIPT = os.path.join(os.sep, "test", "bin", "testMXNet")
+MX_MNIST_TRAINING_SCRIPT = os.path.join(CONTAINER_TESTS_PREFIX, "testMXNet")
+MX_DGL_TRAINING_SCRIPT = os.path.join(CONTAINER_TESTS_PREFIX, "dgl_tests", "testMXNetDGL")
 
 
 @pytest.mark.parametrize("training_script", [MX_MNIST_TRAINING_SCRIPT], indirect=True)
@@ -35,6 +36,51 @@ def test_ecs_mxnet_training_mnist_gpu(gpu_only, ecs_container_instance, mxnet_tr
     GPU mnist test for MXNet Training
 
     Instance Type - p2.8xlarge
+
+    Given above parameters, registers a task with family named after this test, runs the task, and waits for
+    the task to be stopped before doing teardown operations of instance and cluster.
+    """
+    instance_id, cluster_arn = ecs_container_instance
+
+    num_gpus = ec2_utils.get_instance_num_gpus(instance_id)
+
+    ecs_utils.ecs_training_test_executor(ecs_cluster_name, cluster_arn, training_cmd, mxnet_training, instance_id,
+                                         num_gpus=num_gpus)
+
+
+@pytest.mark.parametrize("training_script", [MX_DGL_TRAINING_SCRIPT], indirect=True)
+@pytest.mark.parametrize("ecs_instance_type", ["c4.2xlarge"], indirect=True)
+@pytest.mark.parametrize("ecs_ami", [ECS_AML2_CPU_USWEST2], indirect=True)
+def test_ecs_mxnet_training_dgl_cpu(cpu_only, py3_only, ecs_container_instance, mxnet_training, training_cmd,
+                                    ecs_cluster_name):
+    """
+    CPU DGL test for MXNet Training
+
+    Instance Type - c4.2xlarge
+
+    DGL is only supported in py3, hence we have used the "py3_only" fixture to ensure py2 images don't run
+    on this function.
+
+    Given above parameters, registers a task with family named after this test, runs the task, and waits for
+    the task to be stopped before doing teardown operations of instance and cluster.
+    """
+    instance_id, cluster_arn = ecs_container_instance
+
+    ecs_utils.ecs_training_test_executor(ecs_cluster_name, cluster_arn, training_cmd, mxnet_training, instance_id)
+
+
+@pytest.mark.parametrize("training_script", [MX_DGL_TRAINING_SCRIPT], indirect=True)
+@pytest.mark.parametrize("ecs_instance_type", ["p2.xlarge"], indirect=True)
+@pytest.mark.parametrize("ecs_ami", [ECS_AML2_GPU_USWEST2], indirect=True)
+def test_ecs_mxnet_training_dgl_gpu(gpu_only, py3_only, ecs_container_instance, mxnet_training, training_cmd,
+                                    ecs_cluster_name):
+    """
+    GPU DGL test for MXNet Training
+
+    Instance Type - p2.xlarge
+
+    DGL is only supported in py3, hence we have used the "py3_only" fixture to ensure py2 images don't run
+    on this function.
 
     Given above parameters, registers a task with family named after this test, runs the task, and waits for
     the task to be stopped before doing teardown operations of instance and cluster.
