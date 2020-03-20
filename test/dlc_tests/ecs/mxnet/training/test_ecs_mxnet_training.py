aa7390ce946bc623cc4f9d80d6b6148572869332
Arjuna <33526713+arjkesh@users.noreply.github.com>
2020-03-19 19:52:31 -0700

    feature: Add GluonNLP training tests (#74)
    * feature: Add GluonNLP training tests
    * Increase client retries limit to 10

--- test/dlc_tests/ecs/mxnet/training/test_ecs_mxnet_training.py
+++ test/dlc_tests/ecs/mxnet/training/test_ecs_mxnet_training.py
@@ -9,6 +9,7 @@ from test.test_utils import ec2 as ec2_utils
 
 MX_MNIST_TRAINING_SCRIPT = os.path.join(CONTAINER_TESTS_PREFIX, "testMXNet")
 MX_DGL_TRAINING_SCRIPT = os.path.join(CONTAINER_TESTS_PREFIX, "dgl_tests", "testMXNetDGL")
+MX_GLUON_NLP_TRAINING_SCRIPT = os.path.join(CONTAINER_TESTS_PREFIX, "gluonnlp_tests", "testNLP")
 
 
 @pytest.mark.parametrize("training_script", [MX_MNIST_TRAINING_SCRIPT], indirect=True)
@@ -78,6 +79,51 @@ def test_ecs_mxnet_training_dgl_gpu(gpu_only, py3_only, ecs_container_instance, 
     GPU DGL test for MXNet Training
 
     Instance Type - p2.xlarge
+
+    DGL is only supported in py3, hence we have used the "py3_only" fixture to ensure py2 images don't run
+    on this function.
+
+    Given above parameters, registers a task with family named after this test, runs the task, and waits for
+    the task to be stopped before doing teardown operations of instance and cluster.
+    """
+    instance_id, cluster_arn = ecs_container_instance
+
+    num_gpus = ec2_utils.get_instance_num_gpus(instance_id)
+
+    ecs_utils.ecs_training_test_executor(ecs_cluster_name, cluster_arn, training_cmd, mxnet_training, instance_id,
+                                         num_gpus=num_gpus)
+
+
+@pytest.mark.parametrize("training_script", [MX_GLUON_NLP_TRAINING_SCRIPT], indirect=True)
+@pytest.mark.parametrize("ecs_instance_type", ["c5.9xlarge"], indirect=True)
+@pytest.mark.parametrize("ecs_ami", [ECS_AML2_CPU_USWEST2], indirect=True)
+def test_ecs_mxnet_training_gluonnlp_cpu(cpu_only, py3_only, ecs_container_instance, mxnet_training, training_cmd,
+                                         ecs_cluster_name):
+    """
+    CPU Gluon NLP for MXNet Training
+
+    Instance Type - c5.9xlarge
+
+    DGL is only supported in py3, hence we have used the "py3_only" fixture to ensure py2 images don't run
+    on this function.
+
+    Given above parameters, registers a task with family named after this test, runs the task, and waits for
+    the task to be stopped before doing teardown operations of instance and cluster.
+    """
+    instance_id, cluster_arn = ecs_container_instance
+
+    ecs_utils.ecs_training_test_executor(ecs_cluster_name, cluster_arn, training_cmd, mxnet_training, instance_id)
+
+
+@pytest.mark.parametrize("training_script", [MX_GLUON_NLP_TRAINING_SCRIPT], indirect=True)
+@pytest.mark.parametrize("ecs_instance_type", ["p2.16xlarge"], indirect=True)
+@pytest.mark.parametrize("ecs_ami", [ECS_AML2_GPU_USWEST2], indirect=True)
+def test_ecs_mxnet_training_gluonnlp_gpu(gpu_only, py3_only, ecs_container_instance, mxnet_training, training_cmd,
+                                         ecs_cluster_name):
+    """
+    GPU Gluon NLP test for MXNet Training
+
+    Instance Type - p2.16xlarge
 
     DGL is only supported in py3, hence we have used the "py3_only" fixture to ensure py2 images don't run
     on this function.
