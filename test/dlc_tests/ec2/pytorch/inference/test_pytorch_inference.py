d6b49d001aa6ea1962a18fafc61c1b4336f20b31
Sai Parthasarathy Miduthuri <54188298+saimidu@users.noreply.github.com>
2020-03-23 12:24:24 -0700

    feature: Add placeholders for EC2 tests (#84)

---
+++ test/dlc_tests/ec2/pytorch/inference/test_pytorch_inference.py
@@ -0,0 +1,6 @@
+def test_placeholder_gpu(pytorch_inference, gpu_only):
+    print(pytorch_inference)
+
+
+def test_placeholder_cpu(pytorch_inference, cpu_only):
+    print(pytorch_inference)
---
+++ test/dlc_tests/ec2/tensorflow/training/test_tensorflow_training.py
@@ -0,0 +1,6 @@
+def test_placeholder_gpu(tensorflow_training, gpu_only):
+    print(tensorflow_training)
+
+
+def test_placeholder_cpu(tensorflow_training, cpu_only):
+    print(tensorflow_training)
