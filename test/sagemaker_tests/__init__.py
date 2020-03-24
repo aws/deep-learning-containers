fcbd1d59898ecbfbc72c38292d09108a9dbc5b83
Arjuna Keshavan <arjunake@amazon.com>
2020-03-24 11:46:58 -0700

    add required reason to skipif

--- test/sagemaker_tests/__init__.py
+++ test/sagemaker_tests/__init__.py
@@ -1,5 +1,9 @@
 import os
 
 
+# Reason string for skipping tests in PR context
+SKIP_PR_REASON = "Skipping test in PR context to speed up iteration time. Test will be run in nightly/release pipeline."
+
+
 def is_pr_context():
     return os.getenv("BUILD_CONTEXT") == "PR"
