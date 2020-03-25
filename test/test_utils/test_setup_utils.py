b89263018bc66b61801f08381498d9a254430b51
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2020-03-25 14:36:37 -0700

    Add fix to allow TF 2.1 to work

--- test/test_utils/test_setup_utils.py
+++ test/test_utils/test_setup_utils.py
@@ -8,10 +8,18 @@ def host_setup_for_tensorflow_inference(container_name, framework_version):
     context = Context()
     home_dir = context.run("pwd", hide="out").stdout.strip("\n")
     src_location = join(home_dir, f"{container_name}-serving")
-    if exists("serving"):
-        context.run("rm -rf serving", echo=True)
-    fw_short_version = re.search(r"\d+\.\d+", framework_version).group()
-    context.run(f"git clone -b r{fw_short_version} https://github.com/tensorflow/serving.git {src_location}", echo=True)
+    if exists(src_location):
+        context.run(f"rm -rf {src_location}", echo=True)
+
+    if framework_version.startswith('1.'):
+        fw_short_version = re.search(r"\d+\.\d+", framework_version).group()
+        context.run(
+            f"git clone -b r{fw_short_version} https://github.com/tensorflow/serving.git {src_location}", echo=True
+        )
+    else:
+        # tensorflow/serving is not yet updated with scripts for TF 2.1, so using locally modified scripts
+        script_location = join("container_tests", "bin", "tensorflow_serving")
+        context.run(f"cp -r {script_location} {src_location}")
 
     context.run(f"""pip install -qq -U tensorflow=={framework_version}"""
                 f""" "tensorflow-serving-api<={framework_version}" """, echo=True)
