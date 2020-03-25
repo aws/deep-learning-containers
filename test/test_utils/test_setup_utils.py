6d351803f802f0e2db07e9c2dedc17b8f992bfbe
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2020-03-25 10:24:25 -0700

    Make all paths absolute

--- test/test_utils/test_setup_utils.py
+++ test/test_utils/test_setup_utils.py
@@ -11,30 +11,30 @@ def host_setup_for_tensorflow_inference(container_name, framework_version):
     context.run(f"git clone https://github.com/tensorflow/serving.git {src_location}", echo=True)
 
     venv_location = join(home_dir, container_name)
-    context.run(f"virtualenv {venv_location}", echo=True)
+    context.run(f"python3 -m virtualenv {venv_location}", echo=True)
     if exists(venv_location):
         print(f"{venv_location} exists")
         if exists(f"{venv_location}/bin"):
             print(f"{venv_location}/bin exists")
             if exists(f"{venv_location}/bin/activate"):
                 print(f"{venv_location}/bin/activate exists")
-    with context.prefix(f"source {venv_location}/bin/activate"):
-        context.run(f"""pip install -qq -U tensorflow=={framework_version}"""
-                    f""" "tensorflow-serving-api<={framework_version}" """, echo=True)
-        with context.cd(src_location):
-            script = join("tensorflow_serving", "example", "mnist_saved_model.py")
-            model_path = join("models", "mnist")
-            context.run(f"python {script} {model_path}", hide="out")
-    abs_model_path = join(src_location, model_path)
-    return src_location, venv_location, abs_model_path
+
+    context.run(f"source {venv_location}/bin/activate")
+    context.run(f"""pip install -qq -U tensorflow=={framework_version}"""
+                f""" "tensorflow-serving-api<={framework_version}" """, echo=True)
+    script = join(src_location, "tensorflow_serving", "example", "mnist_saved_model.py")
+    model_path = join(src_location, "models", "mnist")
+    context.run(f"python {script} {model_path}", hide="out")
+    context.run("deactivate")
+    return src_location, venv_location, model_path
 
 
 def request_tensorflow_inference_grpc(src_location, venv_location, ip_address="127.0.0.1", port="8500"):
     context = Context()
-    with context.prefix(f"source {venv_location}/bin/activate"):
-        with context.cd(src_location):
-            script = join("tensorflow_serving", "example", "mnist_client.py")
-            context.run(f"python {script} --num_tests=1000 --server={ip_address}:{port}", echo=True)
+    context.run(f"source {venv_location}/bin/activate")
+    script = join(src_location, "tensorflow_serving", "example", "mnist_client.py")
+    context.run(f"python {script} --num_tests=1000 --server={ip_address}:{port}", echo=True)
+    context.run("deactivate")
 
 
 def tensorflow_inference_test_cleanup(src_location, venv_location):
