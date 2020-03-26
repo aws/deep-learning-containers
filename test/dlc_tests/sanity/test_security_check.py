760b7a0622c4aeb176f81e485e56828a205ed0db
Satish Gollaprolu <sgollapr@amazon.com>
2020-03-26 09:46:49 -0700

    pull the docker-image for pytorch-inference before running container

--- test/dlc_tests/sanity/test_security_check.py
+++ test/dlc_tests/sanity/test_security_check.py
@@ -8,6 +8,9 @@ def test_security(image):
     repo_name, image_tag = image.split('/')[-1].split(':')
     container_name = f"{repo_name}-{image_tag}-security"
 
+    # To avoid cluttering the logs with pull request statements
+    run(f"docker pull {image}", hide='out')
+
     run(f"docker run -itd --name {container_name} --mount " \
                  f"type=bind,src=$(pwd)/container_tests,target=/test" \
                  f" --entrypoint='/bin/bash' " \
@@ -17,4 +20,6 @@ def test_security(image):
         run(f"{docker_exec_cmd} python /test/bin/security_checks.py ")
     finally:
         run(f"docker rm -f {container_name}", echo=True)
+        run(f"docker rmi -f {image}", echo=True)
+
 
