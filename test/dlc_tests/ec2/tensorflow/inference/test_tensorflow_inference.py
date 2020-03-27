8bd6871cb492825191ffcc2cca5a8c392f5dcb0f
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2020-03-27 10:36:54 -0700

    Trying to print to stdout

--- test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
+++ test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
@@ -11,4 +11,5 @@ def test_placeholder_gpu(tensorflow_inference, gpu_only):
 @pytest.mark.parametrize("ec2_instance_ami", [UBUNTU_16_BASE_DLAMI], indirect=True)
 def test_placeholder_cpu(tensorflow_inference, ec2_connection, cpu_only):
     print(tensorflow_inference)
-    ec2_connection.run("ls -l")
+    output = ec2_connection.run("ls -l")
+    print(output.stdout)
