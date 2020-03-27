ae3d1fe2cf01475f17e17fe57f667645322c63b8
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2020-03-27 11:41:24 -0700

    Make test more explicit

--- test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
+++ test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
@@ -10,6 +10,5 @@ def test_placeholder_gpu(tensorflow_inference, gpu_only):
 @pytest.mark.parametrize("ec2_instance_type", ["c5.4xlarge"], indirect=True)
 @pytest.mark.parametrize("ec2_instance_ami", [UBUNTU_16_BASE_DLAMI], indirect=True)
 def test_placeholder_cpu(tensorflow_inference, ec2_connection, cpu_only):
-    print(tensorflow_inference)
-    output = ec2_connection.run("ls -l")
-    print(output.stdout)
+    output = ec2_connection.run(f"echo {tensorflow_inference}").stdout.strip("\n")
+    assert output == tensorflow_inference, f"Fabric output did not match -- {output}"
