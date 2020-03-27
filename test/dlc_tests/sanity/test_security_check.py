eef5486705d2d6350679f49a768ac47cfd21cc9f
Satish Gollaprolu <sgollapr@amazon.com>
2020-03-27 10:19:52 -0700

    don't delete the image after security test is done

--- test/dlc_tests/sanity/test_security_check.py
+++ test/dlc_tests/sanity/test_security_check.py
@@ -5,21 +5,18 @@ from invoke import run
 
 
 def test_security(image):
-    repo_name, image_tag = image.split('/')[-1].split(':')
+    repo_name, image_tag = image.split("/")[-1].split(":")
     container_name = f"{repo_name}-{image_tag}-security"
 
-    # To avoid cluttering the logs with pull request statements
-    run(f"docker pull {image}", hide=True)
-
-    run(f"docker run -itd --name {container_name} --mount " \
-                 f"type=bind,src=$(pwd)/container_tests,target=/test" \
-                 f" --entrypoint='/bin/bash' " \
-                 f"{image}",  echo=True)
+    run(
+        f"docker run -itd --name {container_name} "
+        f"--mount type=bind,src=$(pwd)/container_tests,target=/test"
+        f" --entrypoint='/bin/bash' "
+        f"{image}",
+        echo=True,
+    )
     try:
         docker_exec_cmd = f"docker exec -i {container_name}"
         run(f"{docker_exec_cmd} python /test/bin/security_checks.py ")
     finally:
         run(f"docker rm -f {container_name}", hide=True)
-        run(f"docker rmi -f {image}", hide=True)
-
-
