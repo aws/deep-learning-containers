92db068793d1b3e3c0a7d16d967da3eff82a36d8
satish Kumar Gollaprolu <satishgollaprolu@gmail.com>
2020-03-27 19:18:47 -0700

    Changeset -trigger subset of test jobs (#91)
    * run test code build jobs only when the relative test files changed
    
    * syntax changes to address cr comments
    
    * regex exp change for dlc, sagemaker test changes
    
    * enable builds for the build file changes
    
    Co-authored-by: Satish Gollaprolu <sgollapr@amazon.com>

--- src/constants.py
+++ src/constants.py
@@ -49,5 +49,13 @@ JOB_FRAMEWORK = os.environ.get("FRAMEWORK")
 JOB_FRAMEWORK_VERSION = os.environ.get("VERSION")
 
 # Test environment file
-TEST_ENV = os.path.join(os.path.expanduser("~"), "testenv.json")
+TEST_ENV_PATH = os.path.join(os.path.expanduser("~"), "testenv.json")
+# Test images for all test types (sm,ecs,eks,ec2) to pass on to code build jobs
+TEST_TYPE_IMAGES_PATH = os.path.join(os.path.expanduser("~"), "test_type_images.json")
 
+# Test types for running code build test jobs
+SAGEMAKER_TESTS = "sagemaker"
+EC2_TESTS = "ec2"
+ECS_TESTS = "ecs"
+EKS_TESTS = "eks"
+ALL_TESTS = ["sagemaker", "ec2", "eks", "ecs"]
