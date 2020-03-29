396813053eeb041349bde37d85b0403b713a280e
Sai Parthasarathy Miduthuri <54188298+saimidu@users.noreply.github.com>
2020-03-29 14:12:32 -0700

    feature: New fixture for fabric connection to new instance (#96)

--- test/dlc_tests/conftest.py
+++ test/dlc_tests/conftest.py
@@ -1,12 +1,14 @@
 import os
 
 import boto3
+from botocore.config import Config
 import docker
+from fabric import Connection
 import pytest
 
-from botocore.config import Config
-
-from test.test_utils import run_subprocess_cmd, UBUNTU_16_BASE_DLAMI, DEFAULT_REGION
+from test.test_utils import destroy_ssh_keypair, generate_ssh_keypair, run_subprocess_cmd
+from test.test_utils import DEFAULT_REGION, UBUNTU_16_BASE_DLAMI
+import test.test_utils.ec2 as ec2_utils
 
 
 # Immutable constant for framework specific image fixtures
@@ -30,6 +32,11 @@ def pytest_addoption(parser):
     )
 
 
+@pytest.fixture(scope="function")
+def ec2_key_name(request):
+    return request.param
+
+
 @pytest.fixture(scope="session")
 def region():
     return os.getenv("AWS_REGION", DEFAULT_REGION)
@@ -44,50 +51,83 @@ def docker_client(region):
 
 
 @pytest.fixture(scope="session")
-def ec2_client():
-    return boto3.client("ec2", config=Config(retries={'max_attempts': 10}))
+def ec2_client(region):
+    return boto3.client("ec2", region_name=region, config=Config(retries={'max_attempts': 10}))
 
 
 @pytest.fixture(scope="session")
-def ec2_resource():
-    return boto3.resource("ec2")
+def ec2_resource(region):
+    return boto3.resource("ec2", region_name=region, config=Config(retries={'max_attempts': 10}))
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def ec2_instance_type(request):
-    return request.param
+    return request.param if hasattr(request, "param") else "g4dn.xlarge"
+
+
+@pytest.fixture(scope="function")
+def ec2_instance_role_name(request):
+    return request.param if hasattr(request, "param") else ""
+
+
+@pytest.fixture(scope="function")
+def ec2_instance_ami(request):
+    return request.param if hasattr(request, "param") else UBUNTU_16_BASE_DLAMI
 
 
 @pytest.mark.timeout(300)
-@pytest.fixture(scope="session")
-def ec2_instance(request, ec2_client, ec2_instance_type, ec2_resource):
+@pytest.fixture(scope="function")
+def ec2_instance(
+        request, ec2_client, ec2_resource, ec2_instance_type, ec2_key_name, ec2_instance_role_name, ec2_instance_ami, region
+):
+    print(f"Creating instance: CI-CD {ec2_key_name}")
+    key_filename = generate_ssh_keypair(ec2_client, ec2_key_name)
     instances = ec2_resource.create_instances(
-        KeyName="pytest.pem", ImageId=UBUNTU_16_BASE_DLAMI, InstanceType=ec2_instance_type, MaxCount=1, MinCount=1,
+        KeyName=ec2_key_name,
+        ImageId=ec2_instance_ami,
+        InstanceType=ec2_instance_type,
+        IamInstanceProfile={"Name": ec2_instance_role_name},
+        TagSpecifications=[
+            {
+                "ResourceType": "instance",
+                "Tags": [{"Key": "Name", "Value": f"CI-CD {ec2_key_name}"}]
+            },
+        ],
+        MaxCount=1,
+        MinCount=1,
     )
     instance_id = instances[0].id
 
     # Define finalizer to terminate instance after this fixture completes
     def terminate_ec2_instance():
         ec2_client.terminate_instances(InstanceIds=[instance_id])
+        destroy_ssh_keypair(ec2_client, key_filename)
 
     request.addfinalizer(terminate_ec2_instance)
 
-    waiter = ec2_client.get_waiter("instance_running")
-    waiter.wait(InstanceIds=[instance_id])
-    return instances[0]
+    ec2_utils.check_instance_state(instance_id, state="running", region=region)
+    ec2_utils.check_system_state(
+        instance_id, system_status="ok", instance_status="ok", region=region
+    )
+    return instance_id, key_filename
 
 
-@pytest.fixture(scope="session")
-def ec2_connection(ec2_instance):
+@pytest.fixture(scope="function")
+def ec2_connection(ec2_instance, region):
     """
-    Not implemented fixture to establish connection with EC2 instance if necessary
-
+    Fixture to establish connection with EC2 instance if necessary
     :param ec2_instance: ec2_instance pytest fixture
+    :param region: Region where ec2 instance is launched
     :return: Fabric connection object
     """
-    # With Connection(ec2_instance.public_ip_address_ as c:
-    #     yield c
-    pass
+    instance_id, instance_pem_file = ec2_instance
+    user = ec2_utils.get_instance_user(instance_id, region=region)
+    conn = Connection(
+        user=user,
+        host=ec2_utils.get_public_ip(instance_id, region),
+        connect_kwargs={"key_filename": [instance_pem_file]}
+    )
+    return conn
 
 
 @pytest.fixture(scope="session")
@@ -116,6 +156,32 @@ def py3_only():
     pass
 
 
+def generate_unique_values_for_fixtures(metafunc_obj, images_to_parametrize, values_to_generate_for_fixture):
+    """
+    Take a dictionary (values_to_generate_for_fixture), that maps a fixture name used in a test function to another
+    fixture that needs to be parametrized, and parametrize to create unique resources for a test.
+    :param metafunc_obj: pytest metafunc object
+    :param images_to_parametrize: <list> list of image URIs which are used in a test
+    :param values_to_generate_for_fixture: <dict> Mapping of "Fixture used" -> "Fixture to be parametrized"
+    :return: <dict> Mapping of "Fixture to be parametrized" -> "Unique values for fixture to be parametrized"
+    """
+    fixtures_parametrized = {}
+    if images_to_parametrize:
+        for key, new_fixture_name in values_to_generate_for_fixture.items():
+            if key in metafunc_obj.fixturenames:
+                fixtures_parametrized[new_fixture_name] = []
+                for index, image in enumerate(images_to_parametrize):
+                    image_tag = image.split(":")[-1].replace(".", "-")
+                    fixtures_parametrized[new_fixture_name].append(
+                        (
+                            image,
+                            f"{metafunc_obj.function.__name__}-{image_tag}-"
+                            f"{os.getenv('CODEBUILD_RESOLVED_SOURCE_VERSION')}-{index}",
+                        )
+                    )
+    return fixtures_parametrized
+
+
 def pytest_generate_tests(metafunc):
     images = metafunc.config.getoption("--images")
 
@@ -137,19 +203,16 @@ def pytest_generate_tests(metafunc):
             if images_to_parametrize and "py3_only" in metafunc.fixturenames:
                 images_to_parametrize = [py3_image for py3_image in images_to_parametrize if 'py2' not in py3_image]
 
-            # Parametrize tests that spin up an ecs cluster with unique name
-            if images_to_parametrize and "ecs_container_instance" in metafunc.fixturenames:
-                ecs_parametrization = []
-                for index, image in enumerate(images_to_parametrize):
-                    image_tag = image.split(":")[-1].replace(".", "-")
-                    ecs_parametrization.append(
-                        (
-                            image,
-                            f"{metafunc.function.__name__}-{image_tag}-"
-                            f"{os.getenv('CODEBUILD_RESOLVED_SOURCE_VERSION')}-{index}",
-                        )
-                    )
-                metafunc.parametrize(f"{fixture},ecs_cluster_name", ecs_parametrization)
+            # Parametrize tests that spin up an ecs cluster or tests that spin up an EC2 instance with a unique name
+            values_to_generate_for_fixture = {"ecs_container_instance": "ecs_cluster_name",
+                                              "ec2_connection": "ec2_key_name"}
+
+            fixtures_parametrized = generate_unique_values_for_fixtures(
+                metafunc, images_to_parametrize, values_to_generate_for_fixture
+            )
+            if fixtures_parametrized:
+                for new_fixture_name, test_parametrization in fixtures_parametrized.items():
+                    metafunc.parametrize(f"{fixture},{new_fixture_name}", test_parametrization)
             else:
                 metafunc.parametrize(fixture, images_to_parametrize)
 
