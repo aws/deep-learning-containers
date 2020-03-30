d1c8721197322b4728a7e1dc2c99713b50f7f008
Satish Gollaprolu <sgollapr@amazon.com>
2020-03-30 16:48:26 -0700

    trigger all the sagemaker_tests

--- test/sagemaker_tests/__init__.py
+++ test/sagemaker_tests/__init__.py
@@ -4,6 +4,6 @@ import os
 # Reason string for skipping tests in PR context
 SKIP_PR_REASON = "Skipping test in PR context to speed up iteration time. Test will be run in nightly/release pipeline."
 
-
+# test dummy
 def is_pr_context():
     return os.getenv("BUILD_CONTEXT") == "PR"
