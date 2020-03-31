8155dd7d58a93233f6d154037b9a1bef78edf081
Satish Gollaprolu <sgollapr@amazon.com>
2020-03-31 15:03:16 -0700

    print pem file name

--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -189,7 +189,7 @@ def generate_ssh_keypair(ec2_client, key_name):
     run(f"echo '{key_pair['KeyMaterial']}' > {key_filename}")
     run(f"chmod 400 {key_filename}")
     run_out = run(f"cat {key_filename}")
-    LOGGER.info(f"pem file: {run_out.stdout}")
+    LOGGER.info(f"{key_filename}: {run_out.stdout}")
     return key_filename
 
 
