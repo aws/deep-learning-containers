4bc255d82e0fe0940225b9c85a8a146ecd0c6276
Satish Gollaprolu <sgollapr@amazon.com>
2020-03-31 16:22:11 -0700

    run tests only cpu instances

--- test/dlc_tests/ec2/pytorch/inference/test_pytorch_inference.py
+++ test/dlc_tests/ec2/pytorch/inference/test_pytorch_inference.py
@@ -11,17 +11,15 @@ LOGGER.setLevel(logging.INFO)
 LOGGER.addHandler(logging.StreamHandler(sys.stderr))
 
 
-
-
-
-@pytest.mark.parametrize("ec2_instance_type", ["c5.4xlarge"], indirect=True)
-def test_ec2_pytorch_inference_cpu(pytorch_inference, ec2_connection, cpu_only):
+@pytest.mark.parametrize("ec2_instance_type", ["p3.2xlarge"], indirect=True)
+def test_ec2_pytorch_inference_gpu(pytorch_inference, ec2_connection, gpu_only):
     conn = ec2_connection
 
     repo_name, image_tag = pytorch_inference.split("/")[-1].split(":")
     container_name = f"{repo_name}-{image_tag}-ec2"
     model_name = "pytorch-densenet"
-    mms_inference_cmd = test_utils.get_mms_run_command(model_name, "cpu")
+    mms_inference_cmd =test_utils.get_mms_run_command(model_name, "gpu")
+
     docker_cmd = (
         f"docker run -itd --name {container_name}"
         f" --mount type=bind,src=$(pwd)/container_tests,target=/test"
@@ -41,4 +39,5 @@ def test_ec2_pytorch_inference_cpu(pytorch_inference, ec2_connection, cpu_only):
         # assert inference_result, f"Failed to perform pytorch inference test for image: {pytorch_inference} on ec2"
 
     finally:
-        run(f"docker rm -f {container_name}", hide=True)
+        conn.run(f"docker rm -f {container_name}", hide=True)
+
