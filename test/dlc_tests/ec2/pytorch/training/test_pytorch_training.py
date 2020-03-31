07f578161e3347cdd2f44d2af5342f64de127c97
Arjuna Keshavan <arjunake@amazon.com>
2020-03-30 19:06:36 -0700

    add in all tests

--- test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
+++ test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
@@ -3,67 +3,88 @@ import os
 import pytest
 
 from test.test_utils import CONTAINER_TESTS_PREFIX
+from test.test_utils.ec2 import execute_ec2_training_test
 
 
-CONTAINER_TEST_LOCAL_DIR = os.path.join(os.path.expanduser('~'), 'container_tests')
+PT_STANDALONE_CMD = os.path.join(CONTAINER_TESTS_PREFIX, 'pytorch_tests', 'testPyTorchStandalone')
+PT_MNIST_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorch")
+PT_REGRESSION_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchRegression")
+PT_DGL_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "dgl_tests", "testPyTorchDGL")
+PT_SMDEBUG_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "testSmdebug pytorch")
+
+PT_EC2_GPU_INSTANCE_TYPE = "p3.2xlarge"
+PT_EC2_CPU_INSTANCE_TYPE = "c4.4xlarge"
 
 
-@pytest.mark.parametrize("ec2_instance_type", ["p3.8xlarge"], indirect=True)
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_standalone_gpu(pytorch_training, ec2_connection, gpu_only):
-    conn = ec2_connection
-
-    conn.run(f"docker run -v {CONTAINER_TEST_LOCAL_DIR}:{os.path.join(os.sep, 'test')} {pytorch_training} "
-             f"{os.path.join(os.sep, 'bin', 'bash')} -c "
-             f"{os.path.join(CONTAINER_TESTS_PREFIX, 'pytorch_tests', 'testPyTorchStandalone')}")
+    execute_ec2_training_test(ec2_connection, pytorch_training, PT_STANDALONE_CMD)
 
 
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_CPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_standalone_cpu(pytorch_training, ec2_connection, cpu_only):
-    print(pytorch_training)
+    execute_ec2_training_test(ec2_connection, pytorch_training, PT_STANDALONE_CMD)
 
 
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_train_mnist_gpu(pytorch_training, ec2_connection, gpu_only):
-    print(pytorch_training)
+    execute_ec2_training_test(ec2_connection, pytorch_training, PT_MNIST_CMD)
 
 
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_CPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_train_mnist_cpu(pytorch_training, ec2_connection, cpu_only):
-    print(pytorch_training)
+    execute_ec2_training_test(ec2_connection, pytorch_training, PT_MNIST_CMD)
 
 
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_linear_regression_gpu(pytorch_training, ec2_connection, gpu_only):
-    print(pytorch_training)
+    execute_ec2_training_test(ec2_connection, pytorch_training, PT_REGRESSION_CMD)
 
 
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_CPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_linear_regression_cpu(pytorch_training, ec2_connection, cpu_only):
-    print(pytorch_training)
+    execute_ec2_training_test(ec2_connection, pytorch_training, PT_REGRESSION_CMD)
 
 
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_train_dgl_gpu(pytorch_training, ec2_connection, gpu_only, py3_only):
-    print(pytorch_training)
+    execute_ec2_training_test(ec2_connection, pytorch_training, PT_DGL_CMD)
 
 
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_CPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_train_dgl_cpu(pytorch_training, ec2_connection, cpu_only, py3_only):
-    print(pytorch_training)
+    execute_ec2_training_test(ec2_connection, pytorch_training, PT_DGL_CMD)
 
 
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_with_horovod(pytorch_training, ec2_connection, gpu_only):
-    print(pytorch_training)
+    test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPTHVD")
+    execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
 
 
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_gloo(pytorch_training, ec2_connection, gpu_only, py3_only):
-    print(pytorch_training)
+    test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchGloo")
+    execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
 
 
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_nccl(pytorch_training, ec2_connection, gpu_only, py3_only):
-    print(pytorch_training)
+    test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchNccl")
+    execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
 
 
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_mpi(pytorch_training, ec2_connection, gpu_only, py3_only):
-    print(pytorch_training)
+    test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchMpi")
+    execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
 
 
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_smdebug_gpu(pytorch_training, ec2_connection, gpu_only, py3_only):
-    print(pytorch_training)
+    execute_ec2_training_test(ec2_connection, pytorch_training, PT_SMDEBUG_CMD)
 
 
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_CPU_INSTANCE_TYPE], indirect=True)
 def test_pytorch_smdebug_cpu(pytorch_training, ec2_connection, cpu_only, py3_only):
-    print(pytorch_training)
+    execute_ec2_training_test(ec2_connection, pytorch_training, PT_SMDEBUG_CMD)
--- test/test_utils/ec2.py
+++ test/test_utils/ec2.py
@@ -1,3 +1,5 @@
+import os
+
 import boto3
 from retrying import retry
 
@@ -266,3 +268,11 @@ def get_instance_num_gpus(instance_id, region=DEFAULT_REGION):
     """
     instance_info = get_instance_details(instance_id, region=region)
     return sum(gpu_type["Count"] for gpu_type in instance_info["GpuInfo"]["Gpus"])
+
+
+def execute_ec2_training_test(connection, ecr_uri, test_cmd, region=DEFAULT_REGION):
+    docker_cmd = "nvidia-docker" if "gpu" in ecr_uri else "docker"
+    container_test_local_dir = os.path.join("$HOME", "container_tests")
+    connection.run(f"$(aws ecr get-login --no-include-email --region {region} --registry-ids {os.getenv('ACCOUNT_ID')})"
+                   f" && {docker_cmd} run -v {container_test_local_dir}:{os.path.join(os.sep, 'test')} {ecr_uri} "
+                   f"{os.path.join(os.sep, 'bin', 'bash')} -c {test_cmd}")
