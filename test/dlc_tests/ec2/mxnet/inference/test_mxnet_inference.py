803ebe847872555ce78498b879c957e8214bc87c
Satish Gollaprolu <sgollapr@amazon.com>
2020-04-01 15:41:50 -0700

    login to ecr repo before docker run

--- test/dlc_tests/ec2/mxnet/inference/test_mxnet_inference.py
+++ test/dlc_tests/ec2/mxnet/inference/test_mxnet_inference.py
@@ -9,16 +9,22 @@ BERT_MODEL = "bert_sst"
 
 @pytest.mark.parametrize("ec2_instance_type", ["p3.2xlarge"], indirect=True)
 def test_ec2_mxnet_inference_gpu(mxnet_inference, ec2_connection, region, gpu_only):
+    login_to_ecr_repo(ec2_connection, region)
     run_ec2_mxnet_inference(mxnet_inference, SQUEEZENET_MODEL, "squeezenet", ec2_connection, "gpu", 80, 8081)
     if "py3" in mxnet_inference:
         run_ec2_mxnet_inference(mxnet_inference, BERT_MODEL, "gluonnlp", ec2_connection, "gpu", 90, 9091)
 
 @pytest.mark.parametrize("ec2_instance_type", ["c5.4xlarge"], indirect=True)
 def test_ec2_mxnet_inference_cpu(mxnet_inference, ec2_connection, region, cpu_only):
+    login_to_ecr_repo(ec2_connection, region)
     run_ec2_mxnet_inference(mxnet_inference, SQUEEZENET_MODEL, "squeezenet", ec2_connection, "cpu", 80, 8081)
     if "py3" in mxnet_inference:
         run_ec2_mxnet_inference(mxnet_inference, BERT_MODEL, "gluonnlp", ec2_connection, "cpu", 90, 9091)
 
+def login_to_ecr_repo(ec2_connection, region):
+    ec2_connection.run(
+        f"$(aws ecr get-login --no-include-email --region {region})", hide=True
+    )
 
 def run_ec2_mxnet_inference(image_uri, model_name, container_tag, ec2_connection, processor, target_port, target_management_port):
     repo_name, image_tag = image_uri.split("/")[-1].split(":")
