b59424caf9bb4a193db59bc54a635d3d4f92fddc
Satish Gollaprolu <sgollapr@amazon.com>
2020-04-01 15:54:37 -0700

    run tests for py3-cpu images

--- test/dlc_tests/ec2/mxnet/inference/test_mxnet_inference.py
+++ test/dlc_tests/ec2/mxnet/inference/test_mxnet_inference.py
@@ -7,19 +7,20 @@ from test.dlc_tests.conftest import LOGGER
 SQUEEZENET_MODEL = "squeezenet"
 BERT_MODEL = "bert_sst"
 
-@pytest.mark.parametrize("ec2_instance_type", ["p3.2xlarge"], indirect=True)
-def test_ec2_mxnet_inference_gpu(mxnet_inference, ec2_connection, region, gpu_only):
-    login_to_ecr_repo(ec2_connection, region)
-    run_ec2_mxnet_inference(mxnet_inference, SQUEEZENET_MODEL, "squeezenet", ec2_connection, "gpu", 80, 8081)
-    if "py3" in mxnet_inference:
-        run_ec2_mxnet_inference(mxnet_inference, BERT_MODEL, "gluonnlp", ec2_connection, "gpu", 90, 9091)
+# @pytest.mark.parametrize("ec2_instance_type", ["p3.2xlarge"], indirect=True)
+# def test_ec2_mxnet_inference_gpu(mxnet_inference, ec2_connection, region, gpu_only):
+#     login_to_ecr_repo(ec2_connection, region)
+#     run_ec2_mxnet_inference(mxnet_inference, SQUEEZENET_MODEL, "squeezenet", ec2_connection, "gpu", 80, 8081)
+#     if "py3" in mxnet_inference:
+#         run_ec2_mxnet_inference(mxnet_inference, BERT_MODEL, "gluonnlp", ec2_connection, "gpu", 90, 9091)
 
 @pytest.mark.parametrize("ec2_instance_type", ["c5.4xlarge"], indirect=True)
 def test_ec2_mxnet_inference_cpu(mxnet_inference, ec2_connection, region, cpu_only):
     login_to_ecr_repo(ec2_connection, region)
-    run_ec2_mxnet_inference(mxnet_inference, SQUEEZENET_MODEL, "squeezenet", ec2_connection, "cpu", 80, 8081)
     if "py3" in mxnet_inference:
-        run_ec2_mxnet_inference(mxnet_inference, BERT_MODEL, "gluonnlp", ec2_connection, "cpu", 90, 9091)
+        run_ec2_mxnet_inference(mxnet_inference, SQUEEZENET_MODEL, "squeezenet", ec2_connection, "cpu", 80, 8081)
+    # if "py3" in mxnet_inference:
+    #     run_ec2_mxnet_inference(mxnet_inference, BERT_MODEL, "gluonnlp", ec2_connection, "cpu", 90, 9091)
 
 def login_to_ecr_repo(ec2_connection, region):
     ec2_connection.run(
