824860302f88d8aaa1523eb6e8d05a34abf43a31
Nikhil Kulkarni <nikhilsk@amazon.com>
2020-04-01 17:39:08 -0700

    Disable update kubeconfig

--- test/test_utils/eks.py
+++ test/test_utils/eks.py
@@ -132,7 +132,7 @@ def eks_write_kubeconfig(eks_cluster_name, region="us-west-2"):
                                          --name {} --region {}""".format(eks_cluster_name, region)
     run(eksctl_write_kubeconfig_command)
 
-    run(f"aws eks --region us-west-2 update-kubeconfig --name {eks_cluster_name} --kubeconfig /root/.kube/config --role-arn arn:aws:iam::669063966089:role/nikhilsk-eks-test-role")
+    #run(f"aws eks --region us-west-2 update-kubeconfig --name {eks_cluster_name} --kubeconfig /root/.kube/config --role-arn arn:aws:iam::669063966089:role/nikhilsk-eks-test-role")
 
     LOGGER.info("kubeconfig successfully written to folder ~/.kube/config")
     run("cat /root/.kube/config", warn=True)
