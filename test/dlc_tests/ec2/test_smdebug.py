843ed1deedc24027ba6b7d4d1a3b9ed83eb25ca9
Arjuna Keshavan <arjunake@amazon.com>
2020-04-02 17:47:57 -0700

    add training/inference fixture for job specific but not framework specific tests

--- test/dlc_tests/ec2/test_smdebug.py
+++ test/dlc_tests/ec2/test_smdebug.py
@@ -9,36 +9,30 @@ SMDEBUG_SCRIPT = os.path.join(CONTAINER_TESTS_PREFIX, "testSmdebug")
 
 
 @pytest.mark.parametrize("ec2_instance_type", ["p3.8xlarge"], indirect=True)
-def test_smdebug_gpu(image, ec2_connection, gpu_only, py3_only):
-    if "inference" in image:
-        pytest.skip("Skipping testSMDebug on inference images")
-
-    framework = get_framework_from_image_uri(image)
+def test_smdebug_gpu(training, ec2_connection, region, gpu_only, py3_only):
+    framework = get_framework_from_image_uri(training)
     container_test_local_dir = os.path.join("$HOME", "container_tests")
     ec2_connection.run(
         f"$(aws ecr get-login --no-include-email --region {region})", hide=True
     )
 
     ec2_connection.run(
-        f"""nvidia-docker run -v {container_test_local_dir}:{os.path.join(os.sep, 'test')} {image} 
+        f"""nvidia-docker run -v {container_test_local_dir}:{os.path.join(os.sep, 'test')} {training} 
             {os.path.join(os.sep, 'bin', 'bash')} -c "{SMDEBUG_SCRIPT} '{framework}'" """,
         hide=True,
     )
 
 
 @pytest.mark.parametrize("ec2_instance_type", ["c5.9xlarge"], indirect=True)
-def test_smdebug_cpu(image, ec2_connection, region, cpu_only, py3_only, framework=None):
-    if "inference" in image:
-        pytest.skip("Skipping testSMDebug on inference images")
-
-    framework = get_framework_from_image_uri(image)
+def test_smdebug_cpu(training, ec2_connection, region, cpu_only, py3_only):
+    framework = get_framework_from_image_uri(training)
     container_test_local_dir = os.path.join("$HOME", "container_tests")
     ec2_connection.run(
         f"$(aws ecr get-login --no-include-email --region {region})", hide=True
     )
 
     ec2_connection.run(
-        f"""docker run -v {container_test_local_dir}:{os.path.join(os.sep, 'test')} {image} 
+        f"""docker run -v {container_test_local_dir}:{os.path.join(os.sep, 'test')} {training} 
             {os.path.join(os.sep, 'bin', 'bash')} -c "{SMDEBUG_SCRIPT} '{framework}'" """,
         hide=True,
     )
