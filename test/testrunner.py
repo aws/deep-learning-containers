e2a4d3d75626e130ce445745cc30fe38f57b85e4
Nikhil Kulkarni <nikhilsk@amazon.com>
2020-04-02 20:40:37 -0700

    Pring test status

--- test/testrunner.py
+++ test/testrunner.py
@@ -138,7 +138,7 @@ def main():
             eks_utils.eks_setup(framework)
 
         # Execute dlc_tests pytest command
-        pytest_cmd = ["-s", test_type, f"--junitxml={report}", "-n=auto"]
+        pytest_cmd = ["-s", "-rA", test_type, f"--junitxml={report}", "-n=auto"]
         sys.exit(pytest.main(pytest_cmd))
     elif test_type == "sagemaker":
         run_sagemaker_tests(dlc_images.split(" "))
