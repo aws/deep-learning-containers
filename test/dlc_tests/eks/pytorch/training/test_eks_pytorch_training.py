5e3b6dfb1b44a69a4a893fe0ece0ff15ffa52b2e
Satish Gollaprolu <sgollapr@amazon.com>
2020-04-14 22:04:30 -0700

    add envs, namespaces to create the multinode jobs
    Merge branch 'master' of https://github.com/aws/deep-learning-containers
    
     into pt_multnode

--- test/dlc_tests/eks/pytorch/training/test_eks_pytorch_training.py
+++ test/dlc_tests/eks/pytorch/training/test_eks_pytorch_training.py
@@ -2,120 +2,123 @@ import json
 import os
 import random
 
+import pytest
 from invoke import run
 from invoke.context import Context
 from retrying import retry
 
 import test.test_utils.eks as eks_utils
 from src.github import GitHubHandler
-from test.dlc_tests.conftest import LOGGER
+from test.test_utils import is_pr_context, SKIP_PR_REASON
 
 
-# def test_eks_pytorch_single_node_training(pytorch_training):
-#     """
-#     Function to create a pod using kubectl and given container image, and run MXNet training
-#     Args:
-#         :param setup_utils: environment in which EKS tools are setup
-#         :param pytorch_training: the ECR URI
-#     """
-#
-#     training_result = False
-#
-#     rand_int = random.randint(4001, 6000)
-#
-#     yaml_path = os.path.join(os.sep, "tmp", f"pytorch_single_node_training_{rand_int}.yaml")
-#     pod_name = f"pytorch-single-node-training-{rand_int}"
-#
-#     args = "git clone https://github.com/pytorch/examples.git && python examples/mnist/main.py"
-#
-#     # TODO: Change hardcoded value to read a mapping from the EKS cluster instance.
-#     cpu_limit = 72
-#     cpu_limit = str(int(cpu_limit) / 2)
-#
-#     search_replace_dict = {
-#         "<POD_NAME>": pod_name,
-#         "<CONTAINER_NAME>": pytorch_training,
-#         "<ARGS>": args,
-#         "<CPU_LIMIT>": cpu_limit,
-#     }
-#
-#     eks_utils.write_eks_yaml_file_from_template(
-#         eks_utils.SINGLE_NODE_TRAINING_TEMPLATE_PATH, yaml_path, search_replace_dict
-#     )
-#
-#     try:
-#         run("kubectl create -f {}".format(yaml_path))
-#
-#         if eks_utils.is_eks_training_complete(pod_name):
-#             pytorch_out = run("kubectl logs {}".format(pod_name)).stdout
-#             if "Accuracy" in pytorch_out:
-#                 training_result = True
-#             else:
-#                 eks_utils.LOGGER.info("**** training output ****")
-#                 eks_utils.LOGGER.debug(pytorch_out)
-#         assert training_result, f"Training failed"
-#     finally:
-#         run("kubectl delete pods {}".format(pod_name))
-#
-#
-# def test_eks_pytorch_dgl_single_node_training(pytorch_training, py3_only):
-#
-#     """
-#     Function to create a pod using kubectl and given container image, and run
-#     DGL training with PyTorch backend
-#     Args:
-#         :param pytorch_training: the ECR URI
-#     """
-#
-#     training_result = False
-#
-#     rand_int = random.randint(4001, 6000)
-#
-#     yaml_path = os.path.join(os.sep, "tmp", f"pytorch_single_node_training_dgl_{rand_int}.yaml")
-#     pod_name = f"pytorch-single-node-training-dgl-{rand_int}"
-#
-#     args = (
-#         "git clone https://github.com/dmlc/dgl.git && "
-#         "cd /dgl/examples/pytorch/gcn/ && DGLBACKEND=pytorch python train.py --dataset cora"
-#     )
-#
-#     # TODO: Change hardcoded value to read a mapping from the EKS cluster instance.
-#     cpu_limit = 72
-#     cpu_limit = str(int(cpu_limit) / 2)
-#
-#     if "gpu" in pytorch_training:
-#         args = args + " --gpu 0"
-#     else:
-#         args = args + " --gpu -1"
-#
-#     search_replace_dict = {
-#         "<POD_NAME>": pod_name,
-#         "<CONTAINER_NAME>": pytorch_training,
-#         "<ARGS>": args,
-#         "<CPU_LIMIT>": cpu_limit,
-#     }
-#
-#     eks_utils.write_eks_yaml_file_from_template(
-#         eks_utils.SINGLE_NODE_TRAINING_TEMPLATE_PATH, yaml_path, search_replace_dict
-#     )
-#
-#     try:
-#         run("kubectl create -f {}".format(yaml_path))
-#
-#         if eks_utils.is_eks_training_complete(pod_name):
-#             dgl_out = run("kubectl logs {}".format(pod_name)).stdout
-#             if "Test accuracy" in dgl_out:
-#                 training_result = True
-#             else:
-#                 eks_utils.LOGGER.info("**** training output ****")
-#                 eks_utils.LOGGER.debug(dgl_out)
-#
-#         assert training_result, f"Training failed"
-#     finally:
-#         run("kubectl delete pods {}".format(pod_name))
+LOGGER = eks_utils.LOGGER
+
+def test_eks_pytorch_single_node_training(pytorch_training):
+    """
+    Function to create a pod using kubectl and given container image, and run MXNet training
+    Args:
+        :param setup_utils: environment in which EKS tools are setup
+        :param pytorch_training: the ECR URI
+    """
+
+    training_result = False
+
+    rand_int = random.randint(4001, 6000)
+
+    yaml_path = os.path.join(os.sep, "tmp", f"pytorch_single_node_training_{rand_int}.yaml")
+    pod_name = f"pytorch-single-node-training-{rand_int}"
+
+    args = "git clone https://github.com/pytorch/examples.git && python examples/mnist/main.py"
+
+    # TODO: Change hardcoded value to read a mapping from the EKS cluster instance.
+    cpu_limit = 72
+    cpu_limit = str(int(cpu_limit) / 2)
+
+    search_replace_dict = {
+        "<POD_NAME>": pod_name,
+        "<CONTAINER_NAME>": pytorch_training,
+        "<ARGS>": args,
+        "<CPU_LIMIT>": cpu_limit,
+    }
+
+    eks_utils.write_eks_yaml_file_from_template(
+        eks_utils.SINGLE_NODE_TRAINING_TEMPLATE_PATH, yaml_path, search_replace_dict
+    )
+
+    try:
+        run("kubectl create -f {}".format(yaml_path))
+
+        if eks_utils.is_eks_training_complete(pod_name):
+            pytorch_out = run("kubectl logs {}".format(pod_name)).stdout
+            if "Accuracy" in pytorch_out:
+                training_result = True
+            else:
+                eks_utils.LOGGER.info("**** training output ****")
+                eks_utils.LOGGER.debug(pytorch_out)
+        assert training_result, f"Training failed"
+    finally:
+        run("kubectl delete pods {}".format(pod_name))
 
 
-def test_eks_pytorch_multinode_node_training(pytorch_training, example):
+def test_eks_pytorch_dgl_single_node_training(pytorch_training, py3_only):
+
+    """
+    Function to create a pod using kubectl and given container image, and run
+    DGL training with PyTorch backend
+    Args:
+        :param pytorch_training: the ECR URI
+    """
+
+    training_result = False
+
+    rand_int = random.randint(4001, 6000)
+
+    yaml_path = os.path.join(os.sep, "tmp", f"pytorch_single_node_training_dgl_{rand_int}.yaml")
+    pod_name = f"pytorch-single-node-training-dgl-{rand_int}"
+
+    args = (
+        "git clone https://github.com/dmlc/dgl.git && "
+        "cd /dgl/examples/pytorch/gcn/ && DGLBACKEND=pytorch python train.py --dataset cora"
+    )
+
+    # TODO: Change hardcoded value to read a mapping from the EKS cluster instance.
+    cpu_limit = 72
+    cpu_limit = str(int(cpu_limit) / 2)
+
+    if "gpu" in pytorch_training:
+        args = args + " --gpu 0"
+    else:
+        args = args + " --gpu -1"
+
+    search_replace_dict = {
+        "<POD_NAME>": pod_name,
+        "<CONTAINER_NAME>": pytorch_training,
+        "<ARGS>": args,
+        "<CPU_LIMIT>": cpu_limit,
+    }
+
+    eks_utils.write_eks_yaml_file_from_template(
+        eks_utils.SINGLE_NODE_TRAINING_TEMPLATE_PATH, yaml_path, search_replace_dict
+    )
+
+    try:
+        run("kubectl create -f {}".format(yaml_path))
+
+        if eks_utils.is_eks_training_complete(pod_name):
+            dgl_out = run("kubectl logs {}".format(pod_name)).stdout
+            if "Test accuracy" in dgl_out:
+                training_result = True
+            else:
+                eks_utils.LOGGER.info("**** training output ****")
+                eks_utils.LOGGER.debug(dgl_out)
+
+        assert training_result, f"Training failed"
+    finally:
+        run("kubectl delete pods {}".format(pod_name))
+
+
+def test_eks_pytorch_multinode_node_training(pytorch_training, example_only):
     """
        Function to create mutliple pods using kubectl and given container image, and run Pytorch training
        Args:
@@ -124,11 +127,11 @@ def test_eks_pytorch_multinode_node_training(pytorch_training, example):
        """
     # TODO: Change hardcoded value to read a mapping from the EKS cluster instance.
 
-    rand_int = random.randint(4001, 6000)
+    unique_id = random.randint(1, 6000)
 
-    namespace = f"pytorch-multi-node-training-{rand_int}"
-    app_name = f"eks-pytorch-mnist-app-{rand_int}"
-    job_name = f"kubeflow-pytorch-gpu-dist-job-{rand_int}"
+    namespace = f"pytorch-multi-node-training-{unique_id}"
+    app_name = f"eks-pytorch-mnist-app-{unique_id}"
+    job_name = f"kubeflow-pytorch-gpu-dist-job-{unique_id}"
     num_masters = "1"
     num_workers = "3"
     gpu_limit = "1"
@@ -141,7 +144,7 @@ def test_eks_pytorch_multinode_node_training(pytorch_training, example):
         "training",
         "multi_node_gpu_training.yaml"
     )
-    remote_yaml_path = os.path.join(os.sep, "tmp", f"pytorch_multinode_node_training_{rand_int}.yaml")
+    remote_yaml_path = os.path.join(os.sep, "tmp", f"pytorch_multinode_node_training_{unique_id}.yaml")
     replace_dict = {
         "<JOB_NAME>": job_name,
         "<NUM_MASTERS>": num_masters,
@@ -153,72 +156,72 @@ def test_eks_pytorch_multinode_node_training(pytorch_training, example):
     }
 
     eks_utils.write_eks_yaml_file_from_template(local_template_file_path, remote_yaml_path, replace_dict)
-    run_eks_pytorch_multi_node_training(namespace, app_name, job_name, remote_yaml_path)
+    run_eks_pytorch_multi_node_training(namespace, app_name, job_name, remote_yaml_path, unique_id)
 
 
-def run_eks_pytorch_multi_node_training(namespace, app_name, job_name, remote_yaml_file_path):
+def run_eks_pytorch_multi_node_training(namespace, app_name, job_name, remote_yaml_file_path, unique_id):
     """Run PyTorch distributed training on EKS using PyTorch Operator
     Args:
     namespace, app_name, job_name, remote_yaml_file_path
     """
-
     KUBEFLOW_VERSION = "v0.6.1"
-    context = Context()
+    home_dir = run("echo $HOME").stdout.strip("\n")
+    path_to_ksonnet_app = os.path.join(home_dir, f"pytorch_multi_node_eks_test-{unique_id}")
+    env = f"{namespace}-env"
 
-    eks_utils.eks_multinode_cleanup("", job_name, namespace)
+    ctx = Context()
 
     # Namespaces will allow parallel runs on the same cluster. Create namespace if it doesnt exist.
     does_namespace_exist = run(f"kubectl get namespace | grep {namespace}",
                                warn=True)
     if not does_namespace_exist:
-        LOGGER.debug(f"kubectl create namespace {namespace}")
         run(f"kubectl create namespace {namespace}")
 
-    # Create a new ksonnet app.
-    LOGGER.debug(f"rm -rf {app_name}")
-    run(f"rm -rf {app_name}")
-    github_handler = GitHubHandler("aws", "kubeflow")
-    github_token = github_handler.get_auth_token()
-    os.environ['GITHUB_TOKEN'] = github_token
-    LOGGER.debug(f"ks init {app_name}")
-    run(f"ks init {app_name}")
+    if not os.path.exists(path_to_ksonnet_app):
+        ctx.run(f"mkdir -p {path_to_ksonnet_app}")
 
-    with context.cd(app_name):
-        LOGGER.debug(f"ks env set default --namespace {namespace}")
-        context.run(f"ks env set default --namespace {namespace}")
+    with ctx.cd(path_to_ksonnet_app):
+        ctx.run(f"rm -rf {app_name}")
+        # Create a new ksonnet app.
+        github_handler = GitHubHandler("aws", "kubeflow")
+        github_token = github_handler.get_auth_token()
+        os.environ['GITHUB_TOKEN'] = github_token
+        ctx.run(f"ks init {app_name} --namespace {namespace}")
 
-        # Check if the kubeflow registry exists and create. Registry will be available in each pod.
-        LOGGER.debug(f"ks registry list | grep kubeflow")
-        does_registry_exist = context.run("ks registry list | grep kubeflow", warn=True)
-        if not does_registry_exist:
-            LOGGER.debug(f"ks registry add kubeflow github.com/kubeflow/kubeflow/tree/{KUBEFLOW_VERSION}/kubeflow")
-            context.run(f"ks registry add kubeflow github.com/kubeflow/kubeflow/tree/{KUBEFLOW_VERSION}/kubeflow")
-            LOGGER.debug(f"ks pkg install kubeflow/pytorch-job@{KUBEFLOW_VERSION}")
-            context.run(f"ks pkg install kubeflow/pytorch-job@{KUBEFLOW_VERSION}")
-            LOGGER.debug(f"ks generate pytorch-operator pytorch-operator")
-            context.run("ks generate pytorch-operator pytorch-operator")
-            try:
-                # use `$ks show default` to see details.
-                LOGGER.debug(f"ks apply default -c pytorch-operator")
-                context.run("ks apply default -c pytorch-operator")
-                # Delete old job with same name if exists
-                LOGGER.debug(f"kubectl delete -f {remote_yaml_file_path}")
-                context.run(f"kubectl delete -f {remote_yaml_file_path}", warn=True)
-                LOGGER.debug(f"kubectl create -f {remote_yaml_file_path}")
-                context.run(f"kubectl create -f {remote_yaml_file_path}")
-                if is_pytorch_eks_multinode_training_complete(job_name):
-                    LOGGER.debug(f"training is complete for jobs {job_name}")
-                    LOGGER.info(f"training is complete for jobs {job_name}")
-            except Exception as e:
-                LOGGER.debug(f"something went wrong! Exception - {e}")
-                raise Exception(f"something went wrong! Exception - {e}")
-            finally:
-                LOGGER.debug(f"kubectl delete -f {remote_yaml_file_path}")
-                context.run(f"kubectl delete -f {remote_yaml_file_path}", warn=True)
-                LOGGER.debug(f"kubectl delete crd pytorchjobs.kubeflow.org")
-                # If different versions of kubeflow used in the cluster, crd must be deleted.
-                context.run("kubectl delete crd pytorchjobs.kubeflow.org")
-                eks_utils.eks_multinode_cleanup("", job_name, namespace)
+        with ctx.cd(app_name):
+            ctx.run(f"ks env add {env} --namespace {namespace}")
+
+            # Check if the kubeflow registry exists and create. Registry will be available in each pod.
+            does_registry_exist = ctx.run("ks registry list | grep kubeflow", warn=True)
+            if not does_registry_exist:
+                ctx.run(f"ks registry add kubeflow github.com/kubeflow/kubeflow/tree/{KUBEFLOW_VERSION}/kubeflow")
+                ctx.run(f"ks pkg install kubeflow/pytorch-job@{KUBEFLOW_VERSION}")
+                ctx.run(f"ks generate pytorch-operator pytorch-operator")
+                try:
+                    # use `$ks show default` to see details.
+                    ctx.run(f"kubectl get pods -n {namespace} -o wide")
+                    LOGGER.debug(f"ks apply {env} -c pytorch-operator -n {namespace}")
+                    ctx.run(f"ks apply {env} -c pytorch-operator -n {namespace}")
+                    # Delete old job with same name if exists
+                    ctx.run(f"kubectl delete -f {remote_yaml_file_path}", warn=True)
+                    ctx.run(f"kubectl create -f {remote_yaml_file_path} -n {namespace}")
+                    training_result = is_pytorch_eks_multinode_training_complete(job_name, namespace)
+                    if training_result:
+                        run_out = run(f"kubectl logs {job_name}-master-0", warn=True).stdout
+                        if "Test accuracy" in run_out:
+                            training_result = True
+                        else:
+                            eks_utils.LOGGER.info("**** training output ****")
+                            eks_utils.LOGGER.debug(run_out)
+                    assert (
+                        training_result, f"Training failed"
+                    )
+                except Exception as e:
+                    LOGGER.debug(f"something went wrong! Exception - {e}")
+                    raise Exception(f"something went wrong! Exception - {e}")
+                finally:
+                    eks_utils.eks_multinode_cleanup(ctx, "", job_name, namespace, env)
+
 
 
 def retry_if_value_error(exception):
@@ -227,17 +230,16 @@ def retry_if_value_error(exception):
 
 
 @retry(stop_max_attempt_number=40, wait_fixed=60000, retry_on_exception=retry_if_value_error)
-def is_pytorch_eks_multinode_training_complete(job_name):
+def is_pytorch_eks_multinode_training_complete(job_name, namespace):
     """Function to check job and pod status for multinode training.
     A separate method is required because kubectl commands for logs and status are different with namespaces.
     Args:
         job_name: str
     """
-    LOGGER.debug(f"kubectl get pytorchjobs {job_name} -o json")
-    run_out = run(f"kubectl get pytorchjobs {job_name} -o json", warn=True)
+    run_out = run(f"kubectl get pytorchjobs -n {namespace} {job_name} -o json", warn=True)
     if run_out.stdout is not None or run_out.stdout != "":
         job_info = json.loads(run_out.stdout)
-        LOGGER.debug(f"job_info: {job_info}")
+        LOGGER.debug(f"Job info: {job_info}")
 
     if 'status' not in job_info:
         raise ValueError("Waiting for job to launch...")
@@ -276,9 +278,7 @@ def is_pytorch_eks_multinode_training_complete(job_name):
                             raise ValueError("Waiting for job to complete...")
                         else:
                             return False
-                    run(f"kubectl logs {job_name}-master-0", warn=True)
                     return True
-
             else:
                 raise ValueError("Waiting for job to run...")
         else:
