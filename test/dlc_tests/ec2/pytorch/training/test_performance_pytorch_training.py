8acf6d1b11947dc2210be67f149ab8d1193ce329
Zhuo Weng <wenzhuo@amazon.com>
2020-04-15 16:08:33 -0700

    Add benchmark PyTorch training GPU py3 synthetic

---
+++ test/dlc_tests/ec2/pytorch/training/test_performance_pytorch_training.py
@@ -0,0 +1,15 @@
+import os
+
+import pytest
+
+from test.test_utils import CONTAINER_TESTS_PREFIX
+from test.test_utils.ec2 import execute_ec2_training_performance_test
+
+
+PT_PERFORMANCE_TRAINING_GPU_SYNTHETIC_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "performance_tests", "run_pytorch_training_performance_gpu_synthetic")
+
+PT_EC2_GPU_SYNTHETIC_INSTANCE_TYPE = "p3.16xlarge"
+
+@pytest.mark.parametrize("ec2_instance_type", [PT_EC2_GPU_SYNTHETIC_INSTANCE_TYPE], indirect=True)
+def test_pytorch_standalone_gpu(pytorch_training, ec2_connection, gpu_only, py3_only):
+    execute_ec2_training_performance_test(ec2_connection, pytorch_training, PT_PERFORMANCE_TRAINING_GPU_SYNTHETIC_CMD)
