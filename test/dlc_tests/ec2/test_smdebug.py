ffe2fb337ab2547a323a3cb6e5b92036c52b25af
Arjuna Keshavan <arjunake@amazon.com>
2020-04-15 20:19:15 -0700

    bugfix

--- test/dlc_tests/ec2/test_smdebug.py
+++ test/dlc_tests/ec2/test_smdebug.py
@@ -35,7 +35,7 @@ def test_smdebug_cpu(training, ec2_connection, region, cpu_only, py3_only):
     )
 
     ec2_connection.run(
-        f"nvidia-docker run -v {container_test_local_dir}:{os.path.join(os.sep, 'test')} "
+        f"docker run -v {container_test_local_dir}:{os.path.join(os.sep, 'test')} "
         f"{training} {os.path.join(os.sep, 'bin', 'bash')} -c \"{test_script} '{framework}'\"",
         hide=True,
     )
