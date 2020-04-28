830cc4a2e0e1342e36bcf5103fc88f3a71e06384
Zhuo Weng <wenzhuo@amazon.com>
2020-04-28 14:12:44 -0700

    Modify log file name

--- test/dlc_tests/ec2/mxnet/inference/test_performance_mxnet_inference.py
+++ test/dlc_tests/ec2/mxnet/inference/test_performance_mxnet_inference.py
@@ -7,7 +7,7 @@ from test.test_utils.ec2 import execute_ec2_training_performance_test
 
 
 TF_PERFORMANCE_INFERENCE_GPU_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "performance_tests", "run_mxnet_inference_performance_gpu")
-TF_PERFORMANCE_INFERENCE_CPU_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "performance_tests", "run_mxnet_inference_performance_gpu")
+TF_PERFORMANCE_INFERENCE_CPU_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "performance_tests", "run_mxnet_inference_performance_cpu")
 
 MX_EC2_GPU_INSTANCE_TYPE = "p3.16xlarge"
 MX_EC2_CPU_INSTANCE_TYPE = "c5.18xlarge"
