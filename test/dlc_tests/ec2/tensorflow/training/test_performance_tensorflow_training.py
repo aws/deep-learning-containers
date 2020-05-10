db23ac0ee98bc57062028b779a7a9af1b23126da
Zhuo Weng <wenzhuo@amazon.com>
2020-05-10 00:21:36 -0700

    disable benchmark scripts

--- test/dlc_tests/ec2/tensorflow/training/test_performance_tensorflow_training.py
+++ test/dlc_tests/ec2/tensorflow/training/test_performance_tensorflow_training.py
@@ -13,17 +13,17 @@ TF_PERFORMANCE_TRAINING_GPU_IMAGENET_CMD = os.path.join(CONTAINER_TESTS_PREFIX, 
 TF_EC2_GPU_INSTANCE_TYPE = "p3.16xlarge"
 TF_EC2_CPU_INSTANCE_TYPE = "c5.18xlarge"
 
-
+@pytest.mark.skip(reason="Skip benchmark tests")
 @pytest.mark.parametrize("ec2_instance_type", [TF_EC2_CPU_INSTANCE_TYPE], indirect=True)
 def test_performance_tensorflow_cpu(tensorflow_training, ec2_connection, cpu_only):
     execute_ec2_training_performance_test(ec2_connection, tensorflow_training, TF_PERFORMANCE_TRAINING_CPU_SYNTHETIC_CMD)
 
-
+@pytest.mark.skip(reason="Skip benchmark tests")
 @pytest.mark.parametrize("ec2_instance_type", [TF_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_performance_tensorflow_gpu_synthetic(tensorflow_training, ec2_connection, gpu_only):
     execute_ec2_training_performance_test(ec2_connection, tensorflow_training, TF_PERFORMANCE_TRAINING_GPU_SYNTHETIC_CMD)
 
-
+@pytest.mark.skip(reason="Skip benchmark tests")
 @pytest.mark.parametrize("ec2_instance_type", [TF_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_performance_tensorflow_gpu_imagenet(tensorflow_training, ec2_connection, gpu_only):
     execute_ec2_training_performance_test(ec2_connection, tensorflow_training, TF_PERFORMANCE_TRAINING_GPU_IMAGENET_CMD)
