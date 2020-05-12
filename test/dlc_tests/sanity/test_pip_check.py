7f710ef59da1f0bb94ee78047682f9431091d7d5
Arjuna Keshavan <arjunake@amazon.com>
2020-05-12 10:29:37 -0700

    Test remove TF

--- test/dlc_tests/sanity/test_pip_check.py
+++ test/dlc_tests/sanity/test_pip_check.py
@@ -7,11 +7,5 @@ def test_pip_check(image):
     """
     Test to run pip sanity tests
     """
-    if "tensorflow-inference" in image:
-        pytest.xfail(
-            reason="Tensorflow serving api requires tensorflow, but we explicitly do not install"
-            "tensorflow in serving containers."
-        )
-
     # Add null entrypoint to ensure command exits immediately
     run_subprocess_cmd(f"docker run --entrypoint='' {image} pip check")
