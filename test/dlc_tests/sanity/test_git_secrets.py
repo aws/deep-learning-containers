a1a9449a3cdb805944de11329b3b580866111c70
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2020-05-12 04:38:54 -0700

    Fix git secrets test for non PR builds

--- test/dlc_tests/sanity/test_git_secrets.py
+++ test/dlc_tests/sanity/test_git_secrets.py
@@ -1,20 +1,41 @@
+import logging
 import os
+import sys
+
+import pytest
 
 from invoke.context import Context
 
+from test.test_utils import is_pr_context, SKIP_NOT_PR_REASON
 
-def test_git_secrets():
-    ctx = Context()
-    pwd = ctx.run("pwd").stdout.strip("\n")
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+LOGGER.addHandler(logging.StreamHandler(sys.stderr))
+
+
+def _recursive_find_repo_path(ctx):
+    pwd = ctx.run("pwd", echo=True).stdout.strip("\n")
     repository_path = pwd
     while os.path.basename(repository_path) != "deep-learning-containers":
         repository_path = os.path.dirname(repository_path)
+        if repository_path == "/":
+            raise EnvironmentError(f"Repository path could not be found from {pwd}")
+    return repository_path
+
+
+@pytest.mark.skipif(not is_pr_context(), reason=SKIP_NOT_PR_REASON)
+def test_git_secrets():
+    ctx = Context()
+    repository_path = os.getenv("CODEBUILD_SRC_DIR")
+    if not repository_path:
+        repository_path = _recursive_find_repo_path(ctx)
+    LOGGER.info(f"repository_path = {repository_path}")
     with ctx.cd(repository_path):
-        ctx.run("git clone https://github.com/awslabs/git-secrets.git")
+        ctx.run("git clone https://github.com/awslabs/git-secrets.git", echo=True)
         with ctx.cd("git-secrets"):
-            ctx.run("make install")
-        ctx.run("git secrets --install")
-        ctx.run("git secrets --register-aws")
+            ctx.run("make install", echo=True)
+        ctx.run("git secrets --install", echo=True)
+        ctx.run("git secrets --register-aws", echo=True)
         ctx.run("git secrets --list", echo=True)
         scan_results = ctx.run("git secrets --scan", echo=True)
     assert scan_results.ok, scan_results.stderr
