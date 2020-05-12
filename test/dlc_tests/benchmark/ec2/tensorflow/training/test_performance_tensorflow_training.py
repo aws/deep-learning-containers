8f225a4861d7b06a0393f785f60d48e22e62e533
Zhuo Weng <wenzhuo@amazon.com>
2020-05-11 18:07:56 -0700

    Enable only tensorflow benchmark

--- test/dlc_tests/benchmark/ec2/tensorflow/training/test_performance_tensorflow_training.py
+++ test/dlc_tests/benchmark/ec2/tensorflow/training/test_performance_tensorflow_training.py
@@ -18,12 +18,12 @@ TF_EC2_CPU_INSTANCE_TYPE = "c5.18xlarge"
 def test_performance_tensorflow_cpu(tensorflow_training, ec2_connection, cpu_only):
     execute_ec2_training_performance_test(ec2_connection, tensorflow_training, TF_PERFORMANCE_TRAINING_CPU_SYNTHETIC_CMD)
 
-@pytest.mark.skip(reason="Skip non-benchmark tests")
+
 @pytest.mark.parametrize("ec2_instance_type", [TF_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_performance_tensorflow_gpu_synthetic(tensorflow_training, ec2_connection, gpu_only):
     execute_ec2_training_performance_test(ec2_connection, tensorflow_training, TF_PERFORMANCE_TRAINING_GPU_SYNTHETIC_CMD)
 
-@pytest.mark.skip(reason="Skip non-benchmark tests")
+
 @pytest.mark.parametrize("ec2_instance_type", [TF_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_performance_tensorflow_gpu_imagenet(tensorflow_training, ec2_connection, gpu_only):
     execute_ec2_training_performance_test(ec2_connection, tensorflow_training, TF_PERFORMANCE_TRAINING_GPU_IMAGENET_CMD)
