2b3de0fa323a1a3c1ebd006812e98f3799ebe16e
Satish Gollaprolu <sgollapr@amazon.com>
2020-05-14 09:59:24 -0700

    disable builds with config

--- src/config/build_config.py
+++ src/config/build_config.py
@@ -1,5 +1,5 @@
 # Disable new builds or build without datetime tag
-DISABLE_DATETIME_TAG = False
+DISABLE_DATETIME_TAG = True
 # Note: Need to build the images at least once with DISABLE_DATETIME_TAG = True
 # before disabling new builds or tests will fail
-DISABLE_NEW_BUILDS = False
+DISABLE_NEW_BUILDS = True
