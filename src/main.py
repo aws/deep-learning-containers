2f27499f0a42d1aca467f3b6a685e2e7198d6b35
Satish Gollaprolu <sgollapr@amazon.com>
2020-05-15 00:03:52 -0700

    disable builds and tests for other frameworks

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import utils
 import constants
 
 from image_builder import image_builder
+from config import build_config
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Program to build docker images")
@@ -30,11 +31,11 @@ if __name__ == "__main__":
         if not args.py_versions == constants.ALL
         else args.py_versions
     )
-
-    utils.build_setup(
-        args.framework,
-        device_types=device_types,
-        image_types=image_types,
-        py_versions=py_versions,
-    )
-    image_builder(args.buildspec)
+    if args.framework not in build_config.DISABLE_FRAMEWORK_TESTS:
+        utils.build_setup(
+            args.framework,
+            device_types=device_types,
+            image_types=image_types,
+            py_versions=py_versions,
+        )
+        image_builder(args.buildspec)
