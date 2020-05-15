1c2d8342de93bce1b104f2b159c56d178c98b9fb
Satish Gollaprolu <sgollapr@amazon.com>
2020-05-14 21:33:45 -0700

    enable sagemaker tests

--- src/config/test_config.py
+++ src/config/test_config.py
@@ -1,5 +1,5 @@
 # Disable the test codebuild jobs to be run
-DISABLE_SAGEMAKER_TESTS = True
+DISABLE_SAGEMAKER_TESTS = False
 DISABLE_ECS_TESTS = False
 DISABLE_EKS_TESTS = False
 DISABLE_EC2_TESTS = False
