c3e9a53ce2320e2354cc6f0ef8d85640585108b7
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2020-05-16 00:40:34 -0700

    Re-enable all tf ec2 tests

--- test/test_utils/ec2.py
+++ test/test_utils/ec2.py
@@ -311,9 +311,9 @@ def execute_ec2_training_test(connection, ecr_uri, test_cmd, region=DEFAULT_REGI
         hide=True,
     )
     connection.run(
-        f"{docker_cmd} exec --user root ec2_training_container {os.path.join(os.sep, 'bin', 'bash')} -c '{test_cmd}'",
+        f"{docker_cmd} exec -i --user root ec2_training_container {test_cmd}",
         hide=True,
-        timeout=1020,
+        timeout=1800,
     )
 
 
