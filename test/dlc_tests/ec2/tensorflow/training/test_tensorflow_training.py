110a7d4c3f69e8270c893b645ba6ae220316c1cb
Arjuna Keshavan <arjunake@amazon.com>
2020-05-17 11:09:30 -0700

    change: Make two CPU tests run on c5.18

--- test/dlc_tests/ec2/tensorflow/training/test_tensorflow_training.py
+++ test/dlc_tests/ec2/tensorflow/training/test_tensorflow_training.py
@@ -43,8 +43,7 @@ def test_tensorflow_train_mnist_gpu(tensorflow_training, ec2_connection, gpu_onl
     execute_ec2_training_test(ec2_connection, tensorflow_training, TF_MNIST_CMD)
 
 
-@pytest.mark.skip(reason="Skipping the test temporarily due to small instance timeout issue")
-@pytest.mark.parametrize("ec2_instance_type", TF_EC2_CPU_INSTANCE_TYPE, indirect=True)
+@pytest.mark.parametrize("ec2_instance_type", ["c5.18xlarge"], indirect=True)
 def test_tensorflow_train_mnist_cpu(tensorflow_training, ec2_connection, cpu_only):
     execute_ec2_training_test(ec2_connection, tensorflow_training, TF_MNIST_CMD)
 
@@ -55,8 +54,7 @@ def test_tensorflow_with_horovod_gpu(tensorflow_training, ec2_connection, gpu_on
     execute_ec2_training_test(ec2_connection, tensorflow_training, test_script)
 
 
-@pytest.mark.skip(reason="Skipping the test temporarily due to timeout issue")
-@pytest.mark.parametrize("ec2_instance_type", TF_EC2_CPU_INSTANCE_TYPE, indirect=True)
+@pytest.mark.parametrize("ec2_instance_type", ["c5.18xlarge"], indirect=True)
 def test_tensorflow_with_horovod_cpu(tensorflow_training, ec2_connection, cpu_only):
     test_script = TF1_HVD_CMD if is_tf1(tensorflow_training) else TF2_HVD_CMD
     execute_ec2_training_test(ec2_connection, tensorflow_training, test_script)
