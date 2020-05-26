1231913da5bc0d923043033fea339fa2b89d5037
Zhuo Weng <wenzhuo@amazon.com>
2020-05-26 14:48:48 -0700

    Convert to mergeable state

--- test/dlc_tests/benchmark/ec2/mxnet/test_performance_mxnet_training.py
+++ test/dlc_tests/benchmark/ec2/mxnet/test_performance_mxnet_training.py
@@ -6,8 +6,8 @@ from test.test_utils import CONTAINER_TESTS_PREFIX
 from test.test_utils.ec2 import execute_ec2_training_performance_test
 
 
-MX_PERFORMANCE_TRAINING_GPU_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "performance_tests", "run_mxnet_training_performance_gpu")
-MX_PERFORMANCE_TRAINING_CPU_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "performance_tests", "run_mxnet_training_performance_cpu")
+MX_PERFORMANCE_TRAINING_GPU_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "benchmark", "run_mxnet_training_performance_gpu")
+MX_PERFORMANCE_TRAINING_CPU_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "benchmark", "run_mxnet_training_performance_cpu")
 
 MX_EC2_GPU_INSTANCE_TYPE = "p3.16xlarge"
 MX_EC2_CPU_INSTANCE_TYPE = "c5.18xlarge"
