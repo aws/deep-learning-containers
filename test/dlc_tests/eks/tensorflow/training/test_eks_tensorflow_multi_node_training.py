085bf5521cbf73c3814bec60195c156809b17d6c
Zhuo Weng <wenzhuo@amazon.com>
2020-06-09 15:50:14 -0700

    Remove assertion for EKS multinode training

--- test/dlc_tests/eks/tensorflow/training/test_eks_tensorflow_multi_node_training.py
+++ test/dlc_tests/eks/tensorflow/training/test_eks_tensorflow_multi_node_training.py
@@ -16,13 +16,10 @@ from test.test_utils import is_pr_context, SKIP_PR_REASON
 
 
 # Test only runs in region us-west-2, on instance type p3.16xlarge, on PR_EKS_CLUSTER_NAME_TEMPLATE cluster
-@pytest.mark.skipif(is_pr_context(), reason=SKIP_PR_REASON)
+# @pytest.mark.skipif(is_pr_context(), reason=SKIP_PR_REASON)
 def test_eks_tensorflow_multi_node_training_gpu(tensorflow_training, example_only):
     eks_cluster_size = 3
     ec2_instance_type = "p3.16xlarge"
-    cluster_name = eks_utils.PR_EKS_CLUSTER_NAME_TEMPLATE.format("tensorflow")
-
-    assert eks_utils.is_eks_cluster_active(cluster_name), f"EKS Cluster {cluster_name} is inactive. Exiting test"
 
     eks_gpus_per_worker = ec2_utils.get_instance_num_gpus(instance_type=ec2_instance_type)
 
