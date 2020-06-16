38dc99d88abf08c927b56df2b0eaa418d61ca049
Arjuna Keshavan <arjunake@amazon.com>
2020-06-16 12:16:50 -0700

    update exception match

--- test/dlc_tests/sanity/test_pip_check.py
+++ test/dlc_tests/sanity/test_pip_check.py
@@ -1,6 +1,9 @@
+import re
+
 import pytest
 
 from invoke.context import Context
+from invoke.exceptions import UnexpectedExit
 
 
 @pytest.mark.canary("Run pip check test regularly on production images")
@@ -10,4 +13,15 @@ def test_pip_check(image):
     """
     # Add null entrypoint to ensure command exits immediately
     ctx = Context()
-    ctx.run(f"docker run --entrypoint='' {image} pip check", hide=True)
+    gpu_suffix = '-gpu' if 'gpu' in image else ''
+
+    # TF inference containers do not have core tensorflow installed by design. Allowing for this pip check error
+    # to occur in order to catch other pip check issues that may be associated with TF inference
+    allowed_exception = re.compile(rf'^tensorflow-serving-api{gpu_suffix} \d\.\d+\.\d+ requires '
+                                   rf'tensorflow{gpu_suffix}, which is not installed.$')
+    try:
+        run_out = ctx.run(f"docker run --entrypoint='' {image} pip check", hide=True)
+    except UnexpectedExit as e:
+        if not allowed_exception.match(run_out):
+            raise
+
