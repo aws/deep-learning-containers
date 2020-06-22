27261ae20d8f028f9f76be37baf7a06de740d663
Qu <huahq@iad7-dhcp-95-181-76.iad7.amazon.com>
2020-06-22 19:31:43 -0400

    fixed send log module

--- TestRequester/LogReturn/sendlog.py
+++ TestRequester/LogReturn/sendlog.py
@@ -1,33 +1,29 @@
 import os
-import random
-import sys
-import logging
-import re
-import json
-import xmltodict
-
-from multiprocessing import Pool
-
 import boto3
-import pytest
-
-from botocore.config import Config
-from invoke import run
-from invoke.context import Context
-from LogReturn.sendlog import LogReturn
-
-from test_utils import eks as eks_utils
-from test_utils import get_dlc_images, is_pr_context, destroy_ssh_keypair, setup_sm_benchmark_tf_train_env
-from test_utils import KEYS_TO_DESTROY_FILE
 
 
-class LogReturn():
-    def __init__(self):
-        pass
 
-    def send_log(self):
-        log_sqs_url = os.getenv("RETURN_SQS_URL")
-        log_location = self.log_locater()
-        sqs = boto3.client("sqs")
-        print(log_sqs_url)
-        sqs.send_message(QueueUrl=log_sqs_url, MessageBody=log_location)
+def log_locater():
+    arn = os.getenv("CODEBUILD_BUILD_ARN")
+    log_group_name = "/aws/codebuild/" + arn.split(":")[-2]
+    log_stream_name = arn.split(":")[-1]
+
+    with open("test/sagemaker.xml") as xml_file:
+        data_dict = xmltodict.parse(xml_file.read())
+        xml_file.close()
+        report_json_data = json.dumps(data_dict)
+
+    content = {}
+    content["LOG_GROUP_NAME"] = log_group_name
+    content["LOG_STREAM_NAME"] = log_stream_name
+    content["TICKET_NAME"] = os.getenv("TICKET_NAME")
+    content["XML_REPORT"] = report_json_data
+
+    return json.dumps(content)
+
+def send_log():
+    log_sqs_url = os.getenv("RETURN_SQS_URL")
+    log_location = log_locater()
+    sqs = boto3.client("sqs")
+    print(log_sqs_url)
+    sqs.send_message(QueueUrl=log_sqs_url, MessageBody=log_location)
--- test/sagemakertestrunner.py
+++ test/sagemakertestrunner.py
@@ -14,7 +14,7 @@ import pytest
 from botocore.config import Config
 from invoke import run
 from invoke.context import Context
-from LogReturn.sendlog import LogReturn
+import LogReturn.sendlog
 
 from test_utils import eks as eks_utils
 from test_utils import get_dlc_images, is_pr_context, destroy_ssh_keypair, setup_sm_benchmark_tf_train_env
@@ -180,7 +180,7 @@ def main():
     #
     # print("\n\nitems in the test directory:")
     # os.listdir("test")
-    LogReturn.send_log()
+    LogReturn.sendlog.send_log()
 
 
 
