b6f39279024ef90cfb722376b6eb4268ae5cfdbf
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2020-06-24 13:09:59 -0700

    Disable MX, PT builds and datetime tags

--- src/config/build_config.py
+++ src/config/build_config.py
@@ -1,7 +1,7 @@
 # Frameworks for which you want to disable both builds and tests
-DISABLE_FRAMEWORK_TESTS = []
+DISABLE_FRAMEWORK_TESTS = ["mxnet", "pytorch"]
 # Disable new builds or build without datetime tag
-DISABLE_DATETIME_TAG = False
+DISABLE_DATETIME_TAG = True
 # Note: Need to build the images at least once with DISABLE_DATETIME_TAG = True
 # before disabling new builds or tests will fail
 DISABLE_NEW_BUILDS = False
