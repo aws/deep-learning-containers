e84c91e7984eceec889742fcb76e6643cc9f28eb
Nikhil Kulkarni <nikhilsk@amazon.com>
2020-06-26 03:20:55 -0700

    test_sanity

--- test/dlc_tests/sanity/test_binary_visibility.py
+++ test/dlc_tests/sanity/test_binary_visibility.py
@@ -9,5 +9,15 @@ from invoke.context import Context
 from test.test_utils import is_pr_context, PR_ONLY_REASON
 
 pytest.mark.skipif(not is_pr_context(), reason=PR_ONLY_REASON)
-def test_binary_visibility():
-    pass
+def test_binary_visibility(image: str):
+    """
+    Test to check if the binary built with image is public/private. Assumes that URIs beginning with 's3://' are private.
+    This will mandate specifying all public links as ones beginning with 'https://'. While s3 objects beginning with
+    'https://' may still be private, codebuild 'build' job uses 'curl' i.e. unsigned request to fetch them and hence should
+    fail if an 'https://' link is still private
+    :param image:
+    :return:
+    """
+
+
+    assert image.startswith("https://")
