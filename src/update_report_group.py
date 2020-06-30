21361c000d6aff1fbbc6d8a4409b8c787bec4898
Zhuo Weng <wenzhuo@amazon.com>
2020-06-30 15:14:06 -0700

    use shorthand

--- src/update_report_group.py
+++ src/update_report_group.py
@@ -12,12 +12,4 @@ report_group_name = os.getenv("CODEBUILD_BUILD_ID").split(":")[0]+"-test_reports
 report_group_arn = ":".join(os.getenv("CODEBUILD_BUILD_ARN").split("/")[0].split(":")[:-1])+":report-group/"+report_group_name
 LOGGER.info(f"{report_group_arn}")
 LOGGER.info(f"{report_group_name}")
-config = {
-  "exportConfigType": "S3",
-  "s3Destination": {
-    "bucket": "dlinfra-dlc-cicd-performance",
-    "path": "test",
-  }
-}
-LOGGER.info(f"{config}")
-Context().run(f"aws codebuild update-report-group --arn {report_group_arn} --export-config {config}")
+Context().run(f"""aws codebuild update-report-group --arn {report_group_arn} --export-config exportConfigType=S3,s3Destination={{bucket="dlinfra-dlc-cicd-performance"}}""")
