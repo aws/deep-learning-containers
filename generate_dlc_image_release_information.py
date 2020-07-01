0221288ff50e31b666e2ea107a79ceec757848ec
Nikhil Kulkarni <nikhilsk@amazon.com>
2020-07-01 07:48:54 -0700

    Generate release information for a DLC

---
+++ generate_dlc_image_release_information.py
@@ -0,0 +1,115 @@
+import argparse
+import logging
+import json
+import os
+import sys
+import tarfile
+
+import boto3
+
+from botocore.exceptions import ClientError
+from release.dlc_release_information import DLCReleaseInformation
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.StreamHandler(sys.stdout))
+LOGGER.setLevel(logging.INFO)
+
+
+def write_to_file(file_name, content):
+    """
+    Uses json package to dump content to a file. The content may or may-not be json-structured.
+    :param file_name:
+    :param content:
+    :return:
+    """
+    with open(file_name, "w") as fp:
+        json.dump(content, fp)
+        LOGGER.info(f"Content written to file: {file_name}")
+
+
+def upload_to_S3(local_file_path, bucket_name, bucket_key):
+    """
+    Upload a local file to s3 with specified bucket name and key
+    :param local_file_path: string
+    :param bucket_name: string
+    :param bucket_key: string
+    :return:
+    """
+    _s3 = boto3.client('s3')
+    try:
+        _s3.upload_file(local_file_path, bucket_name, bucket_key)
+    except ClientError as e:
+        LOGGER.error("Error: Cannot upload file to s3 bucket.")
+        LOGGER.error("Exception: {}".format(e))
+        raise
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Specify bucket name to upload release information for DLC")
+    parser.add_argument("--artifact-bucket", type=str, required=True)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    args = parse_args()
+    dlc_artifact_bucket = args.artifact_bucket
+
+    dlc_tag = os.getenv("ASIMOV_TAG_WITH_VERSION")
+    dlc_account_id = os.getenv("TARGET_ACCOUNT_ID_CLASSIC")
+    dlc_repository = os.getenv("TARGET_ECR_REPOSITORY")
+    dlc_region = os.getenv("REGION")
+
+    if not dlc_tag:
+        raise ValueError(
+            "Environment variable ASIMOV_TAG_WITH_VERSION not set. This environment variable is expected to be set by the promoter stage.")
+
+    dlc_release_information = DLCReleaseInformation(dlc_account_id, dlc_region, dlc_repository, dlc_tag)
+
+    release_info = {
+        "bom_pip_packages": dlc_release_information.bom_pip_packages,
+        "bom_apt_packages": dlc_release_information.bom_apt_packages,
+        "imp_pip_packages": dlc_release_information.imp_pip_packages,
+        "imp_apt_packages": dlc_release_information.imp_apt_packages,
+        "image_digest": dlc_release_information.image_digest,
+        "image_tags": dlc_release_information.image_tags
+    }
+
+    dlc_folder = os.getenv("CODEBUILD_RESOLVED_SOURCE_VERSION")
+
+    directory = os.path.join(os.sep, os.getcwd(), dlc_folder)
+    if directory != "" and not os.path.isdir(directory):
+        LOGGER.info(f"Creating folder: {directory}")
+        os.mkdir(directory)
+
+    # Zip the BOM for pip and apt packages. Store the tar in S3, and save its s3URI in release_info as reference.
+    bom_pip_packages_local_path = os.path.join(os.sep, directory, "bom_pip_packages.md")
+    bom_apt_packages_local_path = os.path.join(os.sep, directory, "bom_apt_packages.md")
+
+    write_to_file(bom_pip_packages_local_path, release_info["bom_pip_packages"])
+    write_to_file(bom_apt_packages_local_path, release_info["bom_apt_packages"])
+
+    # Save the zip as <dlc_tag>_BOM.zip
+    tarfile_path = f"{dlc_tag}_BOM.tar.gz"
+    with tarfile.open(tarfile_path, "w:gz") as dlc_bom_tar:
+        dlc_bom_tar.add(bom_pip_packages_local_path, arcname=os.path.basename(bom_pip_packages_local_path))
+        dlc_bom_tar.add(bom_apt_packages_local_path, arcname=os.path.basename(bom_apt_packages_local_path))
+
+    LOGGER.info(f"Created BOM zip: {tarfile_path}")
+
+    upload_to_S3(tarfile_path, dlc_artifact_bucket, f"{dlc_folder}/{dlc_tag}/{tarfile_path}")
+
+    # Remove zipfile
+    os.remove(tarfile_path)
+
+    s3URI = f"s3://{dlc_artifact_bucket}/{dlc_folder}/{dlc_tag}/"
+
+    release_info["bom_tar_s3_uri"] = f"{s3URI}{tarfile_path}"
+
+    dlc_release_info_json = os.path.join(os.sep, directory, "dlc_release_info.json")
+    write_to_file(dlc_release_info_json, release_info)
+
+    upload_to_S3(dlc_release_info_json, dlc_artifact_bucket, f"{dlc_folder}/{dlc_tag}/dlc_release_info.json")
+
+    LOGGER.info(f"Release Information collected for image: {dlc_release_information.image}")
+    LOGGER.info(f"Release information and BOM uploaded to: s3://{dlc_artifact_bucket}/{dlc_folder}/{dlc_tag}")
---
+++ release/dlc_release_information.py
@@ -0,0 +1,125 @@
+import os
+import json
+import sys
+
+import boto3
+import logging
+import requests
+
+from botocore.exceptions import ClientError
+from invoke import run
+
+from src.buildspec import Buildspec
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.StreamHandler(sys.stdout))
+LOGGER.setLevel(logging.INFO)
+
+
+class DLCReleaseInformation:
+    def __init__(self, dlc_account_id, dlc_region, dlc_repository, dlc_tag):
+        self.dlc_account_id = dlc_account_id
+        self.dlc_region = dlc_region
+        self.dlc_repository = dlc_repository
+        self.dlc_tag = dlc_tag
+
+        self.container_name = self.run_container()
+
+        imp_package_list_path = os.path.join(
+            os.sep, os.path.dirname(__file__), "resources", "important_dlc_packages.yml"
+        )
+        self.imp_packages_to_record = Buildspec()
+        self.imp_packages_to_record.load(imp_package_list_path)
+
+        self._image_details = self.get_image_details_from_ecr()
+
+    def get_boto3_ecr_client(self):
+        return boto3.Session(region_name=self.dlc_region).client('ecr')
+
+    def run_container(self):
+        """
+        Quickly run a container and assign it a name, so that different commands may be run on it
+        :return:
+        """
+        repo_name, image_tag = self.image.split("/")[-1].split(":")
+
+        container_name = f"{repo_name}-{image_tag}-release-information"
+
+        run(f"docker rm -f {container_name}", warn=True, hide=True)
+
+        run(f"docker run -id " f"--name {container_name} " f"--entrypoint='/bin/bash' " f"{self.image}", hide=True)
+
+        return container_name
+
+    def get_container_command_output(self, command):
+        """
+        Get stdout of the command executed.
+        Note: does not handle stderr as it's not important in this context.
+        :param command:
+        :return:
+        """
+        docker_exec_cmd = f"docker exec -i {self.container_name}"
+        run_stdout = run(f"{docker_exec_cmd} {command}", hide=True).stdout.strip()
+
+        return run_stdout.strip()
+
+    def get_image_details_from_ecr(self):
+        _ecr = self.get_boto3_ecr_client()
+
+        try:
+            response = _ecr.describe_images(
+                registryId=self.dlc_account_id,
+                repositoryName=self.dlc_repository,
+                imageIds=[{'imageTag': self.dlc_tag}]
+            )
+        except _ecr.exceptions.ImageNotFoundException:
+            LOGGER.error(f"Image {self.image} does not exist in ECR repo.")
+        except ClientError as e:
+            LOGGER.error("ClientError when performing ECR operation. Exception: {}".format(e))
+
+        return response["imageDetails"][0]
+
+    @property
+    def image(self):
+        return f"{self.dlc_account_id}.dkr.ecr.{self.dlc_region}.amazonaws.com/{self.dlc_repository}:{self.dlc_tag}"
+
+    @property
+    def image_tags(self):
+        return self._image_details['imageTags']
+
+    @property
+    def image_digest(self):
+        return self._image_details['imageDigest']
+
+    @property
+    def bom_pip_packages(self):
+        return self.get_container_command_output("pip freeze").replace("\n", "<br />")
+
+    @property
+    def bom_apt_packages(self):
+        return self.get_container_command_output("apt list --installed").replace("\n", "<br />")
+
+    @property
+    def imp_pip_packages(self):
+        imp_pip_packages = {}
+        container_pip_packages = json.loads(self.get_container_command_output("pip list --format=json"))
+
+        for pip_package in self.imp_packages_to_record["pip_packages"]:
+            for package_entry in container_pip_packages:
+                if package_entry["name"] == pip_package:
+                    imp_pip_packages[package_entry["name"]] = package_entry["version"]
+
+        return imp_pip_packages
+
+    @property
+    def imp_apt_packages(self):
+        imp_apt_packages = []
+
+        for apt_package in self.imp_packages_to_record["apt_packages"]:
+            apt_package_name = self.get_container_command_output(
+                f"dpkg --get-selections | grep -i {apt_package} | awk '{{print $1}}'"
+            )
+            if apt_package_name:
+                imp_apt_packages.append(apt_package_name.replace("\n", " & "))
+
+        return imp_apt_packages
