c35f372486c43da1576ae80a71f03d5ea478cdc4
Tushar Dey <tshdy@amazon.com>
2020-06-30 17:24:30 -0700

    basis autocorrect

--- test/dlc_tests/eks/mxnet/training/test_eks_mxnet_multinode_training.py
+++ test/dlc_tests/eks/mxnet/training/test_eks_mxnet_multinode_training.py
@@ -21,7 +21,7 @@ LOGGER = eks_utils.LOGGER
 
 #Skip these test for temporary basis
 #TODO : unskip this test and find a fix for this
-@pytest.mark.skip(reason="Skipping the EKS Multinode Test for temporary basics. It passes manually, but fails in the pipeline currently")
+@pytest.mark.skip(reason="Skipping the EKS Multinode Test for temporary basis. It passes manually, but fails in the pipeline currently")
 def test_eks_mxnet_multi_node_training_horovod_mnist(mxnet_training, example_only):
     """Run MXNet distributed training on EKS using docker images with MNIST dataset"""
 
@@ -64,7 +64,7 @@ def test_eks_mxnet_multi_node_training_horovod_mnist(mxnet_training, example_onl
 
 #Skip these test for temporary basis
 #TODO : unskip this test and find a fix for this
-@pytest.mark.skip(reason="Skipping the EKS Multinode Test for temporary basics. It passes manually, but fails in the pipeline currently")
+@pytest.mark.skip(reason="Skipping the EKS Multinode Test for temporary basis. It passes manually, but fails in the pipeline currently")
 def test_eks_mxnet_multinode_training(mxnet_training, example_only):
     """Run MXNet distributed training on EKS using docker images with MNIST dataset"""
     random.seed(f"{mxnet_training}-{datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')}")
