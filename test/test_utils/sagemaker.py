ed334a4f3c18c321b4663c678790225867de8eb1
Satish Gollaprolu <sgollapr@amazon.com>
2020-07-07 20:25:51 -0700

    update sagemaker_tests pytorch imports

--- test/test_utils/sagemaker.py
+++ test/test_utils/sagemaker.py
@@ -176,7 +176,7 @@ def install_sm_local_dependencies(framework, job_type, image, ec2_conn):
         install_custom_python("3.6", ec2_conn)
     ec2_conn.run(f"virtualenv env")
     ec2_conn.run(f"source ./env/bin/activate")
-    if framework == "pytorch" and job_type == "inference":
+    if framework == "pytorch":
         # The following distutils package conflict with test dependencies
         ec2_conn.run("apt-get remove python3-scipy python3-yaml -y")
     ec2_conn.run(f"sudo {is_py3} pip install -r requirements.txt ", warn=True)
