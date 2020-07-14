3fa493023352fe27d5ebbd215c1106f640522d2d
Eliza Zhang <yutongzh@amazon.com>
2020-07-14 10:38:32 -0700

    replace the addons test script with simple tfa.image

--- test/dlc_tests/container_tests/bin/testTFAddons.py
+++ test/dlc_tests/container_tests/bin/testTFAddons.py
@@ -1,37 +1,18 @@
 import tensorflow as tf
+import numpy as np
 import tensorflow_addons as tfa
 
-def train():
-  mnist = tf.keras.datasets.mnist
+def test():
+  img_path = tf.keras.utils.get_file('tensorflow.png','https://tensorflow.org/images/tf_logo.png')
+  img_raw = tf.io.read_file(img_path)
+  img = tf.io.decode_image(img_raw)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+  img = tf.image.resize(img, [500,500])
 
-  (x_train, y_train),(x_test, y_test) = mnist.load_data()
-  x_train, x_test = x_train / 255.0, x_test / 255.0
-
-  model = tf.keras.models.Sequential([
-    # Reshape into "channels last" setup.
-    tf.keras.layers.Reshape((28,28,1), input_shape=(28,28)),
-    tf.keras.layers.Conv2D(filters=10, kernel_size=(3,3),data_format="channels_last"),
-    # LayerNorm Layer
-    tfa.layers.InstanceNormalization(axis=3, 
-                                     center=True, 
-                                     scale=True,
-                                     beta_initializer="random_uniform",
-                                     gamma_initializer="random_uniform"),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10, activation='softmax')
-  ])
-
-  model.compile(optimizer='adam',
-                loss='sparse_categorical_crossentropy',
-                metrics=['accuracy'])
-
-  model.fit(x_test, y_test)
-
-  score = model.evaluate(x_test, y_test, verbose=0)
-  print('Test loss:', score[0])
-  print('Test accuracy:', score[1])
+  # Simple test with tfa.image 
+  mean = tfa.image.mean_filter2d(img, filter_shape=11)
+  rotate = tfa.image.rotate(img, tf.constant(np.pi/8))
+  transform = tfa.image.transform(img, [1.0, 1.0, -250, 0.0, 1.0, 0.0, 0.0, 0.0])
 
 if __name__ == '__main__':
-    train()
+    test()
