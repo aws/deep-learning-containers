5335f1c9dfeb655a0ed18af5ab55045b7f5d02ba
Eliza Zhang <yutongzh@amazon.com>
2020-07-14 12:03:32 -0700

    tfa.image not work with cpu use tfa.optimizer instead

--- test/dlc_tests/container_tests/bin/testTFAddons.py
+++ test/dlc_tests/container_tests/bin/testTFAddons.py
@@ -1,18 +1,41 @@
 import tensorflow as tf
-import numpy as np
 import tensorflow_addons as tfa
 
 def test():
-  img_path = tf.keras.utils.get_file('tensorflow.png','https://tensorflow.org/images/tf_logo.png')
-  img_raw = tf.io.read_file(img_path)
-  img = tf.io.decode_image(img_raw)
-  img = tf.image.convert_image_dtype(img, tf.float32)
-  img = tf.image.resize(img, [500,500])
+  batch_size=64
+  epochs=10
+  model = tf.keras.Sequential([
+    tf.keras.layers.Dense(64, input_shape=(784,), activation='relu', name='dense_1'),
+    tf.keras.layers.Dense(64, activation='relu', name='dense_2'),
+    tf.keras.layers.Dense(10, activation='softmax', name='predictions'),
+  ])
+  
+  # Load MNIST dataset as NumPy arrays
+  dataset = {}
+  num_validation = 10000
+  (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-  # Simple test with tfa.image 
-  mean = tfa.image.mean_filter2d(img, filter_shape=11)
-  rotate = tfa.image.rotate(img, tf.constant(np.pi/8))
-  transform = tfa.image.transform(img, [1.0, 1.0, -250, 0.0, 1.0, 0.0, 0.0, 0.0])
+  # Preprocess the data
+  x_train = x_train.reshape(-1, 784).astype('float32') / 255
+  x_test = x_test.reshape(-1, 784).astype('float32') / 255 
+
+  # Compile the model
+  model.compile(
+      optimizer=tfa.optimizers.LazyAdam(0.001),  # Utilize TFA optimizer
+      loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+      metrics=['accuracy'])
+
+  # Train the network
+  history = model.fit(
+      x_train,
+      y_train,
+      batch_size=batch_size,
+      epochs=epochs)
+
+  print('Evaluate on test data:')
+  results = model.evaluate(x_test, y_test, batch_size=128, verbose = 2)
+  print('Test loss = {0}, Test acc: {1}'.format(results[0], results[1]))
+
 
 if __name__ == '__main__':
     test()
