3e8c5d3d7d3aef044a7f1ccc8ee7bb96a8e214cb
Arjuna <33526713+arjkesh@users.noreply.github.com>
2020-07-15 15:12:19 -0700

    [test] feature: Add utility to generate coverage report based on test function metadata (#344)

--- test/dlc_tests/ecs/tensorflow/training/test_ecs_tensorflow_training.py
+++ test/dlc_tests/ecs/tensorflow/training/test_ecs_tensorflow_training.py
@@ -29,7 +29,7 @@ def test_ecs_tensorflow_training_mnist_cpu(cpu_only, ecs_container_instance, ten
 
 
 @pytest.mark.parametrize("training_script", [TF_MNIST_TRAINING_SCRIPT], indirect=True)
-@pytest.mark.parametrize("ecs_instance_type", ["p3.2xlarge"], indirect=True)
+@pytest.mark.parametrize("ecs_instance_type", ["p2.8xlarge"], indirect=True)
 @pytest.mark.parametrize("ecs_ami", [ECS_AML2_GPU_USWEST2], indirect=True)
 def test_ecs_tensorflow_training_mnist_gpu(gpu_only, ecs_container_instance, tensorflow_training, training_cmd,
                                            ecs_cluster_name):
