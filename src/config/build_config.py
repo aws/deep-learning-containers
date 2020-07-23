78fb66bf74ca3f78851c838d36daff7cb3d66539
Zhuo Weng <wenzhuo@amazon.com>
2020-07-23 16:47:27 -0700

    Add EI PR setting

--- src/config/build_config.py
+++ src/config/build_config.py
@@ -1,5 +1,7 @@
-# Frameworks for which you want to disable both builds and tests
+# Frameworks for which you want to disable both builds and tests (General/Non-EI purpose)
 DISABLE_FRAMEWORK_TESTS = []
+# Frameworks for which you want to disable both builds and tests (EI purpose)
+DISABLE_EI_FRAMEWORK_TESTS = ["tensorflow", "mxnet", "pytorch"]
 # Disable new builds or build without datetime tag
 DISABLE_DATETIME_TAG = False
 # Note: Need to build the images at least once with DISABLE_DATETIME_TAG = True
--- src/main.py
+++ src/main.py
@@ -34,8 +34,11 @@ if __name__ == "__main__":
     )
     # create the empty json file for images
     build_context = os.getenv("BUILD_CONTEXT")
+    ei_dedicated = os.getenv("EI_DEDICATED")
     utils.write_to_json_file(constants.TEST_TYPE_IMAGES_PATH, {})
-    if args.framework not in build_config.DISABLE_FRAMEWORK_TESTS or build_context != "PR":
+    if (not ei_dedicated and args.framework not in build_config.DISABLE_FRAMEWORK_TESTS) or \
+       (ei_dedicated and args.framework not in build_config.DISABLE_EI_FRAMEWORK_TESTS) or \
+        build_context != "PR":
         utils.build_setup(
             args.framework,
             device_types=device_types,
