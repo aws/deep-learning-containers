da662dadd73cdab2c4de5bf4f7755bc61c38e799
Arjuna Keshavan <arjunake@amazon.com>
2020-07-24 15:42:46 -0700

    Update

--- test/dlc_tests/sanity/test_generate_coverage_doc.py
+++ test/dlc_tests/sanity/test_generate_coverage_doc.py
@@ -2,15 +2,21 @@ import os
 
 import boto3
 import pytest
+
+from botocore.exceptions import ClientError
 from invoke.context import Context
 
-from test.test_utils import is_pr_context, is_canary_context
+from test.test_utils import is_pr_context, is_canary_context, LOGGER
 from test.test_utils.test_reporting import get_test_coverage_file_path
+
+
+TEST_COVERAGE_REPORT_REGION = "us-west-2"
+TEST_COVERAGE_REPORT_BUCKET = f"dlc-test-coverage-reports-{TEST_COVERAGE_REPORT_REGION}"
 
 
 def coverage_doc_skip_condition():
     # Run this only in us-west-2
-    if is_canary_context() and os.getenv("AWS_REGION") == "us-west-2":
+    if is_canary_context() and os.getenv("AWS_REGION") == TEST_COVERAGE_REPORT_REGION:
         return False
     elif is_pr_context():
         return False
@@ -35,7 +41,15 @@ def test_generate_coverage_doc():
     assert os.path.exists(test_coverage_file), f"Cannot find test coverage report file {test_coverage_file}"
 
     # Write test coverage file to S3
-    if is_canary_context():
+    report_region = TEST_COVERAGE_REPORT_REGION
+    report_bucket = TEST_COVERAGE_REPORT_BUCKET
+    if is_canary_context() and os.getenv("AWS_REGION") == report_region:
         client = boto3.client("s3")
         with open(test_coverage_file, "rb") as test_file:
-            client.put_object(Bucket="dlc-test-report", Key=os.path.basename(test_coverage_file), Body=test_file)
+            try:
+                client.put_object(Bucket=report_bucket,
+                                  Key=os.path.basename(test_coverage_file),
+                                  Body=test_file)
+            except ClientError as e:
+                LOGGER.error(f"Unable to upload report to bucket {report_bucket}. Error: {e}")
+                raise
