834995f1f7b27cf4f542b8a9b2442675b6527155
Arjuna Keshavan <arjunake@amazon.com>
2020-07-24 12:07:42 -0700

    [test]|[canary] Write out test coverage files in canaries and push them to S3

--- test/dlc_tests/sanity/test_generate_coverage_doc.py
+++ test/dlc_tests/sanity/test_generate_coverage_doc.py
@@ -1,14 +1,25 @@
 import os
 
+import boto3
 import pytest
 from invoke.context import Context
 
-from test.test_utils import is_pr_context, PR_ONLY_REASON
+from test.test_utils import is_pr_context, is_canary_context
 from test.test_utils.test_reporting import get_test_coverage_file_path
 
 
-@pytest.mark.skipif(not is_pr_context(), reason=PR_ONLY_REASON)
+def coverage_doc_skip_condition():
+    # Run this only in us-west-2
+    if is_canary_context() and os.getenv("AWS_REGION") == "us-west-2":
+        return False
+    elif is_pr_context():
+        return False
+    return True
+
+
+@pytest.mark.skipif(coverage_doc_skip_condition(), reason="Run only in PR context or us-west-2 canary context")
 @pytest.mark.integration("Generating this coverage doc")
+@pytest.mark.canary("runs only in us-west-2")
 def test_generate_coverage_doc():
     """
     Test generating the test coverage doc
@@ -22,3 +33,9 @@ def test_generate_coverage_doc():
 
     # Ensure that the coverage report is created
     assert os.path.exists(test_coverage_file), f"Cannot find test coverage report file {test_coverage_file}"
+
+    # Write test coverage file to S3
+    if is_canary_context():
+        client = boto3.client("s3")
+        with open(test_coverage_file, "rb") as test_file:
+            client.put_object(Bucket="dlc-test-report", Key=os.path.basename(test_coverage_file), Body=test_file)
