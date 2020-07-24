b5514b6b9273b4e5227e5b9de10e909249e9d437
Arjuna Keshavan <arjunake@amazon.com>
2020-07-24 15:48:23 -0700

    additional updates

--- test/dlc_tests/sanity/test_generate_coverage_doc.py
+++ test/dlc_tests/sanity/test_generate_coverage_doc.py
@@ -23,7 +23,9 @@ def coverage_doc_skip_condition():
     return True
 
 
-@pytest.mark.skipif(coverage_doc_skip_condition(), reason="Run only in PR context or us-west-2 canary context")
+@pytest.mark.skipif(
+    coverage_doc_skip_condition(), reason=f"Run only in PR context or {TEST_COVERAGE_REPORT_REGION} canary context"
+)
 @pytest.mark.integration("Generating this coverage doc")
 @pytest.mark.canary("runs only in us-west-2")
 def test_generate_coverage_doc():
@@ -34,8 +36,11 @@ def test_generate_coverage_doc():
     ctx = Context()
     # Set DLC_TESTS to 'test' to avoid image names affecting function metadata (due to parametrization)
     # Set CODEBUILD_RESOLVED_SOURCE_VERSION to test for ease of running this test locally
-    ctx.run("export DLC_TESTS='test' && export CODEBUILD_RESOLVED_SOURCE_VERSION='test' && export BUILD_CONTEXT=''"
-            "&& pytest -s --collect-only  --generate-coverage-doc --ignore=container_tests/", hide=True)
+    ctx.run(
+        "export DLC_TESTS='test' && export CODEBUILD_RESOLVED_SOURCE_VERSION='test' && export BUILD_CONTEXT=''"
+        "&& pytest -s --collect-only  --generate-coverage-doc --ignore=container_tests/",
+        hide=True,
+    )
 
     # Ensure that the coverage report is created
     assert os.path.exists(test_coverage_file), f"Cannot find test coverage report file {test_coverage_file}"
@@ -47,9 +52,7 @@ def test_generate_coverage_doc():
         client = boto3.client("s3")
         with open(test_coverage_file, "rb") as test_file:
             try:
-                client.put_object(Bucket=report_bucket,
-                                  Key=os.path.basename(test_coverage_file),
-                                  Body=test_file)
+                client.put_object(Bucket=report_bucket, Key=os.path.basename(test_coverage_file), Body=test_file)
             except ClientError as e:
                 LOGGER.error(f"Unable to upload report to bucket {report_bucket}. Error: {e}")
                 raise
