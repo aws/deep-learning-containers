0620e19460a53e5abda52b427123ba7243f2aa6d
Ubuntu <ubuntu@ip-172-31-13-242.us-east-2.compute.internal>
2020-07-31 22:27:48 +0000

    Update syntax

--- test/dlc_tests/ec2/tensorflow/training/test_tensorflow_training.py
+++ test/dlc_tests/ec2/tensorflow/training/test_tensorflow_training.py
@@ -181,17 +181,8 @@ def test_tensorflow_dataservice_gpu(tensorflow_training, ec2_connection, gpu_onl
 	ec2_connection.run('python3 -m pip install --upgrade pip')
 	ec2_connection.run('pip3 install tensorflow')
 	ec2_connection.run('pip3 install tf-nightly')
-	ec2_connection.run('sudo apt install screen')
-	container_test_local_dir = os.path.join("$HOME", "container_tests")
-    connection.run(f'cd {container_test_local_dir}/bin && screen -d -m python3 start_dataservice.py')
-	# start_service = Process(target=execute_data_service_start, args=(ec2_connection,))
-	# start_service.start()
-	# container_test_local_dir = os.path.join("$HOME", "container_tests")
-	# ec2_connection.run(f'cd {container_test_local_dir} && python3 bin/start_dataservice.py&', timeout=10)
-	# time.sleep(60)
-	# start_service.terminate()
-	execute_ec2_training_test(ec2_connection, tensorflow_training, TF_DATASERVICE_TEST_CMD, host_network=True)
 
-def execute_data_service_start(connection):
-    container_test_local_dir = os.path.join("$HOME", "container_tests")
-    connection.run(f'cd {container_test_local_dir}/bin && screen -d -m python start_dataservice.py')
+	container_test_local_dir = os.path.join("$HOME", "container_tests")
+	ec2_connection.run(f'cd {container_test_local_dir}/bin && screen -d -m python3 start_dataservice.py')
+
+	execute_ec2_training_test(ec2_connection, tensorflow_training, TF_DATASERVICE_TEST_CMD, host_network=True)
