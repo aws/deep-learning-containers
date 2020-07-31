295a06c5ed0f54d793459575169cac08046aeeda
Samuel Oshin <sboshin@amazon.com>
2020-07-31 22:02:29 +0000

    Removing unnecessary binary argument, It does nothing, Removing need to download unncessary binary from S3, fixing deprecated API calls in script. Upgraded serving_api to 2.2

--- test/dlc_tests/benchmark/ec2/tensorflow/inference/test_performance_tensorflow_inference.py
+++ test/dlc_tests/benchmark/ec2/tensorflow/inference/test_performance_tensorflow_inference.py
@@ -21,9 +21,7 @@ def ec2_performance_tensorflow_inference(image_uri, processor, ec2_connection, r
     python_version = "py2" if "py2" in image_uri else "py3"
     container_test_local_dir = os.path.join("$HOME", "container_tests")
     tf_version = "1" if is_tf1(image_uri) else "2"
-    tf_api_version = '1.15' if tf_version == '1' else '2.1.0rc1'
-    tf_version_folder = '1.15' if tf_version == '1' else '2.1'
-    processor_folder = "CPU-WITH-MKL" if processor == "cpu" else "GPU"
+    tf_api_version = '1.15' if tf_version == '1' else '2.2.0'
 
     # Make sure we are logged into ECR so we can pull the image
     ec2_connection.run(f"$(aws ecr get-login --no-include-email --region {region})", hide=True)
@@ -34,8 +32,6 @@ def ec2_performance_tensorflow_inference(image_uri, processor, ec2_connection, r
     ec2_connection.run(
         f"pip install boto3 grpcio tensorflow-serving-api=={tf_api_version} --user --no-warn-script-location"
     )
-    ec2_connection.sudo(f"aws s3 cp s3://tensorflow-aws/{tf_version_folder}/Serving/{processor_folder}/tensorflow_model_server /usr/bin/")
-    ec2_connection.sudo(f"chmod +x /usr/bin/tensorflow_model_server")
     time_str = time.strftime('%Y-%m-%d-%H-%M-%S')
     commit_info = os.getenv("CODEBUILD_RESOLVED_SOURCE_VERSION")
     log_file = f"inference_benchmark_results_{commit_info}_{time_str}.log"
