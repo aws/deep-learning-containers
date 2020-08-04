1567917ebcddb2ebb319bceae4c68e06b7e33fe6
ChaiBapchya <chai.bapat@gmail.com>
2020-08-03 20:38:02 -0700

    prevent duplicate gpu commands

--- test/dlc_tests/benchmark/sagemaker/mxnet/training/resources/scripts/smtrain-resnet50-imagenet.sh
+++ test/dlc_tests/benchmark/sagemaker/mxnet/training/resources/scripts/smtrain-resnet50-imagenet.sh
@@ -10,9 +10,9 @@
 
 set -ex
 # p3 instances have larger GPU memory, so a higher batch size can be used
-GPU_MEM=`nvidia-smi --query-gpu=memory.total --format=csv,noheader -i 0 | awk '{print $1}'`
+# GPU_MEM=`nvidia-smi --query-gpu=memory.total --format=csv,noheader -i 0 | awk '{print $1}'`
 # returns per GPU memory in MB
-if [ $GPU_MEM -gt 15000] ; then BATCH_SIZE=1280; else BATCH_SIZE=256; fi
+# if [ $GPU_MEM -gt 15000 ] ; then BATCH_SIZE=1280; else BATCH_SIZE=256; fi
 
 # Training
 
@@ -23,4 +23,4 @@ python mxnet_imagenet_resnet50.py \
 --rec-val-idx $SM_CHANNEL_VALIDX/val-480px-q95.idx \
 --dtype "float16" --data-nthreads "40"  \
 --lr "0.3" --num-layers "50" --lr-step-epochs "20,30" \
---num-epochs "50" --batch-size $BATCH_SIZE
+--num-epochs "50" --batch-size 1280
