7c0b46a248f7d0acb1891da39a9db960cbeefd58
Chaitanya Prakash Bapat <chai.bapat@gmail.com>
2020-08-05 14:20:07 -0700

    Update build_config.py
    disable mxnet instead of TF

--- src/config/build_config.py
+++ src/config/build_config.py
@@ -2,7 +2,7 @@
 # Do remember to revert it back to False before merging any PR (including EI dedicated PR)
 ENABLE_EI_MODE = False
 # Frameworks for which you want to disable both builds and tests
-DISABLE_FRAMEWORK_TESTS = ["tensorflow", "pytorch"]
+DISABLE_FRAMEWORK_TESTS = ["mxnet", "pytorch"]
 # Disable new builds or build without datetime tag
 DISABLE_DATETIME_TAG = True
 # Note: Need to build the images at least once with DISABLE_DATETIME_TAG = True
