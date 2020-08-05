a8e824a88d18aa56a64e5eb5f90c3aa752b9e369
ChaiBapchya <chai.bapat@gmail.com>
2020-08-05 12:28:10 -0700

    trigger SM benchmark test on a PR job; to be reverted once confirmed

--- test/testrunner.py
+++ test/testrunner.py
@@ -241,7 +241,7 @@ def main():
     # Do not create EKS cluster for when EIA Only Images are present
     is_all_images_list_eia = all("eia" in image_uri for image_uri in all_image_list)
     eks_cluster_name = None
-    benchmark_mode = "benchmark" in test_type
+    benchmark_mode = "benchmark" in test_type or is_pr_context()
     specific_test_type = re.sub("benchmark-", "", test_type) if benchmark_mode else test_type
     test_path = os.path.join("benchmark", specific_test_type) if benchmark_mode else specific_test_type
 
