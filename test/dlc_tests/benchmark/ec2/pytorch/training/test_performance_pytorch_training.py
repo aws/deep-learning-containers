7fed0b553a2a906ad255f8bf74935b60efb13a3a
Zhuo Weng <wenzhuo@amazon.com>
2020-08-06 14:20:38 -0700

    Revert config change

--- test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_training.py
+++ test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_training.py
@@ -13,7 +13,7 @@ PT_PERFORMANCE_TRAINING_GPU_IMAGENET_CMD = os.path.join(CONTAINER_TESTS_PREFIX, 
 PT_EC2_GPU_SYNTHETIC_INSTANCE_TYPE = "p3.16xlarge"
 PT_EC2_GPU_IMAGENET_INSTANCE_TYPE = "p3.16xlarge"
 
-@pytest.mark.skip()
+
 @pytest.mark.model("resnet50")
 @pytest.mark.parametrize("ec2_instance_type", [PT_EC2_GPU_SYNTHETIC_INSTANCE_TYPE], indirect=True)
 def test_performance_pytorch_gpu_synthetic(pytorch_training, ec2_connection, gpu_only, py3_only):
