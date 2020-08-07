310b3bd4ef382503661339c56711d7596f2eb52f
Arjuna Keshavan <arjunake@amazon.com>
2020-08-07 12:07:42 -0700

    update

--- src/config/test_config.py
+++ src/config/test_config.py
@@ -1,5 +1,5 @@
 # Disable the test codebuild jobs to be run
-DISABLE_SANITY_TESTS = False
+DISABLE_SANITY_TESTS = True
 DISABLE_SAGEMAKER_TESTS = False
 DISABLE_ECS_TESTS = True
 DISABLE_EKS_TESTS = True
