c4f306f02c0c92fd8cec9d1a9ad9788f8bbfb486
Tejas Chumbalkar <chumbalk@amazon.com>
2020-08-11 16:08:24 -0700

    Separating out sleep intervals

--- test/test_utils/sagemaker.py
+++ test/test_utils/sagemaker.py
@@ -185,8 +185,9 @@ def install_sm_local_dependencies(framework, job_type, image, ec2_conn):
     if framework == "tensorflow" and job_type == "inference":
         # TF inference test fail if run as soon as instance boots, even after health check pass. rootcause:
         # sockets?/nginx startup?/?
-        print("sleep 800s for tensorflow inference images to avoid socket issues")
-        sleep(800)
+        print("sleep 600s for tensorflow inference images to avoid socket issues")
+        sleep(300)
+        sleep(300)
         install_custom_python("3.6", ec2_conn)
     ec2_conn.run(f"virtualenv env")
     ec2_conn.run(f"source ./env/bin/activate")
