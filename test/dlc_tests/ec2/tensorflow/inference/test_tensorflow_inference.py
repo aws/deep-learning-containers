3399f148e412d98c796a39b2d4a7a62820b7d737
Tushar Dey <tshdy@amazon.com>
2020-08-12 12:49:21 -0700

    Lowercase telemetry mode

--- test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
+++ test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
@@ -57,7 +57,7 @@ def test_ec2_tensorflow_inference_cpu_telemetry(tensorflow_inference, ec2_connec
     run_ec2_tensorflow_inference(tensorflow_inference, ec2_connection, "8500", region, True)
 
 
-def run_ec2_tensorflow_inference(image_uri, ec2_connection, grpc_port, region, Telemetry_mode=False):
+def run_ec2_tensorflow_inference(image_uri, ec2_connection, grpc_port, region, telemetry_mode=False):
     repo_name, image_tag = image_uri.split("/")[-1].split(":")
     container_name = f"{repo_name}-{image_tag}-ec2"
     framework_version = get_tensorflow_framework_version(image_uri)
@@ -89,7 +89,7 @@ def run_ec2_tensorflow_inference(image_uri, ec2_connection, grpc_port, region, T
         test_utils.request_tensorflow_inference_grpc(
             script_file_path=mnist_client_path, port=grpc_port, connection=ec2_connection
         )
-        if Telemetry_mode:
+        if telemetry_mode:
             check_telemetry(ec2_connection, container_name)
     finally:
         ec2_connection.run(f"docker rm -f {container_name}", warn=True, hide=True)
