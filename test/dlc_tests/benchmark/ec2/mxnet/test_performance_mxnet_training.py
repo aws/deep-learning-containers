9bc352e44b15fa3af5607781fb4198e3abbbd071
Zhuo Weng <wenzhuo@amazon.com>
2020-08-13 14:37:44 -0700

    test mxnet gpu training

--- test/dlc_tests/benchmark/ec2/mxnet/test_performance_mxnet_training.py
+++ test/dlc_tests/benchmark/ec2/mxnet/test_performance_mxnet_training.py
@@ -4,7 +4,7 @@ import pytest
 
 from test.test_utils import CONTAINER_TESTS_PREFIX
 from test.test_utils.ec2 import execute_ec2_training_performance_test
-from src.benchmark_metrics import MXNET_TRAINING_CPU_CIFAR_THRESHOLD
+from src.benchmark_metrics import MXNET_TRAINING_CPU_CIFAR_THRESHOLD, MXNET_TRAINING_GPU_IMAGENET_THRESHOLD
 
 
 MX_PERFORMANCE_TRAINING_GPU_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "benchmark", "run_mxnet_training_performance_gpu")
@@ -13,12 +13,14 @@ MX_PERFORMANCE_TRAINING_CPU_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "benchmar
 MX_EC2_GPU_INSTANCE_TYPE = "p3.16xlarge"
 MX_EC2_CPU_INSTANCE_TYPE = "c5.18xlarge"
 
-@pytest.mark.skip()
+
 @pytest.mark.integration("imagenet dataset")
 @pytest.mark.model("resnet50_v2")
 @pytest.mark.parametrize("ec2_instance_type", [MX_EC2_GPU_INSTANCE_TYPE], indirect=True)
 def test_performance_ec2_mxnet_training_gpu(mxnet_training, ec2_connection, gpu_only):
-    execute_ec2_training_performance_test(ec2_connection, mxnet_training, MX_PERFORMANCE_TRAINING_GPU_CMD)
+    execute_ec2_training_performance_test(ec2_connection, mxnet_training, MX_PERFORMANCE_TRAINING_GPU_CMD,
+                                          post_process=post_process_mxnet_ec2_performance,
+                                          data_source="imagenet", threshold=MXNET_TRAINING_GPU_IMAGENET_THRESHOLD)
 
 
 @pytest.mark.integration("cifar10 dataset")
