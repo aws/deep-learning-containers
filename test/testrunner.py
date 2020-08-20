1448f5381cb4c587f6e27070886f9c21fb4831d5
Zhuo Weng <wenzhuo@amazon.com>
2020-08-20 01:09:31 -0700

    print error message

--- test/testrunner.py
+++ test/testrunner.py
@@ -251,9 +251,8 @@ def main():
     is_all_images_list_eia = all("eia" in image_uri for image_uri in all_image_list)
     eks_cluster_name = None
     benchmark_mode = "benchmark" in test_type or is_benchmark_dev_context()
-    specific_test_type = test_type if is_benchmark_dev_context else \
-        re.sub("benchmark-", "", test_type) if benchmark_mode else test_type
-    test_path = os.path.join("benchmark", specific_test_type) if benchmark_mode else specific_test_type
+    specific_test_type = re.sub("benchmark-", "", test_type) if "benchmark" in test_type else test_type
+    # test_path = os.path.join("benchmark", specific_test_type) if benchmark_mode else specific_test_type
     test_path = specific_test_type
 
     if specific_test_type in ("sanity", "ecs", "ec2", "eks", "canary", "bai"):
