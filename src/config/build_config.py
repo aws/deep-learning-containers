c28bb18e50c96a96b5c734edd9692dd0c16b03d2
Tushar Dey <tshdy@amazon.com>
2020-08-20 16:21:26 -0700

    Disable EIA Mode

--- src/config/build_config.py
+++ src/config/build_config.py
@@ -1,6 +1,6 @@
 # Please only set it to True if you are preparing an EI related PR
 # Do remember to revert it back to False before merging any PR (including EI dedicated PR)
-ENABLE_EI_MODE = True
+ENABLE_EI_MODE = False
 # Please only set it to True if you are preparing an NEURON related PR
 # Do remember to revert it back to False before merging any PR (including NEURON dedicated PR)
 ENABLE_NEURON_MODE = False
