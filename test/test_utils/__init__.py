1d0cfd92af77e7fb9445157c7fe4049122605215
Zhuo Weng <wenzhuo@amazon.com>
2020-08-19 23:31:00 -0700

    test on router

--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -14,7 +14,7 @@ from invoke import run
 from invoke.context import Context
 from packaging.version import LegacyVersion, Version, parse
 from retrying import retry
-from test.test_utils.ec2 import ENABLE_BENCHMARK_DEV_MODE
+from .ec2 import ENABLE_BENCHMARK_DEV_MODE
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
