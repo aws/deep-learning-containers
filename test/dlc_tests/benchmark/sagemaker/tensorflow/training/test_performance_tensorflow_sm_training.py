d5cf72665cd95a5cf6f7e191283c4c6d3197418e
Arjuna <33526713+arjkesh@users.noreply.github.com>
2020-08-21 15:05:00 -0700

    [test] Explicitly call out multinode tests, break out parametrized tests into separate test functions (#502)

--- test/dlc_tests/benchmark/sagemaker/tensorflow/training/test_performance_tensorflow_sm_training.py
+++ test/dlc_tests/benchmark/sagemaker/tensorflow/training/test_performance_tensorflow_sm_training.py
@@ -13,16 +13,23 @@ from src.benchmark_metrics import (
     TENSORFLOW2_SM_TRAINING_GPU_1NODE_THRESHOLD,
     TENSORFLOW2_SM_TRAINING_GPU_4NODE_THRESHOLD,
 )
-from test.test_utils import BENCHMARK_RESULTS_S3_BUCKET, LOGGER, is_tf1
+from test.test_utils import BENCHMARK_RESULTS_S3_BUCKET, LOGGER
 
 
 @pytest.mark.integration("imagenet dataset")
-@pytest.mark.multinode("multinode")
+@pytest.mark.multinode(4)
 @pytest.mark.model("resnet50")
-@pytest.mark.parametrize("num_nodes", [1, 4], indirect=True)
-def test_tensorflow_sagemaker_training_performance(
-    tensorflow_training, num_nodes, region
-):
+def test_tensorflow_sagemaker_training_performance_multinode(tensorflow_training, region):
+    run_sm_perf_test(tensorflow_training, 4, region)
+
+
+@pytest.mark.integration("imagenet dataset")
+@pytest.mark.model("resnet50")
+def test_tensorflow_sagemaker_training_performance_singlenode(tensorflow_training, region):
+    run_sm_perf_test(tensorflow_training, 1, region)
+
+
+def run_sm_perf_test(image_uri, num_nodes, region):
     """
     Run TF sagemaker training performance tests
 
@@ -32,40 +39,27 @@ def test_tensorflow_sagemaker_training_performance(
     TODO: Refactor the above setup function to be more obviously connected to this function,
     TODO: and install requirements via a requirements.txt file
 
-    :param tensorflow_training: ECR image URI
+    :param image_uri: ECR image URI
     :param num_nodes: Number of nodes to run on
     :param region: AWS region
     """
-    framework_version = re.search(r"[1,2](\.\d+){2}", tensorflow_training).group()
+    framework_version = re.search(r"[1,2](\.\d+){2}", image_uri).group()
     if framework_version.startswith("1."):
         pytest.skip("Skipping benchmark test on TF 1.x images.")
 
-    processor = "gpu" if "gpu" in tensorflow_training else "cpu"
+    processor = "gpu" if "gpu" in image_uri else "cpu"
 
     ec2_instance_type = "p3.16xlarge" if processor == "gpu" else "c5.18xlarge"
 
-    py_version = (
-        "py2"
-        if "py2" in tensorflow_training
-        else "py37"
-        if "py37" in tensorflow_training
-        else "py3"
-    )
+    py_version = "py2" if "py2" in image_uri else "py37" if "py37" in image_uri else "py3"
 
     time_str = time.strftime("%Y-%m-%d-%H-%M-%S")
     commit_info = os.getenv("CODEBUILD_RESOLVED_SOURCE_VERSION")
     target_upload_location = os.path.join(
-        BENCHMARK_RESULTS_S3_BUCKET,
-        "tensorflow",
-        framework_version,
-        "sagemaker",
-        "training",
-        processor,
-        py_version,
+        BENCHMARK_RESULTS_S3_BUCKET, "tensorflow", framework_version, "sagemaker", "training", processor, py_version
     )
     training_job_name = (
-        f"tf{framework_version[0]}-tr-bench-{processor}-{num_nodes}-node-{py_version}"
-        f"-{commit_info[:7]}-{time_str}"
+        f"tf{framework_version[0]}-tr-bench-{processor}-{num_nodes}-node-{py_version}" f"-{commit_info[:7]}-{time_str}"
     )
 
     # Inserting random sleep because this test starts multiple training jobs around the same time, resulting in
@@ -82,7 +76,7 @@ def test_tensorflow_sagemaker_training_performance(
         run_out = ctx.run(
             f"timeout 45m python tf_sm_benchmark.py "
             f"--framework-version {framework_version} "
-            f"--image-uri {tensorflow_training} "
+            f"--image-uri {image_uri} "
             f"--instance-type ml.{ec2_instance_type} "
             f"--node-count {num_nodes} "
             f"--python {py_version} "
@@ -96,17 +90,11 @@ def test_tensorflow_sagemaker_training_performance(
         if not (run_out.ok or run_out.return_code == 124):
             target_upload_location = os.path.join(target_upload_location, "failure_log")
 
-    ctx.run(
-        f"aws s3 cp {os.path.join(test_dir, log_file)} {os.path.join(target_upload_location, log_file)}"
-    )
+    ctx.run(f"aws s3 cp {os.path.join(test_dir, log_file)} {os.path.join(target_upload_location, log_file)}")
 
-    LOGGER.info(
-        f"Test results can be found at {os.path.join(target_upload_location, log_file)}"
-    )
+    LOGGER.info(f"Test results can be found at {os.path.join(target_upload_location, log_file)}")
 
-    result_statement, throughput = _print_results_of_test(
-        os.path.join(test_dir, log_file), processor
-    )
+    result_statement, throughput = _print_results_of_test(os.path.join(test_dir, log_file), processor)
     throughput /= num_nodes
 
     assert run_out.ok, (
@@ -115,11 +103,7 @@ def test_tensorflow_sagemaker_training_performance(
     )
 
     threshold = (
-        (
-            TENSORFLOW2_SM_TRAINING_CPU_1NODE_THRESHOLD
-            if num_nodes == 1
-            else TENSORFLOW2_SM_TRAINING_CPU_4NODE_THRESHOLD
-        )
+        (TENSORFLOW2_SM_TRAINING_CPU_1NODE_THRESHOLD if num_nodes == 1 else TENSORFLOW2_SM_TRAINING_CPU_4NODE_THRESHOLD)
         if processor == "cpu"
         else TENSORFLOW2_SM_TRAINING_GPU_1NODE_THRESHOLD
         if num_nodes == 1
@@ -144,9 +128,7 @@ def _print_results_of_test(file_path, processor):
             if "Total img/sec on " in line:
                 result = line + "\n"
                 throughput = float(
-                    re.search(
-                        r"(CPU\(s\):[ ]*)(?P<throughput>[0-9]+\.?[0-9]+)", line
-                    ).group("throughput")
+                    re.search(r"(CPU\(s\):[ ]*)(?P<throughput>[0-9]+\.?[0-9]+)", line).group("throughput")
                 )
                 break
     elif processor == "gpu":
@@ -157,9 +139,7 @@ def _print_results_of_test(file_path, processor):
                 result_dict[key] = line.strip("\n")
                 if throughput == 0:
                     throughput = float(
-                        re.search(
-                            r"(images/sec:[ ]*)(?P<throughput>[0-9]+\.?[0-9]+)", line
-                        ).group("throughput")
+                        re.search(r"(images/sec:[ ]*)(?P<throughput>[0-9]+\.?[0-9]+)", line).group("throughput")
                     )
         result = "\n".join(result_dict.values()) + "\n"
     LOGGER.info(result)
