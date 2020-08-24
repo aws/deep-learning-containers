87f02197e69bdfceaf6af0298f32c683cb075b7e
Tushar Dey <tshdy@amazon.com>
2020-08-24 16:37:44 -0700

    Keeping only multi model for testing in EIA as well

--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -296,9 +296,8 @@ def get_inference_run_command(image_uri, model_names, processor="cpu"):
         "{}={}".format(name, multi_model_location[name]) for name in model_names
     ]
 
-    if processor == "eia":
-        server_cmd = "mxnet-model-server"
-    elif server_type == "ts":
+
+    if server_type == "ts":
         server_cmd = "torchserve"
     else:
         server_cmd = "multi-model-server"
