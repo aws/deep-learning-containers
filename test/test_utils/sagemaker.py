e3088b090d2abcd60bc5f36024cffa06b92b0554
Tejas Chumbalkar <chumbalk@amazon.com>
2020-08-25 10:28:43 -0700

    bash command string formatting

--- test/test_utils/sagemaker.py
+++ test/test_utils/sagemaker.py
@@ -182,12 +182,11 @@ def install_sm_local_dependencies(framework, job_type, image, ec2_conn):
     sleep(200)
     # waiting for apt-get process to complete if the process is in active state
     # using virtualenv to avoid package conflicts with the current packages
-    bash_command = """
-            while pgrep -x apt-get > /dev/null; do 
-                echo "Waiting for apt-get process to finish"
-                sleep 5; 
-            done && sudo apt-get install virtualenv -y 
-            """
+    bash_command = """while pgrep -x apt-get > /dev/null; do 
+                        echo "Waiting for apt-get process to finish"
+                        sleep 5; 
+                    done && sudo apt-get install virtualenv -y"""
+            
     ec2_conn.run(bash_command, timeout=500)
     
     if framework == "tensorflow" and job_type == "inference":
