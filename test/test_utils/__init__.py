6d3f534f16630c10fca0000800f8fd5468821141
Ubuntu <ubuntu@ip-172-31-14-179.us-east-2.compute.internal>
2020-09-09 00:24:56 +0000

    Update tf23 flag

--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -12,7 +12,10 @@ import pytest
 from botocore.exceptions import ClientError
 from invoke import run
 from invoke.context import Context
+from packaging.version import LegacyVersion, Version, parse
 from retrying import retry
+
+from src.config.test_config import ENABLE_BENCHMARK_DEV_MODE
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
@@ -83,6 +86,21 @@ def above_tf23(image_uri):
     return bool(re.search(r'2\.[3-9]\.\d+', image_uri))
 
 
+def get_repository_local_path():
+    git_repo_path = os.getcwd().split("/test/")[0]
+    return git_repo_path
+
+
+def get_inference_server_type(image_uri):
+    if "pytorch" not in image_uri:
+        return "mms"
+    image_tag = image_uri.split(':')[1]
+    pytorch_ver = parse(image_tag.split('-')[0])
+    if isinstance(pytorch_ver, LegacyVersion) or pytorch_ver < Version("1.6"):
+        return "mms"
+    return "ts"
+
+
 def is_pr_context():
     return os.getenv("BUILD_CONTEXT") == "PR"
 
@@ -101,6 +119,10 @@ def is_empty_build_context():
 
 def is_dlc_cicd_context():
     return os.getenv("BUILD_CONTEXT") in ["PR", "CANARY", "NIGHTLY", "MAINLINE"]
+
+
+def is_benchmark_dev_context():
+    return ENABLE_BENCHMARK_DEV_MODE
 
 
 def run_subprocess_cmd(cmd, failure="Command failed"):
@@ -204,7 +226,7 @@ def request_pytorch_inference_densenet(ip_address="127.0.0.1", port="80", connec
 
     # The run_out.return_code is not reliable, since sometimes predict request may succeed but the returned result
     # is 404. Hence the extra check.
-    if run_out.return_code != 0 or 'flowerpot' not in run_out.stdout:
+    if run_out.return_code != 0 or 'pot' not in run_out.stdout:
         return False
 
     return True
@@ -232,27 +254,6 @@ def request_tensorflow_inference(model_name, ip_address="127.0.0.1", port="8501"
 
     return True
 
-@retry(stop_max_attempt_number=20, wait_fixed=10000, retry_on_result=retry_if_result_is_false)
-def request_tensorflow_inference_nlp(model_name, ip_address="127.0.0.1", port="8501"):
-    """
-    Method to run tensorflow inference on half_plus_two model using CURL command
-    :param model_name:
-    :param ip_address:
-    :param port:
-    :connection: ec2_connection object to run the commands remotely over ssh
-    :return:
-    """
-    inference_string = "'{\"instances\": [[2,1952,25,10901,3]]}'"
-    run_out = run(
-        f"curl -d {inference_string} -X POST http://{ip_address}:{port}/v1/models/{model_name}:predict", warn=True
-    )
-
-    # The run_out.return_code is not reliable, since sometimes predict request may succeed but the returned result
-    # is 404. Hence the extra check.
-    if run_out.return_code != 0 or 'predictions' not in run_out.stdout:
-        return False
-
-    return True
 
 def request_tensorflow_inference_grpc(script_file_path, ip_address="127.0.0.1", port="8500", connection=None):
     """
@@ -267,23 +268,30 @@ def request_tensorflow_inference_grpc(script_file_path, ip_address="127.0.0.1", 
     conn_run(f"python {script_file_path} --num_tests=1000 --server={ip_address}:{port}", hide=True)
 
 
-def get_mms_run_command(model_names, processor="cpu"):
+def get_inference_run_command(image_uri, model_names, processor="cpu"):
     """
     Helper function to format run command for MMS
+    :param image_uri:
     :param model_names:
     :param processor:
     :return: <str> Command to start MMS server with given model
     """
-    if processor != "eia":
+    server_type = get_inference_server_type(image_uri)
+    if processor == "eia":
+        multi_model_location = {
+            "resnet-152-eia": "https://s3.amazonaws.com/model-server/model_archive_1.0/resnet-152-eia.mar",
+            "pytorch-densenet": "https://aws-dlc-sample-models.s3.amazonaws.com/pytorch/densenet_eia/densenet_eia.mar",
+        }
+    elif server_type == "ts":
+        multi_model_location = {
+            "squeezenet": "https://torchserve.s3.amazonaws.com/mar_files/squeezenet1_1.mar",
+            "pytorch-densenet": "https://torchserve.s3.amazonaws.com/mar_files/densenet161.mar"
+        }
+    else:
         multi_model_location = {
             "squeezenet": "https://s3.amazonaws.com/model-server/models/squeezenet_v1.1/squeezenet_v1.1.model",
             "pytorch-densenet": "https://dlc-samples.s3.amazonaws.com/pytorch/multi-model-server/densenet/densenet.mar",
             "bert_sst": "https://aws-dlc-sample-models.s3.amazonaws.com/bert_sst/bert_sst.mar"
-        }
-    else:
-        multi_model_location = {
-            "resnet-152-eia": "https://s3.amazonaws.com/model-server/model_archive_1.0/resnet-152-eia.mar",
-            "pytorch-densenet": "https://aws-dlc-sample-models.s3.amazonaws.com/pytorch/densenet_eia/densenet_eia.mar",
         }
 
     if not isinstance(model_names, list):
@@ -298,9 +306,14 @@ def get_mms_run_command(model_names, processor="cpu"):
     parameters = [
         "{}={}".format(name, multi_model_location[name]) for name in model_names
     ]
-    multi_or_mxnet = "multi" if processor != "eia" else "mxnet"
+
+    if server_type == "ts":
+        server_cmd = "torchserve"
+    else:
+        server_cmd = "multi-model-server"
+
     mms_command = (
-        f"{multi_or_mxnet}-model-server --start --mms-config /home/model-server/config.properties --models "
+        f"{server_cmd} --start --{server_type}-config /home/model-server/config.properties --models "
         + " ".join(parameters)
     )
     return mms_command
@@ -318,11 +331,6 @@ def get_tensorflow_model_name(processor, model_name):
             "cpu": "saved_model_half_plus_two_cpu",
             "gpu": "saved_model_half_plus_two_gpu",
             "eia": "saved_model_half_plus_two",
-        },
-        "albert": {
-            "cpu": "albert",
-            "gpu": "albert",
-            "eia": "albert",
         },
         "saved_model_half_plus_three": {"eia": "saved_model_half_plus_three"},
     }
@@ -423,32 +431,44 @@ def parse_canary_images(framework, region):
             framework = "tensorflow1"
 
     version_regex = {
-        "tensorflow1": r"tf-1.\d+",
-        "tensorflow2": r"tf-2.\d+",
-        "mxnet": r"mx-\d+.\d+",
-        "pytorch": r"pt-\d+.\d+"
+        "tensorflow1": r"tf-(1.\d+)",
+        "tensorflow2": r"tf-(2.\d+)",
+        "mxnet": r"mx-(\d+.\d+)",
+        "pytorch": r"pt-(\d+.\d+)"
     }
 
     repo = git.Repo(os.getcwd(), search_parent_directories=True)
 
-    versions = []
+    versions_counter = {}
 
     for tag in repo.tags:
         tag_str = str(tag)
         match = re.search(version_regex[framework], tag_str)
-        if match and "tr" not in tag_str and "inf" not in tag_str:
-            version = match.group().split('-')[-1]
-            if version not in versions:
-                versions.append(version)
+        if match:
+            version = match.group(1)
+            if not versions_counter.get(version):
+                versions_counter[version] = {"tr": False, "inf": False}
+            if "tr" not in tag_str and "inf" not in tag_str:
+                versions_counter[version]["tr"] = True
+                versions_counter[version]["inf"] = True
+            elif "tr" in tag_str:
+                versions_counter[version]["tr"] = True
+            elif "inf" in tag_str:
+                versions_counter[version]["inf"] = True
 
-    # Sort ascending to descending
-    versions = sorted(versions, reverse=True)
+    versions = []
+    for v, inf_train in versions_counter.items():
+        if inf_train['inf'] and inf_train['tr']:
+            versions.append(v)
+
+    # Sort ascending to descending, use lambda to ensure 2.2 < 2.15, for instance
+    versions.sort(key=lambda version_str: [int(point) for point in version_str.split('.')], reverse=True)
 
     registry = PUBLIC_DLC_REGISTRY
     framework_versions = versions if len(versions) < 4 else versions[:3]
     dlc_images = ""
     for fw_version in framework_versions:
-        py_minor_version = '7' if framework == "tensorflow2" and fw_version == '2.2' else ''
+        py_minor_version = '7' if framework == "tensorflow2" and Version(fw_version) >= Version('2.2') else ''
         images = {
             "tensorflow1":
                 f"{registry}.dkr.ecr.{region}.amazonaws.com/tensorflow-training:{fw_version}-gpu-py3 "
