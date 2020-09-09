51c4d7226912d3f72f677d4b1afa70347aec235e
Ubuntu <ubuntu@ip-172-31-14-179.us-east-2.compute.internal>
2020-09-09 00:20:41 +0000

    revert changes

--- test/dlc_tests/eks/tensorflow/inference/test_eks_tensorflow_inference.py
+++ test/dlc_tests/eks/tensorflow/inference/test_eks_tensorflow_inference.py
@@ -9,8 +9,8 @@ import test.test_utils.eks as eks_utils
 import test.test_utils as test_utils
 
 
-@pytest.mark.model("albert")
-def test_eks_tensorflow_albert(tensorflow_inference):
+@pytest.mark.model("half_plus_two")
+def test_eks_tensorflow_half_plus_two_inference(tensorflow_inference):
     if "eia" in tensorflow_inference:
         pytest.skip("Skipping EKS Test for EIA")
     num_replicas = "1"
@@ -19,9 +19,9 @@ def test_eks_tensorflow_albert(tensorflow_inference):
 
     processor = "gpu" if "gpu" in tensorflow_inference else "cpu"
 
-    model_name = f"albert"
+    model_name = f"saved_model_half_plus_two_{processor}"
     yaml_path = os.path.join(os.sep, "tmp", f"tensorflow_single_node_{processor}_inference_{rand_int}.yaml")
-    inference_service_name = selector_name = f"albert-{processor}-{rand_int}"
+    inference_service_name = selector_name = f"half-plus-two-service-{processor}-{rand_int}"
 
     search_replace_dict = {
         "<MODEL_NAME>": model_name,
@@ -47,7 +47,7 @@ def test_eks_tensorflow_albert(tensorflow_inference):
         if eks_utils.is_service_running(selector_name):
             eks_utils.eks_forward_port_between_host_and_container(selector_name, port_to_forward, "8500")
 
-        assert test_utils.request_tensorflow_inference_nlp(model_name=model_name, port=port_to_forward)
+        assert test_utils.request_tensorflow_inference(model_name=model_name, port=port_to_forward)
     except ValueError as excp:
         eks_utils.LOGGER.error("Service is not running: %s", excp)
     finally:
