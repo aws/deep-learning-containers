f6e6d35e6d5c45157e6df590992ea85623abe1be
ChaiBapchya <chai.bapat@gmail.com>
2020-09-13 21:17:54 -0700

    fix indents, formatting, make global variable ALL CAPS

--- test/dlc_tests/container_tests/bin/test_efa_sanity.py
+++ test/dlc_tests/container_tests/bin/test_efa_sanity.py
@@ -2,9 +2,9 @@ import subprocess
 import sys
 import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler(sys.stdout))
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+LOGGER.addHandler(logging.StreamHandler(sys.stdout))
 
 def main():
     check_ib_uverbs()
@@ -18,9 +18,9 @@ def check_ib_uverbs():
     try:
         command = "lsmod | grep ib_uverbs"
         subprocess.check_call(command, shell=True, executable="/bin/bash")
-        logger.info("ib_uverbs module is present in the modules - test succeeded")
+        LOGGER.info("ib_uverbs module is present in the modules - test succeeded")
     except subprocess.CalledProcessError:
-        logger.error("Error: test check_ib_uverbs failed.")
+        LOGGER.error("Error: test check_ib_uverbs failed.")
         raise
     return True
 
@@ -28,9 +28,9 @@ def check_fi_info():
     try:
         command = "fi_info -p efa"
         response = subprocess.check_output(command, shell=True, executable="/bin/bash")
-        logger.info("Efa provider info is present in response")
+        LOGGER.info("Efa provider info is present in response")
     except subprocess.CalledProcessError:
-        logger.error("Error: test Check Efa provider failed.")
+        LOGGER.error("Error: test Check Efa provider failed.")
         raise
     return True
 
@@ -38,9 +38,9 @@ def check_efa_installed_packages():
     try:
         command = "cat /opt/amazon/efa_installed_packages"
         subprocess.check_call(command, shell=True, executable="/bin/bash")
-        logger.info("efa packages are successfully installed in the correct path")
+        LOGGER.info("efa packages are successfully installed in the correct path")
     except subprocess.CalledProcessError:
-        logger.error("Error: test check EFA packages failed.")
+        LOGGER.error("Error: test check EFA packages failed.")
         raise
     return True
 
@@ -48,9 +48,9 @@ def run_fi_pingpong_test():
     try:
         command = "cd ~ && ./src/bin/efa-tests/efa_test.sh"
         subprocess.check_call(command, shell=True, executable="/bin/bash")
-        logger.info("fi ping test is successful")
+        LOGGER.info("fi ping test is successful")
     except subprocess.CalledProcessError:
-        logger.error("Error: fi ping test failed.")
+        LOGGER.error("Error: fi ping test failed.")
         raise
     return True
 
