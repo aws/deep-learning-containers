e7459325c01b5e6595a8e7cafecb6192315f77bd
Teng Xu <xuteng@amazon.com>
2020-09-23 17:21:46 +0000

    Add mnist model

--- test/dlc_tests/ecs/tensorflow/training/test_ecs_tensorflow_training.py
+++ test/dlc_tests/ecs/tensorflow/training/test_ecs_tensorflow_training.py
@@ -30,6 +30,26 @@ def test_ecs_tensorflow_training_mnist_cpu(cpu_only, ecs_container_instance, ten
     ecs_utils.ecs_training_test_executor(ecs_cluster_name, cluster_arn, training_cmd, tensorflow_training, instance_id)
 
 
+@pytest.mark.model("mnist")
+@pytest.mark.parametrize("training_script", [TF_MNIST_TRAINING_SCRIPT], indirect=True)
+@pytest.mark.parametrize("ecs_instance_type", ["p2.8xlarge"], indirect=True)
+@pytest.mark.parametrize("ecs_ami", [ECS_AML2_GPU_USWEST2], indirect=True)
+def test_ecs_tensorflow_training_mnist_gpu(gpu_only, ecs_container_instance, tensorflow_training, training_cmd,
+                                           ecs_cluster_name):
+    """
+    GPU mnist test for TF Training
+    Instance Type - p3.2xlarge
+    Given above parameters, registers a task with family named after this test, runs the task, and waits for
+    the task to be stopped before doing teardown operations of instance and cluster.
+    """
+    instance_id, cluster_arn = ecs_container_instance
+
+    num_gpus = ec2_utils.get_instance_num_gpus(instance_id)
+
+    ecs_utils.ecs_training_test_executor(ecs_cluster_name, cluster_arn, training_cmd, tensorflow_training, instance_id,
+                                         num_gpus=num_gpus)
+
+
 @pytest.mark.model("FasterRCNN")
 @pytest.mark.parametrize("training_script", [TF_FasterRCNN_TRAINING_SCRIPT], indirect=True)
 @pytest.mark.parametrize("ecs_instance_type", ["g3.8xlarge"], indirect=True)
