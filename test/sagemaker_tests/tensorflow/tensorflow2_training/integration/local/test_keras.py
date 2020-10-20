be237b43103992e3eba73810535d1ccf8235b7c7
Arjuna Keshavan <arjunake@amazon.com>
2020-10-20 10:47:26 -0700

    save progress

--- test/sagemaker_tests/tensorflow/tensorflow2_training/integration/local/test_keras.py
+++ test/sagemaker_tests/tensorflow/tensorflow2_training/integration/local/test_keras.py
@@ -36,8 +36,8 @@ def test_keras_training(sagemaker_local_session, docker_image, tmpdir, framework
     estimator = TensorFlow(
         entry_point=entry_point,
         role='SageMakerRole',
-        train_instance_count=1,
-        train_instance_type='local',
+        instance_count=1,
+        instance_type='local',
         image_name=docker_image,
         sagemaker_session=sagemaker_local_session,
         model_dir='/opt/ml/model',
