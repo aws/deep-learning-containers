2e4ffb1f1b300156f164eba37c0324a9730c9087
Teng Xu <xuteng@amazon.com>
2020-10-27 23:43:49 +0000

    Test timeout

--- test/dlc_tests/ec2/test_smdebug.py
+++ test/dlc_tests/ec2/test_smdebug.py
@@ -61,7 +61,7 @@ def run_smdebug_test(
             f"{container_test_local_dir}:{os.path.join(os.sep, 'test')}{shm_setting}{image_uri} "
             f"./{test_script} {framework}",
             hide=True,
-            timeout=3000,
+            timeout=20000,
         )
     except Exception as e:
         debug_output = ec2_connection.run(f"docker logs {container_name}")
