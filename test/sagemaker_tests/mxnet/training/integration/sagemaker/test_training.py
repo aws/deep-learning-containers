f078ee938c4a8a9d6ff7c934e1ba5095bac9827a
Joe Evans <joseph.evans@gmail.com>
2020-11-13 23:44:42 -0800

    [mxnet] MXNet 1.8.0 containers (#709)

--- test/sagemaker_tests/mxnet/training/integration/sagemaker/test_training.py
+++ test/sagemaker_tests/mxnet/training/integration/sagemaker/test_training.py
@@ -28,16 +28,17 @@ SCRIPT_PATH = os.path.join(DATA_PATH, 'mnist.py')
 @pytest.mark.model("mnist")
 @pytest.mark.integration("smexperiments")
 @pytest.mark.skip_test_in_region
-def test_training(sagemaker_session, ecr_image, instance_type, instance_count):
+def test_training(sagemaker_session, ecr_image, instance_type, instance_count, framework_version):
     hyperparameters = {'sagemaker_parameter_server_enabled': True} if instance_count > 1 else {}
     hyperparameters['epochs'] = 1
 
     mx = MXNet(entry_point=SCRIPT_PATH,
                role='SageMakerRole',
-               train_instance_count=instance_count,
-               train_instance_type=instance_type,
+               instance_count=instance_count,
+               instance_type=instance_type,
                sagemaker_session=sagemaker_session,
-               image_name=ecr_image,
+               image_uri=ecr_image,
+               framework_version=framework_version,
                hyperparameters=hyperparameters)
 
     with timeout(minutes=15):
