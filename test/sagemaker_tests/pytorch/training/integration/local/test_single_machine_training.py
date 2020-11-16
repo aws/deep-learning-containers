4eab96bf29a722ffc6a49cfe7ae7fee41b4e499e
Sai Parthasarathy Miduthuri <54188298+saimidu@users.noreply.github.com>
2020-11-16 15:12:25 -0800

    [pytorch] [build, test] [sagemaker] Upgrade pytorch sagemaker version (#681)

--- test/sagemaker_tests/pytorch/training/integration/local/test_single_machine_training.py
+++ test/sagemaker_tests/pytorch/training/integration/local/test_single_machine_training.py
@@ -23,14 +23,16 @@ from ...integration import data_dir, fastai_path, fastai_mnist_script, mnist_scr
 
 @pytest.mark.model("mnist")
 def test_mnist(docker_image, processor, instance_type, sagemaker_local_session, tmpdir):
-    estimator = PyTorch(entry_point=mnist_script,
-                        role=ROLE,
-                        image_name=docker_image,
-                        train_instance_count=1,
-                        train_instance_type=instance_type,
-                        sagemaker_session=sagemaker_local_session,
-                        hyperparameters={'processor': processor},
-                        output_path='file://{}'.format(tmpdir))
+    estimator = PyTorch(
+        entry_point=mnist_script,
+        role=ROLE,
+        image_uri=docker_image,
+        instance_count=1,
+        instance_type=instance_type,
+        sagemaker_session=sagemaker_local_session,
+        hyperparameters={'processor': processor},
+        output_path='file://{}'.format(tmpdir),
+    )
 
     _train_and_assert_success(estimator, data_dir, str(tmpdir))
 
@@ -39,13 +41,15 @@ def test_mnist(docker_image, processor, instance_type, sagemaker_local_session, 
 @pytest.mark.model("mnist")
 @pytest.mark.skip_py2_containers
 def test_fastai_mnist(docker_image, instance_type, py_version, sagemaker_local_session, tmpdir):
-    estimator = PyTorch(entry_point=fastai_mnist_script,
-                        role=ROLE,
-                        image_name=docker_image,
-                        train_instance_count=1,
-                        train_instance_type=instance_type,
-                        sagemaker_session=sagemaker_local_session,
-                        output_path='file://{}'.format(tmpdir))
+    estimator = PyTorch(
+        entry_point=fastai_mnist_script,
+        role=ROLE,
+        image_uri=docker_image,
+        instance_count=1,
+        instance_type=instance_type,
+        sagemaker_session=sagemaker_local_session,
+        output_path='file://{}'.format(tmpdir),
+    )
 
     input_dir = os.path.join(fastai_path, 'mnist_tiny')
     _train_and_assert_success(estimator, input_dir, str(tmpdir))
@@ -54,8 +58,5 @@ def test_fastai_mnist(docker_image, instance_type, py_version, sagemaker_local_s
 def _train_and_assert_success(estimator, input_dir, output_path):
     estimator.fit({'training': 'file://{}'.format(os.path.join(input_dir, 'training'))})
 
-    success_files = {
-        'model': ['model.pth'],
-        'output': ['success'],
-    }
+    success_files = {'model': ['model.pth'], 'output': ['success']}
     assert_files_exist(output_path, success_files)
