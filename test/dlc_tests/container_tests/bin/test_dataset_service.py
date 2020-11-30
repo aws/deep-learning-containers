5c636741920e23389d3629eeb64276e97d222d10
Teng Xu <xuteng@amazon.com>
2020-11-30 10:19:41 +0000

    Update port number

--- test/dlc_tests/container_tests/bin/test_dataset_service.py
+++ test/dlc_tests/container_tests/bin/test_dataset_service.py
@@ -11,7 +11,7 @@ FLAGS = flags.FLAGS
 
 def local_service():
     print("Starting Local Service")
-    dispatcher = tf.data.experimental.service.DispatchServer(tf.data.experimental.service.DispatcherConfig(port=5050))
+    dispatcher = tf.data.experimental.service.DispatchServer(tf.data.experimental.service.DispatcherConfig(port=50050))
     dispatcher_address = dispatcher.target.split("://")[1]
     worker = tf.data.experimental.service.WorkerServer(tf.data.experimental.service.WorkerConfig(
         dispatcher_address=dispatcher_address))
