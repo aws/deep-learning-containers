5f9fef8535df8d4b30b7294324b4c8ad36ca8b45
Chunxi Wang <wanchunx@amazon.com>
2020-12-16 13:17:51 -0800

    Updated test artifacts for PyT EIA 1.5.1

--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -320,7 +320,7 @@ def get_inference_run_command(image_uri, model_names, processor="cpu"):
     if processor == "eia":
         multi_model_location = {
             "resnet-152-eia": "https://s3.amazonaws.com/model-server/model_archive_1.0/resnet-152-eia.mar",
-            "pytorch-densenet": "https://aws-dlc-sample-models.s3.amazonaws.com/pytorch/densenet_eia/densenet_eia.mar",
+            "pytorch-densenet": "https://aws-dlc-sample-models.s3.amazonaws.com/pytorch/densenet_eia/densenet_eia_v1_5_1.mar",
         }
     elif server_type == "ts":
         multi_model_location = {
