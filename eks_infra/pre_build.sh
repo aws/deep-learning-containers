4921a5f5f76f0925332da59951b350e5d69a6925
tejaschumbalkar <tejaschumbalkar@gmail.com>
2020-12-23 19:06:15 -0800

    install kubectl cli

--- eks_infra/pre_build.sh
+++ eks_infra/pre_build.sh
@@ -5,16 +5,17 @@ set -e
 aws sts get-caller-identity
 which kubectl
 kubectl version --short --client
-rm /usr/local/bin/kubectl
-kubectl version --short --client
+#rm /usr/local/bin/kubectl
+#kubectl version --short --client
 
 eksctl version
 #install kubectl
 
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+#mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+#echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+sudo mv ./kubectl $(which kubectl)
 kubectl version --short --client
 
 
