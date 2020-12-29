72fc512a50a53a639a83538d7aeb71e28e263472
tejaschumbalkar <tejaschumbalkar@gmail.com>
2020-12-29 11:22:45 -0800

    trigger cb on pre_build.sh

--- eks_infra/build.sh
+++ eks_infra/build.sh
@@ -3,9 +3,17 @@ set -e
 
 #create eks cluster
 
+eksctl create cluster --name <CLUSTER_NAME> --nodegroup-name <NG-NAME> --nodes-min 1 --nodes-max 1 --node-type <INSTANCE_TYPE>
+
 #create nodegroups
+
+eksctl create nodegroup -f eks_infra/nodegroup.yaml
 
 #install cluster autoscalar
 
+
+
 #install kubeflow
+
+bash ./eks_infra/install_kubeflow_custom_kfctl.sh
 
--- eks_infra/pre_build.sh
+++ eks_infra/pre_build.sh
@@ -2,29 +2,18 @@
 set -e
 
 #aws
-aws sts get-caller-identity
-which kubectl
-kubectl version --short --client
-#rm /usr/local/bin/kubectl
-#kubectl version --short --client
 
 eksctl version
 #install kubectl
 
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-#mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-#echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 mv ./kubectl $(which kubectl)
 kubectl version --short --client
-
 
 #install eksctl
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 mv /tmp/eksctl /usr/local/bin
 eksctl version
-
-#install iam auth (opt)
-
 
