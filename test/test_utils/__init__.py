87a27154caeefe26635fef5298faed819df46e60
Chunxi Wang <wanchunx@amazon.com>
2020-12-30 12:09:02 -0800

    Fix lint issue of string comparison

--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -352,7 +352,7 @@ def get_inference_run_command(image_uri, model_names, processor="cpu"):
     else:
         server_cmd = "multi-model-server"
 
-    if processor is not "neuron":
+    if processor != "neuron":
         mms_command = (
             f"{server_cmd} --start --{server_type}-config /home/model-server/config.properties --models "
             + " ".join(parameters)
