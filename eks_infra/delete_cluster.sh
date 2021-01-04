ff97e2c420b13b6d984382b1ef8b38ad2728e23a
tejaschumbalkar <tejaschumbalkar@gmail.com>
2021-01-04 15:03:40 -0800

    check sts identity

--- eks_infra/delete_cluster.sh
+++ eks_infra/delete_cluster.sh
@@ -11,15 +11,6 @@ function delete_ec2_key_pair() {
     aws ec2 delete-key-pair --key-name "${1}-KeyPair" --region ${2}
 }
 
-function delete_nodegroups(){
-
-    LIST_NODE_GROUPS=$(eksctl get nodegroup --cluster eks-cluster -o json | jq -r '.[].StackName')
-
-    for NODEGROUP in $LIST_NODE_GROUPS; do
-      eksctl delete nodegroup --name $NODEGROUP --cluster ${1} --region ${2} --wait
-    done
-}
-
 if [ $# -ne 2 ]; then
     echo $0: usage: ./delete_cluster.sh cluster_name aws_region
     exit 1
--- eks_infra/env_setup.sh
+++ eks_infra/env_setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+#aws caller
+aws sts get-caller-identity
+
 #install kubectl
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl
 chmod +x ./kubectl
