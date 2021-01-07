7e630a71877a552b640fc1be761b793f94973ecb
tejaschumbalkar <tejaschumbalkar@gmail.com>
2021-01-07 13:48:13 -0800

    delete mxnet cluster and rollback eksctl to 0.34.0

--- eks_infra/env_setup.sh
+++ eks_infra/env_setup.sh
@@ -11,7 +11,12 @@ mv ./kubectl $(which kubectl)
 kubectl version --short --client
 
 #install eksctl
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+# rolling the eksctl version to 0.34.0 due to a bug https://github.com/weaveworks/eksctl/issues/3005 causing the nodes not joining cluster
+# as kubelet service on worker node fails. Fix included in v0.36.0 which is in pre-release state
+# "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz"
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/0.34.0/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 mv /tmp/eksctl /usr/local/bin
 eksctl version
 
