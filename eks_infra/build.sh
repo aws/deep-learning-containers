0e73f127f89958f6bb7f8a322804f40c6163fdd9
tejaschumbalkar <tejaschumbalkar@gmail.com>
2021-01-21 13:02:48 -0800

    modify eks logic

--- eks_infra/build.sh
+++ eks_infra/build.sh
@@ -8,7 +8,6 @@ OPERATION=$(jq -r '.operation' eks_infra/build_param.json)
 EKS_CLUSTERS=($(jq -r '.eks_clusters[]' eks_infra/build_param.json))
 CONTEXTS=($(jq -r '.contexts[]' eks_infra/build_param.json))
 EKS_VERSION=$(jq -r '.eks_version' eks_infra/build_param.json)
-LIST_CLUSTER=($(eksctl get cluster -o json | jq -r '.[].metadata.name'))
 CLUSTER_AUTOSCALAR_IMAGE_VERSION=$(jq -r '.cluster_autoscalar_image_version' eks_infra/build_param.json)
 
 # Create operation function
@@ -24,7 +23,7 @@ function create_cluster(){
     for CLUSTER in "${EKS_CLUSTERS[@]}"; do
       CLUSTER_NAME=${CLUSTER}-${CONTEXT}
 
-      if [[ ! " ${LIST_CLUSTER[@]} " =~ " ${CLUSTER_NAME} " ]]; then
+      if ! check_cluster_status $CLUSTER_NAME; then
         ./create_cluster.sh $CLUSTER_NAME $EKS_VERSION
         ./add_iam_identity.sh $CLUSTER_NAME
         ./install_cluster_components.sh $CLUSTER_NAME $CLUSTER_AUTOSCALAR_IMAGE_VERSION
@@ -53,7 +52,7 @@ function upgrade_cluster(){
   for CONTEXT in "${CONTEXTS[@]}"; do
     for CLUSTER in "${EKS_CLUSTERS[@]}"; do
       CLUSTER_NAME=${CLUSTER}-${CONTEXT}
-      if [[ " ${LIST_CLUSTER[@]} " =~ " ${CLUSTER_NAME} " ]]; then
+      if check_cluster_status $CLUSTER_NAME; then
         ./upgrade_cluster.sh $CLUSTER_NAME $EKS_VERSION $CLUSTER_AUTOSCALAR_IMAGE_VERSION
       else
         echo "EKS Cluster :: ${CLUSTER_NAME} :: does not exists. Skipping upgrade operation."
@@ -72,7 +71,7 @@ function delete_cluster(){
   for CONTEXT in "${CONTEXTS[@]}"; do
     for CLUSTER in "${EKS_CLUSTERS[@]}"; do
       CLUSTER_NAME=${CLUSTER}-${CONTEXT}
-      if [[ " ${LIST_CLUSTER[@]} " =~ " ${CLUSTER_NAME} " ]]; then
+      if check_cluster_status $CLUSTER_NAME; then
         ./delete_cluster.sh $CLUSTER_NAME
       else
         echo "EKS Cluster :: ${CLUSTER_NAME} :: does not exists. Skipping delete operation."
@@ -80,6 +79,10 @@ function delete_cluster(){
     done
   done
   
+}
+
+function check_cluster_status(){
+    aws eks describe-cluster --name ${1} --region ${AWS_REGION} --query cluster.status --out text | grep -q ACTIVE
 }
 
 case ${OPERATION} in 
