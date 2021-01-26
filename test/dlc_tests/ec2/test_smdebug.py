1a33e0744b194561d4b5f8ea0decd8b04e67c555
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2021-01-26 12:35:28 -0800

    pull images before test begins

--- test/dlc_tests/ec2/test_smdebug.py
+++ test/dlc_tests/ec2/test_smdebug.py
@@ -99,6 +99,7 @@ def run_smdebug_test(
     framework = get_framework_from_image_uri(image_uri)
     container_test_local_dir = os.path.join("$HOME", "container_tests")
     ec2_connection.run(f"$(aws ecr get-login --no-include-email --region {region})", hide=True)
+    ec2_connection.run(f"docker pull -q {image_uri}")
 
     try:
         ec2_connection.run(
@@ -136,6 +137,7 @@ def run_smprofiler_test(
     framework = get_framework_from_image_uri(image_uri)
     container_test_local_dir = os.path.join("$HOME", "container_tests")
     ec2_connection.run(f"$(aws ecr get-login --no-include-email --region {region})", hide=True)
+    ec2_connection.run(f"docker pull -q {image_uri}")
 
     try:
         ec2_connection.run(
