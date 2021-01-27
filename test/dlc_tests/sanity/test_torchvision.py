bbfbacd8f578fbb933938d57d523637c9e108d13
Daiming Yang <ydaiming@amazon.com>
2021-01-28 06:47:46 +0800

    remove tf checks and modify function definitions

--- test/dlc_tests/sanity/test_torchvision.py
+++ test/dlc_tests/sanity/test_torchvision.py
@@ -10,16 +10,28 @@ from test.test_utils import (
 
 
 @pytest.mark.model("N/A")
-def test_torchvision_nms_training_cpu(pytorch_training, cpu):
+def test_torchvision_nms_training(pytorch_training):
     """
     Check that the internally built torchvision binary is used to resolve the missing nms issue.
     :param pytorch_training: framework fixture for pytorch training
-    :param cpu: ECR image URI with "cpu" in the name
     """
-    image = cpu
-    if "tensorflow" in image or "mxnet" in image:
-        pytest.skip(msg="TF and MXNet don't have torchvision installed.")
+    image = pytorch_training
+    ctx = Context()
+    container_name = get_container_name("framework-version", image)
+    start_container(container_name, image, ctx)
+    output = run_cmd_on_container(
+        container_name, ctx, f"import torch; import torchvision; print(torch.ops.torchvision.nms)", executable="python"
+    )
+    assert "RuntimeError" not in output.stdout.strip()
 
+
+@pytest.mark.model("N/A")
+def test_torchvision_nms_inference(pytorch_inference):
+    """
+    Check that the internally built torchvision binary is used to resolve the missing nms issue.
+    :param pytorch_inference: framework fixture for pytorch inference
+    """
+    image = pytorch_inference
     ctx = Context()
     container_name = get_container_name("framework-version", image)
     start_container(container_name, image, ctx)
