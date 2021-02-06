6b180ae428c22783c6aa34d9275c8be66ad77521
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2021-02-05 16:11:59 -0800

    [tensorflow][sagemaker] Fix SM inference test input args

--- test/sagemaker_tests/tensorflow/inference/test/integration/sagemaker/test_tfs.py
+++ test/sagemaker_tests/tensorflow/inference/test/integration/sagemaker/test_tfs.py
@@ -30,13 +30,20 @@ def repo(request):
     return request.config.getoption('--repo') or 'sagemaker-tensorflow-serving'
 
 
+@pytest.fixture(scope='session')
+def processor(request, instance_type):
+    return request.config.getoption('--processor') or (
+        'gpu'
+        if instance_type.startswith('ml.p') or instance_type.startswith('ml.g')
+        else 'cpu'
+    )
+
+
 @pytest.fixture
-def tag(request, version, instance_type):
+def tag(request, version, instance_type, processor):
     if request.config.getoption('--tag'):
         return request.config.getoption('--tag')
-
-    arch = 'gpu' if instance_type.startswith('ml.p') else 'cpu'
-    return f'{version}-{arch}'
+    return f'{version}-{processor}'
 
 
 @pytest.fixture
