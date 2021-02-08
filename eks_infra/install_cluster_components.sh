ff396483eb7968f47a5979e5c65261a16dc3af74
tejaschumbalkar <tejaschumbalkar@gmail.com>
2021-02-08 13:27:06 -0800

    install neuron plugin

--- eks_infra/install_cluster_components.sh
+++ eks_infra/install_cluster_components.sh
@@ -15,6 +15,11 @@ function install_cluster_autoscalar(){
 
 }
 
+# Function to install neuron plugin
+function install_neuron_plugin(){
+  kubectl apply -f ../test/dlc_tests/eks/eks_manifest_templates/neuron/neuron_device_plugin.yaml
+}
+
 # Check for input arguments
 if [ $# -ne 2 ]; then
     echo "usage: ./${0} eks_cluster_name cluster_autoscalar_image_version"
@@ -31,6 +36,7 @@ CLUSTER_NAME=${1}
 CLUSTER_AUTOSCALAR_IMAGE_VERSION=${2}
 
 install_cluster_autoscalar ${CLUSTER_NAME} ${CLUSTER_AUTOSCALAR_IMAGE_VERSION}
+install_neuron_plugin
 
 # install kubeflow
 ../test/dlc_tests/eks/eks_manifest_templates/kubeflow/install_kubeflow_custom_kfctl.sh ${CLUSTER_NAME} ${AWS_REGION}
