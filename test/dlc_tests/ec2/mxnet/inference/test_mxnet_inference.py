53ffebbcd3bf8d3f399b83c486e44f9c55506ef5
Yuchen Nie <yuchnie@amazon.com>
2021-02-13 03:25:25 +0000

    fix ec2 test error

--- test/dlc_tests/ec2/mxnet/inference/test_mxnet_inference.py
+++ test/dlc_tests/ec2/mxnet/inference/test_mxnet_inference.py
@@ -45,7 +45,7 @@ def test_ec2_mxnet_resnet_inference_eia_cpu(mxnet_inference_eia, ec2_connection,
     model_name = RESNET_EIA_MODEL
     image_framework, image_framework_version = get_framework_and_version_from_tag(mxnet_inference_eia)
     if image_framework_version == "1.5.1":
-        RESNET_EIA_MODEL = "resnet-152-eia-1-5-1"
+        model_name = "resnet-152-eia-1-5-1"
     run_ec2_mxnet_inference(mxnet_inference_eia, model_name, "resnet-152-eia", ec2_connection, "eia", region, 80, 8081)
 
 
@@ -58,7 +58,7 @@ def test_ec2_mxnet_resnet_inferencei_eia_gpu(mxnet_inference_eia, ec2_connection
     model_name = RESNET_EIA_MODEL
     image_framework, image_framework_version = get_framework_and_version_from_tag(mxnet_inference_eia)
     if image_framework_version == "1.5.1":
-        RESNET_EIA_MODEL = "resnet-152-eia-1-5-1"
+        model_name = "resnet-152-eia-1-5-1"
     run_ec2_mxnet_inference(mxnet_inference_eia, model_name, "resnet-152-eia", ec2_connection, "eia", region, 80, 8081)
 
 
