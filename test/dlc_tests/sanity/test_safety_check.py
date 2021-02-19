a8ba754b61ae238737d46aa19249636a05eec56e
Sergey Togulev <togulev@amazon.com>
2021-02-19 13:49:53 -0800

    Updated pyyaml version.
    Removed 39611 from ignored issues

--- test/dlc_tests/sanity/test_safety_check.py
+++ test/dlc_tests/sanity/test_safety_check.py
@@ -27,26 +27,18 @@ IGNORE_SAFETY_IDS = {
         "training": {
             # 38449, 38450, 38451, 38452: for shipping pillow<=6.2.2 - the last available version for py2
             # 35015: for shipping pycrypto<=2.6.1 - the last available version for py2
-            "py2": ['38449', '38450', '38451', '38452', '35015'],
-            # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-            "py3": ['39611']
+            "py2": ['38449', '38450', '38451', '38452', '35015']
         },
         "inference": {
             # for shipping pillow<=6.2.2 - the last available version for py2
-            "py2": ['38449', '38450', '38451', '38452'],
-            # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-            "py3": ['39611']
+            "py2": ['38449', '38450', '38451', '38452']
         },
         "inference-eia": {
             # for shipping pillow<=6.2.2 - the last available version for py2
-            "py2": ['38449', '38450', '38451', '38452'],
-            # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-            "py3": ['39611']
+            "py2": ['38449', '38450', '38451', '38452']
         },
         "inference-neuron": {
             "py3": [
-                # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-                '39611',
                 # 39409, 39408, 39407, 39406: TF 1.15.5 is on par with TF 2.0.4, 2.1.3, 2.2.2, 2.3.2 in security patches
                 '39409', '39408', '39407', '39406',
             ],
@@ -57,25 +49,17 @@ IGNORE_SAFETY_IDS = {
             # numpy<=1.16.0 -- This has to only be here while we publish MXNet 1.4.1 EI DLC v1.0
             "py2": ['36810',
                     # for shipping pillow<=6.2.2 - the last available version for py2
-                    '38449', '38450', '38451', '38452'],
-            # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-            "py3": ['39611']
+                    '38449', '38450', '38451', '38452']
         },
         "inference": {
             # for shipping pillow<=6.2.2 - the last available version for py2
-            "py2": ['38449', '38450', '38451', '38452'],
-            # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-            "py3": ['39611']
+            "py2": ['38449', '38450', '38451', '38452']
         },
         "training": {
             # for shipping pillow<=6.2.2 - the last available version for py2
-            "py2": ['38449', '38450', '38451', '38452'],
-            # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-            "py3": ['39611']
+            "py2": ['38449', '38450', '38451', '38452']
         },
-        "inference-neuron":{
-            # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-            "py3": ['39611']
+        "inference-neuron": {
         }
     },
     "pytorch": {
@@ -83,21 +67,13 @@ IGNORE_SAFETY_IDS = {
             # astropy<3.0.1
             "py2": ['35810',
                     # for shipping pillow<=6.2.2 - the last available version for py2
-                    '38449', '38450', '38451', '38452'],
-            # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-            "py3": ['39611']
+                    '38449', '38450', '38451', '38452']
         },
         "inference": {
-            # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-            "py3": ['39611']
         },
         "inference-eia": {
-            # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-            "py3": ['39611']
         },
-        "inference-neuron":{
-            # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-            "py3": ['39611']
+        "inference-neuron": {
         }
     }
 }
@@ -137,9 +113,9 @@ def _get_latest_package_version(package):
 
 @pytest.mark.model("N/A")
 @pytest.mark.skipif(not is_dlc_cicd_context(), reason="Skipping test because it is not running in dlc cicd infra")
-@pytest.mark.skipif(not is_mainline_context(),
-                    reason="Skipping the test to decrease the number of calls to the Safety Check DB. "
-                           "Test will be executed in the 'mainline' pipeline only")
+# @pytest.mark.skipif(not is_mainline_context(),
+#                   reason="Skipping the test to decrease the number of calls to the Safety Check DB. "
+#                           "Test will be executed in the 'mainline' pipeline only")
 def test_safety(image):
     """
     Runs safety check on a container with the capability to ignore safety issues that cannot be fixed, and only raise
