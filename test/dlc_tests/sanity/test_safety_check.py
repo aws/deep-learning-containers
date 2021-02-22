85b2c516152cd63b93ea70734920bdeffae51b23
Tejas Chumbalkar <34728580+tejaschumbalkar@users.noreply.github.com>
2021-02-22 13:33:21 -0800

    [build][pytorch][test] upgrade py version for PT1.7.1 (#866)
    * upgrade py version for PT1.7.1
    
    * build config and safety changes
    
    * nit changes
    
    * add ruamel_yaml package
    
    * install python first
    
    * revert build config

--- test/dlc_tests/sanity/test_safety_check.py
+++ test/dlc_tests/sanity/test_safety_check.py
@@ -84,12 +84,10 @@ IGNORE_SAFETY_IDS = {
             "py2": ['35810',
                     # for shipping pillow<=6.2.2 - the last available version for py2
                     '38449', '38450', '38451', '38452'],
-            # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-            "py3": ['39611']
+            "py3": []
         },
         "inference": {
-            # for shipping pyyaml v5.3.1 - blocked on upgrading to v5.4.1 due to dependency on awscli
-            "py3": ['39611']
+            "py3": []
         },
         "inference-eia": {
             "py3": []
