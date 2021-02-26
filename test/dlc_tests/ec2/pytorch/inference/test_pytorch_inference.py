f9db61d36d8336fc636e45dcf1738f88fc12ecc0
Sai Parthasarathy Miduthuri <54188298+saimidu@users.noreply.github.com>
2021-02-26 13:37:51 -0800

    [pytorch][test][ec2] Fix PT Neuron EC2 test (#806)

--- test/dlc_tests/ec2/pytorch/inference/test_pytorch_inference.py
+++ test/dlc_tests/ec2/pytorch/inference/test_pytorch_inference.py
@@ -25,10 +25,10 @@ PT_TELEMETRY_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "test_p
 
 
 @pytest.mark.model("resnet")
-@pytest.mark.parametrize("ec2_instance_ami", [test_utils.NEURON_AL2_DLAMI], indirect=True)
+@pytest.mark.parametrize("ec2_instance_ami", [test_utils.UBUNTU_18_BASE_DLAMI_US_WEST_2], indirect=True)
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_NEURON_INSTANCE_TYPE, indirect=True)
-def test_ec2_pytorch_inference_gpu(pytorch_inference, ec2_connection, region, gpu_only):
-    ec2_pytorch_inference(pytorch_inference, "neuron", ec2_connection, region)
+def test_ec2_pytorch_inference_neuron(pytorch_inference_neuron, ec2_connection, region, neuron_only):
+    ec2_pytorch_inference(pytorch_inference_neuron, "neuron", ec2_connection, region)
 
 
 @pytest.mark.model("densenet")
@@ -63,17 +63,18 @@ def ec2_pytorch_inference(image_uri, processor, ec2_connection, region):
     repo_name, image_tag = image_uri.split("/")[-1].split(":")
     container_name = f"{repo_name}-{image_tag}-ec2"
     model_name = "pytorch-densenet"
-    if processor is "eia":
+    if processor == "eia":
         image_framework, image_framework_version = get_framework_and_version_from_tag(image_uri)
         if image_framework_version == "1.3.1":
             model_name = "pytorch-densenet-v1-3-1"
-    if processor is "neuron":
+    if processor == "neuron":
         model_name = "pytorch-resnet-neuron"
         
     inference_cmd = test_utils.get_inference_run_command(image_uri, model_name, processor)
     docker_cmd = "nvidia-docker" if "gpu" in image_uri else "docker"
 
-    if processor is "neuron":
+    if processor == "neuron":
+        ec2_connection.run("sudo systemctl stop neuron-rtd")  # Stop neuron-rtd in host env for DLC to start it
         docker_run_cmd = (
             f"{docker_cmd} run -itd --name {container_name}"
             f" -p 80:8080 -p 8081:8081"
@@ -93,7 +94,8 @@ def ec2_pytorch_inference(image_uri, processor, ec2_connection, region):
         LOGGER.info(docker_run_cmd)
         ec2_connection.run(docker_run_cmd, hide=True)
         inference_result = test_utils.request_pytorch_inference_densenet(
-            connection=ec2_connection
+            connection=ec2_connection,
+            model_name=model_name,
         )
         assert (
             inference_result
