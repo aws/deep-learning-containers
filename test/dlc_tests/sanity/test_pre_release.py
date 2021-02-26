68db1b9c799f3379bd4bfb3c413319b8733ba00f
Sai Parthasarathy Miduthuri <54188298+saimidu@users.noreply.github.com>
2021-02-26 12:34:42 -0800

    [tensorflow][neuron] Correction on tf version (#864)

--- test/dlc_tests/sanity/test_pre_release.py
+++ test/dlc_tests/sanity/test_pre_release.py
@@ -120,14 +120,16 @@ def test_ubuntu_version(image):
 
 
 @pytest.mark.model("N/A")
-@pytest.mark.canary("Run cpu framework version test regularly on production images")
-def test_framework_version_cpu(cpu):
+@pytest.mark.canary("Run non-gpu framework version test regularly on production images")
+def test_framework_version_cpu(image):
     """
     Check that the framework version in the image tag is the same as the one on a running container.
+    This function tests CPU, EIA, and Neuron images.
 
-    :param cpu: ECR image URI with "cpu" in the name
+    :param image: ECR image URI
     """
-    image = cpu
+    if "gpu" in image:
+        pytest.skip("GPU images will have their framework version tested in test_framework_and_cuda_version_gpu")
     if "tensorflow-inference" in image:
         pytest.skip(msg="TF inference does not have core tensorflow installed")
 
@@ -257,6 +259,13 @@ def test_dependency_check_cpu(cpu, ec2_connection):
 @pytest.mark.skipif(is_pr_context(), reason="Do not run dependency check on PR tests")
 def test_dependency_check_gpu(gpu, ec2_connection):
     _run_dependency_check_test(gpu, ec2_connection, "gpu")
+
+
+@pytest.mark.model("N/A")
+@pytest.mark.parametrize("ec2_instance_type", ["inf1.xlarge"], indirect=True)
+@pytest.mark.skipif(is_pr_context(), reason="Do not run dependency check on PR tests")
+def test_dependency_check_neuron(neuron, ec2_connection):
+    _run_dependency_check_test(neuron, ec2_connection, "neuron")
 
 
 @pytest.mark.model("N/A")
