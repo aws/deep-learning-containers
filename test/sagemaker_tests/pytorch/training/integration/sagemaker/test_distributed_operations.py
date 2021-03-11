c53a05f67d13c6b77cca2cc4a94f4133cd303107
Rahul Huilgol <huilgolr@amazon.com>
2021-03-10 18:00:27 -0800

    Fix indent

--- test/sagemaker_tests/pytorch/training/integration/sagemaker/test_distributed_operations.py
+++ test/sagemaker_tests/pytorch/training/integration/sagemaker/test_distributed_operations.py
@@ -29,7 +29,7 @@ MULTI_GPU_INSTANCE = 'ml.p3.8xlarge'
 
 
 def validate_or_skip_smmodelparallel(ecr_image):
-	_, image_framework_version = get_framework_and_version_from_tag(ecr_image)
+    _, image_framework_version = get_framework_and_version_from_tag(ecr_image)
     image_cuda_version = get_cuda_version_from_tag(ecr_image)
     if not(Version(image_framework_version) in SpecifierSet(">=1.6,<1.9")) or image_cuda_version != "cu110":
         pytest.skip("Model Parallelism only supports CUDA 11 on PyTorch 1.6, 1.7 and 1.8")
