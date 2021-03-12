1b1229981b7021d886acc1327aa7db99411caa9e
Tejas Chumbalkar <34728580+tejaschumbalkar@users.noreply.github.com>
2021-03-12 09:30:23 -0800

    [test][pytorch][mxnet] fix mnist dataset download 503 issue (#949)
    * temp fix for mnist download
    
    * disable tf and mx builds
    
    * remove unwanted code
    
    * add mxnet mnist fix
    
    * nit changes
    
    * fix mxnet eks test
    
    * fix eks test
    
    * fix sm test
    
    * revert build config

--- test/sagemaker_tests/pytorch/training/resources/mnist/smdebug_mnist.py
+++ test/sagemaker_tests/pytorch/training/resources/mnist/smdebug_mnist.py
@@ -129,6 +129,13 @@ def train(model, device, optimizer, hook, epochs, log_interval, training_dir):
     criterion = nn.CrossEntropyLoss()
     hook.register_loss(criterion)
 
+    # Temporariy fix for 503 error while downloading MNIST dataset. See https://github.com/pytorch/vision/issues/3549
+    datasets.MNIST.resources = [
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz', 'f68b3c2dcbeaaa9fbdd348bbdeb94873'),
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz', 'd53e105ee54ea40749a09fcbcd1e9432'),
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz', '9fb629c4189551a2d022fa330f9573f3'),
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz', 'ec29112dd5afa0611ce80d1b7f02629c')
+        ]
     trainloader = _get_train_data_loader(4, training_dir)
     validloader = _get_test_data_loader(4, training_dir)
 
