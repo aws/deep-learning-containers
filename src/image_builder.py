e3fe64e58b2bf8013014fbab839d11963c7d9b03
Jeetendra Patil <jspatil@amazon.com>
2021-03-12 14:28:51 -0800

    add license attribution for dlc images

--- src/image_builder.py
+++ src/image_builder.py
@@ -29,6 +29,7 @@ from buildspec import Buildspec
 from output import OutputFormatter
 from config import build_config
 
+import boto3
 
 def _find_image_object(images_list, image_name):
     """
@@ -119,6 +120,7 @@ def image_builder(buildspec):
                 extra_build_args["DATASETS_VERSION"] = image_config.get("datasets_version")
             else:
                 raise KeyError(f"HuggingFace buildspec.yml must contain 'datasets_version' field for each image")
+
 
         ARTIFACTS.update(
             {
