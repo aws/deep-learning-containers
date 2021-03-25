fced40bfa94a18435ce6c6cc9c326ba187f0a279
Jeetendra Patil <jspatil@amazon.com>
2021-03-25 15:01:33 -0700

    to revert

--- test/dlc_tests/ec2/pytorch/inference/test_pytorch_inference.py
+++ test/dlc_tests/ec2/pytorch/inference/test_pytorch_inference.py
@@ -24,102 +24,102 @@ PT_EC2_SINGLE_GPU_INSTANCE_TYPE = get_ec2_instance_type(
 PT_TELEMETRY_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "test_pt_dlc_telemetry_test")
 
 
-@pytest.mark.model("resnet")
-@pytest.mark.parametrize("ec2_instance_ami", [test_utils.UBUNTU_18_BASE_DLAMI_US_WEST_2], indirect=True)
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_NEURON_INSTANCE_TYPE, indirect=True)
-def test_ec2_pytorch_inference_neuron(pytorch_inference_neuron, ec2_connection, region, neuron_only):
-    ec2_pytorch_inference(pytorch_inference_neuron, "neuron", ec2_connection, region)
+# @pytest.mark.model("resnet")
+# @pytest.mark.parametrize("ec2_instance_ami", [test_utils.UBUNTU_18_BASE_DLAMI_US_WEST_2], indirect=True)
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_NEURON_INSTANCE_TYPE, indirect=True)
+# def test_ec2_pytorch_inference_neuron(pytorch_inference_neuron, ec2_connection, region, neuron_only):
+#     ec2_pytorch_inference(pytorch_inference_neuron, "neuron", ec2_connection, region)
 
 
-@pytest.mark.model("densenet")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
-def test_ec2_pytorch_inference_gpu(pytorch_inference, ec2_connection, region, gpu_only, ec2_instance_type):
-    if test_utils.is_image_incompatible_with_instance_type(pytorch_inference, ec2_instance_type):
-        pytest.skip(f"Image {pytorch_inference} is incompatible with instance type {ec2_instance_type}")
-    ec2_pytorch_inference(pytorch_inference, "gpu", ec2_connection, region)
+# @pytest.mark.model("densenet")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+# def test_ec2_pytorch_inference_gpu(pytorch_inference, ec2_connection, region, gpu_only, ec2_instance_type):
+#     if test_utils.is_image_incompatible_with_instance_type(pytorch_inference, ec2_instance_type):
+#         pytest.skip(f"Image {pytorch_inference} is incompatible with instance type {ec2_instance_type}")
+#     ec2_pytorch_inference(pytorch_inference, "gpu", ec2_connection, region)
 
 
-@pytest.mark.model("densenet")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
-def test_ec2_pytorch_inference_cpu(pytorch_inference, ec2_connection, region, cpu_only):
-    ec2_pytorch_inference(pytorch_inference, "cpu", ec2_connection, region)
+# @pytest.mark.model("densenet")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
+# def test_ec2_pytorch_inference_cpu(pytorch_inference, ec2_connection, region, cpu_only):
+#     ec2_pytorch_inference(pytorch_inference, "cpu", ec2_connection, region)
 
 
-@pytest.mark.integration("elastic_inference")
-@pytest.mark.model("resnet")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
-@pytest.mark.parametrize("ei_accelerator_type", PT_EC2_EIA_ACCELERATOR_TYPE, indirect=True)
-def test_ec2_pytorch_inference_eia_cpu(pytorch_inference_eia, ec2_connection, region, eia_only, pt14_and_above_only):
-    ec2_pytorch_inference(pytorch_inference_eia, "eia", ec2_connection, region)
+# @pytest.mark.integration("elastic_inference")
+# @pytest.mark.model("resnet")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
+# @pytest.mark.parametrize("ei_accelerator_type", PT_EC2_EIA_ACCELERATOR_TYPE, indirect=True)
+# def test_ec2_pytorch_inference_eia_cpu(pytorch_inference_eia, ec2_connection, region, eia_only, pt14_and_above_only):
+#     ec2_pytorch_inference(pytorch_inference_eia, "eia", ec2_connection, region)
 
 
-@pytest.mark.integration("elastic_inference")
-@pytest.mark.model("resnet")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_EIA_INSTANCE_TYPE, indirect=True)
-@pytest.mark.parametrize("ei_accelerator_type", PT_EC2_EIA_ACCELERATOR_TYPE, indirect=True)
-def test_ec2_pytorch_inference_eia_gpu(pytorch_inference_eia, ec2_connection, region, eia_only, pt14_and_above_only):
-    ec2_pytorch_inference(pytorch_inference_eia, "eia", ec2_connection, region)
+# @pytest.mark.integration("elastic_inference")
+# @pytest.mark.model("resnet")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_EIA_INSTANCE_TYPE, indirect=True)
+# @pytest.mark.parametrize("ei_accelerator_type", PT_EC2_EIA_ACCELERATOR_TYPE, indirect=True)
+# def test_ec2_pytorch_inference_eia_gpu(pytorch_inference_eia, ec2_connection, region, eia_only, pt14_and_above_only):
+#     ec2_pytorch_inference(pytorch_inference_eia, "eia", ec2_connection, region)
 
 
-def ec2_pytorch_inference(image_uri, processor, ec2_connection, region):
-    repo_name, image_tag = image_uri.split("/")[-1].split(":")
-    container_name = f"{repo_name}-{image_tag}-ec2"
-    model_name = "pytorch-densenet"
-    if processor == "eia":
-        image_framework, image_framework_version = get_framework_and_version_from_tag(image_uri)
-        if image_framework_version == "1.3.1":
-            model_name = "pytorch-densenet-v1-3-1"
-    if processor == "neuron":
-        model_name = "pytorch-resnet-neuron"
+# def ec2_pytorch_inference(image_uri, processor, ec2_connection, region):
+#     repo_name, image_tag = image_uri.split("/")[-1].split(":")
+#     container_name = f"{repo_name}-{image_tag}-ec2"
+#     model_name = "pytorch-densenet"
+#     if processor == "eia":
+#         image_framework, image_framework_version = get_framework_and_version_from_tag(image_uri)
+#         if image_framework_version == "1.3.1":
+#             model_name = "pytorch-densenet-v1-3-1"
+#     if processor == "neuron":
+#         model_name = "pytorch-resnet-neuron"
 
-    inference_cmd = test_utils.get_inference_run_command(image_uri, model_name, processor)
-    docker_cmd = "nvidia-docker" if "gpu" in image_uri else "docker"
+#     inference_cmd = test_utils.get_inference_run_command(image_uri, model_name, processor)
+#     docker_cmd = "nvidia-docker" if "gpu" in image_uri else "docker"
 
-    if processor == "neuron":
-        ec2_connection.run("sudo systemctl stop neuron-rtd")  # Stop neuron-rtd in host env for DLC to start it
-        docker_run_cmd = (
-            f"{docker_cmd} run -itd --name {container_name}"
-            f" -p 80:8080 -p 8081:8081"
-            f" --device=/dev/neuron0 --cap-add IPC_LOCK"
-            f" {image_uri} {inference_cmd}"
-        )
-    else:
-        docker_run_cmd = (
-            f"{docker_cmd} run -itd --name {container_name}"
-            f" -p 80:8080 -p 8081:8081"
-            f" {image_uri} {inference_cmd}"
-        )
-    try:
-        ec2_connection.run(
-            f"$(aws ecr get-login --no-include-email --region {region})", hide=True
-        )
-        LOGGER.info(docker_run_cmd)
-        ec2_connection.run(docker_run_cmd, hide=True)
-        server_type = get_inference_server_type(image_uri)
-        inference_result = test_utils.request_pytorch_inference_densenet(
-            connection=ec2_connection,
-            model_name=model_name,
-            server_type=server_type
-        )
-        assert (
-            inference_result
-        ), f"Failed to perform pytorch inference test for image: {image_uri} on ec2"
+#     if processor == "neuron":
+#         ec2_connection.run("sudo systemctl stop neuron-rtd")  # Stop neuron-rtd in host env for DLC to start it
+#         docker_run_cmd = (
+#             f"{docker_cmd} run -itd --name {container_name}"
+#             f" -p 80:8080 -p 8081:8081"
+#             f" --device=/dev/neuron0 --cap-add IPC_LOCK"
+#             f" {image_uri} {inference_cmd}"
+#         )
+#     else:
+#         docker_run_cmd = (
+#             f"{docker_cmd} run -itd --name {container_name}"
+#             f" -p 80:8080 -p 8081:8081"
+#             f" {image_uri} {inference_cmd}"
+#         )
+#     try:
+#         ec2_connection.run(
+#             f"$(aws ecr get-login --no-include-email --region {region})", hide=True
+#         )
+#         LOGGER.info(docker_run_cmd)
+#         ec2_connection.run(docker_run_cmd, hide=True)
+#         server_type = get_inference_server_type(image_uri)
+#         inference_result = test_utils.request_pytorch_inference_densenet(
+#             connection=ec2_connection,
+#             model_name=model_name,
+#             server_type=server_type
+#         )
+#         assert (
+#             inference_result
+#         ), f"Failed to perform pytorch inference test for image: {image_uri} on ec2"
 
-    finally:
-        ec2_connection.run(f"docker rm -f {container_name}", warn=True, hide=True)
-
-
-@pytest.mark.integration("telemetry")
-@pytest.mark.model("N/A")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_SINGLE_GPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_inference_telemetry_gpu(pytorch_inference, ec2_connection, gpu_only, ec2_instance_type, pt15_and_above_only):
-    if test_utils.is_image_incompatible_with_instance_type(pytorch_inference, ec2_instance_type):
-        pytest.skip(f"Image {pytorch_inference} is incompatible with instance type {ec2_instance_type}")
-    execute_ec2_inference_test(ec2_connection, pytorch_inference, PT_TELEMETRY_CMD)
+#     finally:
+#         ec2_connection.run(f"docker rm -f {container_name}", warn=True, hide=True)
 
 
-@pytest.mark.integration("telemetry")
-@pytest.mark.model("N/A")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_inference_telemetry_cpu(pytorch_inference, ec2_connection, cpu_only, pt15_and_above_only):
-    execute_ec2_inference_test(ec2_connection, pytorch_inference, PT_TELEMETRY_CMD)
+# @pytest.mark.integration("telemetry")
+# @pytest.mark.model("N/A")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_SINGLE_GPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_inference_telemetry_gpu(pytorch_inference, ec2_connection, gpu_only, ec2_instance_type, pt15_and_above_only):
+#     if test_utils.is_image_incompatible_with_instance_type(pytorch_inference, ec2_instance_type):
+#         pytest.skip(f"Image {pytorch_inference} is incompatible with instance type {ec2_instance_type}")
+#     execute_ec2_inference_test(ec2_connection, pytorch_inference, PT_TELEMETRY_CMD)
+
+
+# @pytest.mark.integration("telemetry")
+# @pytest.mark.model("N/A")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_inference_telemetry_cpu(pytorch_inference, ec2_connection, cpu_only, pt15_and_above_only):
+#     execute_ec2_inference_test(ec2_connection, pytorch_inference, PT_TELEMETRY_CMD)
--- test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
+++ test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
@@ -30,134 +30,134 @@ PT_EC2_MULTI_GPU_INSTANCE_TYPE = get_ec2_instance_type(
 )
 
 
-@pytest.mark.integration("pytorch_sanity_test")
-@pytest.mark.model("N/A")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_standalone_gpu(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
-    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
-        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
-    execute_ec2_training_test(ec2_connection, pytorch_training, PT_STANDALONE_CMD)
+# @pytest.mark.integration("pytorch_sanity_test")
+# @pytest.mark.model("N/A")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_standalone_gpu(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
+#     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+#         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+#     execute_ec2_training_test(ec2_connection, pytorch_training, PT_STANDALONE_CMD)
 
 
-@pytest.mark.integration("pytorch_sanity_test")
-@pytest.mark.model("N/A")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_standalone_cpu(pytorch_training, ec2_connection, cpu_only):
-    execute_ec2_training_test(ec2_connection, pytorch_training, PT_STANDALONE_CMD)
+# @pytest.mark.integration("pytorch_sanity_test")
+# @pytest.mark.model("N/A")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_standalone_cpu(pytorch_training, ec2_connection, cpu_only):
+#     execute_ec2_training_test(ec2_connection, pytorch_training, PT_STANDALONE_CMD)
 
 
-@pytest.mark.model("mnist")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_train_mnist_gpu(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
-    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
-        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
-    execute_ec2_training_test(ec2_connection, pytorch_training, PT_MNIST_CMD)
+# @pytest.mark.model("mnist")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_train_mnist_gpu(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
+#     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+#         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+#     execute_ec2_training_test(ec2_connection, pytorch_training, PT_MNIST_CMD)
 
 
-@pytest.mark.model("mnist")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_train_mnist_cpu(pytorch_training, ec2_connection, cpu_only):
-    execute_ec2_training_test(ec2_connection, pytorch_training, PT_MNIST_CMD)
+# @pytest.mark.model("mnist")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_train_mnist_cpu(pytorch_training, ec2_connection, cpu_only):
+#     execute_ec2_training_test(ec2_connection, pytorch_training, PT_MNIST_CMD)
 
 
-@pytest.mark.model("linear_regression")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_linear_regression_gpu(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
-    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
-        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
-    execute_ec2_training_test(ec2_connection, pytorch_training, PT_REGRESSION_CMD)
+# @pytest.mark.model("linear_regression")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_linear_regression_gpu(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
+#     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+#         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+#     execute_ec2_training_test(ec2_connection, pytorch_training, PT_REGRESSION_CMD)
 
 
-@pytest.mark.model("linear_regression")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_linear_regression_cpu(pytorch_training, ec2_connection, cpu_only):
-    execute_ec2_training_test(ec2_connection, pytorch_training, PT_REGRESSION_CMD)
+# @pytest.mark.model("linear_regression")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_linear_regression_cpu(pytorch_training, ec2_connection, cpu_only):
+#     execute_ec2_training_test(ec2_connection, pytorch_training, PT_REGRESSION_CMD)
 
 
-@pytest.mark.integration("dgl")
-@pytest.mark.model("gcn")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_train_dgl_gpu(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
-    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
-        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
-    execute_ec2_training_test(ec2_connection, pytorch_training, PT_DGL_CMD)
+# @pytest.mark.integration("dgl")
+# @pytest.mark.model("gcn")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_train_dgl_gpu(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
+#     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+#         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+#     execute_ec2_training_test(ec2_connection, pytorch_training, PT_DGL_CMD)
 
 
-@pytest.mark.integration("dgl")
-@pytest.mark.model("gcn")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_train_dgl_cpu(pytorch_training, ec2_connection, cpu_only, py3_only):
-    execute_ec2_training_test(ec2_connection, pytorch_training, PT_DGL_CMD)
+# @pytest.mark.integration("dgl")
+# @pytest.mark.model("gcn")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_train_dgl_cpu(pytorch_training, ec2_connection, cpu_only, py3_only):
+#     execute_ec2_training_test(ec2_connection, pytorch_training, PT_DGL_CMD)
 
 
-@pytest.mark.integration("horovod")
-@pytest.mark.model("mnist")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_with_horovod(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
-    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
-        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
-    test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPTHVD")
-    execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
+# @pytest.mark.integration("horovod")
+# @pytest.mark.model("mnist")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_with_horovod(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
+#     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+#         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+#     test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPTHVD")
+#     execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
 
 
-@pytest.mark.integration("gloo")
-@pytest.mark.model("resnet18")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_gloo(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
-    """
-    Tests gloo backend
-    """
-    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
-        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
-    test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchGloo")
-    execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
+# @pytest.mark.integration("gloo")
+# @pytest.mark.model("resnet18")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_gloo(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
+#     """
+#     Tests gloo backend
+#     """
+#     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+#         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+#     test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchGloo")
+#     execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
 
 
-@pytest.mark.integration("nccl")
-@pytest.mark.model("resnet18")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_nccl(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
-    """
-    Tests nccl backend
-    """
-    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
-        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
-    test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchNccl")
-    execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
+# @pytest.mark.integration("nccl")
+# @pytest.mark.model("resnet18")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_nccl(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
+#     """
+#     Tests nccl backend
+#     """
+#     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+#         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+#     test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchNccl")
+#     execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
 
 
-@pytest.mark.integration("mpi")
-@pytest.mark.model("resnet18")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_mpi(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
-    """
-    Tests mpi backend
-    """
-    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
-        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
-    test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchMpi")
-    execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
+# @pytest.mark.integration("mpi")
+# @pytest.mark.model("resnet18")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_mpi(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
+#     """
+#     Tests mpi backend
+#     """
+#     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+#         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+#     test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchMpi")
+#     execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
 
 
-@pytest.mark.integration("nvidia_apex")
-@pytest.mark.model("N/A")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
-def test_nvapex(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
-    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
-        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
-    execute_ec2_training_test(ec2_connection, pytorch_training, PT_APEX_CMD)
+# @pytest.mark.integration("nvidia_apex")
+# @pytest.mark.model("N/A")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+# def test_nvapex(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
+#     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+#         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+#     execute_ec2_training_test(ec2_connection, pytorch_training, PT_APEX_CMD)
 
 
-@pytest.mark.integration("amp")
-@pytest.mark.model("resnet50")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_MULTI_GPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_amp(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
-    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
-    if Version(image_framework_version) < Version("1.6"):
-        pytest.skip("Native AMP was introduced in PyTorch 1.6")
-    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
-        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
-    execute_ec2_training_test(ec2_connection, pytorch_training, PT_AMP_CMD)
+# @pytest.mark.integration("amp")
+# @pytest.mark.model("resnet50")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_MULTI_GPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_amp(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
+#     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
+#     if Version(image_framework_version) < Version("1.6"):
+#         pytest.skip("Native AMP was introduced in PyTorch 1.6")
+#     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+#         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+#     execute_ec2_training_test(ec2_connection, pytorch_training, PT_AMP_CMD)
 
 
 # This is placeholder test needed for EFA
@@ -171,16 +171,16 @@ def test_pytorch_telemetry_gpu(pytorch_training, ec2_connection, gpu_only, ec2_i
     else:
         print("EFA tests disabled")
 
-@pytest.mark.integration("telemetry")
-@pytest.mark.model("N/A")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_SINGLE_GPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_telemetry_gpu(pytorch_training, ec2_connection, gpu_only, ec2_instance_type, pt15_and_above_only):
-    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
-        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+# @pytest.mark.integration("telemetry")
+# @pytest.mark.model("N/A")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_SINGLE_GPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_telemetry_gpu(pytorch_training, ec2_connection, gpu_only, ec2_instance_type, pt15_and_above_only):
+#     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+#         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
 
 
-@pytest.mark.integration("telemetry")
-@pytest.mark.model("N/A")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
-def test_pytorch_telemetry_cpu(pytorch_training, ec2_connection, cpu_only, pt15_and_above_only):
-    execute_ec2_training_test(ec2_connection, pytorch_training, PT_TELEMETRY_CMD)
+# @pytest.mark.integration("telemetry")
+# @pytest.mark.model("N/A")
+# @pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
+# def test_pytorch_telemetry_cpu(pytorch_training, ec2_connection, cpu_only, pt15_and_above_only):
+#     execute_ec2_training_test(ec2_connection, pytorch_training, PT_TELEMETRY_CMD)
