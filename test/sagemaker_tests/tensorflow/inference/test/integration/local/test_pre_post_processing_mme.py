fad768efee122ddbb8ff7ce572a9880325c744cb
Jinpeng Qi <qijinpen@amazon.com>
2021-04-01 17:31:56 +0800

    fix ModuleNotFoundError in test_pre_post_processing_mme

--- test/sagemaker_tests/tensorflow/inference/test/integration/local/test_pre_post_processing_mme.py
+++ test/sagemaker_tests/tensorflow/inference/test/integration/local/test_pre_post_processing_mme.py
@@ -21,7 +21,7 @@ import pytest
 
 import requests
 
-from multi_model_endpoint_test_utils import make_load_model_request, make_headers
+from .multi_model_endpoint_test_utils import make_load_model_request, make_headers
 
 
 PING_URL = "http://localhost:8080/ping"
