be2d45a8053aaccd63f6ebc3ccacd17e2169cfbf
Roshani Nagmote <roshanin@amazon.com>
2021-04-08 10:00:48 -0700

    Updated requirements and init

--- test/sagemaker_tests/huggingface_pytorch/training/integration/__init__.py
+++ test/sagemaker_tests/huggingface_pytorch/training/integration/__init__.py
@@ -15,23 +15,8 @@ from __future__ import absolute_import
 import os
 
 resources_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'resources'))
-mnist_path = os.path.join(resources_path, 'mnist')
-mnist_script = os.path.join(mnist_path, 'mnist.py')
-smdataparallel_mnist_script = os.path.join(mnist_path, 'smdataparallel_mnist_script_mode.sh')
-fastai_path = os.path.join(resources_path, 'fastai')
-fastai_cifar_script = os.path.join(fastai_path, 'train_cifar.py')
-fastai_mnist_script = os.path.join(fastai_path, 'mnist.py')
-data_dir = os.path.join(mnist_path, 'data')
-training_dir = os.path.join(data_dir, 'training')
-dist_operations_path = os.path.join(resources_path, 'distributed_operations.py')
-smdebug_mnist_script = os.path.join(mnist_path, 'smdebug_mnist.py')
-
-mnist_1d_script = os.path.join(mnist_path, 'mnist_1d.py')
-model_cpu_dir = os.path.join(mnist_path, 'model_cpu')
-model_cpu_1d_dir = os.path.join(model_cpu_dir, '1d')
-model_gpu_dir = os.path.join(mnist_path, 'model_gpu')
-model_gpu_1d_dir = os.path.join(model_gpu_dir, '1d')
-call_model_fn_once_script = os.path.join(resources_path, 'call_model_fn_once.py')
+scripts_path = os.path.join(resources_path, 'scripts')
+distrilbert_script = os.path.join(scripts_path, 'train.py')
 
 ROLE = 'dummy/unused-role'
 DEFAULT_TIMEOUT = 20
