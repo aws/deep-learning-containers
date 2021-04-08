a0983e9d91687c0f1122d061b5e618e447a29e59
Mansi Mane <mansmane@amazon.com>
2021-04-08 11:43:46 -0700

    Added sagemaker session

--- test/sagemaker_tests/huggingface_pytorch/training/integration/sagemaker/test_smmp.py
+++ test/sagemaker_tests/huggingface_pytorch/training/integration/sagemaker/test_smmp.py
@@ -32,12 +32,12 @@ def test_smmp_gpu(sagemaker_session, framework_version, ecr_image, instance_type
             'model_name_or_path': 'roberta-large',
             'task_name': 'mnli',
             'per_device_train_batch_size': 16,
-            'per_device_eval_batch_size': 16,
+            'per_device_eval_batch_size': 4,
             'do_train': True,
             'do_eval': True,
             'do_predict': True,
             'output_dir': '/opt/ml/model',
-            'max_steps': 500,
+            'max_steps': 50,
         }
 
         # configuration for running training on smdistributed Model Parallel
@@ -69,28 +69,21 @@ def test_smmp_gpu(sagemaker_session, framework_version, ecr_image, instance_type
 
         # git configuration to download our fine-tuning script
         git_config = {'repo': 'https://github.com/huggingface/notebooks.git', 'branch': 'master'}
-        # metric definition to extract the results
-        metric_definitions = [
-            {'Name': 'train_runtime', 'Regex': "train_runtime.*=\D*(.*?)$"},
-            {'Name': 'train_samples_per_second', 'Regex': "train_samples_per_second.*=\D*(.*?)$"},
-            {'Name': 'epoch', 'Regex': "epoch.*=\D*(.*?)$"},
-            {'Name': 'f1', 'Regex': "f1.*=\D*(.*?)$"},
-            {'Name': 'exact_match', 'Regex': "exact_match.*=\D*(.*?)$"}]
 
         huggingface_estimator = HuggingFace(entry_point='run_glue.py',
                                             source_dir='./sagemaker/04_distributed_training_model_parallelism/scripts/',
                                             git_config=git_config,
-                                            metrics_definition=metric_definitions,
                                             instance_type=instance_type,
                                             instance_count=instance_count,
                                             volume_size=volume_size,
                                             role='SageMakerRole',
                                             image_uri=ecr_image,
-                                            framework_version='1.6.0',
+                                            framework_version=framework_version,
                                             transformers_version='4.4.2',
-                                            pytorch_version='1.6.0',
+                                            pytorch_version=framework_version,
                                             py_version='py36',
                                             distribution=distribution,
                                             hyperparameters=hyperparameters,
+                                            sagemaker_session=sagemaker_session,
                                             debugger_hook_config=False)
         huggingface_estimator.fit()
