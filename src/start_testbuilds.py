fe1a05203e8854c5481ea73ab2da65a8f8834ee3
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2021-04-09 16:05:32 -0700

    Add SM tests for HF DLCs

--- src/start_testbuilds.py
+++ src/start_testbuilds.py
@@ -94,10 +94,7 @@ def main():
             images_str = " ".join(images)
             if is_test_job_enabled(test_type):
                 # TODO: remove "sagemaker" once sagemaker tests are ready
-                if "huggingface" in images_str and test_type in [constants.EC2_TESTS,
-                                                                 constants.ECS_TESTS,
-                                                                 constants.EKS_TESTS,
-                                                                 constants.SAGEMAKER_TESTS]:
+                if "huggingface" in images_str and test_type in [constants.EC2_TESTS, constants.ECS_TESTS, constants.EKS_TESTS]:
                     LOGGER.debug(f"Skipping huggingface {test_type} test")
                     continue
                 run_test_job(commit, pr_test_job, images_str)
