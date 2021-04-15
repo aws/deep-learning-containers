a18e09a31db8fa28a08e45b6b539a10fe2e3a8f8
Sergey Togulev <togulev@amazon.com>
2021-04-15 13:41:03 -0700

    fixed typo

--- test/dlc_tests/conftest.py
+++ test/dlc_tests/conftest.py
@@ -422,7 +422,7 @@ def generate_unique_values_for_fixtures(metafunc_obj, images_to_parametrize, val
     :return: <dict> Mapping of "Fixture to be parametrized" -> "Unique values for fixture to be parametrized"
     """
     job_type_map = {"training": "tr", "inference": "inf"}
-    framework_name_maps = {
+    framework_name_map = {
         "tensorflow": "tf",
         "mxnet": "mx",
         "pytorch": "pt",
