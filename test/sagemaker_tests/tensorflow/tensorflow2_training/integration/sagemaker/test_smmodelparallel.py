57a871b6794073adba9fcd6e8c530fb810e1fa85
akhilmehra <akhilrmehra@gmail.com>
2021-04-15 20:40:50 -0700

    [tensorflow] | [test] | [sagemaker] (#188)
    add efa test for tf2
    
    Co-authored-by: Jeetendra Patil <jeet4320@users.noreply.github.com>
    Co-authored-by: Karan Jariwala <karankjariwala@gmail.com>
    Co-authored-by: Lai Wei <royweilai@gmail.com>
    Co-authored-by: yselivonchyk <y.selivonchyk@gmail.com>

--- test/sagemaker_tests/tensorflow/tensorflow2_training/integration/sagemaker/test_smmodelparallel.py
+++ test/sagemaker_tests/tensorflow/tensorflow2_training/integration/sagemaker/test_smmodelparallel.py
@@ -25,6 +25,80 @@ from ...integration.utils import processor, py_version, unique_name_from_base  #
 
 RESOURCE_PATH = os.path.join(os.path.dirname(__file__), '..', '..', 'resources')
 
+@pytest.mark.integration("smmodelparallel")
+@pytest.mark.processor("gpu")
+@pytest.mark.model("mnist")
+@pytest.mark.skip_cpu
+@pytest.mark.skip_py2_containers
+@pytest.mark.parametrize("test_script, num_processes", [("tf2_conv.py", 2), ("tf2_conv_xla.py", 2), ("smmodelparallel_hvd2_conv.py", 4), ("send_receive_checkpoint.py", 2), ("tf2_checkpoint_test.py", 2)])
+@pytest.mark.efa()
+def test_smmodelparallel_efa(sagemaker_session, instance_type, ecr_image, tmpdir, framework_version, test_script, num_processes):
+    """
+    Tests SM Modelparallel in sagemaker
+    """
+    instance_type = "ml.p3dn.24xlarge"
+    _, image_framework_version = get_framework_and_version_from_tag(ecr_image)
+    image_cuda_version = get_cuda_version_from_tag(ecr_image)
+    if Version(image_framework_version) < Version("2.4.1") or image_cuda_version != "cu110":
+        pytest.skip("Model Parallelism with EFA only supports CUDA 11, and on TensorFlow 2.4.1 or higher")
+    smmodelparallel_path = os.path.join(RESOURCE_PATH, 'smmodelparallel')
+    estimator = TensorFlow(entry_point=test_script,
+                           role='SageMakerRole',
+                           instance_count=1,
+                           instance_type=instance_type,
+                           source_dir=smmodelparallel_path,
+                           distributions={
+                               "mpi": {
+                                   "enabled": True,
+                                   "processes_per_host": num_processes,
+                                   "custom_mpi_options": "-verbose --mca orte_base_help_aggregate 0 -x FI_EFA_USE_DEVICE_RDMA=1 -x FI_PROVIDER=efa -x RDMAV_FORK_SAFE=1 ",
+                                }
+                           },
+                           sagemaker_session=sagemaker_session,
+                           image_uri=ecr_image,
+                           framework_version=framework_version,
+                           py_version='py3',
+                           base_job_name='smp-test1')
+    estimator.fit()
+
+
+@pytest.mark.integration("smmodelparallel")
+@pytest.mark.processor("gpu")
+@pytest.mark.model("mnist")
+@pytest.mark.multinode(2)
+@pytest.mark.skip_cpu
+@pytest.mark.skip_py2_containers
+@pytest.mark.parametrize("test_script, num_processes", [("smmodelparallel_hvd2_conv_multinode.py", 2)])
+@pytest.mark.efa()
+def test_smmodelparallel_multinode_efa(sagemaker_session, instance_type, ecr_image, tmpdir, framework_version, test_script, num_processes):
+    """
+    Tests SM Modelparallel in sagemaker
+    """
+    instance_type = "ml.p3dn.24xlarge"
+    _, image_framework_version = get_framework_and_version_from_tag(ecr_image)
+    image_cuda_version = get_cuda_version_from_tag(ecr_image)
+    if Version(image_framework_version) < Version("2.4.1") or image_cuda_version != "cu110":
+        pytest.skip("Model Parallelism on EFA only supports CUDA 11, and on TensorFlow 2.4.1 or higher")
+    smmodelparallel_path = os.path.join(RESOURCE_PATH, 'smmodelparallel')
+    estimator = TensorFlow(entry_point=test_script,
+                           role='SageMakerRole',
+                           instance_count=2,
+                           instance_type=instance_type,
+                           source_dir=smmodelparallel_path,
+                           distributions={
+                               "mpi": {
+                                   "enabled": True,
+                                   "processes_per_host": num_processes,
+                                   "custom_mpi_options": "-verbose --mca orte_base_help_aggregate 0 -x FI_EFA_USE_DEVICE_RDMA=1 -x FI_PROVIDER=efa -x RDMAV_FORK_SAFE=1 ",
+                                }
+                           },
+                           sagemaker_session=sagemaker_session,
+                           image_uri=ecr_image,
+                           framework_version=framework_version,
+                           py_version='py3',
+                           base_job_name='smp-test2')
+    estimator.fit()
+
 
 def validate_or_skip_smmodelparallel(ecr_image):
     if not can_run_smmodelparallel(ecr_image):
