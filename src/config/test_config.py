7a491cd2275d719a679850faee8438a369d6b505
Jeetendra Patil <jspatil@amazon.com>
2021-05-05 15:34:08 -0700

    update ld library path for efa

--- src/config/test_config.py
+++ src/config/test_config.py
@@ -7,9 +7,9 @@ ENABLE_BENCHMARK_DEV_MODE = False
 # It is recommended to set DISABLE_EFA_TESTS to True to disable EFA tests if there is no change to EFA installer version or Frameworks.
 DISABLE_EFA_TESTS = False
 
-DISABLE_SANITY_TESTS = False
-DISABLE_SAGEMAKER_TESTS = False
-DISABLE_ECS_TESTS = False
-DISABLE_EKS_TESTS = False
-DISABLE_EC2_TESTS = False
+DISABLE_SANITY_TESTS = True
+DISABLE_SAGEMAKER_TESTS = True
+DISABLE_ECS_TESTS = True
+DISABLE_EKS_TESTS = True
+DISABLE_EC2_TESTS = True
 USE_SCHEDULER = False
