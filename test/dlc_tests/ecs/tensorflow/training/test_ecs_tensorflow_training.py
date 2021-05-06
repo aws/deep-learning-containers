c5593453b8ffc4ca307625e67baef41555111f06
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2021-05-05 21:12:13 -0700

    [test][tensorflow] Add ECS Horovod test for TF

--- test/dlc_tests/ecs/tensorflow/training/test_ecs_tensorflow_training.py
+++ test/dlc_tests/ecs/tensorflow/training/test_ecs_tensorflow_training.py
@@ -2,7 +2,8 @@ import os
 
 import pytest
 
-from test.test_utils import ECS_AML2_CPU_USWEST2, ECS_AML2_GPU_USWEST2, CONTAINER_TESTS_PREFIX, is_nightly_context
+from test.test_utils import ECS_AML2_CPU_USWEST2, ECS_AML2_GPU_USWEST2, CONTAINER_TESTS_PREFIX
+from test.test_utils import is_nightly_context, is_pr_context
 from test.test_utils import ecs as ecs_utils
 from test.test_utils import ec2 as ec2_utils
 
@@ -12,7 +13,6 @@ TF_FasterRCNN_TRAINING_SCRIPT = os.path.join(CONTAINER_TESTS_PREFIX, "testFaster
 TF_MPI_TRAINING_SCRIPT = os.path.join(CONTAINER_TESTS_PREFIX, "testTF2HVD")
 
 
-@pytest.mark.skip(reason="I can do what I want.")
 @pytest.mark.model("mnist")
 @pytest.mark.parametrize("training_script", [TF_MNIST_TRAINING_SCRIPT], indirect=True)
 @pytest.mark.parametrize("ecs_instance_type", ["c5.4xlarge"], indirect=True)
@@ -32,7 +32,6 @@ def test_ecs_tensorflow_training_mnist_cpu(cpu_only, ecs_container_instance, ten
     ecs_utils.ecs_training_test_executor(ecs_cluster_name, cluster_arn, training_cmd, tensorflow_training, instance_id)
 
 
-@pytest.mark.skip(reason="I can do what I want.")
 @pytest.mark.model("mnist")
 @pytest.mark.parametrize("training_script", [TF_MNIST_TRAINING_SCRIPT], indirect=True)
 @pytest.mark.parametrize("ecs_instance_type", ["p3.8xlarge"], indirect=True)
@@ -76,7 +75,8 @@ def test_ecs_tensorflow_training_fasterrcnn_gpu(gpu_only, ecs_container_instance
                                          num_gpus=num_gpus)
 
 
-@pytest.mark.model("FasterRCNN")
+@pytest.mark.model("tf2mpi")
+@pytest.mark.skipif(not is_pr_context(), reason="Running additional model in nightly context only")
 @pytest.mark.parametrize("training_script", [TF_MPI_TRAINING_SCRIPT], indirect=True)
 @pytest.mark.parametrize("ecs_instance_type", ["p3dn.24xlarge"], indirect=True)
 @pytest.mark.parametrize("ecs_ami", [ECS_AML2_GPU_USWEST2], indirect=True)
@@ -84,9 +84,9 @@ def test_ecs_tensorflow_training_mpi_gpu(
     gpu_only, ecs_container_instance, tensorflow_training, training_cmd, ecs_cluster_name,
 ):
     """
-    GPU Faster RCNN test for TF Training
+    GPU MPI test for TF Training
 
-    Instance Type - g3.8xlarge
+    Instance Type - p3dn.24xlarge
 
     Given above parameters, registers a task with family named after this test, runs the task, and waits for
     the task to be stopped before doing teardown operations of instance and cluster.
