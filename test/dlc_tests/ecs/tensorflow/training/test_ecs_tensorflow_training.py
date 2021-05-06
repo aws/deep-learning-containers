c9a81e72b9b0414ecdc5f29b056da7260786e2c7
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2021-05-05 20:30:49 -0700

    Try HVD test on ECS

--- test/dlc_tests/ecs/tensorflow/training/test_ecs_tensorflow_training.py
+++ test/dlc_tests/ecs/tensorflow/training/test_ecs_tensorflow_training.py
@@ -9,8 +9,10 @@ from test.test_utils import ec2 as ec2_utils
 
 TF_MNIST_TRAINING_SCRIPT = os.path.join(CONTAINER_TESTS_PREFIX, "testTensorFlow")
 TF_FasterRCNN_TRAINING_SCRIPT = os.path.join(CONTAINER_TESTS_PREFIX, "testFasterRCNN")
+TF_MPI_TRAINING_SCRIPT = os.path.join(CONTAINER_TESTS_PREFIX, "testTF2HVD")
 
 
+@pytest.mark.skip(reason="I can do what I want.")
 @pytest.mark.model("mnist")
 @pytest.mark.parametrize("training_script", [TF_MNIST_TRAINING_SCRIPT], indirect=True)
 @pytest.mark.parametrize("ecs_instance_type", ["c5.4xlarge"], indirect=True)
@@ -30,6 +32,7 @@ def test_ecs_tensorflow_training_mnist_cpu(cpu_only, ecs_container_instance, ten
     ecs_utils.ecs_training_test_executor(ecs_cluster_name, cluster_arn, training_cmd, tensorflow_training, instance_id)
 
 
+@pytest.mark.skip(reason="I can do what I want.")
 @pytest.mark.model("mnist")
 @pytest.mark.parametrize("training_script", [TF_MNIST_TRAINING_SCRIPT], indirect=True)
 @pytest.mark.parametrize("ecs_instance_type", ["p3.8xlarge"], indirect=True)
@@ -71,3 +74,27 @@ def test_ecs_tensorflow_training_fasterrcnn_gpu(gpu_only, ecs_container_instance
 
     ecs_utils.ecs_training_test_executor(ecs_cluster_name, cluster_arn, training_cmd, tensorflow_training, instance_id,
                                          num_gpus=num_gpus)
+
+
+@pytest.mark.model("FasterRCNN")
+@pytest.mark.parametrize("training_script", [TF_MPI_TRAINING_SCRIPT], indirect=True)
+@pytest.mark.parametrize("ecs_instance_type", ["p3dn.24xlarge"], indirect=True)
+@pytest.mark.parametrize("ecs_ami", [ECS_AML2_GPU_USWEST2], indirect=True)
+def test_ecs_tensorflow_training_mpi_gpu(
+    gpu_only, ecs_container_instance, tensorflow_training, training_cmd, ecs_cluster_name,
+):
+    """
+    GPU Faster RCNN test for TF Training
+
+    Instance Type - g3.8xlarge
+
+    Given above parameters, registers a task with family named after this test, runs the task, and waits for
+    the task to be stopped before doing teardown operations of instance and cluster.
+    """
+    instance_id, cluster_arn = ecs_container_instance
+
+    num_gpus = ec2_utils.get_instance_num_gpus(instance_id)
+
+    ecs_utils.ecs_training_test_executor(
+        ecs_cluster_name, cluster_arn, training_cmd, tensorflow_training, instance_id, num_gpus=num_gpus,
+    )
