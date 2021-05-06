2363faff45b9d767dd5dedda1ea84d7caad4c26c
Jeetendra Patil <jspatil@amazon.com>
2021-05-06 16:40:09 -0700

    revert

--- test/test_utils/sagemaker.py
+++ test/test_utils/sagemaker.py
@@ -123,13 +123,6 @@ def generate_sagemaker_pytest_cmd(image, sagemaker_test_type):
     else:
         integration_path = os.path.join("integration", sagemaker_test_type)
 
-    if job_type == "training":
-        if framework == "tensorflow":
-            if framework_major_version == "2":
-                integration_path = f"integration/sagemaker/test_mnist.py::test_smdataparallel_smmodelparallel_mnist"
-            else:
-                integration_path = f"integration/sagemaker/test_tuning_model_dir.py"
-
     # Conditions for modifying tensorflow SageMaker pytest commands
     if framework == "tensorflow" and sagemaker_test_type == SAGEMAKER_REMOTE_TEST_TYPE:
         if job_type == "inference":
