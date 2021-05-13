1426d0c3cba63d3cd17a34fdb859c2564373f9b8
tejaschumbalkar <tejaschumbalkar@gmail.com>
2021-05-13 15:50:01 -0700

    remove parallel execution

--- test/test_utils/sagemaker.py
+++ test/test_utils/sagemaker.py
@@ -147,8 +147,7 @@ def generate_sagemaker_pytest_cmd(image, sagemaker_test_type):
     remote_pytest_cmd = (
         f"pytest -rA {integration_path} --region {region} --processor {processor} {docker_base_arg} "
         f"{sm_remote_docker_base_name} --tag {tag} {framework_version_arg} {framework_version} "
-        f"{aws_id_arg} {account_id} {instance_type_arg} {instance_type} {efa_flag} --multi-region-support --junitxml {test_report} "
-        f"--reruns {reruns} --reruns-delay {reruns_delay} -n auto"
+        f"{aws_id_arg} {account_id} {instance_type_arg} {instance_type} {efa_flag} --multi-region-support --junitxml {test_report}"
     )
 
     if processor == "eia" :
