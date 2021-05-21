bd9c9e3a77dc5bdac15ce43c6120bac2e800b504
tejaschumbalkar <tejaschumbalkar@gmail.com>
2021-05-20 19:39:55 -0700

    disable efa test

--- src/config/test_config.py
+++ src/config/test_config.py
@@ -5,7 +5,7 @@ ENABLE_BENCHMARK_DEV_MODE = False
 # Disable the test codebuild jobs to be run
 
 # It is recommended to set DISABLE_EFA_TESTS to True to disable EFA tests if there is no change to EFA installer version or Frameworks.
-DISABLE_EFA_TESTS = True
+DISABLE_EFA_TESTS = False
 
 DISABLE_SANITY_TESTS = False
 DISABLE_SAGEMAKER_TESTS = False
