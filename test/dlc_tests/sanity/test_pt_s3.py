7d39de7621ea7421e7f845bad9445fe9376d3430
Ubuntu <roshaninagmote2@gmail.com>
2021-05-26 18:46:38 +0000

    fixing sanity test

--- test/dlc_tests/sanity/test_pt_s3.py
+++ test/dlc_tests/sanity/test_pt_s3.py
@@ -25,7 +25,8 @@ def test_pt_s3_sanity(pytorch_training):
     ctx = Context()
     container_name = get_container_name("pt-s3", pytorch_training)
     start_container(container_name, pytorch_training, ctx)
+    s3_path = 's3://pt-s3plugin-test-data-west2/test_0.JPEG'
     run_cmd_on_container(
-        container_name, ctx, f"import awsio; print(awsio.__version__); from awsio.python.lib.io.s3.s3dataset import file_exists; print(f\"file_exists: {file_exists('s3://pt-s3plugin-test-data-west2/test_0.JPEG')}\") ", executable="python"
+        container_name, ctx, f"import awsio; print(awsio.__version__); from awsio.python.lib.io.s3.s3dataset import file_exists; print(file_exists(\""+s3_path+"\"))", executable="python"
     )
 
