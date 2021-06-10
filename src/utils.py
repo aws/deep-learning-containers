815dd56128bebba23140c6983c518581bb324f62
Bhat <bhradhik@88665a01ca73.ant.amazon.com>
2021-06-10 14:18:06 -0700

    Checking webhooks

--- src/utils.py
+++ src/utils.py
@@ -12,7 +12,7 @@ distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 ANY KIND, either express or implied. See the License for the specific
 language governing permissions and limitations under the License.
 Check Webhook events 
-OnAny
+OnAny Webhook Probot App
 """
 import os
 import re
