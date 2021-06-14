14845756042b4fe1a8351eec0dc81a24db81bbc5
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2021-06-14 07:33:01 -0700

    Fix test_framework_version

--- test/dlc_tests/sanity/test_pre_release.py
+++ test/dlc_tests/sanity/test_pre_release.py
@@ -24,11 +24,12 @@ from test.test_utils import (
     is_dlc_cicd_context,
     is_pr_context,
     run_cmd_on_container,
-    start_container, 
-    is_time_for_canary_safety_scan, 
+    start_container,
+    is_time_for_canary_safety_scan,
     is_mainline_context,
     is_nightly_context,
     get_repository_local_path,
+    get_repository_and_tag_from_image_uri,
 )
 
 
@@ -136,13 +137,16 @@ def test_framework_version_cpu(image):
     """
     if "gpu" in image:
         pytest.skip("GPU images will have their framework version tested in test_framework_and_cuda_version_gpu")
-    if "tensorflow-inference" in image:
-        pytest.skip(msg="TF inference does not have core tensorflow installed")
+    image_repo_name = get_repository_and_tag_from_image_uri(image)
+    if re.fullmatch("(pr-|beta-|nightly-)?tensorflow-inference(-eia)?", image_repo_name):
+        pytest.skip(msg="TF inference for CPU/GPU/EIA does not have core tensorflow installed")
 
     tested_framework, tag_framework_version = get_framework_and_version_from_tag(image)
 
+    # Framework name may include huggingface
+    tested_framework = tested_framework.lstrip("huggingface_")
     # Module name is torch
-    if tested_framework == "pytorch":
+    if tested_framework in "pytorch":
         tested_framework = "torch"
     ctx = Context()
     container_name = get_container_name("framework-version", image)
@@ -172,13 +176,11 @@ def test_framework_and_cuda_version_gpu(gpu, ec2_connection):
     # Framework Version Check #
     # Skip framework version test for tensorflow-inference, since it doesn't have core TF installed
     if "tensorflow-inference" not in image:
+        # Framework name may include huggingface
+        tested_framework = tested_framework.lstrip("huggingface_")
         # Module name is "torch"
         if tested_framework == "pytorch":
             tested_framework = "torch"
-        if tested_framework == "huggingface_pytorch":
-            tested_framework = "torch"
-        if tested_framework == "huggingface_tensorflow":
-            tested_framework = "tensorflow"
         cmd = f"import {tested_framework}; print({tested_framework}.__version__)"
         output = ec2.execute_ec2_training_test(ec2_connection, image, cmd, executable="python")
 
