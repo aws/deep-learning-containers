08b39c83d2cba734876d553314b41b9baf5d653b
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2021-06-18 09:41:45 -0700

    [build] Fix MX Neuron

--- src/config/build_config.py
+++ src/config/build_config.py
@@ -3,11 +3,11 @@
 ENABLE_EI_MODE = False
 # Please only set it to True if you are preparing an NEURON related PR
 # Do remember to revert it back to False before merging any PR (including NEURON dedicated PR)
-ENABLE_NEURON_MODE = False
+ENABLE_NEURON_MODE = True
 # Frameworks for which you want to disable both builds and tests
-DISABLE_FRAMEWORK_TESTS = []
+DISABLE_FRAMEWORK_TESTS = ["tensorflow", "pytorch"]
 # Disable new builds or build without datetime tag
-DISABLE_DATETIME_TAG = False
+DISABLE_DATETIME_TAG = True
 # Note: Need to build the images at least once with DISABLE_DATETIME_TAG = True
 # before disabling new builds or tests will fail
 DISABLE_NEW_BUILDS = False
