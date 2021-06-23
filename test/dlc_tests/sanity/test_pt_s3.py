17be8d7ccf18a264050c0633885f55b750896c20
Jeetendra Patil <jspatil@amazon.com>
2021-06-23 10:51:32 -0700

    fix sanity for pt s3 plugin

--- test/dlc_tests/sanity/test_pt_s3.py
+++ test/dlc_tests/sanity/test_pt_s3.py
@@ -21,7 +21,7 @@ def test_pt_s3_sanity(pytorch_training):
     """
     _, framework_version = get_framework_and_version_from_tag(pytorch_training)
     if Version(framework_version) < Version("1.8"):
-        pytest.skip("S3 plugin sanity is support on PyTorch version >=1.8")
+        pytest.skip("S3 plugin sanity is supported on PyTorch version >=1.8")
     ctx = Context()
     container_name = get_container_name("pt-s3", pytorch_training)
     start_container(container_name, pytorch_training, ctx)
