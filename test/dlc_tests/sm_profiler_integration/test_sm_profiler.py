d4d8386998a44b0c4ee74266b4331d9981c6f0a3
arjunkes <arjunkes@amazon.com>
2021-06-22 22:21:28 -0700

    first pass at test

--- test/dlc_tests/sm_profiler_integration/test_sm_profiler.py
+++ test/dlc_tests/sm_profiler_integration/test_sm_profiler.py
@@ -1,27 +1,45 @@
+import os
+import json
+
 from packaging.version import Version
 
 import pytest
 
 from invoke.context import Context
 
-from test.test_utils import get_framework_and_version_from_tag, get_processor_from_image_uri, get_cuda_version_from_tag, is_mainline_context
+from test.test_utils import is_mainline_context, get_container_name
 
 
+@pytest.mark.integration("smprofiler")
+@pytest.mark.model("N/A")
 @pytest.mark.skipif(not is_mainline_context(), reason="Mainline only test")
-def test_sm_profiler(training):
-    tf_version_cutoff = "2.3.1"
-    pt_version_cutoff = "1.6.0"
-    cuda_cutoff = ""
-    fw, fw_version = get_framework_and_version_from_tag(training)
-    if fw not in ("tensorflow", "pytorch"):
-        pytest.skip(f"Skipping SM Profiler integration test on unsupported framework {fw}")
-    else:
-        if fw == "tensorflow" and Version(fw_version) < Version("2.3.1"):
-            pytest.skip("")
-
-    processor = get_processor_from_image_uri(training)
-    if processor not in ("cpu", "gpu"):
-        pytest.skip("")
+def test_sm_profiler_pt(pytorch_training):
+    run_sm_profiler_tests(pytorch_training)
 
 
+@pytest.mark.integration("smprofiler")
+@pytest.mark.model("N/A")
+@pytest.mark.skipif(not is_mainline_context(), reason="Mainline only test")
+def test_sm_profiler_tf(tensorflow_training):
+    run_sm_profiler_tests(tensorflow_training)
 
+
+def run_sm_profiler_tests(image):
+    ctx = Context()
+    smdebug_version = ctx.run(f"docker run {image} python -c 'import smdebug; print(smdebug.__version__)'").stdout.strip()
+
+    if Version(smdebug_version) < Version("1"):
+        pytest.skip(f"smdebug version {smdebug_version} is less than 1, so smprofiler not expected to be present")
+    profiler_tests_dir = os.path.join(os.getenv('CODEBUILD_SRC_DIR'), 'smprofiler_tests')
+    sm_tests_zip = "sagemaker-tests.zip"
+    ctx.run(f'aws s3 cp s3://smprofiler-test-artifacts/{sm_tests_zip} {profiler_tests_dir}')
+
+    test_results_outfile = f"{get_container_name('smprof', image)}.txt"
+    with ctx.prefix(f'cd {profiler_tests_dir}'):
+        ctx.run(f'unzip {sm_tests_zip}')
+        with ctx.prefix(f"cd {sm_tests_zip.strip('.zip')}/tests"):
+            test_output = ctx.run(f"pytest --json-report --json-report-file={test_results_outfile} -n=auto -v -s -W=ignore", warn=True)
+        with open(test_results_outfile) as outfile:
+            result_data = json.load(outfile)
+        if test_output.return_code != 0:
+            pytest.fail(f"Failed SM Profiler tests. Results: {result_data}")
