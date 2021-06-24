de6751827706a0b15bffe29d991257f4e02c9a14
Radhika Bhat <bhradhik@amazon.com>
2021-06-24 16:32:52 -0700

    Build Project New

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,6 @@ or in the "license" file accompanying this file. This file is
 distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 ANY KIND, either express or implied. See the License for the specific
 language governing permissions and limitations under the License.
-Check for Build
 """
 import os
 import re
