3ad6e28188b00a8fae6e3abe12a3a531e214a61e
Radhika Bhat <bhradhik@amazon.com>
2021-06-25 10:03:38 -0700

    New commit

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,8 @@ or in the "license" file accompanying this file. This file is
 distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 ANY KIND, either express or implied. See the License for the specific
 language governing permissions and limitations under the License.
-Ceck for commits.data in the last commit
+Check for commits.data in the last commit
+Pushing a new commit
 """
 import os
 import re
