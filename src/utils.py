9f44b925653f74b58c2d99ff07824daed4054dfd
Radhika Bhat <bhradhik@amazon.com>
2021-06-29 10:07:30 -0700

    Only Pytorch Version

--- src/utils.py
+++ src/utils.py
@@ -12,7 +12,7 @@ distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 ANY KIND, either express or implied. See the License for the specific
 language governing permissions and limitations under the License.
 Check for commits.data in the last commit
-Check req 1
+Checking pytorch
 """
 import os
 import re
