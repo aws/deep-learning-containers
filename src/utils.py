f4af1e29ad485f65df08c0c6da9ae33a38025331
Radhika Bhat <bhradhik@amazon.com>
2021-06-29 10:13:09 -0700

    Mxnet Version

--- src/utils.py
+++ src/utils.py
@@ -11,8 +11,7 @@ or in the "license" file accompanying this file. This file is
 distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 ANY KIND, either express or implied. See the License for the specific
 language governing permissions and limitations under the License.
-Check for commits.data in the last commit
-Checking pytorch Version only
+Checking mxnet
 """
 import os
 import re
