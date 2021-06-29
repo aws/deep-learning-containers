21396730b68c26392d32f142bafa8d55412bda44
Radhika Bhat <bhradhik@amazon.com>
2021-06-29 11:22:46 -0700

    Mxnet Version-new

--- src/utils.py
+++ src/utils.py
@@ -12,7 +12,7 @@ distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 ANY KIND, either express or implied. See the License for the specific
 language governing permissions and limitations under the License.
 Checking mxnet version
-Checking
+Just one build
 """
 import os
 import re
