6276a479c4b68ba4fe560925ea0eaab61695f345
arjunkes <arjunkes@amazon.com>
2021-06-29 20:30:03 -0700

    update buildspec to build PT training imagse

--- src/config/build_config.py
+++ src/config/build_config.py
@@ -7,7 +7,7 @@ ENABLE_NEURON_MODE = False
 # Frameworks for which you want to disable both builds and tests
 DISABLE_FRAMEWORK_TESTS = ['mxnet']
 # Disable new builds or build without datetime tag
-DISABLE_DATETIME_TAG = False
+DISABLE_DATETIME_TAG = True
 # Note: Need to build the images at least once with DISABLE_DATETIME_TAG = True
 # before disabling new builds or tests will fail
 DISABLE_NEW_BUILDS = False
