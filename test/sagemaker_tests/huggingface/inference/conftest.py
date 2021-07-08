601205f4a696b58b89261c8e11c5e22a0d0286f3
Junpu Fan <junpu@amazon.com>
2021-07-08 13:00:35 -0700

    [test][sagemaker][huggingface] fix hugging face conftest.py (#1219)

--- test/sagemaker_tests/huggingface/inference/conftest.py
+++ test/sagemaker_tests/huggingface/inference/conftest.py
@@ -105,7 +105,7 @@ def pytest_addoption(parser):
     parser.addoption("--docker-base-name", default="huggingface_pytorch")
     parser.addoption("--region", default="us-west-2")
     parser.addoption("--framework-version", default="")
-    parser.addoption("--py-version", choices=["2", "3"], default=str(sys.version_info.major))
+    parser.addoption("--py-version", choices=["2", "3", "37", "38", "39"], default=str(sys.version_info.major))
     # Processor is still "cpu" for EIA tests
     parser.addoption("--processor", choices=["gpu", "cpu", "eia"], default="cpu")
     # If not specified, will default to {framework-version}-{processor}-py{py-version}
