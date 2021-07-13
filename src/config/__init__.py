0064460c49f415f391abfa0c344e1eb477944ea7
arjunkes <arjunkes@amazon.com>
2021-07-12 17:53:28 -0700

    test invoke changes

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -7,9 +7,11 @@ from invoke.context import Context
 
 def get_dlc_developer_config_path():
     ctx = Context()
-    alt_root_dir = ctx.run("git rev-parse --show-toplevel", hide=True, warn=True).stdout.strip()
 
-    return os.path.join(os.getenv("CODEBUILD_SRC_DIR", alt_root_dir), "dlc_developer_config.toml")
+    return os.path.join(
+        os.getenv("CODEBUILD_SRC_DIR", ctx.run("git rev-parse --show-toplevel", hide=True, warn=True).stdout.strip()),
+        "dlc_developer_config.toml"
+    )
 
 
 def parse_dlc_developer_configs(section, option, tomlfile=get_dlc_developer_config_path()):
