ae9fa5f6f5a6e313c2331207f6f08f3f8765f892
arjunkes <arjunkes@amazon.com>
2021-07-12 18:31:46 -0700

    update

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,17 +1,22 @@
 import os
+import re
 
 import toml
 
-from invoke.context import Context
-
 
 def get_dlc_developer_config_path():
-    ctx = Context()
+    root_dir_pattern = re.compile(r'^(\S+/deep-learning-containers)')
+    pwd = os.getcwd()
+    dev_config_parent_dir = os.getenv("CODEBUILD_SRC_DIR")
 
-    return os.path.join(
-        os.getenv("CODEBUILD_SRC_DIR", ctx.run("git rev-parse --show-toplevel", hide=True, warn=True).stdout.strip()),
-        "dlc_developer_config.toml"
-    )
+    # Ensure we are inside some directory called "deep-learning-containers
+    try:
+        if not dev_config_parent_dir:
+            dev_config_parent_dir = root_dir_pattern.match(pwd).group(1)
+    except AttributeError as e:
+        raise RuntimeError(f"Unable to find DLC root directory in path {pwd}, and no CODEBUILD_SRC_DIR set") from e
+
+    return os.path.join(dev_config_parent_dir, "dlc_developer_config.toml")
 
 
 def parse_dlc_developer_configs(section, option, tomlfile=get_dlc_developer_config_path()):
