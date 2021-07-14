32326c43bd7601ff282402b770d6db004870a738
Mansi Mane <mansmane@amazon.com>
2021-07-13 17:31:47 -0700

    Skipping fastai test on all 1.9 DLCs

--- test/sagemaker_tests/pytorch/training/integration/local/test_single_machine_training.py
+++ test/sagemaker_tests/pytorch/training/integration/local/test_single_machine_training.py
@@ -43,8 +43,8 @@ def test_mnist(docker_image, processor, instance_type, sagemaker_local_session, 
 @pytest.mark.skip_py2_containers
 def test_fastai_mnist(docker_image, instance_type, py_version, sagemaker_local_session, tmpdir):
     _, image_framework_version = get_framework_and_version_from_tag(docker_image)
-    if Version(image_framework_version) == Version("1.9.0"):
-        pytest.skip("Fast AI is not supported on PT 1.9.0 DLC")
+    if Version(image_framework_version) == Version("1.9"):
+        pytest.skip("Fast AI is not supported on PT 1.9 DLC")
     estimator = PyTorch(
         entry_point=fastai_mnist_script,
         role=ROLE,
