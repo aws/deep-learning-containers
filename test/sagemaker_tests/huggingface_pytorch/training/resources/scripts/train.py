cbdb679389e5662417bae7a1851bf8068fd0827b
philschmid <schmidphilipp1995@gmail.com>
2021-07-28 08:38:53 +0200

    fix training test

--- test/sagemaker_tests/huggingface_pytorch/training/resources/scripts/train.py
+++ test/sagemaker_tests/huggingface_pytorch/training/resources/scripts/train.py
@@ -25,8 +25,6 @@ if __name__ == "__main__":
     parser.add_argument("--output-data-dir", type=str, default=os.environ["SM_OUTPUT_DATA_DIR"])
     parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
     parser.add_argument("--n_gpus", type=str, default=os.environ["SM_NUM_GPUS"])
-    parser.add_argument("--training_dir", type=str, default=os.environ["SM_CHANNEL_TRAIN"])
-    parser.add_argument("--test_dir", type=str, default=os.environ["SM_CHANNEL_TEST"])
 
     args, _ = parser.parse_known_args()
 
