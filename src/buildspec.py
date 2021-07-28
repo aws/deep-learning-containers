ed1abb2ce10e95c3cb9868da016ba6884731a97d
tejaschumbalkar <tejaschumbalkar@gmail.com>
2021-07-28 16:38:57 -0700

    nit changes

--- src/buildspec.py
+++ src/buildspec.py
@@ -5,6 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License"). You
 may not use this file except in compliance with the License. A copy of
 the License is located at
 
+
     http://aws.amazon.com/apache2.0/
 
 or in the "license" file accompanying this file. This file is
