73bd4a64779730a7bb546717be2138372bda98be
Shantanu Tripathi <shantanutripathi237@gmail.com>
2021-08-25 21:45:52 +0000

    Pushing images sequentially

--- src/image_builder.py
+++ src/image_builder.py
@@ -342,10 +342,14 @@ def get_dummy_boto_client():
 
 def push_images(images):
     THREADS = {}
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        for image in images:
-            THREADS[image.name] = executor.submit(image.push_image)
-    FORMATTER.progress(THREADS)
+    for image in images:
+        THREADS[image.name] = image.push_image()
+    FORMATTER.title("Completed PUSH")
+    # FORMATTER.progress(THREADS)
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    #     for image in images:
+    #         THREADS[image.name] = executor.submit(image.push_image)
+    # FORMATTER.progress(THREADS)
 
 
 
