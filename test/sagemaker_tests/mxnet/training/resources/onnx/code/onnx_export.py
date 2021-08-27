d74bb888275d27de3d3bd55da105c37c9a1d8148
Wei Chu <weichu@amazon.com>
2021-08-27 15:41:51 -0700

    use pretrained model

--- test/sagemaker_tests/mxnet/training/resources/onnx/code/onnx_export.py
+++ test/sagemaker_tests/mxnet/training/resources/onnx/code/onnx_export.py
@@ -17,51 +17,39 @@ import json
 import os
 
 # test the new mxnet-onnx module that released since mxnet 1.9.0
+import mxnet
 from mxnet import onnx as onnx_mxnet
 import numpy as np
+import gluoncv
 import onnx
 from onnx import checker
-
-
-def _read_data_shapes(path, preferred_batch_size=1):
-    with open(path, 'r') as f:
-        signature = json.load(f)
-
-    data_shapes = []
-
-    for s in signature:
-        shape = s['shape']
-
-        if preferred_batch_size:
-            shape[0] = preferred_batch_size
-
-        data_shapes.append(shape)
-
-    return data_shapes
-
 
 def _assert_onnx_validity(model_path):
     model_proto = onnx.load_model(model_path)
     checker.check_graph(model_proto.graph)
 
 
-def main(training_dir, model_dir):
-    sym = os.path.join(training_dir, 'resnet18_v2-symbol.json')
-    params = os.path.join(training_dir, 'resnet18_v2-0000.params')
-    data_shapes = _read_data_shapes(os.path.join(training_dir, 'model-shapes.json'))
+def main():
+    prefix = './resnet18_v2'
+    # input shape and type
+    in_shape = (1, 3, 224, 224)
+    in_dtype = 'float32'
+    # download mxnet model
+    gluon_model = gluoncv.model_zoo.get_model('resnet18_v2', pretrained=True)
+    gluon_model.hybridize()
+    # forward with dummy input and save model
+    dummy_input = mxnet.nd.zeros(in_shape, dtype=in_dtype)
+    gluon_model.forward(dummy_input)
+    gluon_model.export(prefix, 0)
 
-    output_path = os.path.join(model_dir, 'model.onnx')
-    converted_path = onnx_mxnet.export_model(sym, params, data_shapes, np.float32, output_path)
-    _assert_onnx_validity(converted_path)
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
-    parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
-    return parser.parse_args()
+    mx_sym = prefix + '-symbol.json'
+    mx_params = prefix + '-0000.params'
+    onnx_file = prefix + '.onnx'
+    in_shapes = [in_shape]
+    in_dtypes = [in_dtype]
+    onnx_mxnet.export_model(mx_sym, mx_params, [in_shape], [in_dtype], onnx_file)
+    _assert_onnx_validity(onnx_file)
 
 
 if __name__ == '__main__':
-    args = parse_args()
-    main(args.train, args.model_dir)
+    main()
