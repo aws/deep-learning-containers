ca3a7653b7b8725fb990d9c80449d76bf0de2a10
Manu Seth <sethman@amazon.com>
2021-09-01 00:30:29 +0000

    remove newline

--- test/sagemaker_tests/mxnet/training/integration/local/test_keras_training.py
+++ test/sagemaker_tests/mxnet/training/integration/local/test_keras_training.py
@@ -24,7 +24,6 @@ from ...integration import MODEL_SUCCESS_FILES, RESOURCE_PATH
 
 @pytest.mark.integration("keras")
 @pytest.mark.model("mnist")
-
 def test_keras_training(docker_image, sagemaker_local_session, local_instance_type,
                         framework_version, tmpdir):
     if framework_version in ['1.9.0']:
