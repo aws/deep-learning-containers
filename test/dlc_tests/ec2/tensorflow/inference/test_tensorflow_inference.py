07c828e70f75e054a959c2458a2fb3dd1452c435
Venky Natham <vrnatham@amazon.com>
2021-09-14 13:42:43 -0700

    enable neuron monitor in the test
    Signed-off-by: Venky Natham <vrnatham@amazon.com>

--- test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
+++ test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
@@ -100,6 +100,7 @@ def run_ec2_tensorflow_inference(image_uri, ec2_connection, ec2_instance_ami, gr
         docker_run_cmd = (
             f"{docker_cmd} run -id --name {container_name} -p {grpc_port}:8500 "
             f"--device=/dev/neuron0 --net=host  --cap-add IPC_LOCK "
+            f"-e NEURON_MONITOR_CW_REGION=us-east-1 -e NEURON_MONITOR_CW_NAMESPACE=tf1 "
             f"--mount type=bind,source={model_path},target=/models/mnist -e TEST_MODE=1 -e MODEL_NAME=mnist"
             f" {image_uri}"
         )
