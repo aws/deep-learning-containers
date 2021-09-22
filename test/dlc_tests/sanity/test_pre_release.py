dfd75a12d191b18e218cb3d7c7fc071be05920b2
Philipp Schmid <32632186+philschmid@users.noreply.github.com>
2021-09-22 09:13:58 +0200

    update pre_sanity to pass openssl

--- test/dlc_tests/sanity/test_pre_release.py
+++ test/dlc_tests/sanity/test_pre_release.py
@@ -252,8 +252,8 @@ def _run_dependency_check_test(image, ec2_connection, processor):
         "tensorflow": ["1.15", "2.3", "2.4", "2.5", "2.6"],
         "mxnet": ["1.9"],
         "pytorch": [],
-        "huggingface_pytorch": ["1.8"],
-        "huggingface_tensorflow": ["2.4"]
+        "huggingface_pytorch": ["1.8", "1.9"],
+        "huggingface_tensorflow": ["2.4", "2.5"]
     }
 
     if short_fw_version in allow_openssl_cve_fw_versions.get(framework, []):
