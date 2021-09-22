742d1c955012cf804d7081bc7da15e59c3b163a0
Shantanu Tripathi <shantanutripathi237@gmail.com>
2021-09-22 00:35:07 +0000

    Made the get_safety_ignore_dict method bit generic

--- src/common_stage_image.py
+++ src/common_stage_image.py
@@ -38,14 +38,16 @@ class CommonStageImage(DockerImage):
         # Call the update_pre_build_configuration steps from the parent class
         super(CommonStageImage, self).update_pre_build_configuration()
         # Generate safety scan report for the first stage image and add the file to artifacts
-        first_stage_image_uri = self.build_args["PRE_PUSH_IMAGE"]
-        processed_image_uri = first_stage_image_uri.replace(".", "-").replace("/", "-").replace(":", "-")
+        pre_push_stage_image_uri = self.build_args["PRE_PUSH_IMAGE"]
+        processed_image_uri = pre_push_stage_image_uri.replace(".", "-").replace("/", "-").replace(":", "-")
         image_name = self.name
         tarfile_name_for_context = f"{processed_image_uri}-{image_name}"
         storage_file_path = os.path.join(
             os.sep, get_root_folder_path(), "src", f"{tarfile_name_for_context}_safety_report.json",
         )
-        generate_safety_report_for_image(first_stage_image_uri, storage_file_path=storage_file_path)
+        generate_safety_report_for_image(
+            pre_push_stage_image_uri, image_info=self.info, storage_file_path=storage_file_path
+        )
         self.context = self.generate_common_stage_context(storage_file_path, tarfile_name=tarfile_name_for_context)
 
     def generate_common_stage_context(self, safety_report_path, tarfile_name="common-stage-file"):
