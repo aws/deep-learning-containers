8fd20a9e890e55bb6501b189d380adfdbbc89cb6
Tejas Chumbalkar <34728580+tejaschumbalkar@users.noreply.github.com>
2021-09-25 18:13:39 -0700

    [build] Add safety report to docker image (#1186)
    * inital commit
    
    * Add safety report to docker image
    
    * adapt build logic
    
    * fix safety dir
    
    * address file strcuture
    
    * add copy cmd
    
    * add bash option
    
    * fix file name
    
    * add quotes
    
    * install jq
    
    * remove safety pacakge
    
    * modify buildspec
    
    * add safety for TF2.5
    
    * support safety for TF2.5
    
    * adapt the new safety workflow on the sanity test
    
    * run sanity test
    
    * run safety on PR
    
    * revert unrelated change
    
    * revert temp changes
    
    * multi stage docker build
    
    * Fixed a few issues. Edited Dockerfiles to allow running the script faster in local. Will revert.
    
    * Made Context to properly run conclude stage Dockerfile
    
    * Renaming to Conclusion_Stage
    
    * Created Safety Scan Reports
    
    * Added the generate_safety_report_for_image function in utils.py
    
    * Added the report generation functionality in image class
    
    * Added ConclusionStage class
    
    * Added logic to first genereate conclusion and then example. Also added dummy boto client creation.
    
    * Added logic to build conclusion images at the very end
    
    * Fixed the pre build step
    
    * Refactored First Stage to Initial Stage
    
    * Added the image push logic
    
    * Added the working for upload metrics
    
    * Added the set_test_env portion
    
    * Resolved the same name tarfile conflict
    
    * Added the safety tests
    
    * Added reason to ignore field in safety_check_v2
    
    * Added the new safety_check_v2 script
    
    * Modified the tests to run on new safety report
    
    * Added the ignore dict logic with the data
    
    * Refactored PYTHONPATH to ROOT_FOLDER_PATH
    
    * Refactored comments in safety_check_v2.py
    
    * Deleted src/test_and_generate_python_safety_report.py
    
    * Resolved mxnet Dockerfile cpu conflict
    
    * Resolved mxnet buildpsec
    
    * Reverted test_safety_check.py to master
    
    * Deleted safety_report.py
    
    * Added container removal logic
    
    * Added safety report generator class
    
    * Changes the utils generate_safety_report_for_image function
    
    * Added Formatter messages
    
    * Add null entrypoint while running docker images
    
    * Added extended timeouts
    
    * Added debug print lines
    
    * Pushing images sequentially
    
    * Added client timeout in constants with parallelization
    
    * Changed the test. Removed the safety_check_v2.py
    
    * Reverted print and debug statements
    
    * Restricted worker count for pushing images
    
    * Reverted modified Dockerfile from pull request
    
    * removed Safety Data Json
    
    * Made changes to build images in non Codebuild Context
    
    * Changed the test files to run on desired contexts
    
    * Changing dummy client
    
    * Handling context deletion
    
    * Changed the context remove
    
    * Refactoring image, conclusion, misc_docker
    
    * Refactoring Conclusion to Common and Initial to Pre_push
    
    * Added the get_root_folder_path method
    
    * Changed safety report generator
    
    * Changing the key in test file
    
    * Added new logging logic
    
    * Removed extra vars, Added docstring
    
    * Changed test file
    
    * Renamed test file. Added buildspec
    
    * Added logging method to image
    
    * Ran black -l 120 (https://github.com/psf/black) for following files:
    1. common_stage
    2. constant.py (Only to this PR's code)
    3. image_buider (Only to this PR's code)
    4. image
    5. test_safety_report_file
    6. metrics.py (Only to this PR's code)
    7. utils.py (Only to this PR's code)
    8. safety_report_generato (Already ran)
    
    Not run on:
    1. Dockerfile.common
    2. Data Json
    3. test_safety_check
    
    * Minor changed
    
    * Add multistage.common tag
    
    * Added the process_image function to build and push images. Changed tag name.
    
    * Made the get_safety_ignore_dict method bit generic
    
    * Removed the if condition for context
    
    * Addressed the nit changes
    
    * Ignoring TF2.6 vulnerability
    
    * Added the multi-tagging mechanism
    
    * Configured test env file to dump no appended tags. Other loggin related changes
    
    * Disabling Common Stage
    
    * Fix
    
    Co-authored-by: Shantanu Tripathi <shantanutripathi237@gmail.com>

--- src/image_builder.py
+++ src/image_builder.py
@@ -21,14 +21,19 @@ from copy import deepcopy
 
 import constants
 import utils
+import boto3
+import itertools
 
 from context import Context
 from metrics import Metrics
 from image import DockerImage
+from common_stage_image import CommonStageImage
 from buildspec import Buildspec
 from output import OutputFormatter
 from config import parse_dlc_developer_configs
 
+FORMATTER = OutputFormatter(constants.PADDING)
+build_context = os.getenv("BUILD_CONTEXT")
 
 def _find_image_object(images_list, image_name):
     """
@@ -47,11 +52,11 @@ def _find_image_object(images_list, image_name):
 
 # TODO: Abstract away to ImageBuilder class
 def image_builder(buildspec):
-    FORMATTER = OutputFormatter(constants.PADDING)
 
     BUILDSPEC = Buildspec()
     BUILDSPEC.load(buildspec)
-    IMAGES = []
+    PRE_PUSH_STAGE_IMAGES = []
+    COMMON_STAGE_IMAGES = []
 
     if "huggingface" in str(BUILDSPEC["framework"]) or "autogluon" in str(BUILDSPEC["framework"]):
         os.system("echo login into public ECR")
@@ -71,7 +76,6 @@ def image_builder(buildspec):
         if image_config.get("context") is not None:
             ARTIFACTS.update(image_config["context"])
 
-        build_context = os.getenv("BUILD_CONTEXT")
         image_tag = (
             tag_image_with_pr_number(image_config["tag"])
             if build_context == "PR"
@@ -86,7 +90,7 @@ def image_builder(buildspec):
         )
         base_image_uri = None
         if image_config.get("base_image_name") is not None:
-            base_image_object = _find_image_object(IMAGES, image_config["base_image_name"])
+            base_image_object = _find_image_object(PRE_PUSH_STAGE_IMAGES, image_config["base_image_name"])
             base_image_uri = base_image_object.ecr_url
 
         if image_config.get("download_artifacts") is not None:
@@ -155,110 +159,270 @@ def image_builder(buildspec):
             "labels": labels,
             "extra_build_args": extra_build_args
         }
-
-        image_object = DockerImage(
+        
+        # Create pre_push stage docker object
+        pre_push_stage_image_object = DockerImage(
             info=info,
             dockerfile=image_config["docker_file"],
             repository=image_repo_uri,
-            tag=image_tag,
+            tag=append_tag(image_tag, "pre-push"),
             to_build=image_config["build"],
+            stage=constants.PRE_PUSH_STAGE,
             context=context,
+            additional_tags=[image_tag],
         )
 
-        IMAGES.append(image_object)
+        ##### Create Common stage docker object #####
+        # If for a pre_push stage image we create a common stage image, then we do not push the pre_push stage image
+        # to the repository. Instead, we just push its common stage image to the repository. Therefore,
+        # inside function get_common_stage_image_object we make pre_push_stage_image_object non pushable.
+        # common_stage_image_object = generate_common_stage_image_object(pre_push_stage_image_object, image_tag)
+        # COMMON_STAGE_IMAGES.append(common_stage_image_object)
+
+        PRE_PUSH_STAGE_IMAGES.append(pre_push_stage_image_object)
+        FORMATTER.separator()
 
     FORMATTER.banner("DLC")
-    FORMATTER.title("Status")
 
+    # Standard images must be built before example images
+    # Example images will use standard images as base
+    standard_images = [image for image in PRE_PUSH_STAGE_IMAGES if "example" not in image.name.lower()]
+    example_images = [image for image in PRE_PUSH_STAGE_IMAGES if "example" in image.name.lower()]
+    ALL_IMAGES = PRE_PUSH_STAGE_IMAGES + COMMON_STAGE_IMAGES
+    IMAGES_TO_PUSH = [image for image in ALL_IMAGES if image.to_push and image.to_build]
+
+    pushed_images = []
+    pushed_images += process_images(standard_images, "Standard")
+    pushed_images += process_images(example_images, "Example")
+
+    assert all(image in pushed_images for image in IMAGES_TO_PUSH), "Few images could not be pushed."
+
+    # After the build, display logs/summary for all the images.
+    FORMATTER.banner("Summary")
+    show_build_info(ALL_IMAGES)
+
+    FORMATTER.banner("Errors")
+    is_any_build_failed, is_any_build_failed_size_limit = show_build_errors(ALL_IMAGES)
+
+    # From all images, filter the images that were supposed to be built and upload their metrics
+    BUILT_IMAGES = [image for image in ALL_IMAGES if image.to_build]
+
+    FORMATTER.banner("Upload Metrics")
+    upload_metrics(BUILT_IMAGES, BUILDSPEC, is_any_build_failed, is_any_build_failed_size_limit)
+
+    FORMATTER.banner("Test Env")
+    # Set environment variables to be consumed by test jobs
+    test_trigger_job = utils.get_codebuild_project_name()
+    # Tests should only run on images that were pushed to the repository
+    utils.set_test_env(
+        IMAGES_TO_PUSH, 
+        use_latest_additional_tag=True, 
+        BUILD_CONTEXT=os.getenv("BUILD_CONTEXT"), 
+        TEST_TRIGGER=test_trigger_job
+    )
+
+
+def process_images(pre_push_image_list, pre_push_image_type="Pre-push"):
+    """
+    Handles all the tasks related to a particular type of Pre Push images. It takes in the list of
+    pre push images and then builds it. After the pre-push images have been built, it extracts the 
+    corresponding common stage images for the pre-push images and builds those common stage images.
+    After the common stage images have been built, it finds outs the docker images that need to be 
+    pushed and pushes them accordingly.
+
+    Note that the common stage images should always be built after the pre-push images of a
+    particular kind. This is because the Common stage images use are built on respective 
+    Standard and Example images.
+
+    :param pre_push_image_list: list[DockerImage], list of pre-push images
+    :param pre_push_image_type: str, used to display the message on the logs
+    :return: list[DockerImage], images that were supposed to be pushed.
+    """
+    FORMATTER.banner(f"{pre_push_image_type} Build")
+    build_images(pre_push_image_list)
+
+    FORMATTER.banner(f"{pre_push_image_type} Common Build")
+    common_stage_image_list = [
+        image.corresponding_common_stage_image
+        for image in pre_push_image_list
+        if image.corresponding_common_stage_image is not None
+    ]
+    build_images(common_stage_image_list, make_dummy_boto_client=True)
+
+    FORMATTER.banner(f"{pre_push_image_type} Push Images")
+    all_images = pre_push_image_list + common_stage_image_list
+    images_to_push =  [image for image in all_images if image.to_push and image.to_build]
+    push_images(images_to_push)
+
+    FORMATTER.banner(f"{pre_push_image_type} Retagging")
+    retag_and_push_images(images_to_push)
+    return images_to_push
+
+
+def generate_common_stage_image_object(pre_push_stage_image_object, image_tag):
+    """
+    Creates a common stage image object for a pre_push stage image. If for a pre_push stage image we create a common 
+    stage image, then we do not push the pre_push stage image to the repository. Instead, we just push its common stage 
+    image to the repository. Therefore, inside the function pre_push_stage_image_object is made NON-PUSHABLE.
+
+    :param pre_push_stage_image_object: DockerImage, an object of class DockerImage
+    :return: CommonStageImage, an object of class CommonStageImage. CommonStageImage inherits DockerImage.
+    """
+    common_stage_info = deepcopy(pre_push_stage_image_object.info)
+    common_stage_info["extra_build_args"].update(
+        {"PRE_PUSH_IMAGE": pre_push_stage_image_object.ecr_url,}
+    )
+    common_stage_image_object = CommonStageImage(
+        info=common_stage_info,
+        dockerfile=os.path.join(os.sep, utils.get_root_folder_path(), "miscellaneous_dockerfiles", "Dockerfile.common"),
+        repository=pre_push_stage_image_object.repository,
+        tag=append_tag(image_tag, "multistage-common"),
+        to_build=pre_push_stage_image_object.to_build,
+        stage=constants.COMMON_STAGE,
+        additional_tags=[image_tag],
+    )
+    pre_push_stage_image_object.to_push = False
+    pre_push_stage_image_object.corresponding_common_stage_image = common_stage_image_object
+    return common_stage_image_object
+
+
+def show_build_info(images):
+    """
+    Displays the build info for a list of input images.
+
+    :param images: list[DockerImage]
+    """
+
+    if not os.path.isdir("logs"):
+        os.makedirs("logs")
+
+    for image in images:
+        image_description = f"{image.name}-{image.stage}"
+        FORMATTER.title(image_description)
+        FORMATTER.table(image.info.items())
+
+        flattened_logs = list(itertools.chain(*image.log))
+        with open(f"logs/{image_description}", "w") as fp:
+            fp.write("/n".join(flattened_logs))
+            image.summary["log"] = f"logs/{image_description}"
+        FORMATTER.table(image.summary.items())
+
+        FORMATTER.title(f"Ending Logs for {image_description}")
+        FORMATTER.print_lines(image.log[-1][-2:])
+
+
+def show_build_errors(images):
+    """
+    Iterates through each image to check if there is any image that has a failed status. In case
+    an image with a failed status is found, it raises an exception.
+
+    :param images: list[DockerImage]
+    """
+    is_any_build_failed = False
+    is_any_build_failed_size_limit = False
+
+    for image in images:
+        if image.build_status == constants.FAIL:
+            FORMATTER.title(image.name)
+            FORMATTER.print_lines(image.log[-1][-10:])
+            is_any_build_failed = True
+        else:
+            if image.build_status == constants.FAIL_IMAGE_SIZE_LIMIT:
+                is_any_build_failed_size_limit = True
+    if is_any_build_failed:
+        raise Exception("Build failed")
+    else:
+        if is_any_build_failed_size_limit:
+            FORMATTER.print("Build failed. Image size limit breached.")
+        else:
+            FORMATTER.print("No errors")
+    return is_any_build_failed, is_any_build_failed_size_limit
+
+
+def upload_metrics(images, BUILDSPEC, is_any_build_failed, is_any_build_failed_size_limit):
+    """
+    Uploads Metrics for a list of images.
+
+    :param images: list[DockerImage]
+    :param BUILDSPEC: Buildspec
+    :param is_any_build_failed: bool
+    :param is_any_build_failed_size_limit: bool
+    """
+    metrics = Metrics(
+        context=constants.BUILD_CONTEXT, region=BUILDSPEC["region"], namespace=constants.METRICS_NAMESPACE,
+    )
+    for image in images:
+        try:
+            metrics.push_image_metrics(image)
+        except Exception as e:
+            if is_any_build_failed or is_any_build_failed_size_limit:
+                raise Exception(f"Build failed.{e}")
+            else:
+                raise Exception(f"Build passed. {e}")
+
+    if is_any_build_failed_size_limit:
+        raise Exception("Build failed because of file limit")
+
+    FORMATTER.print("Metrics Uploaded")
+
+
+def build_images(images, make_dummy_boto_client=False):
+    """
+    Takes a list of images and executes their build process concurrently. 
+
+    :param images: list[DockerImage]
+    :param make_dummy_boto_client: bool, specifies if a dummy client should be declared or not.
+
+    TODO: The parameter make_dummy_boto_client should be removed when get_dummy_boto_client method is removed.
+    """
     THREADS = {}
-
     # In the context of the ThreadPoolExecutor each instance of image.build submitted
     # to it is executed concurrently in a separate thread.
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        # Parent images do not inherit from any containers built in this job
-        # Child images use one of the parent images as their base image
-        parent_images = [image for image in IMAGES if not image.is_child_image]
-        child_images = [image for image in IMAGES if image.is_child_image]
-
-        for image in parent_images:
+        #### TODO: Remove this entire if block when get_dummy_boto_client is removed ####
+        if make_dummy_boto_client:
+            get_dummy_boto_client()
+        for image in images:
             THREADS[image.name] = executor.submit(image.build)
+    # the FORMATTER.progress(THREADS) function call also waits until all threads have completed
+    FORMATTER.progress(THREADS)
 
-        # the FORMATTER.progress(THREADS) function call also waits until all threads have completed
-        FORMATTER.progress(THREADS)
 
-        for image in child_images:
-            THREADS[image.name] = executor.submit(image.build)
+#### TODO: Remove this entire method when https://github.com/boto/boto3/issues/1592 is resolved ####
+def get_dummy_boto_client():
+    """
+    Makes a dummy boto3 client to ensure that boto3 clients behave in a thread safe manner.
+    In absence of this method, the behaviour documented in https://github.com/boto/boto3/issues/1592 is observed.
+    Once https://github.com/boto/boto3/issues/1592 is resolved, this method can be removed.
 
-        # the FORMATTER.progress(THREADS) function call also waits until all threads have completed
-        FORMATTER.progress(THREADS)
+    :return: BotocoreClientSTS
+    """
+    return boto3.client("sts", region_name=os.getenv("REGION"))
 
-        FORMATTER.title("Build Logs")
 
-        if not os.path.isdir("logs"):
-            os.makedirs("logs")
+def push_images(images):
+    """
+    Takes a list of images and PUSHES them to ECR concurrently. 
 
-        for image in IMAGES:
-            FORMATTER.title(image.name)
-            FORMATTER.table(image.info.items())
-            FORMATTER.separator()
-            FORMATTER.print_lines(image.log)
-            with open(f"logs/{image.name}", "w") as fp:
-                fp.write("/n".join(image.log))
-                image.summary["log"] = f"logs/{image.name}"
+    :param images: list[DockerImage]
+    """
+    THREADS = {}
+    with concurrent.futures.ThreadPoolExecutor(max_workers=constants.MAX_WORKER_COUNT_FOR_PUSHING_IMAGES) as executor:
+        for image in images:
+            THREADS[image.name] = executor.submit(image.push_image)
+    FORMATTER.progress(THREADS)
 
-        FORMATTER.title("Summary")
+def retag_and_push_images(images):
+    """
+    Takes a list of images, retags them and pushes to the repository
 
-        for image in IMAGES:
-            FORMATTER.title(image.name)
-            FORMATTER.table(image.summary.items())
-
-        FORMATTER.title("Errors")
-        is_any_build_failed = False
-        is_any_build_failed_size_limit = False
-        for image in IMAGES:
-            if image.build_status == constants.FAIL:
-                FORMATTER.title(image.name)
-                FORMATTER.print_lines(image.log[-10:])
-                is_any_build_failed = True
-            else:
-                if image.build_status == constants.FAIL_IMAGE_SIZE_LIMIT:
-                    is_any_build_failed_size_limit = True
-        if is_any_build_failed:
-            raise Exception("Build failed")
-        else:
-            if is_any_build_failed_size_limit:
-                FORMATTER.print("Build failed. Image size limit breached.")
-            else:
-                FORMATTER.print("No errors")
-
-        FORMATTER.title("Uploading Metrics")
-        metrics = Metrics(
-            context=constants.BUILD_CONTEXT,
-            region=BUILDSPEC["region"],
-            namespace=constants.METRICS_NAMESPACE,
-        )
-        for image in IMAGES:
-            try:
-                metrics.push_image_metrics(image)
-            except Exception as e:
-                if is_any_build_failed or is_any_build_failed_size_limit:
-                    raise Exception(f"Build failed.{e}")
-                else:
-                    raise Exception(f"Build passed. {e}")
-
-        if is_any_build_failed_size_limit:
-            raise Exception("Build failed because of file limit")
-
-        FORMATTER.separator()
-
-        # Set environment variables to be consumed by test jobs
-        test_trigger_job = utils.get_codebuild_project_name()
-        utils.set_test_env(
-            IMAGES,
-            BUILD_CONTEXT=os.getenv("BUILD_CONTEXT"),
-            TEST_TRIGGER=test_trigger_job,
-        )
-
+    :param images: list[DockerImage]
+    """
+    THREADS = {}
+    with concurrent.futures.ThreadPoolExecutor(max_workers=constants.MAX_WORKER_COUNT_FOR_PUSHING_IMAGES) as executor:
+        for image in images:
+            THREADS[image.name] = executor.submit(image.push_image_with_additional_tags)
+    FORMATTER.progress(THREADS)
 
 def tag_image_with_pr_number(image_tag):
     pr_number = os.getenv("CODEBUILD_SOURCE_VERSION").replace("/", "-")
@@ -268,6 +432,16 @@ def tag_image_with_pr_number(image_tag):
 def tag_image_with_datetime(image_tag):
     datetime_suffix = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     return f"{image_tag}-{datetime_suffix}"
+
+
+def append_tag(image_tag, append_str):
+    """
+    Appends image_tag with append_str
+
+    :param image_tag: str, original image tag
+    :param append_str: str, string to be appended
+    """
+    return f"{image_tag}-{append_str}"
 
 
 def modify_repository_name_for_context(image_repo_uri, build_context):
