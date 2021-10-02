8b67e6434ea8a7b8f22b73b54dca11f743a6ac97
tejaschumbalkar <tejaschumbalkar@gmail.com>
2021-10-01 18:35:15 -0700

    address comments

--- src/utils.py
+++ src/utils.py
@@ -382,6 +382,8 @@ def build_setup(framework, device_types=None, image_types=None, py_versions=None
     if build_context == "PR":
         pr_number = os.getenv("PR_NUMBER")
         LOGGER.info(f"pr number: {pr_number}")
+        if pr_number is not None:
+            pr_number = int(pr_number)
         device_types, image_types, py_versions = pr_build_setup(pr_number, framework)
 
     if device_types != constants.ALL:
