c341ec338710d7fe5fdbdeba442264340e19db15
Wei Chu <weichu@amazon.com>
2021-10-05 14:52:57 -0700

    fix test nccl version

--- test/dlc_tests/container_tests/bin/pytorch_tests/testPyTorchNcclVersion.py
+++ test/dlc_tests/container_tests/bin/pytorch_tests/testPyTorchNcclVersion.py
@@ -13,15 +13,20 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 def test_nccl_version():
     try:
         pt_nccl = torch.cuda.nccl.version()
+        assert isinstance(pt_nccl, int) or isinstance(pt_nccl, tuple), \
+            "Error: PT NCCL version should be either int or tuple"
         find = subprocess.Popen(['find', '/usr', '-name', 'libnccl.so*'], stdout=PIPE)
         sort = subprocess.Popen(['sort', '-r'], stdin=find.stdout, stdout=PIPE)
         tail = subprocess.Popen(['head', '-n1'], stdin=sort.stdout, stdout=PIPE)
         result = subprocess.Popen(['sed', '-r', 's/^.*\.so\.//'], stdin=tail.stdout, stdout=PIPE)
         output, error = result.communicate()
-        numbers = [int(x, 10) for x in output.decode("utf-8").strip().split('.')]
-        assert len(numbers) == 3, \
+        system_nccl = [int(x, 10) for x in output.decode("utf-8").strip().split('.')]
+        assert len(system_nccl) == 3, \
             "Error: failed to parse system nccl version"
-        system_nccl = numbers[0] * 1000 + numbers[1] * 100 + numbers[2]
+        if isinstance(pt_nccl, int):
+            system_nccl = system_nccl[0] * 1000 + system_nccl[1] * 100 + system_nccl[2]
+        else:
+            pt_nccl = list(pt_nccl)
         assert pt_nccl == system_nccl, \
             "Error: PT NCCL version does not match system NCCL: {} vs {}".format(pt_nccl, system_nccl)
     except Exception as excp:
