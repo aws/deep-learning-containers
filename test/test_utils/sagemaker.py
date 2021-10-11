bffc1e18ed486398c694eb278dbaf9e611c8a4c0
Sergey Togulev <togulev@amazon.com>
2021-10-11 13:06:40 -0700

    Added -p to mkdir

--- test/test_utils/sagemaker.py
+++ test/test_utils/sagemaker.py
@@ -327,7 +327,7 @@ def execute_sagemaker_remote_tests(image):
         context.run(f"virtualenv {tag}")
         with context.prefix(f"source {tag}/bin/activate"):
             # context.run("pip install -r requirements.txt", warn=True)
-            context.run("mkdir /.pytest_cache && mkdir /.pytest_cache/v && mkdir /.pytest_cache/v/cache")
+            context.run("mkdir -p /.pytest_cache && mkdir -p /.pytest_cache/v && mkdir -p /.pytest_cache/v/cache")
             s3 = boto3.client("s3")
             s3.download_file('dlc-test-execution-results-669063966089', 'lastfailed', '/.pytest_cache/v/cache/lastfailed')
             res = context.run("pytest --cache-show", warn=True)
