e00970be6e129b8fed14de9596db4c7ee0b98db7
arjkesh <33526713+arjkesh@users.noreply.github.com>
2021-10-22 12:07:23 -0700

    Address review changes

--- test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
+++ test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
@@ -81,6 +81,7 @@ def test_pytorch_linear_regression_cpu(pytorch_training, ec2_connection, cpu_onl
 def test_pytorch_train_dgl_gpu(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     image_cuda_version = get_cuda_version_from_tag(pytorch_training)
+    # TODO: Remove when DGL with cuda 11.3 support is released
     if Version(image_framework_version) == Version("1.10") and image_cuda_version == "cu113":
         pytest.skip("DGL CUDA 11.3 was not introduced in PyTorch 1.10")
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
@@ -92,6 +93,10 @@ def test_pytorch_train_dgl_gpu(pytorch_training, ec2_connection, gpu_only, py3_o
 @pytest.mark.model("gcn")
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
 def test_pytorch_train_dgl_cpu(pytorch_training, ec2_connection, cpu_only, py3_only):
+    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
+    # TODO: Remove when DGL 0.7.1 is released
+    if Version(image_framework_version) == Version("1.10"):
+        pytest.skip("Official DGL releases do not yet support PyTorch 1.10")
     execute_ec2_training_test(ec2_connection, pytorch_training, PT_DGL_CMD)
 
 
