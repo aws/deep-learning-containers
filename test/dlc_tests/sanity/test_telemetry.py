cab3ee6df2fd7f9cfc48a0a7e8a02b6474a0a2e4
tejaschumbalkar <tejaschumbalkar@gmail.com>
2021-11-11 19:19:49 -0800

    revert temp config

--- test/dlc_tests/sanity/test_telemetry.py
+++ test/dlc_tests/sanity/test_telemetry.py
@@ -70,7 +70,7 @@ def test_telemetry_instance_tag_success_cpu(cpu, ec2_client, ec2_instance, ec2_c
 @pytest.mark.integration("telemetry")
 @pytest.mark.parametrize("ec2_instance_type", ["c6g.4xlarge"], indirect=True)
 @pytest.mark.parametrize("ec2_instance_ami", [test_utils.AML2_CPU_ARM64_US_WEST_2], indirect=True)
-def test_telemetry_instance_tag_success_cpu(cpu, ec2_client, ec2_instance, ec2_connection, graviton_compatible_only):
+def test_telemetry_instance_tag_success_graviton_cpu(cpu, ec2_client, ec2_instance, ec2_connection, graviton_compatible_only):
     _run_tag_success(cpu, ec2_client, ec2_instance, ec2_connection)
 
 
