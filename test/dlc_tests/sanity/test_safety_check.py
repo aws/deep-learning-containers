09501301ecfaa928109cc859a731f2a1b6815fa6
jeet <jspatil@amazon.com>
2021-11-18 10:03:04 -0800

    fix sanituy

--- test/dlc_tests/sanity/test_safety_check.py
+++ test/dlc_tests/sanity/test_safety_check.py
@@ -45,6 +45,7 @@ IGNORE_SAFETY_IDS = {
                 # CVE vulnerabilities in TF < 2.7.0 ignoring to be able to build TF containers
                 "42098",
                 "42062",
+                "41994"
             ],
         },
         "inference": {
