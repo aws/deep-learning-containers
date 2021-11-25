2d314095b5362c2878f96af098fa806a846dfe68
arjkesh <33526713+arjkesh@users.noreply.github.com>
2021-11-24 19:07:31 -0800

    [graviton] Add arch_type-specific env variable for graviton EC2 tests (#1550)
    * [graviton] Update mainline infra for graviton tests
    
    * update conftest

--- test/dlc_tests/eks/tensorflow/inference/test_eks_tensorflow_inference.py
+++ test/dlc_tests/eks/tensorflow/inference/test_eks_tensorflow_inference.py
@@ -60,6 +60,8 @@ def test_eks_tensorflow_half_plus_two_inference(tensorflow_inference):
     __test_eks_tensorflow_half_plus_two_inference(tensorflow_inference)
 
 
+# TODO: Enable after adding EKS infrastructure to support graviton
+@pytest.mark.skip(reason="EKS graviton tests require further development")
 @pytest.mark.model("half_plus_two")
 def test_eks_tensorflow_half_plus_two_inference_graviton(tensorflow_inference_graviton):
     __test_eks_tensorflow_half_plus_two_inference(tensorflow_inference_graviton)
@@ -115,7 +117,8 @@ def test_eks_tensorflow_albert(tensorflow_inference):
     __test_eks_tensorflow_albert(tensorflow_inference)
 
 
-@pytest.mark.skipif(not test_utils.is_nightly_context(), reason="Running additional model in nightly context only")
+# TODO: Enable after adding EKS infrastructure to support graviton
+@pytest.mark.skip(reason="EKS graviton tests require further development")
 @pytest.mark.model("albert")
 def test_eks_tensorflow_albert_graviton(tensorflow_inference_graviton):
     __test_eks_tensorflow_albert(tensorflow_inference_graviton)
