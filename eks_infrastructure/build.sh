c7b5dfdeef2e2e1bb3df853c0a7b15785a3a2e30
tejaschumbalkar <tejaschumbalkar@gmail.com>
2021-12-10 15:22:31 -0800

    nit change

--- eks_infrastructure/build.sh
+++ eks_infrastructure/build.sh
@@ -110,7 +110,7 @@ EKS_VERSION=$(jq -r '.eks_version' build_param.json)
 CLUSTER_AUTOSCALAR_IMAGE_VERSION=$(jq -r '.cluster_autoscalar_image_version' build_param.json)
 
 # Create a cluster if the context is predeploy and operation is other than create
-if [ "${CONTEXTS}" = "beta" ] && [ "${OPERATION}" != "create" ]; then
+if [ "${CONTEXTS}" = "BETA" ] && [ "${OPERATION}" != "create" ]; then
   echo "Create a cluster inorder to perform operations other than creating cluster"
   create_cluster
 fi
@@ -138,8 +138,8 @@ new_operation)
   ;;
 esac
 
-# Cleanup the EKS cluster if the context is predeploy
-if [ "${CONTEXT}" = "beta" ]; then
+# Cleanup the EKS cluster if the context is BETA
+if [ "${CONTEXT}" = "BETA" ]; then
   echo "Delete the pre-deploy cluster"
   delete_cluster
 fi
