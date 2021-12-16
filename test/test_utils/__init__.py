b14f5cf5ca0f81b40dcc7b2ec93b1e99950d6335
arjkesh <33526713+arjkesh@users.noreply.github.com>
2021-12-16 14:47:11 -0800

    [canary] Update huggingface canary logic (#1589)

--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -829,8 +829,8 @@ def parse_canary_images(framework, region):
         "tensorflow2": rf"tf{customer_type_tag}-(2.\d+)",
         "mxnet": rf"mx{customer_type_tag}-(\d+.\d+)",
         "pytorch": rf"pt{customer_type_tag}-(\d+.\d+)",
-        "huggingface_pytorch": r"hf-pt-(\d+.\d+)",
-        "huggingface_tensorflow": r"hf-tf-(\d+.\d+)",
+        "huggingface_pytorch": r"hf-\S*pt-(\d+.\d+)",
+        "huggingface_tensorflow": r"hf-\S*tf-(\d+.\d+)",
         "autogluon": r"ag-(\d+.\d+)",
     }
 
@@ -853,10 +853,10 @@ def parse_canary_images(framework, region):
             elif "inf" in tag_str:
                 versions_counter[version]["inf"] = True
 
-    # Adding huggingface here since we dont have inference HF containers now
     versions = []
     for v, inf_train in versions_counter.items():
-        if inf_train["inf"] and inf_train["tr"]:
+        # Earlier versions of huggingface did not have inference
+        if (inf_train["inf"] and inf_train["tr"]) or framework.startswith("huggingface"):
             versions.append(v)
 
     # Sort ascending to descending, use lambda to ensure 2.2 < 2.15, for instance
