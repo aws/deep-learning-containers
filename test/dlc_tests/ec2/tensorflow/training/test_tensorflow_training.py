a0aa8fbb3ffca5347f2a2fce54435868e3d66833
omrialmog <omri.almog@intel.com>
2022-02-12 10:07:23 -0800

    Habana DLC Perf/TestSuite TF/PT tests -- gaudi test suite (#1567)
    * Habana DLC Perf/TestSuite TF/PT tests
    * Add Habana DLAMI Tensorflow Performance Benchmarks
    * Add Habana DLAMI PyTorch Performance Benchmarks
    * Add Habana DLAMI Tensorflow Test Suite
    * Add Habana DLAMI PyTorch Test Suite
    
    * Apply gaudi-test-suite to test bert, rn50, maskrcnn, framework, etc.
    
    * Test cleanup and exit code fix
    
    * Fix gaudi-test-suite branch name
    
    * To extract the Throughput correctly
    
    * Update scripts for 1.2.0 release
    
    * Add tf requirement installation
    
    * Remove comments
    
    * fix test scripts
    
    * enable habana mode
    
    * configure git creds
    
    * build habana images
    
    * adjust test dir
    
    * run benchmark tests
    
    * fix docker command
    
    * update pt binary
    
    * build new image
    
    * use dedicated github granch
    
    * nit change
    
    * pin pt setuptools
    
    * pin setuptools
    
    * fix log file
    
    * fix benchmark test
    
    * awscli support
    
    * fix dep check
    
    * nit changes
    
    * run benchmark test
    
    * adjust pytest timeout for habana
    
    * turn off benchmark mode
    
    * add habana fixture
    
    * run benchmark test
    
    * increase timeout
    
    * revert temp config
    
    * increase timeout to 5hr
    
    * build image
    
    * run benchmark test
    
    * revert temp config
    
    Co-authored-by: Sergey Togulev <34056697+SergTogul@users.noreply.github.com>
    Co-authored-by: Buke Ao <bukeao@bukeao-vm.habana-labs.com>
    Co-authored-by: Anny Chung <achung@habana.ai>
    Co-authored-by: tejaschumbalkar <tejaschumbalkar@gmail.com>
    Co-authored-by: Tejas Chumbalkar <34728580+tejaschumbalkar@users.noreply.github.com>
    Co-authored-by: Wei Chu <weichu@amazon.com>

--- test/dlc_tests/ec2/tensorflow/training/test_tensorflow_training.py
+++ test/dlc_tests/ec2/tensorflow/training/test_tensorflow_training.py
@@ -5,7 +5,7 @@ import pytest
 import test.test_utils as test_utils
 import test.test_utils.ec2 as ec2_utils
 
-from test.test_utils import CONTAINER_TESTS_PREFIX, LOGGER, is_tf_version, get_python_invoker
+from test.test_utils import CONTAINER_TESTS_PREFIX, UBUNTU_18_HPU_DLAMI_US_WEST_2, LOGGER, is_tf_version, get_python_invoker
 from test.test_utils.ec2 import execute_ec2_training_test, get_ec2_instance_type
 
 
@@ -22,6 +22,7 @@ TF_TENSORBOARD_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "testTensorBoard")
 TF_ADDONS_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "testTFAddons")
 TF_DATASERVICE_TEST_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "testDataservice")
 TF_DATASERVICE_DISTRIBUTE_TEST_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "testDataserviceDistribute")
+TF_HABANA_TEST_SUITE_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "testHabanaTFSuite")
 
 TF_EC2_SINGLE_GPU_INSTANCE_TYPE = get_ec2_instance_type(
     default="p3.2xlarge", processor="gpu", filter_function=ec2_utils.filter_only_single_gpu
@@ -262,9 +263,9 @@ def test_tensorflow_distribute_dataservice_gpu(
     run_data_service_test(ec2_connection, ec2_instance_ami, tensorflow_training, TF_DATASERVICE_DISTRIBUTE_TEST_CMD)
 
 
-# Placeholder for habana test
+@pytest.mark.integration('tensorflow-dataservice-distribute-test')
 @pytest.mark.model("N/A")
-#@pytest.mark.parametrize("ec2_instance_type", TF_EC2_HPU_INSTANCE_TYPE, indirect=True)
-#@pytest.mark.parametrize("ec2_instance_ami", [test_utils.HPU_AL2_DLAMI], indirect=True)
-def test_tensorflow_standalone_hpu(tensorflow_training_habana):
-    pass
+@pytest.mark.parametrize("ec2_instance_type", TF_EC2_HPU_INSTANCE_TYPE, indirect=True)
+@pytest.mark.parametrize("ec2_instance_ami", [UBUNTU_18_HPU_DLAMI_US_WEST_2], indirect=True)
+def test_tensorflow_standalone_hpu(tensorflow_training_habana, ec2_connection, upload_habana_test_artifact):
+    execute_ec2_training_test(ec2_connection, tensorflow_training_habana, TF_HABANA_TEST_SUITE_CMD, container_name="ec2_training_habana_tensorflow_container")
