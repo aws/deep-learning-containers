61af6727e3f7998145c159fdb45396fdbb1f5306
Lai Wei <royweilai@gmail.com>
2022-02-23 10:30:42 -0800

    update test to smddp 1.4.0

--- test/sagemaker_tests/pytorch/training/resources/smdataparallel/smdataparallel_throughput.py
+++ test/sagemaker_tests/pytorch/training/resources/smdataparallel/smdataparallel_throughput.py
@@ -16,8 +16,10 @@ import time
 import os
 import sys
 import argparse
+import smdistributed.dataparallel.torch.torch_smddp
 
 THRESHOLD = 28.0
+NUM_LOCAL_GPUS = 8
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--size",
@@ -54,50 +56,39 @@ parser.add_argument('--nccl',
                     help="Run nccl or herring")
 parser.set_defaults(fp32=False)
 parser.set_defaults(nccl=False)
-args, unknown = parser.parse_known_args()
-print(args)
+args = parser.parse_args()
 
 size, rank, local_size, local_rank = None, None, None, None
-if args.nccl:
-    if "MASTER_ADDR" not in os.environ:
-        assert "SMDATAPARALLEL_SERVER_ADDR" in os.environ
-        os.environ["MASTER_ADDR"] = os.environ["SMDATAPARALLEL_SERVER_ADDR"]
-        os.environ["MASTER_PORT"] = str(
-            int(os.environ["SMDATAPARALLEL_SERVER_PORT"]) + 2)
-    size = int(os.getenv("OMPI_COMM_WORLD_SIZE"))
-    rank = int(os.getenv("OMPI_COMM_WORLD_RANK"))
-    local_size = int(os.getenv("OMPI_COMM_WORLD_LOCAL_SIZE"))
-    local_rank = int(os.getenv("OMPI_COMM_WORLD_LOCAL_RANK"))
 
-    import torch.distributed as dist
+if "MASTER_ADDR" not in os.environ:
+    assert "SMDATAPARALLEL_SERVER_ADDR" in os.environ
+    os.environ["MASTER_ADDR"] = os.environ["SMDATAPARALLEL_SERVER_ADDR"]
+    os.environ["MASTER_PORT"] = str(
+        int(os.environ["SMDATAPARALLEL_SERVER_PORT"]) + 2)
+size = int(os.getenv("OMPI_COMM_WORLD_SIZE"))
+rank = int(os.getenv("OMPI_COMM_WORLD_RANK"))
+local_size = int(os.getenv("OMPI_COMM_WORLD_LOCAL_SIZE"))
+local_rank = int(os.getenv("OMPI_COMM_WORLD_LOCAL_RANK"))
+
+import torch.distributed as dist
+if args.nccl:
     dist.init_process_group("nccl",
                             world_size=size,
                             rank=rank,
                             store=None,
                             group_name='')
 else:
-    import smdistributed.dataparallel.torch.distributed as dist
-    import smdistributed.dataparallel as h
-    import smddpcommon as hm
-
-    dist.init_process_group()
-    hm.setBucketSize(args.bucket_size * 1024 * 1024)
-    size = dist.get_world_size()
-    rank = dist.get_rank()
-    local_size = 8
-    local_rank = dist.get_local_rank()
-    HDTYPE = hm.herringFloat16
-    if args.fp32:
-        HDTYPE = hm.herringFloat32
+    dist.init_process_group("smddp",
+                            world_size=size,
+                            rank=rank,
+                            store=None,
+                            group_name='')
 
 torch.cuda.set_device(local_rank)
 
 if rank == 0:
     print("NUM WORKERS", size, "WSIZE/RANK/LSIZE/LRANK:", size, rank,
           local_size, local_rank)
-
-DTYPE = torch.float16
-DTSIZE = 2
 
 if args.fp32:
     DTYPE = torch.float32
@@ -110,8 +101,7 @@ bandwidth = []
 artime = []
 
 
-def test(warmup=False, size=104857600, num_tensors=100, iterations=1,
-         first_iteration=False):
+def test(warmup=False, size=104857600, num_tensors=100, iterations=1):
     if rank == 0: print("Warmup  " if warmup else "\n", end="\t")
 
     # SETUP
@@ -133,38 +123,24 @@ def test(warmup=False, size=104857600, num_tensors=100, iterations=1,
 
         for i, test_array in enumerate(tests):
             if i < len(tests):
-                if not args.nccl:
-                    if (first_iteration):
-                        out = test_array.data_ptr()
-                    else:
-                        out = 0
-                    results.append(
-                        hm.allReduce(test_array.data_ptr(),
-                                     out, HDTYPE,
-                                     test_array.numel(), i, len(tests),
-                                     h._get_id_for_herring_task()).request)
-                else:
-                    results.append(dist.all_reduce(test_array, async_op=True))
+                results.append(dist.all_reduce(test_array, async_op=True))
 
         for i, res in enumerate(results):
-            if not args.nccl:
-                hm.wait(res)
-            else:
-                res.wait()
+            res.wait()
 
         torch.cuda.synchronize()  # ????^)*(&*&***??????
+        tdif = (time.time() - before)
+        B_IN_GB = 1024 * 1024 * 1024
+
         if rank == 0:
-            tdif = (time.time() - before)
             print("[%2d/%d %s %d %s]" %
                   (k, iterations, "%5.3fs" % tdif, size, "%5.2fGB/s" %
-                   (size * num_tensors / 1024 / 1024 / 1024 /
-                    (time.time() - before))),
+                   (size * num_tensors / B_IN_GB / tdif)),
                   end="\t" if k % 4 != 3 else "\n\t")
             sys.stdout.flush()
-            if warmup: continue
-            bandwidth.append(size * num_tensors / 1024 / 1024 / 1024 /
-                             (time.time() - before))
-            artime.append(tdif)
+        if warmup: continue
+        bandwidth.append(size * num_tensors / B_IN_GB / tdif)
+        artime.append(tdif)
 
 
 get_size = lambda: int(args.size * 1024 * 1024)
@@ -172,9 +148,7 @@ test(
     True,
     size=get_size(),
     num_tensors=args.num_tensors,
-    iterations=1,
-    first_iteration=True,
-)
+    iterations=1)
 test(True,
      size=get_size(),
      num_tensors=args.num_tensors,
