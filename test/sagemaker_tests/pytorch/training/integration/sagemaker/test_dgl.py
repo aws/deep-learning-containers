74e83c193319b6285ea19e7470efdb62f9201148
Daiming Yang <ydaiming@outlook.com>
2022-03-06 16:57:36 -0800

    enable DGL tests for PT1.11 and ping DGL 0.8.0

--- test/sagemaker_tests/pytorch/training/integration/sagemaker/test_dgl.py
+++ test/sagemaker_tests/pytorch/training/integration/sagemaker/test_dgl.py
@@ -38,8 +38,8 @@ DGL_SCRIPT_PATH = os.path.join(DGL_DATA_PATH, "gcn.py")
 def test_dgl_gcn_training_cpu(ecr_image, sagemaker_regions, instance_type):
     # TODO: Remove when DGL gpu test on ecs get fixed
     _, image_framework_version = get_framework_and_version_from_tag(ecr_image)
-    if Version(image_framework_version) >= Version("1.10"):
-        pytest.skip("ecs test for DGL gpu fails since pt 1.10")
+    if Version(image_framework_version) == Version("1.10"):
+        pytest.skip("ecs test for DGL gpu fails for pt 1.10")
 
     instance_type = instance_type or "ml.c4.xlarge"
     function_args = {
@@ -58,8 +58,8 @@ def test_dgl_gcn_training_gpu(ecr_image, sagemaker_regions, instance_type):
     image_cuda_version = get_cuda_version_from_tag(ecr_image)
     
     # TODO: Remove when DGL gpu test on ecs get fixed
-    if Version(image_framework_version) >= Version("1.10") and image_cuda_version == "cu113":
-        pytest.skip("ecs test for DGL gpu fails since pt 1.10")
+    if Version(image_framework_version) == Version("1.10") and image_cuda_version == "cu113":
+        pytest.skip("ecs test for DGL gpu fails for pt 1.10")
 
     if Version(image_framework_version) == Version("1.6") and image_cuda_version == "cu110":
         pytest.skip("DGL does not support CUDA 11 for PyTorch 1.6")
