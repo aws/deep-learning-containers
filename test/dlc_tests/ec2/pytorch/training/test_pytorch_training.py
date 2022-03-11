d14548edc5751da1f47825db7101604b94cf7bf6
Daiming Yang <ydaiming@outlook.com>
2022-03-11 09:34:05 -0800

    fix typo in training tests

--- test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
+++ test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
@@ -233,7 +233,7 @@ def test_pytorch_s3_plugin_cpu(pytorch_training, ec2_connection, cpu_only, ec2_i
 @pytest.mark.model("N/A")
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
 def test_pytorch_training_torchaudio_gpu(pytorch_training, ec2_connection, gpu_only, ec2_instance_type, pt111_and_above_only):
-    _, image_framework_version = get_framework_and_version_from_tag(pytorch_inference)
+    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     if Version(image_framework_version) < Version("1.11"):
         pytest.skip("torchdata not enabled before 1.11")
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
@@ -245,7 +245,7 @@ def test_pytorch_training_torchaudio_gpu(pytorch_training, ec2_connection, gpu_o
 @pytest.mark.model("N/A")
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
 def test_pytorch_training_torchaudio_cpu(pytorch_training, ec2_connection, cpu_only, ec2_instance_type, pt111_and_above_only):
-    _, image_framework_version = get_framework_and_version_from_tag(pytorch_inference)
+    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     if Version(image_framework_version) < Version("1.11"):
         pytest.skip("torchdata not enabled before 1.11")
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
@@ -257,7 +257,7 @@ def test_pytorch_training_torchaudio_cpu(pytorch_training, ec2_connection, cpu_o
 @pytest.mark.model("N/A")
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
 def test_pytorch_training_torchdata_gpu(pytorch_training, ec2_connection, gpu_only, ec2_instance_type, pt111_and_above_only):
-    _, image_framework_version = get_framework_and_version_from_tag(pytorch_inference)
+    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     if Version(image_framework_version) < Version("1.11"):
         pytest.skip("torchdata not enabled before 1.11")
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
@@ -269,7 +269,7 @@ def test_pytorch_training_torchdata_gpu(pytorch_training, ec2_connection, gpu_on
 @pytest.mark.model("N/A")
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
 def test_pytorch_training_torchdata_cpu(pytorch_training, ec2_connection, cpu_only, ec2_instance_type, pt111_and_above_only):
-    _, image_framework_version = get_framework_and_version_from_tag(pytorch_inference)
+    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     if Version(image_framework_version) < Version("1.11"):
         pytest.skip("torchdata not enabled before 1.11")
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
