4654a8ea7296c54dcb60cd72f6ec722a130efb2c
Shantanu Tripathi <shantanutripathi237@gmail.com>
2022-03-15 16:09:21 -0700

    Add COPY command to copy the dlc_container file

--- test/testrunner.py
+++ test/testrunner.py
@@ -352,7 +352,7 @@ def main():
                 context.run("git clone https://github.com/HabanaAI/gaudi-test-suite.git")
                 context.run("tar -c -f gaudi-test-suite.tar.gz gaudi-test-suite")
 
-            pytest_cmd += ["--reruns=1", "--reruns-delay=10"]
+            # pytest_cmd += ["--reruns=1", "--reruns-delay=10"]
         if is_pr_context():
             if specific_test_type == "eks":
                 pytest_cmd.append("--timeout=2340")
