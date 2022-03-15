b3289f16bb610e57462d6392f23f8ddecd53408d
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2022-03-15 14:56:04 -0700

    Fix 44717

--- test/dlc_tests/sanity/test_safety_check.py
+++ test/dlc_tests/sanity/test_safety_check.py
@@ -45,6 +45,7 @@ IGNORE_SAFETY_IDS = {
                 # CVE vulnerabilities in TF 2.6 ignoring to be able to build TF containers
                 "44715",
                 "44716",
+                "44717",
                 "43453",
                 # CVE vulnerabilities in TF < 2.7.0 ignoring to be able to build TF containers
                 "42098",
