7eb998482085169637c8b61523599377a7ed47b6
philschmid <schmidphilipp1995@gmail.com>
2022-03-25 21:17:48 +0100

    changed instance type to ml.p3.2xlarge

--- test/sagemaker_tests/huggingface/inference/integration/sagemaker/test_speech_model.py
+++ test/sagemaker_tests/huggingface/inference/integration/sagemaker/test_speech_model.py
@@ -39,7 +39,7 @@ def test_speech_model_cpu(sagemaker_session, framework_version, ecr_image, insta
 @pytest.mark.processor("gpu")
 @pytest.mark.gpu_test
 def test_speech_model_gpu(sagemaker_session, framework_version, ecr_image, instance_type, region):
-    instance_type = instance_type or "ml.p2.xlarge"
+    instance_type = instance_type or "ml.p3.2xlarge"
     try:
         _test_speech_model(sagemaker_session, framework_version, ecr_image, instance_type, model_dir)
     except Exception as e:
