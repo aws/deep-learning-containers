19f46c6f415d184f286e4796e557f8f6e5eeb952
tejaschumbalkar <tejaschumbalkar@gmail.com>
2022-03-25 13:22:59 -0700

    reorganize files

--- eks_infrastructure/eks_cleanup/eks_cleanup_job.py
+++ eks_infrastructure/eks_cleanup/eks_cleanup_job.py
@@ -2,11 +2,12 @@ from kubernetes import client, config
 from datetime import datetime
 from invoke import run
 import pytz
+import boto3
 
 JOB_TIMEOUT = 3
 EKS_CLUSTERS = ["mxnet1-PR"]
-#EKS_CLUSTERS = ["mxnet-PR", "pytorch-PR", "tensorflow-PR", "mxnet-MAINLINE", "pytorch-MAINLINE", "tensorflow-MAINLINE"]
-EKS_CLUSTER_MANAGER_ROLE = "arn:aws:iam::332057208146:role/clusterManagerRole"
+# EKS_CLUSTERS = ["mxnet-PR", "pytorch-PR", "tensorflow-PR", "mxnet-MAINLINE", "pytorch-MAINLINE", "tensorflow-MAINLINE"]
+EKS_CLUSTER_MANAGER_ROLE_NAME = "clusterManagerRole"
 
 
 def get_run_time(creation_time):
@@ -14,8 +15,9 @@ def get_run_time(creation_time):
     diff = current_time - creation_time
     days, seconds = diff.days, diff.seconds
     hours = days * 24 + seconds // 3600
-    
+
     return hours
+
 
 def delete_resources(list_item, k8s_api, job_type, namespace):
 
@@ -25,7 +27,7 @@ def delete_resources(list_item, k8s_api, job_type, namespace):
         # Do not delete the kubeflow mxnet operator
         if item_name == "mxnet-operator":
             continue
-    
+
         hours = get_run_time(item.metadata.creation_timestamp)
 
         if hours >= JOB_TIMEOUT:
@@ -47,16 +49,20 @@ def run_cron_job():
     delete_resources(list_pod_default, core_v1_api, "pod", "default")
 
 
-
-
 def main():
-    # Handle k8s context switch
+
+    sts_client = boto3.client("sts")
+    account_id = sts_client.get_caller_identity().get("Account")
+    EKS_CLUSTER_MANAGER_ROLE = f"arn:aws:iam::{account_id}:role/{EKS_CLUSTER_MANAGER_ROLE_NAME}"
+
     for cluster in EKS_CLUSTERS:
-        #run(f"aws eks update-kubeconfig --name {cluster} --role-arn {EKS_CLUSTER_MANAGER_ROLE}")
-        run(f"eksctl utils write-kubeconfig --cluster {cluster} --authenticator-role-arn {EKS_CLUSTER_MANAGER_ROLE} --region us-west-2")
+        run(
+            f"eksctl utils write-kubeconfig --cluster {cluster} --authenticator-role-arn {EKS_CLUSTER_MANAGER_ROLE} --region us-west-2"
+        )
         config.load_kube_config()
         _, active_context = config.list_kube_config_contexts()
         print(f"EKS Cluster {active_context['name']}")
-        #run_cron_job()
+        # run_cron_job()
+
 
 main()
