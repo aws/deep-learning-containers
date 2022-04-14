1f90d31e162cda6050f0f237c48f0f939d792d86
Kevin Yang <kevyng@amazon.com>
2022-04-14 22:37:38 +0000

    update anaconda sanity test

--- test/dlc_tests/sanity/test_anaconda.py
+++ test/dlc_tests/sanity/test_anaconda.py
@@ -12,8 +12,16 @@ def test_repo_anaconda_not_present(image):
     container_name = test_utils.get_container_name("anaconda", image)
     test_utils.start_container(container_name, image, ctx)
 
-    run_out = test_utils.run_cmd_on_container(container_name, ctx, "conda list --explicit | grep repo.anaconda.com")
-    output = run_out.stdout.strip()
-    if output:
-        raise RuntimeError(f"Image contains packages installed from repo.anaconda.com: {output}")
+    # First check to see if image has conda installed, if not, skip test since no packages installed from conda present
+    conda_present = test_utils.run_cmd_on_container(container_name, ctx, "find . -name conda").stdout.strip()
+    if not conda_present:
+        pytest.skip("Image does not have conda installed, skipping test.")
+    
+    # Commands are split in 2 because if warn=True, then even if first command fails silently, no error is raised
+    conda_list_output = test_utils.run_cmd_on_container(container_name, ctx, "conda list --explicit > repo_list.txt", warn=True).stdout.strip()
+    if not conda_list_output:
+        raise RuntimeError("Error due to conda list --explicit command")
 
+    grep_result = test_utils.run_cmd_on_container(container_name, ctx, "grep repo.anaconda.com repo_list.txt", warn=True).stdout.strip()
+    if grep_result:
+        raise RuntimeError(f"Image contains packages installed from repo.anaconda.com: {grep_result}")
