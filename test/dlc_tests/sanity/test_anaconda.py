52b03e92958006c57c2962c93790f9181bcad019
arjkesh <33526713+arjkesh@users.noreply.github.com>
2022-04-14 11:21:01 -0700

    Update test_anaconda.py

--- test/dlc_tests/sanity/test_anaconda.py
+++ test/dlc_tests/sanity/test_anaconda.py
@@ -2,14 +2,18 @@ import pytest
 from invoke.context import Context
 from test import test_utils
 
+
+@pytest.mark.usefixtures("sagemaker")
 @pytest.mark.model("N/A")
+@pytest.mark.integration("anaconda_removal")
 def test_repo_anaconda_not_present(image):
     """ Test to see if all packages installed in the image do not come from repo.anaconda.com """
     ctx = Context()
     container_name = test_utils.get_container_name("anaconda", image)
     test_utils.start_container(container_name, image, ctx)
 
-    output = test_utils.run_cmd_on_container(container_name, ctx, "conda list --explicit | grep repo.anaconda.com").stdout.strip()
+    run_out = test_utils.run_cmd_on_container(container_name, ctx, "conda list --explicit | grep repo.anaconda.com")
+    output = run_out.stdout.strip()
     if output:
-        pytest.fail(f"Image contains packages installed from repo.anaconda.com: {output}")
+        raise RuntimeError(f"Image contains packages installed from repo.anaconda.com: {output}")
 
