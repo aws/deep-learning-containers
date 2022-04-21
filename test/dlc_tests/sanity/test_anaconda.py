d7f156a0108b8af216fffd49ee2d47e4031c1adb
kevinyang8 <40340762+kevinyang8@users.noreply.github.com>
2022-04-21 16:33:18 -0700

    [test] Remove install from repo.anaconda.com for PT 1.11 (E3) images (#1812)
    * remove install from repo.anaconda.com from PT 1.11 images
    
    * update training images and developer config file
    
    * add sanity test to check repo.anaconda.com present in image
    
    * fix env variable
    
    * remove CONDA_PREFIX environment variable due to problems with mamba installation
    
    * Update dlc_developer_config.toml
    
    * Update test_anaconda.py
    
    * update anaconda sanity test
    
    * update anaconda test:
    
    * disable build in toml
    
    * address comments for test and update black version
    
    * add mamba version arg
    
    * fix env var
    
    * fix mamba arg
    
    * remove whitespace and make mamba version arg consistent
    
    * add to runtime error message
    
    * revert developer config toml
    
    * Update test/dlc_tests/sanity/test_anaconda.py
    
    Co-authored-by: Sai Parthasarathy Miduthuri <54188298+saimidu@users.noreply.github.com>
    
    * set datetime to true
    
    Co-authored-by: Kevin Yang <kevyng@amazon.com>
    Co-authored-by: arjkesh <33526713+arjkesh@users.noreply.github.com>
    Co-authored-by: Sai Parthasarathy Miduthuri <54188298+saimidu@users.noreply.github.com>

---
+++ test/dlc_tests/sanity/test_anaconda.py
@@ -0,0 +1,33 @@
+import pytest
+from invoke.context import Context
+from test import test_utils
+
+
+@pytest.mark.usefixtures("sagemaker")
+@pytest.mark.model("N/A")
+@pytest.mark.integration("anaconda_removal")
+def test_repo_anaconda_not_present(image):
+    """Test to see if all packages installed in the image do not come from repo.anaconda.com"""
+    try:
+        ctx = Context()
+        container_name = test_utils.get_container_name("anaconda", image)
+        test_utils.start_container(container_name, image, ctx)
+
+        # First check to see if image has conda installed, if not, skip test since no packages installed from conda present
+        conda_present = test_utils.run_cmd_on_container(container_name, ctx, "find . -name conda").stdout.strip()
+        if not conda_present:
+            pytest.skip(f"Image {image} does not have conda installed, skipping test.")
+
+        # Commands are split in 2 because if warn=True, then even if first command fails silently, no error is raised
+        test_utils.run_cmd_on_container(container_name, ctx, "conda list --explicit > repo_list.txt")
+
+        grep_result = test_utils.run_cmd_on_container(
+            container_name, ctx, "grep repo.anaconda.com repo_list.txt", warn=True
+        ).stdout.strip()
+        if grep_result:
+            raise RuntimeError(
+                f"Image {image} contains packages installed from repo.anaconda.com. "
+                f"Please ensure that these packages are obtained through conda-forge or other alternatives: {grep_result}"
+            )
+    finally:
+        test_utils.stop_and_remove_container(container_name, ctx)
