7bc3d503c109196a081588bedcbcc955df8bd7b4
Mike Schneider <xncqr@amazon.com>
2022-05-16 23:13:36 +0000

    get only graviton images for graviton ARCH

--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -929,9 +929,24 @@ def parse_canary_images(framework, region):
                     f"{registry}.dkr.ecr.{region}.amazonaws.com/autogluon-training:{fw_version}-cpu-{py_version}",
                 ],
             }
+            # Graviton machines are completely different ARCH (ARM) and should only pull graviton images
+            graviton_images = {
+                "tensorflow": [
+                    f"{registry}.dkr.ecr.{region}.amazonaws.com/tensorflow-inference-graviton:{fw_version}-cpu-{py_version}",
+                ],
+                "mxnet": [
+                    f"{registry}.dkr.ecr.{region}.amazonaws.com/mxnet-inference-graviton:{fw_version}-cpu-{py_version}",
+                ],
+                "pytorch": [
+                    f"{registry}.dkr.ecr.{region}.amazonaws.com/pytorch-inference-graviton:{fw_version}-cpu-{py_version}",
+                ],
+            }
             # E3 Images have an additional "e3" tag to distinguish them from the regular "sagemaker" tag
+            # Only get graviton images for graviton build systems
             if customer_type == "e3":
                 dlc_images += [f"{img}-e3" for img in images[framework]]
+            elif os.getenv("ARCH_TYPE") == "graviton"
+                dlc_images += graviton_images[framework]
             else:
                 dlc_images += images[framework]
     
