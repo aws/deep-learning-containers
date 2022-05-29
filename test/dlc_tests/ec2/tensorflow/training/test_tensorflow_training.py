c049fc4e16f5eb9f965ed72ba10c79bf2eb1e79a
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2022-05-29 16:23:33 -0700

    Build and test only training, fix dataservice

--- test/dlc_tests/ec2/tensorflow/training/test_tensorflow_training.py
+++ test/dlc_tests/ec2/tensorflow/training/test_tensorflow_training.py
@@ -212,6 +212,7 @@ def run_data_service_test(ec2_connection, ec2_instance_ami, tensorflow_training,
     _, tensorflow_version = test_utils.get_framework_and_version_from_tag(tensorflow_training)
     ec2_connection.run(f"{python_invoker} -m pip install --upgrade pip")
     ec2_connection.run(f"{python_invoker} -m pip install tensorflow=={tensorflow_version}")
+    ec2_connection.run(f"{python_invoker} -m pip install 'protobuf>=3.20,<3.21'")
     container_test_local_dir = os.path.join("$HOME", "container_tests")
     ec2_connection.run(f"cd {container_test_local_dir}/bin && screen -d -m {python_invoker} start_dataservice.py")
     execute_ec2_training_test(ec2_connection, tensorflow_training, cmd, host_network=True)
