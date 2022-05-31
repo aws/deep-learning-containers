a923997f387f8986800609aa191da83fb33270bf
Shantanu Tripathi <shantanutripathi237@gmail.com>
2022-05-30 18:38:12 -0700

    Making the scripts backward compatible

--- test/sagemaker_tests/pytorch/training/resources/mnist/smdataparallel_smmodelparallel_mnist_script_mode.sh
+++ test/sagemaker_tests/pytorch/training/resources/mnist/smdataparallel_smmodelparallel_mnist_script_mode.sh
@@ -4,6 +4,13 @@
 
 set -ex
 
-smddpsinglenode python smdataparallel_mnist.py
+TORCH_VERSION=$(python -c "import torch; print(torch.__version__)")
+echo $TORCH_VERSION
+if [[ $(python -c "import torch; from packaging.version import Version; is_less_than_pt10 = Version(torch.__version__) < Version('1.10'); print(is_less_than_pt10)") == 'True' ]]
+then
+    smddpsinglenode python smdataparallel_mnist.py
+else
+    smddp_singlenode_dev python smdataparallel_mnist.py
+fi
 
 bash smmodelparallel_mnist_script_mode.sh
