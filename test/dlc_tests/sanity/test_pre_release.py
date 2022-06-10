1a8e0d99958255b8dac62452744e7292a7c3fcf5
matherit <97054789+matherit@users.noreply.github.com>
2022-06-10 18:18:42 -0500

    [tensorflow] [build] Fix TF 2.5 docker image build. (#1958)
    * Update to manually pull in debs from old repo.
    
    * Temp toml changes.
    
    * Force TF 2.5 builds
    
    * Fix dependency check
    
    * Fix safety check and pip check issues
    
    * Add exception for TF 2.5
    
    * Pin protobuf to fix horovod compilation.
    
    * Pin protobuf installation.
    
    * Fix urllib3 version issue.
    
    * Bump requests package.
    
    * Align how horovod is installed with other versions
    
    * Modify NVIDIA_REQUIRE_CUDA to get SM Endpoints working.
    
    * Bring NVIDIA_REQUIRE_CUDA change to diy file.
    
    * Install protobuf in the host instance for the dataservice test and the inference test.
    
    * Fix duplicate key.
    
    * Add tensorflow-datasets to dockerfile
    
    * Bring in 48551 to safety check allowlist.
    
    * update tf2.5.3 wheels
    
    * add arch_type
    
    * remove openssl installed from source on cpu image
    
    * change ami id
    
    * revert temp changes
    
    * change inf instance type
    
    * build inference only
    
    * fix nit issue
    
    * revert temp change
    
    * add configs back
    
    * build cpu training image
    
    * run sm
    
    * revert temp change
    
    Co-authored-by: Sai Parthasarathy Miduthuri <saimidu@amazon.com>
    Co-authored-by: Wei Chu <weichu@amazon.com>
    Co-authored-by: tejaschumbalkar <tejaschumbalkar@gmail.com>
    Co-authored-by: Tejas Chumbalkar <34728580+tejaschumbalkar@users.noreply.github.com>

--- test/dlc_tests/sanity/test_pre_release.py
+++ test/dlc_tests/sanity/test_pre_release.py
@@ -412,7 +412,7 @@ def _run_dependency_check_test(image, ec2_connection):
         },
         "tensorflow": {
             "1.15": ["neuron"],
-            "2.5": ["neuron"],
+            "2.5": ["cpu", "gpu", "neuron"],
             "2.6": ["cpu", "gpu"],
             "2.7": ["cpu", "gpu", "hpu"],
             "2.8": ["cpu", "gpu"],
