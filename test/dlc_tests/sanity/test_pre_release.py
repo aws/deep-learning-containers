cce40bbb07b25f33b8f9dcfef34fd09ac99f2fce
aws-vrnatham <73142315+aws-vrnatham@users.noreply.github.com>
2022-08-12 19:23:29 -0700

    [NEURON][PT][TRAINING] - Add the CVE to allowlist for neuron (#446)
    * Update release_images.yml
    
    For hf neuron for the time being have disable_sm_tag to True
    
    * Update release_images.yml
    
    * training DLC's
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * create py3
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * use python3.6
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Add neuron collectives.
    
    This docker image was tested with multi node all reduce
    on trn1 instance and works fine
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Create copies of collectives and nccom lib
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * do neuron test/build
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Fix the buildspecs for neuron
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Increase the size
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * revert enable neuron test
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Add basic ec2 test for training
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * enable neuron test
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Not build inference image for now
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Use the right region for launch of trn1 instance
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Run the training test from default region
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * change sdk version to tmp 2.1.1
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Update dlc_developer_config.toml
    
    * Update to use the beta repo and also py37
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Fix the repo path
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * fix the pip repo path
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Fix version manifest
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * fix the neuron version manifest
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Fix the py version in buildspec
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * add ignore some safety checks
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Remove tf2.6 install
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Fix the ignore safety check file
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * remove unwanted files
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * fix the safety_scan json file
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * fix test failures
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * add some extra package build
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Use the right ami for neuron test
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * remove smdebug/smclarify
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * fix the pre release vulnerability
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * remove sagemaker-experiments
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    * Update dlc_developer_config.toml
    
    * Update test_pytorch_training.py
    
    * fix the cve for prerelease
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    
    Signed-off-by: Venky Natham <vrnatham@amazon.com>
    Co-authored-by: arjkesh <33526713+arjkesh@users.noreply.github.com>

--- test/dlc_tests/sanity/test_pre_release.py
+++ test/dlc_tests/sanity/test_pre_release.py
@@ -411,7 +411,7 @@ def _run_dependency_check_test(image, ec2_connection):
             "2.9": ["cpu", "gpu"]
         },
         "mxnet": {"1.8": ["neuron"], "1.9": ["cpu", "gpu"]},
-        "pytorch": {"1.8": ["cpu", "gpu"], "1.10": ["cpu", "hpu"], "1.11": ["cpu", "gpu"]},
+        "pytorch": {"1.8": ["cpu", "gpu"], "1.10": ["cpu", "hpu", "neuron"], "1.11": ["cpu", "gpu"]},
         "huggingface_pytorch": {"1.8": ["cpu", "gpu"], "1.9": ["cpu", "gpu"]},
         "huggingface_tensorflow": {"2.4": ["cpu", "gpu"], "2.5": ["cpu", "gpu"], "2.6": ["cpu", "gpu"]},
         "huggingface_tensorflow_trcomp": {"2.6": ["gpu"]},
