7fa58da3bd93f7cd18a10bf260ad38a5b4fbf5d1
Mike Schneider <xncqr@amazon.com>
2022-08-17 14:16:25 -0700

    fixing test only

--- test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
+++ test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
@@ -199,7 +199,7 @@ def host_setup_for_tensorflow_inference(
     if is_graviton:
         # TF training binary is used that is compatible for graviton instance type
 #        TF_URL = "https://aws-dlc-graviton-training-binaries.s3.us-west-2.amazonaws.com/tensorflow/2.6.0/tensorflow-2.6.0-cp38-cp38-linux_aarch64.whl"
-#        ec2_connection.run((f"{python_invoker} -m pip install --no-cache-dir -U {TF_URL}"), hide=True)
+        ec2_connection.run((f"{python_invoker} -m pip install --no-cache-dir -U tensorflow>=2.9,<2.10"), hide=True)
         ec2_connection.run(
             (
                 f"{python_invoker} -m pip install --no-dependencies --no-cache-dir tensorflow-serving-api=={framework_version}"
