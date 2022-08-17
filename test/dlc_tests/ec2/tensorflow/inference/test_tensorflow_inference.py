a329157cdba1662a51e17748c32a228b5a41b8fb
Mike Schneider <xncqr@amazon.com>
2022-08-17 09:42:58 -0700

    refactor buildspec and use pip3

--- test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
+++ test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
@@ -196,19 +196,17 @@ def host_setup_for_tensorflow_inference(
     if is_graviton:
         # TF training binary is used that is compatible for graviton instance type
         TF_URL = "https://aws-dlc-graviton-training-binaries.s3.us-west-2.amazonaws.com/tensorflow/2.6.0/tensorflow-2.6.0-cp38-cp38-linux_aarch64.whl"
-        ec2_connection.run(f"conda init bash")
-        ec2_connection.run(f"conda activate base")
-        ec2_connection.run((f"{python_invoker} -m pip install --no-cache-dir -U {TF_URL}"), hide=True)
+        ec2_connection.run((f"{python_invoker} -m pip3 install --no-cache-dir -U {TF_URL}"), hide=True)
         ec2_connection.run(
             (
-                f"{python_invoker} -m pip install --no-dependencies --no-cache-dir tensorflow-serving-api=={framework_version}"
+                f"{python_invoker} -m pip3 install --no-dependencies --no-cache-dir tensorflow-serving-api=={framework_version}"
             ),
             hide=True,
         )
     else:
         ec2_connection.run(
             (
-                f"{python_invoker} -m pip install --user -qq -U 'tensorflow<={framework_version}' "
+                f"{python_invoker} -m pip3 install --user -qq -U 'tensorflow<={framework_version}' "
                 f" 'tensorflow-serving-api<={framework_version}' 'protobuf>=3.20,<3.21'"
             ), hide=True
         )
