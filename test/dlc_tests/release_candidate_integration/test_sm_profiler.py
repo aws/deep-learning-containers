d7a60380729f2981a1fdad69a04bef1a1657720a
Shibo Xing <shibox@amazon.com>
2022-09-08 19:58:50 +0000

    feat: disable profiler test for >1.10

--- test/dlc_tests/release_candidate_integration/test_sm_profiler.py
+++ test/dlc_tests/release_candidate_integration/test_sm_profiler.py
@@ -32,7 +32,7 @@ def test_sm_profiler_pt(pytorch_training):
         pytest.skip(f"Processor {processor} not supported. Skipping test.")
 
     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
-    if Version(image_framework_version) in SpecifierSet(">=1.12"):
+    if Version(image_framework_version) in SpecifierSet(">=1.10"):
         pytest.skip("sm profiler ZCC test is not supported in PT 1.12 and above")
 
     ctx = Context()
