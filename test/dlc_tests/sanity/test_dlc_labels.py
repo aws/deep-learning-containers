cc1c362e7d0b6e7ceab6da30b6980dca0f802132
Kristopher Siman <ksiman@amazon.com>
2022-09-29 16:40:28 +0000

    Separating training into another PR

--- test/dlc_tests/sanity/test_dlc_labels.py
+++ test/dlc_tests/sanity/test_dlc_labels.py
@@ -135,7 +135,7 @@ def test_dlc_major_version_dockerfiles(image):
         "tensorflow2": SpecifierSet("<2.2"),
         "tensorflow1": SpecifierSet("<1.16"),
         "mxnet": SpecifierSet("<1.7"),
-        # HACK Temporary exception PT 1.11 and PT 1.12 since they use different cude versions for ec2 and SM 
+        # HACK Temporary exception PT 1.11 and PT 1.12 since they use different cuda versions for ec2 and SM 
         "pytorch": SpecifierSet("<1.5,==1.11.*,==1.12.*"),
         # autogluon 0.3.1 and 0.3.2 DLCs are both v1, and are meant to exist in the repo simultaneously
         "autogluon": SpecifierSet("==0.3.*"),
