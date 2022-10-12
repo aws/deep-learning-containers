014715d51f111ba0c249464b2b0c8d6db35f2c39
Shantanu Tripathi <shantanutripathi237@gmail.com>
2022-10-12 21:11:33 +0000

    Add test to check that safety is not installed

--- test/dlc_tests/sanity/test_safety_report_file.py
+++ test/dlc_tests/sanity/test_safety_report_file.py
@@ -103,3 +103,21 @@ def test_safety_file_exists_and_is_valid(image):
         LOGGER.info(f"Safety report file validation is successfully complete and report exists at {SAFETY_FILE}")
     finally:
         run(f"docker rm -f {container_name}", hide=True, warn=True)
+
+
+@pytest.mark.model("N/A")
+def test_safety_package_installed(image):
+    """
+    We do not ship safety within DLCs. This test confirms that safety is not installed in the image.
+    """
+    repo_name, image_tag = image.split("/")[-1].split(":")
+    # Make sure this container name doesn't conflict with the safety check test container name
+    container_name = f"{repo_name}-{image_tag}-safety-package-check"
+    # Add null entrypoint to ensure command exits immediately
+    try:
+        run(f"docker run -id " f"--name {container_name} " f"--entrypoint='/bin/bash' " f"{image}", hide=True)
+        docker_exec_cmd = f"docker exec -i {container_name}"
+        safety_package_installed = run(f"{docker_exec_cmd} pip show safety", warn=True, hide=True)
+        assert not safety_package_installed.ok, f"Safety is installed in image: {image}"
+    finally:
+        run(f"docker rm -f {container_name}", hide=True, warn=True)
