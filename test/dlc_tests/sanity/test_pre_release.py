b54d3b3279a2bf395f4083c7b86f2abd63209fbc
Harsha Balluru <hballuru@amazon.com>
2022-11-02 18:20:31 +0000

    allowlist update for tf 2.11 GPU CVE-2022-1292 & CVE-2021-3711

--- test/dlc_tests/sanity/test_pre_release.py
+++ test/dlc_tests/sanity/test_pre_release.py
@@ -427,6 +427,7 @@ def _run_dependency_check_test(image, ec2_connection):
             "2.8": ["cpu", "gpu", "hpu"],
             "2.9": ["cpu", "gpu", "hpu"],
             "2.10": ["cpu", "gpu", "hpu"],
+            "2.11": ["cpu", "gpu"],
         },
         "mxnet": {"1.8": ["neuron"], "1.9": ["cpu", "gpu"]},
         "pytorch": {
@@ -463,7 +464,7 @@ def _run_dependency_check_test(image, ec2_connection):
             "2.8": ["cpu", "gpu", "hpu"],
             "2.9": ["cpu", "gpu", "hpu"],
             "2.10": ["cpu", "gpu", "hpu"],
-            "2.11": ["cpu"],
+            "2.11": ["cpu","gpu"],
         },
         "mxnet": {"1.8": ["neuron"], "1.9": ["cpu", "gpu"]},
         "huggingface_tensorflow": {"2.5": ["gpu"], "2.6": ["gpu"]},
