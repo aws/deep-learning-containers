6aa8fecf9467926ba41833b93d67673451fbdfb4
Mike Schneider <xncqr@amazon.com>
2022-11-06 08:19:04 -0800

    typo in comment

--- test/dlc_tests/sanity/test_pre_release.py
+++ test/dlc_tests/sanity/test_pre_release.py
@@ -407,7 +407,7 @@ def _run_dependency_check_test(image, ec2_connection):
         "CVE-2016-6303",
         "CVE-2016-2182",
         "CVE-2022-2068",
-        # Latest installed openssl on ubuntu 20.04is 1.1.1f-1ubuntu2.16 which is higher than these CVEs. False Positive
+        # Latest installed openssl on ubuntu 20.04 is 1.1.1f-1ubuntu2.16 which is higher than these CVEs. False Positive
         "CVE-2022-1292",
         "CVE-2021-3711",
     }
