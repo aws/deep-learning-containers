092c4e61069c32ecfacf1596a6ff5520ab8ad7a6
Shibo Xing <shibox@amazon.com>
2022-11-06 13:34:24 +0000

    test: skip s3 plugin tests for pt>=1.13

--- test/dlc_tests/ecs/pytorch/training/test_ecs_pytorch_training.py
+++ test/dlc_tests/ecs/pytorch/training/test_ecs_pytorch_training.py
@@ -82,58 +82,60 @@ def test_ecs_pytorch_training_mnist_gpu(
     )
 
 
-# @pytest.mark.usefixtures("feature_s3_plugin_present")
-# @pytest.mark.model("resnet18")
-# @pytest.mark.integration("pt_s3_plugin")
-# @pytest.mark.parametrize("training_script", [PT_S3_PLUGIN_CMD], indirect=True)
-# @pytest.mark.parametrize("ecs_instance_type", ["c5.9xlarge"], indirect=True)
-# @pytest.mark.parametrize("ecs_ami", [ECS_AML2_CPU_USWEST2], indirect=True)
-# def test_ecs_pytorch_s3_plugin_training_cpu(
-#     cpu_only, ecs_container_instance, pytorch_training, training_cmd, ecs_cluster_name, pt17_and_above_only
-# ):
-#     """
-#     CPU resnet18 test for PyTorch Training using S3 plugin
+@pytest.mark.usefixtures("feature_s3_plugin_present")
+@pytest.mark.model("resnet18")
+@pytest.mark.integration("pt_s3_plugin")
+@pytest.mark.parametrize("training_script", [PT_S3_PLUGIN_CMD], indirect=True)
+@pytest.mark.parametrize("ecs_instance_type", ["c5.9xlarge"], indirect=True)
+@pytest.mark.parametrize("ecs_ami", [ECS_AML2_CPU_USWEST2], indirect=True)
+def test_ecs_pytorch_s3_plugin_training_cpu(
+    cpu_only, ecs_container_instance, pytorch_training, training_cmd, ecs_cluster_name
+):
+    """
+    CPU resnet18 test for PyTorch Training using S3 plugin
 
-#     Instance Type - c5.9xlarge
+    Instance Type - c5.9xlarge
 
-#     Given above parameters, registers a task with family named after this test, runs the task, and waits for
-#     the task to be stopped before doing teardown operations of instance and cluster.
-#     """
-#     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
-#     if Version(image_framework_version) < Version("1.8"):
-#         pytest.skip("S3 plugin is supported on PyTorch version >=1.8")
-#     instance_id, cluster_arn = ecs_container_instance
+    Given above parameters, registers a task with family named after this test, runs the task, and waits for
+    the task to be stopped before doing teardown operations of instance and cluster.
+    """
+    _, framework_version = get_framework_and_version_from_tag(pytorch_training)
+    if Version(framework_version) < Version("1.8") or Version(framework_version) >= Version("1.13"):
+        pytest.skip("S3 plugin sanity is supported on PyTorch version < 1.8 or >= 1.13")
+    instance_id, cluster_arn = ecs_container_instance
 
-#     ecs_utils.ecs_training_test_executor(ecs_cluster_name, cluster_arn, training_cmd, pytorch_training, instance_id)
+    ecs_utils.ecs_training_test_executor(ecs_cluster_name, cluster_arn, training_cmd, pytorch_training, instance_id)
 
 
-# @pytest.mark.usefixtures("feature_s3_plugin_present")
-# @pytest.mark.model("resnet18")
-# @pytest.mark.integration("pt_s3_plugin")
-# @pytest.mark.parametrize("training_script", [PT_S3_PLUGIN_CMD], indirect=True)
-# @pytest.mark.parametrize("ecs_instance_type", ["p3.8xlarge"], indirect=True)
-# @pytest.mark.parametrize("ecs_ami", [ECS_AML2_GPU_USWEST2], indirect=True)
-# def test_ecs_pytorch_s3_plugin_training_gpu(
-#     gpu_only, ecs_container_instance, pytorch_training, training_cmd, ecs_cluster_name, pt17_and_above_only
-# ):
-#     """
-#     GPU resnet18 test for PyTorch Training using S3 plugin
+@pytest.mark.usefixtures("feature_s3_plugin_present")
+@pytest.mark.model("resnet18")
+@pytest.mark.integration("pt_s3_plugin")
+@pytest.mark.parametrize("training_script", [PT_S3_PLUGIN_CMD], indirect=True)
+@pytest.mark.parametrize("ecs_instance_type", ["p3.8xlarge"], indirect=True)
+@pytest.mark.parametrize("ecs_ami", [ECS_AML2_GPU_USWEST2], indirect=True)
+def test_ecs_pytorch_s3_plugin_training_gpu(
+    gpu_only, ecs_container_instance, pytorch_training, training_cmd, ecs_cluster_name
+):
+    """
+    GPU resnet18 test for PyTorch Training using S3 plugin
 
-#     Instance Type - p3.8xlarge
+    Instance Type - p3.8xlarge
 
-#     Given above parameters, registers a task with family named after this test, runs the task, and waits for
-#     the task to be stopped before doing teardown operations of instance and cluster.
-#     """
-#     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
-#     if Version(image_framework_version) < Version("1.8"):
-#         pytest.skip("S3 plugin is supported on PyTorch version >=1.8")
-#     instance_id, cluster_arn = ecs_container_instance
+    Given above parameters, registers a task with family named after this test, runs the task, and waits for
+    the task to be stopped before doing teardown operations of instance and cluster.
+    """
+    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
+    if image_framework_version >= Version("1.13"):
+        pytest.skip(f"S3 plugin is supported on PyTorch version >=1.13")
+    if Version(image_framework_version) < Version("1.8"):
+        pytest.skip("S3 plugin is supported on PyTorch version <1.8")
+    instance_id, cluster_arn = ecs_container_instance
 
-#     num_gpus = ec2_utils.get_instance_num_gpus(instance_id)
+    num_gpus = ec2_utils.get_instance_num_gpus(instance_id)
 
-#     ecs_utils.ecs_training_test_executor(
-#         ecs_cluster_name, cluster_arn, training_cmd, pytorch_training, instance_id, num_gpus=num_gpus
-#     )
+    ecs_utils.ecs_training_test_executor(
+        ecs_cluster_name, cluster_arn, training_cmd, pytorch_training, instance_id, num_gpus=num_gpus
+    )
 
 
 @pytest.mark.integration("dgl")
--- test/dlc_tests/sanity/test_pt_s3.py
+++ test/dlc_tests/sanity/test_pt_s3.py
@@ -11,22 +11,22 @@ from test.test_utils import (
 )
 
 
-# @pytest.mark.usefixtures("feature_s3_plugin_present")
-# @pytest.mark.usefixtures("sagemaker", "huggingface")
-# @pytest.mark.integration("pt_s3_plugin_sanity")
-# @pytest.mark.model("N/A")
-# def test_pt_s3_sanity(pytorch_training, pt17_and_above_only):
-#     """
-#     Check that the internally built PT S3 binary is properly installed.
-#     :param pytorch_training: framework fixture for pytorch training
-#     """
-#     _, framework_version = get_framework_and_version_from_tag(pytorch_training)
-#     if Version(framework_version) < Version("1.8"):
-#         pytest.skip("S3 plugin sanity is supported on PyTorch version >=1.8")
-#     ctx = Context()
-#     container_name = get_container_name("pt-s3", pytorch_training)
-#     start_container(container_name, pytorch_training, ctx)
-#     s3_path = 's3://pt-s3plugin-test-data-west2/test_0.JPEG'
-#     run_cmd_on_container(
-#         container_name, ctx, f"import awsio; print(awsio.__version__); from awsio.python.lib.io.s3.s3dataset import file_exists; print(file_exists(\""+s3_path+"\"))", executable="python"
-#     )
+@pytest.mark.usefixtures("feature_s3_plugin_present")
+@pytest.mark.usefixtures("sagemaker", "huggingface")
+@pytest.mark.integration("pt_s3_plugin_sanity")
+@pytest.mark.model("N/A")
+def test_pt_s3_sanity(pytorch_training):
+    """
+    Check that the internally built PT S3 binary is properly installed.
+    :param pytorch_training: framework fixture for pytorch training
+    """
+    _, framework_version = get_framework_and_version_from_tag(pytorch_training)
+    if Version(framework_version) < Version("1.8") or Version(framework_version) >= Version("1.13"):
+        pytest.skip("S3 plugin sanity is supported on PyTorch version < 1.8 or >= 1.13")
+    ctx = Context()
+    container_name = get_container_name("pt-s3", pytorch_training)
+    start_container(container_name, pytorch_training, ctx)
+    s3_path = 's3://pt-s3plugin-test-data-west2/test_0.JPEG'
+    run_cmd_on_container(
+        container_name, ctx, f"import awsio; print(awsio.__version__); from awsio.python.lib.io.s3.s3dataset import file_exists; print(file_exists(\""+s3_path+"\"))", executable="python"
+    )
