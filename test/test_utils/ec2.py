ecdd7727bb090af5cb3ad1a625b243c9c5e348a7
Kristopher Siman <ksiman@amazon.com>
2022-11-16 17:31:11 +0000

    Bumping the size further based on internal discussions.

--- test/test_utils/ec2.py
+++ test/test_utils/ec2.py
@@ -145,7 +145,7 @@ def launch_instance(
         "TagSpecifications": [
             {"ResourceType": "instance", "Tags": [{"Key": "Name", "Value": f"CI-CD {instance_name}"}],},
         ],
-        "BlockDeviceMappings": [{"DeviceName": "/dev/sda1", "Ebs": {"VolumeSize": 100,}}]
+        "BlockDeviceMappings": [{"DeviceName": "/dev/sda1", "Ebs": {"VolumeSize": 150,}}]
     }
     if user_data:
         arguments_dict["UserData"] = user_data
