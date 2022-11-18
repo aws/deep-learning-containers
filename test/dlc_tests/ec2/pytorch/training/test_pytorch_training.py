7c4e5353b1e8e54753b3f28602d576f9dec77481
Harish Tummalacherla <hartum@amazon.com>
2022-11-17 16:10:25 -0800

    disable horovod test for trcomp

--- test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
+++ test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
@@ -136,6 +136,8 @@ def test_pytorch_train_dgl_cpu(pytorch_training, ec2_connection, cpu_only, py3_o
 @pytest.mark.model("mnist")
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
 def test_pytorch_with_horovod(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
+    if 'trcomp' in pytorch_training and Version(image_framework_version) in SpecifierSet("<1.13.*"):
+        pytest.skip(f"Image {pytorch_training} doesn't package horovod. Hence test is skipped.")
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
     test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPTHVD")
