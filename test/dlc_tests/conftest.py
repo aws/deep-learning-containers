58647c95a6225fa571d4c71108a70778c7aa65a9
Mike Schneider <xncqr@amazon.com>
2022-12-06 17:52:26 -0700

    take 3 on bypass. using platform.machine

--- test/dlc_tests/conftest.py
+++ test/dlc_tests/conftest.py
@@ -446,9 +446,7 @@ def ec2_connection(request, ec2_instance, ec2_key_name, ec2_instance_type, regio
 
     request.addfinalizer(delete_s3_artifact_copy)
 
-    # Graviton DLAMI Images are missing /etc/profile.d/dlami.sh. bypassing for graviton
-    if not test_utils.is_graviton_architecture():
-        ec2_utils.install_python_in_instance(conn, python_version="3.9")
+    ec2_utils.install_python_in_instance(conn, python_version="3.9")
 
     conn.run(f"aws s3 cp --recursive {test_utils.TEST_TRANSFER_S3_BUCKET}/{artifact_folder} $HOME/container_tests")
     conn.run(f"mkdir -p $HOME/container_tests/logs && chmod -R +x $HOME/container_tests/*")
