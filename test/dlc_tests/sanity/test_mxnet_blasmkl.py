52e36ca882a53b5a60517766eeddb62b8b0e146e
Isaac T. Mathers <matherit@amazon.com>
2022-12-08 21:42:11 +0000

    Rename test.

--- test/dlc_tests/sanity/test_mxnet_blasmkl.py
+++ test/dlc_tests/sanity/test_mxnet_blasmkl.py
@@ -15,8 +15,8 @@ from test.test_utils import (
 @pytest.mark.usefixtures("sagemaker")
 @pytest.mark.model("N/A")
 @pytest.mark.processor("cpu")
-@pytest.mark.integration("mxnet_mkl_sanity")
-def test_mxnet_mkl_sanity(mxnet_inference):
+@pytest.mark.integration("mxnet_blasmkl_sanity")
+def test_mxnet_blasmkl_sanity(mxnet_inference):
     """
     Check that the container's version of MXNet includes BLAS MKL.
     :param mxnet_inference: framework fixture for mxnet inference
