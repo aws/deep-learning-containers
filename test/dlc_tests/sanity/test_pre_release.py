163a51cb2651b596472d31a4aed68041a9fbbd8b
Harsha Balluru <hballuru@amazon.com>
2023-01-18 17:22:19 +0000

    removed debug info

--- test/dlc_tests/sanity/test_pre_release.py
+++ test/dlc_tests/sanity/test_pre_release.py
@@ -254,10 +254,7 @@ def test_sm_toolkit_and_ts_version(pytorch_inference, region):
         ts_version_from_output = ((str(output_ts.stdout).split(' '))[3]).strip()
         image_labels = get_labels_from_ecr_image(image, region)
         expected_label=f"com.amazonaws.ml.engines.sagemaker.dlc.inference-toolkit.{toolkit_version_from_output}.torchserve.{ts_version_from_output}"
-        #remove the below print before PR review
-        print(f"label expected by me {expected_label}")        
         required_label = image_labels.get(expected_label, None)
-        print(f"label found in actuality {required_label}")
         assert required_label, \
             f"The required label {expected_label} which enforces compatability between sagemaker inference toolkit and torchserve seems to be invalid/missing for the image {image}"
     except Exception as e:
