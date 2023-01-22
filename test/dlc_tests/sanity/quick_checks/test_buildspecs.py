c03ca10c45bb4de5b3470b0f849d52c49bafef4d
arjkesh <33526713+arjkesh@users.noreply.github.com>
2023-01-21 23:39:29 -0800

    Update test_buildspecs.py

--- test/dlc_tests/sanity/quick_checks/test_buildspecs.py
+++ test/dlc_tests/sanity/quick_checks/test_buildspecs.py
@@ -10,7 +10,7 @@ import pytest
 def test_train_inference_buildspec():
     """
     Walk through all buildspecs. Ensure the following:
-    1. "training" or "inference is in the path
+    1. "training" or "inference" is in the path
     2. if "training" is in the path, make sure "inference" is not in the file
     3. if "inference" is in the path, make sure "training" is not in the file
 
