9fc6fde3aeffde1bf1cc7c7d5521c74a3fbd03ae
Shibo Xing <shibox@amazon.com>
2023-02-04 22:46:27 +0000

    fix: specifier set syntax

--- test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
+++ test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
@@ -320,8 +320,6 @@ def test_pytorch_training_torchdata_gpu(
     pytorch_training, ec2_connection, gpu_only, ec2_instance_type, pt111_and_above_only
 ):
     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
-    if 'trcomp' in pytorch_training and Version(image_framework_version) in SpecifierSet("<1.13.*"):
-        pytest.skip(f"Image {pytorch_training} doesn't package torch_data. Hence test is skipped.")
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
     # HACK including PT 1.13 in this condition because the Torchdata 0.5.0 tag includes old tests data
@@ -343,7 +341,7 @@ def test_pytorch_training_torchdata_cpu(
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
     # HACK including PT 1.13 in this condition because the Torchdata 0.5.0 tag includes old tests data
-    if Version(image_framework_version) in SpecifierSet(">=1.11,<=1.13"):
+    if Version(image_framework_version) in SpecifierSet(">=1.11,<=1.13.1"):
         execute_ec2_training_test(ec2_connection, pytorch_training, PT_TORCHDATA_DEV_CMD)
     else:
         execute_ec2_training_test(ec2_connection, pytorch_training, PT_TORCHDATA_CMD)
