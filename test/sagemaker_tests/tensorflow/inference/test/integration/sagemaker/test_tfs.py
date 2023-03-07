5740d0cc15c34c937f803758d945c9c7782dd782
EC2 Default User <ec2-user@ip-172-31-34-198.us-west-2.compute.internal>
2023-03-07 00:51:23 +0000

    Update test_python_model_with_requirements

--- test/sagemaker_tests/tensorflow/inference/test/integration/sagemaker/test_tfs.py
+++ test/sagemaker_tests/tensorflow/inference/test/integration/sagemaker/test_tfs.py
@@ -178,11 +178,13 @@ def test_python_model_with_requirements(boto_session, sagemaker_client,
         pytest.skip("skip for p3 instance")
 
     # the python service needs to transform this to get a valid prediction
-    input_data = {"instances": [[1.0, 2.0, 5.0]]}
+    input_data = {"instances": [1.0, 2.0, 5.0]}
     output_data = util.create_and_invoke_endpoint(boto_session, sagemaker_client,
                                                   sagemaker_runtime_client, model_name,
                                                   python_model_with_requirements, image_uri,
                                                   instance_type, accelerator_type, input_data)
+    assert output_data["python"] is True
+    assert output_data["pillow"] == "6.0.0"
 
 
 
