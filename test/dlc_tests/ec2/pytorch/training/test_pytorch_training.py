eabcf424f5339bec6b0258d7d3743c14dabc00a8
YuchengT <tyc7654321@gmail.com>
2023-03-16 19:44:06 +0000

    enable inductor tests

--- test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
+++ test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
@@ -13,10 +13,12 @@ from test.test_utils.ec2 import execute_ec2_training_test, get_ec2_instance_type
 
 PT_STANDALONE_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchStandalone")
 PT_MNIST_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorch")
+PT_BERT_INDUCTOR_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorch")
 PT_REGRESSION_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchRegression")
 PT_DGL_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "dgl_tests", "testPyTorchDGL")
 PT_APEX_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testNVApex")
 PT_AMP_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchAMP")
+PT_AMP_INDUCTOR_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchAMPwithInductor")
 PT_TELEMETRY_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "test_pt_dlc_telemetry_test")
 PT_S3_PLUGIN_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchS3Plugin")
 PT_HABANA_TEST_SUITE_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "testHabanaPTSuite")
@@ -63,7 +65,6 @@ def test_pytorch_standalone_gpu(pytorch_training, ec2_connection, gpu_only, ec2_
         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
     execute_ec2_training_test(ec2_connection, pytorch_training, PT_STANDALONE_CMD)
 
-
 @pytest.mark.usefixtures("sagemaker")
 @pytest.mark.integration("pytorch_sanity_test")
 @pytest.mark.model("N/A")
@@ -79,6 +80,16 @@ def test_pytorch_train_mnist_gpu(pytorch_training, ec2_connection, gpu_only, ec2
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
     execute_ec2_training_test(ec2_connection, pytorch_training, PT_MNIST_CMD)
+
+
+@pytest.mark.usefixtures("sagemaker")
+@pytest.mark.model("bert")
+@pytest.mark.integration("inductor")
+@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+def test_pytorch_train_bert_gpu(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
+    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+    execute_ec2_training_test(ec2_connection, pytorch_training, PT_BERT_INDUCTOR_CMD)
 
 
 @pytest.mark.usefixtures("sagemaker")
@@ -144,6 +155,20 @@ def test_pytorch_with_horovod(pytorch_training, ec2_connection, gpu_only, ec2_in
     test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPTHVD")
     execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
 
+@pytest.mark.usefixtures("sagemaker")
+@pytest.mark.integration("horovod")
+@pytest.mark.integration("inductor")
+@pytest.mark.model("mnist")
+@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+def test_pytorch_with_horovod_inductor(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
+    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
+    if 'trcomp' in pytorch_training and Version(image_framework_version) in SpecifierSet("<2.0"):
+        pytest.skip(f"Image {pytorch_training} doesn't package horovod. Hence test is skipped.")
+    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+    test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPTHVDwithInductor")
+    execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
+
 
 @pytest.mark.usefixtures("sagemaker")
 @pytest.mark.integration("gloo")
@@ -156,6 +181,20 @@ def test_pytorch_gloo_gpu(pytorch_training, ec2_connection, gpu_only, py3_only, 
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
     test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchGloo")
+    execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
+
+@pytest.mark.usefixtures("sagemaker")
+@pytest.mark.integration("gloo")
+@pytest.mark.integration("inductor")
+@pytest.mark.model("resnet18")
+@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+def test_pytorch_gloo_inductor_gpu(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
+    """
+    Tests gloo backend with torch inductor
+    """
+    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+    test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchGloowithInductor")
     execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
 
 
@@ -182,6 +221,20 @@ def test_pytorch_nccl(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
     test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchNccl")
+    execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
+
+@pytest.mark.usefixtures("sagemaker")
+@pytest.mark.integration("nccl")
+@pytest.mark.integration("inductor")
+@pytest.mark.model("resnet18")
+@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+def test_pytorch_nccl_inductor(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
+    """
+    Tests nccl backend with inductor
+    """
+    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+    test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchNcclwithInductor")
     execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
 
 
@@ -216,6 +269,22 @@ def test_pytorch_mpi_gpu(pytorch_training, ec2_connection, gpu_only, py3_only, e
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
     test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchMpi")
+    execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
+
+@pytest.mark.usefixtures("sagemaker")
+@pytest.mark.integration("mpi")
+@pytest.mark.integration("inductor")
+@pytest.mark.model("resnet18")
+@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+def test_pytorch_mpi_inductor_gpu(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type, pt111_and_above_only):
+    """
+    Tests mpi backend with torch inductor
+    """
+    if 'trcomp' in pytorch_training:
+        pytest.skip(f"Image {pytorch_training} is incompatible with distribution type MPI.")
+    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+    test_cmd = os.path.join(CONTAINER_TESTS_PREFIX, "pytorch_tests", "testPyTorchMpiwithInductor")
     execute_ec2_training_test(ec2_connection, pytorch_training, test_cmd)
 
 
@@ -254,6 +323,19 @@ def test_pytorch_amp(pytorch_training, ec2_connection, gpu_only, ec2_instance_ty
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
         pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
     execute_ec2_training_test(ec2_connection, pytorch_training, PT_AMP_CMD)
+
+@pytest.mark.usefixtures("sagemaker")
+@pytest.mark.integration("amp")
+@pytest.mark.integration("inductor")
+@pytest.mark.model("resnet50")
+@pytest.mark.parametrize("ec2_instance_type", PT_EC2_MULTI_GPU_INSTANCE_TYPE, indirect=True)
+def test_pytorch_amp_inductor(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
+    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
+    if Version(image_framework_version) < Version("1.6"):
+        pytest.skip("Native AMP was introduced in PyTorch 1.6")
+    if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
+        pytest.skip(f"Image {pytorch_training} is incompatible with instance type {ec2_instance_type}")
+    execute_ec2_training_test(ec2_connection, pytorch_training, PT_AMP_INDUCTOR_CMD)
 
 
 @pytest.mark.usefixtures("feature_s3_plugin_present")
