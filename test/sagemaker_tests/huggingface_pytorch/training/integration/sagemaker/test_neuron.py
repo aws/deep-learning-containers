efeced5c3b0f2057fa59561e7cdc8aacaeeeed7d
JingyaHuang <huang_jingya@outlook.com>
2023-03-29 15:29:00 +0000

    add evaluate and ping numpy

--- test/sagemaker_tests/huggingface_pytorch/training/integration/sagemaker/test_neuron.py
+++ test/sagemaker_tests/huggingface_pytorch/training/integration/sagemaker/test_neuron.py
@@ -13,14 +13,15 @@
 from __future__ import absolute_import
 
 import os
+import re
 import pytest
 import sagemaker
 from sagemaker import utils
 from sagemaker.huggingface import HuggingFace
-from ..... import invoke_sm_helper_function
 from ...integration import DEFAULT_TIMEOUT
 from ...integration.sagemaker.timeout import timeout
 from retrying import retry
+from sagemaker import get_execution_role
 
 # configuration for running training on torch distributed Data Parallel
 distribution = {"torch_distributed": {"enabled": True}}
@@ -31,7 +32,6 @@ hyperparameters = {
     'dataset_name': 'squad',
     'do_train': True,
     'do_eval': True,
-    'fp16': True,
     'per_device_train_batch_size': 4,
     'per_device_eval_batch_size': 4,
     'num_train_epochs': 1,
@@ -53,10 +53,49 @@ metric_definitions = [
 def get_optimum_neuron_version(ecr_image):
     optimum_neuron_version_search = re.search(r"optimum(\d+(\.\d+){1,2})", ecr_image)
     if optimum_neuron_version_search:
-        optimum_neuron_version = transformers_version_search.group(1)
+        optimum_neuron_version = optimum_neuron_version_search.group(1)
         return optimum_neuron_version
     else:
         raise LookupError("HF optimum-neuron version not found in image URI")
+
+# ValueError: Must setup local AWS configuration with a region supported by SageMaker.
+def retry_if_value_error(exception):
+    """Return True if we should retry (in this case when it's an ValueError), False otherwise"""
+    return isinstance(exception, ValueError)
+
+# TBD. This function is mainly there to handle capacity issues now. Once trn1 capacaity issues
+# are fixed, we can remove this function
+@retry(
+    stop_max_attempt_number=360, wait_fixed=10000, retry_on_exception=retry_if_value_error,
+)
+def invoke_neuron_helper_function(ecr_image, sagemaker_regions, helper_function, helper_function_args):
+    """
+    Used to invoke SM job defined in the helper functions in respective test file. The ECR image and the sagemaker
+    session are passed explicitly depending on the AWS region.
+    This function will rerun for all SM regions after a defined wait time if capacity issues are seen.
+
+    :param ecr_image: ECR image in us-west-2 region
+    :param sagemaker_regions: List of SageMaker regions
+    :param helper_function: Function to invoke
+    :param helper_function_args: Helper function args
+
+    :return: None
+    """
+    from ..... import get_ecr_image_region, get_sagemaker_session, get_ecr_image
+
+    ecr_image_region = get_ecr_image_region(ecr_image)
+    for region in sagemaker_regions:
+        sagemaker_session = get_sagemaker_session(region)
+        # Reupload the image to test region if needed
+        tested_ecr_image = get_ecr_image(ecr_image, region) if region != ecr_image_region else ecr_image
+        try:
+            helper_function(tested_ecr_image, sagemaker_session, **helper_function_args)
+            return
+        except sagemaker.exceptions.UnexpectedStatusException as e:
+            if "CapacityError" in str(e):
+                raise ValueError("CapacityError: Retry.")
+            else:
+                raise e
 
 @pytest.mark.processor("neuron")
 @pytest.mark.model("hf-pt-qa-neuron")
@@ -69,7 +108,7 @@ def test_neuron_question_answering(ecr_image, sagemaker_regions, py_version, ins
         'instance_count': 1,
         'num_neuron_cores': 2,
     }
-    invoke_sm_helper_function(ecr_image, sagemaker_regions, _test_neuron_question_answering_function, function_args)
+    invoke_neuron_helper_function(ecr_image, sagemaker_regions, _test_neuron_question_answering_function, function_args)
 
 def _test_neuron_question_answering_function(ecr_image, sagemaker_session, py_version, instance_type= "ml.trn1.32xlarge", instance_count=1, num_neuron_cores=2):
     optimum_neuron_version = get_optimum_neuron_version(ecr_image)
@@ -77,21 +116,21 @@ def _test_neuron_question_answering_function(ecr_image, sagemaker_session, py_ve
 
     source_dir = "./examples/question-answering"
 
-    hyperparameters = {**hyperparameters, 'nproc-per-node': num_neuron_cores, 'nnodes': instance_count}
-
+    role = get_execution_role()
     with timeout(minutes=DEFAULT_TIMEOUT):
         estimator = HuggingFace(
             entry_point='run_qa.py',
             source_dir=source_dir,
             git_config=git_config,
             metric_definitions=metric_definitions,
-            role='SageMakerRole',
+            # role='SageMakerRole',
+            role=role,
             image_uri=ecr_image,
             instance_count=instance_count,
             instance_type=instance_type,
             sagemaker_session=sagemaker_session,
             py_version=py_version,
             # distribution=distribution,  # Uncomment when it is enabled by HuggingFace Estimator
-            hyperparameters=hyperparameters,
+            hyperparameters={**hyperparameters, 'nproc_per_node': num_neuron_cores, 'nnodes': instance_count},
         )
         estimator.fit(job_name=sagemaker.utils.unique_name_from_base('test-hf-pt-qa-neuron'))
