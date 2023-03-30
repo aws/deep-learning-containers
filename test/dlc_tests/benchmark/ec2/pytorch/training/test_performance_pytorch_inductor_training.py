c693c541d2b7bd01c2a224a113a8d1b4060df5d4
YuchengT <tyc7654321@gmail.com>
2023-03-30 04:48:01 +0000

    modified the execution function for inductor benchmark

--- test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
+++ test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
@@ -33,8 +33,8 @@ def test_performance_pytorch_gpu_inductor_huggingface(pytorch_training, ec2_conn
     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     if Version(image_framework_version) < Version("2.0"):
         pytest.skip("Torch inductor was introduced in PyTorch 2.0")
-    execute_pytorch_gpu_py3_imagenet_ec2_training_performance_test(
-        ec2_connection, pytorch_training, PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD, ec2_instance_type
+    execute_ec2_training_performance_test(
+        ec2_connection, pytorch_training, PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD, ec2_instance_type, model_suite="huggingface"
     )
 
 @pytest.mark.integration("inductor")
@@ -45,8 +45,8 @@ def test_performance_pytorch_gpu_inductor_timm(pytorch_training, ec2_connection,
     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     if Version(image_framework_version) < Version("2.0"):
         pytest.skip("Torch inductor was introduced in PyTorch 2.0")
-    execute_pytorch_gpu_py3_imagenet_ec2_training_performance_test(
-        ec2_connection, pytorch_training, PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_TIMM_CMD, ec2_instance_type
+    execute_ec2_training_performance_test(
+        ec2_connection, pytorch_training, PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_TIMM_CMD, ec2_instance_type, model_suite="timm"
     )
 
 @pytest.mark.integration("inductor")
@@ -57,36 +57,30 @@ def test_performance_pytorch_gpu_inductor_torchbench(pytorch_training, ec2_conne
     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     if Version(image_framework_version) < Version("2.0"):
         pytest.skip("Torch inductor was introduced in PyTorch 2.0")
-    execute_pytorch_gpu_py3_imagenet_ec2_training_performance_test(
-        ec2_connection, pytorch_training, PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_TORCHBENCH_CMD, ec2_instance_type
+    execute_ec2_training_performance_test(
+        ec2_connection, pytorch_training, PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_TORCHBENCH_CMD, ec2_instance_type, model_suite="torchbench"
     )
 
 
-def execute_pytorch_gpu_py3_imagenet_ec2_training_performance_test(
-    connection, ecr_uri, test_cmd, ec2_instance_type, region=DEFAULT_REGION
+def execute_ec2_training_performance_test(
+    connection, ecr_uri, test_cmd, ec2_instance_type, model_suite, region=DEFAULT_REGION,
 ):
-    _, framework_version = get_framework_and_version_from_tag(ecr_uri)
-    repo_name, image_tag = ecr_uri.split("/")[-1].split(":")
+    docker_cmd = "nvidia-docker" if "gpu" in ecr_uri else "docker"
     container_test_local_dir = os.path.join("$HOME", "container_tests")
-    container_name = f"{repo_name}-performance-{image_tag}-ec2"
+
+    timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+    log_name = f"{model_suite}_results_{os.getenv('CODEBUILD_RESOLVED_SOURCE_VERSION')}_{timestamp}.txt"
+    log_location = os.path.join(container_test_local_dir, "benchmark", "logs", log_name)
 
     # Make sure we are logged into ECR so we can pull the image
     connection.run(f"$(aws ecr get-login --no-include-email --region {region})", hide=True)
-    # Do not add -q to docker pull as it leads to a hang for huge images like trcomp
-    connection.run(f"nvidia-docker pull {ecr_uri}")
-    timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
-    log_name = f"inductor_{os.getenv('CODEBUILD_RESOLVED_SOURCE_VERSION')}_{timestamp}.txt"
-    log_location = os.path.join(container_test_local_dir, "benchmark", "logs", log_name)
+    connection.run(f"{docker_cmd} pull -q {ecr_uri}")
+
     # Run training command
-    try:
-        connection.run(
-            f"nvidia-docker run --user root "
-            f"-e LOG_FILE={os.path.join(os.sep, 'test', 'benchmark', 'logs', log_name)} "
-            f"-e PR_CONTEXT={1 if is_pr_context() else 0} "
-            f"--shm-size 8G --env OMP_NUM_THREADS=1 --name {container_name} "
-            f"-v {container_test_local_dir}:{os.path.join(os.sep, 'test')} "
-            f"-v /home/ubuntu/:/root/:delegated "
-            f"{ecr_uri} {os.path.join(os.sep, 'bin', 'bash')} -c {test_cmd} {ec2_instance_type}"
-        )
-    finally:
-        connection.run(f"docker rm -f {container_name}", warn=True, hide=True)
+    connection.run(
+        f"{docker_cmd} run --user root "
+        f"-e LOG_FILE={os.path.join(os.sep, 'test', 'benchmark', 'logs', log_name)} "
+        f"-e PR_CONTEXT={1 if is_pr_context() else 0} "
+        f"-v {container_test_local_dir}:{os.path.join(os.sep, 'test')} {ecr_uri} "
+        f"{os.path.join(os.sep, 'bin', 'bash')} -c {test_cmd} {ec2_instance_type}"
+    )
