e0fc6b7693924ae40cc40330a96e680073438e04
YuchengT <tyc7654321@gmail.com>
2023-03-31 17:37:11 +0000

    pass region from the source script and only performance test

--- test/dlc_tests/container_tests/bin/benchmark/read_upload_benchmarking_results.py
+++ test/dlc_tests/container_tests/bin/benchmark/read_upload_benchmarking_results.py
@@ -1,7 +1,6 @@
 import boto3
 from botocore.config import Config
 from botocore.exceptions import ClientError
-from test.test_utils import DEFAULT_REGION
 import datetime
 import pandas as pd
 import os
@@ -13,20 +12,21 @@ parser.add_argument("-namespace", "--namespace", type=str, default="PyTorch/EC2/
 parser.add_argument("-instance_type", "--instance_type", type=str)
 parser.add_argument("-model_suite", "--model_suite", type=str)
 parser.add_argument("-precision", "--precision", type=str)
+parser.add_argument("-region", "--region", type=str)
 
 
-def get_boto3_session(region=DEFAULT_REGION):
+def get_boto3_session(region="us-east-1"):
     """Get boto3 session with us-east-1 as default region used to connect to AWS services."""
     return boto3.session.Session(region_name=region)
 
-def get_cloudwatch_client(region=DEFAULT_REGION):
+def get_cloudwatch_client(region="us-east-1"):
     """Get AWS CloudWatch client object. Currently assume region is IAD (us-east-1)"""
     return get_boto3_session(region=region).client("cloudwatch")
 
-def put_metric_data(metric_name, namespace, unit, value, dimensions):
+def put_metric_data(metric_name, namespace, unit, value, dimensions, region):
     """Puts data points to cloudwatch metrics"""
-    cloudwatch_client = get_cloudwatch_client()
-    current_timestamp = datetime.datetime.utcnow()
+    cloudwatch_client = get_cloudwatch_client(region=region)
+    current_timestamp = datetime.datetime.utcnow(region=region)
     try:
         response = cloudwatch_client.put_metric_data(
             Namespace=namespace,
@@ -66,9 +66,9 @@ if __name__ == '__main__':
          ]
     speedup = read_metric(os.path.join(args.directory, "geomean.csv"))
     comp_time = read_metric(os.path.join(args.directory,"comp_time.csv"))
-    #memory = read_metric(os.path.join(args.directory,"memory.csv"))
+    memory = read_metric(os.path.join(args.directory,"memory.csv"))
     #passrate = read_metric(os.path.join(args.directory,"passrate.csv"))
-    put_metric_data("Speedup", args.namespace, "None", speedup, dimensions)
-    put_metric_data("CompilationTime", args.namespace, "Seconds", comp_time, dimensions)
-    #put_metric_data("PeakMemoryFootprintCompressionRatio", args.namespace, "None", memory, dimensions)
-    #put_metric_data("PassRate", args.namespace, "Percent", passrate, dimensions)
+    put_metric_data("Speedup", args.namespace, "None", speedup, dimensions, region)
+    put_metric_data("CompilationTime", args.namespace, "Seconds", comp_time, dimensions, region)
+    put_metric_data("PeakMemoryFootprintCompressionRatio", args.namespace, "None", memory, dimensions, region)
+    #put_metric_data("PassRate", args.namespace, "Percent", passrate, dimensions, region)
