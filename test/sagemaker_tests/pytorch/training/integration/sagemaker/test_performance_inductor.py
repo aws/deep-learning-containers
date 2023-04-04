e82464489fb482563088c074d17bc64d1a34f995
BruceChang9688 <dingheng9688@gmail.com>
2023-04-04 23:50:40 +0000

    add sm inductor benchmark results to cloudwatch

--- test/sagemaker_tests/pytorch/training/integration/sagemaker/test_performance_inductor.py
+++ test/sagemaker_tests/pytorch/training/integration/sagemaker/test_performance_inductor.py
@@ -13,9 +13,10 @@ limitations under the License.
 from __future__ import absolute_import
 
 import os
-
 import boto3
 import pytest
+import tarfile, subprocess
+
 from sagemaker.instance_group import InstanceGroup
 from sagemaker.pytorch import PyTorch
 from sagemaker import utils
@@ -25,6 +26,7 @@ from packaging.specifiers import SpecifierSet
 from ...integration import training_dir, DEFAULT_TIMEOUT, inductor_path
 from ...integration.sagemaker.timeout import timeout
 from .... import invoke_pytorch_helper_function
+from .test_inductor_helper import put_metric_data, read_metric
 
 instance_types=("ml.p3.2xlarge", "ml.g5.4xlarge", "ml.g4dn.4xlarge")
 
@@ -33,11 +35,12 @@ instance_types=("ml.p3.2xlarge", "ml.g5.4xlarge", "ml.g4dn.4xlarge")
 @pytest.mark.skip_inductor_test
 @pytest.mark.model("huggingface")
 @pytest.mark.parametrize("instance_type", [instance_types], indirect=True)
-def test_inductor_huggingface(framework_version, ecr_image, sagemaker_regions, instance_type):
+def test_inductor_huggingface(framework_version, ecr_image, sagemaker_regions, instance_type, tmpdir):
     function_args = {
             'framework_version': framework_version,
             'instance_type': instance_type,
             'suites': "huggingface",
+            'tmpdir': tmpdir,
         }
 
     invoke_pytorch_helper_function(ecr_image, sagemaker_regions, _test_inductor_performance, function_args)
@@ -47,11 +50,12 @@ def test_inductor_huggingface(framework_version, ecr_image, sagemaker_regions, i
 @pytest.mark.skip_inductor_test
 @pytest.mark.model("timm")
 @pytest.mark.parametrize("instance_type", [instance_types], indirect=True)
-def test_inductor_timm(framework_version, ecr_image, sagemaker_regions, instance_type):
+def test_inductor_timm(framework_version, ecr_image, sagemaker_regions, instance_type, tmpdir):
     function_args = {
             'framework_version': framework_version,
             'instance_type': instance_type,
             'suites': "timm",
+            'tmpdir': tmpdir,
         }
 
     invoke_pytorch_helper_function(ecr_image, sagemaker_regions, _test_inductor_performance, function_args)
@@ -61,17 +65,19 @@ def test_inductor_timm(framework_version, ecr_image, sagemaker_regions, instance
 @pytest.mark.skip_inductor_test
 @pytest.mark.model("torchbench")
 @pytest.mark.parametrize("instance_type", [instance_types], indirect=True)
-def test_inductor_torchbench(framework_version, ecr_image, sagemaker_regions, instance_type):
+def test_inductor_torchbench(framework_version, ecr_image, sagemaker_regions, instance_type, tmpdir):
     function_args = {
             'framework_version': framework_version,
             'instance_type': instance_type,
             'suites': "torchbench",
+            'tmpdir': tmpdir,
         }
 
     invoke_pytorch_helper_function(ecr_image, sagemaker_regions, _test_inductor_performance, function_args)
 
-def _test_inductor_performance(ecr_image, sagemaker_session, framework_version, instance_type, suites):
-    with timeout(minutes=DEFAULT_TIMEOUT):
+def _test_inductor_performance(ecr_image, sagemaker_session, framework_version, instance_type, suites, tmpdir):
+    with timeout(minutes=DEFAULT_TIMEOUT):   
+        output_path = f"s3://sagemaker-inductor-test/{suites}"
         pytorch = PyTorch(
             entry_point=f"run_inductor_{suites}.sh",
             source_dir=inductor_path,
@@ -81,9 +87,29 @@ def _test_inductor_performance(ecr_image, sagemaker_session, framework_version, 
             sagemaker_session=sagemaker_session,
             image_uri=ecr_image,
             framework_version=framework_version,
-            output_path=f"s3://sagemaker-inductor-test/{suites}",
+            output_path=output_path,
         )
-        # training_input = pytorch.sagemaker_session.upload_data(path=training_dir, key_prefix='pytorch/inductor')
         pytorch.fit(job_name=utils.unique_name_from_base(f'test-pt-performance-inductor-{suites}'))
+        dimensions = [
+             {"Name": "InstanceType", "Value": instance_type},
+             {"Name": "ModelSuite", "Value": suites},
+             {"Name": "Precision", "Value": "AMP"},
+             {"Name": "WorkLoad", "Value": "Training"},
+        ]
+        s3_artifact_path = os.path.join(output_path,job_name,"output","output.tar.gz")
+        local_artifact = os.path.join(tmpdir, "output.tar.gz")
+        subprocess.check_output(["aws", "s3", "cp", s3_artifact_path, local_artifact])
+        with tarfile.open(local_artifact, "r:gz") as result:
+            result.extractall(path=tmpdir)
+        result_path = os.path.join(tmpdir, "benchmark", "bin","pytorch",f"{suites}_logs")
+        speedup = read_metric(os.path.join(result_path, "geomean.csv"))
+        comp_time = read_metric(os.path.join(result_path,"comp_time.csv"))
+        memory = read_metric(os.path.join(result_path,"memory.csv"))
+        passrate = read_metric(os.path.join(result_path,"passrate.csv"))
+        namespace = "PyTorch/SM/Benchmarks/TorchDynamo/Inductor"
+        put_metric_data("Speedup", namespace, "None", speedup, dimensions)
+        put_metric_data("CompilationTime", namespace, "Seconds", comp_time, dimensions)
+        put_metric_data("PeakMemoryFootprintCompressionRatio", namespace, "None", memory, dimensions)
+        put_metric_data("PassRate", namespace, "Percent", passrate, dimensions)
 
 
