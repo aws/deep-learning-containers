f725ba00f208b13f1d8eff3743b327fada2f2ee6
YuchengT <tyc7654321@gmail.com>
2023-04-06 20:38:16 +0000

    typo fix

--- test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
+++ test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
@@ -103,7 +103,6 @@ def test_performance_pytorch_gpu_inductor_torchbench(pytorch_training, ec2_conne
     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     if Version(image_framework_version) < Version("2.0"):
         pytest.skip("Torch inductor was introduced in PyTorch 2.0")
-    test_cmd = 
     execute_ec2_training_performance_test(
         ec2_connection, pytorch_training, PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_TORCHBENCH_CMD, ec2_instance_type, model_suite="torchbench"
     )
