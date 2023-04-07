1735b2936bb5cfcea67dffb36c5e5572b9247a4b
BruceChang9688 <dingheng9688@gmail.com>
2023-04-07 19:44:48 +0000

    move inductor performance test into benchmark folder

---
+++ test/dlc_tests/benchmark/sagemaker/__init__.py
@@ -0,0 +1,3 @@
+import os
+
+inductor_path = os.path.join(resources_path, 'inductor')
--- test/sagemaker_tests/pytorch/training/integration/__init__.py
+++ test/sagemaker_tests/pytorch/training/integration/__init__.py
@@ -26,7 +26,6 @@ fastai_path = os.path.join(resources_path, 'fastai')
 fastai_cifar_script = os.path.join(fastai_path, 'train_cifar.py')
 fastai_mnist_script = os.path.join(fastai_path, 'mnist.py')
 resnet18_path = os.path.join(resources_path, 'resnet18')
-inductor_path = os.path.join(resources_path, 'inductor')
 
 data_dir = os.path.join(mnist_path, 'data')
 training_dir = os.path.join(data_dir, 'training')
