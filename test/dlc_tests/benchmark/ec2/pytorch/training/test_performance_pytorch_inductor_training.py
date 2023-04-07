47518e6555665d84d460af9d401d01944709aa9f
Yucheng Tu <109311506+YuchengT@users.noreply.github.com>
2023-04-07 13:07:20 -0700

    pass s3_pth directly into test_cmd

--- test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
+++ test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
@@ -40,14 +40,14 @@ PT_EC2_GPU_INDUCTOR_INSTANCE_TYPE_G4DN = ["g4dn.4xlarge"]
 @pytest.mark.parametrize("ec2_instance_ami", [UBUNTU_18_BASE_DLAMI_US_WEST_2], indirect=True)
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INDUCTOR_INSTANCE_TYPE_P3, indirect=True)
 def test_performance_pytorch_gpu_inductor_huggingface_p3(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
-    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
+    fw, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     current_timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
-    s3_key = os.path.join(PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD, current_timestamp)
+    s3_key = os.path.join(BENCHMARK_RESULTS_S3_BUCKET_TRCOMP, fw, PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD, current_timestamp)
     if Version(image_framework_version) < Version("2.0"):
         pytest.skip("Torch inductor was introduced in PyTorch 2.0")
-    test_cmd = PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD + " " + ec2_instance_type
+    test_cmd = PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD + " " + ec2_instance_type + " " + s3_key
     execute_ec2_training_performance_test(
-        ec2_connection, pytorch_training, test_cmd, s3_key, "huggingface"
+        ec2_connection, pytorch_training, test_cmd, "huggingface"
     )
     trcomp_perf_data_io(ec2_connection, os.path.join(".", os.sep, "huggingface", os.sep, current_timestamp), s3_key, fw="pytorch", is_upload=False)
     read_upload_benchmarking_result_to_cw("Speedup", os.path.join(".", os.sep, "huggingface", os.sep, current_timestamp))
@@ -64,7 +64,7 @@ def test_performance_pytorch_gpu_inductor_huggingfac_p4d(pytorch_training, ec2_c
         pytest.skip("Torch inductor was introduced in PyTorch 2.0")
     test_cmd = PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD + " " + ec2_instance_type
     execute_ec2_training_performance_test(
-        ec2_connection, pytorch_training, test_cmd, s3_key, "huggingface"
+        ec2_connection, pytorch_training, test_cmd, "huggingface"
     )
     
 @pytest.mark.skip("skip for now")
@@ -79,7 +79,7 @@ def test_performance_pytorch_gpu_inductor_huggingface_g5(pytorch_training, ec2_c
         pytest.skip("Torch inductor was introduced in PyTorch 2.0")
     test_cmd = PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD + " " + ec2_instance_type
     execute_ec2_training_performance_test(
-        ec2_connection, pytorch_training, test_cmd, s3_key, "huggingface",
+        ec2_connection, pytorch_training, test_cmd, "huggingface",
     )
 
 @pytest.mark.skip("skip for now")
@@ -94,7 +94,7 @@ def test_performance_pytorch_gpu_inductor_huggingface_g4dn(pytorch_training, ec2
         pytest.skip("Torch inductor was introduced in PyTorch 2.0")
     test_cmd = PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD + " " + ec2_instance_type
     execute_ec2_training_performance_test(
-        ec2_connection, pytorch_training, test_cmd, s3_key, "huggingface"
+        ec2_connection, pytorch_training, test_cmd, "huggingface"
     )
     
 @pytest.mark.skip("skip for now")
@@ -125,7 +125,7 @@ def test_performance_pytorch_gpu_inductor_torchbench(pytorch_training, ec2_conne
 
 
 def execute_ec2_training_performance_test(
-    connection, ecr_uri, test_cmd, s3_key, model_suite, region=DEFAULT_REGION):
+    connection, ecr_uri, test_cmd, model_suite, region=DEFAULT_REGION):
     fw, image_framework_version = get_framework_and_version_from_tag(ecr_uri)
 
     docker_cmd = "nvidia-docker" if "gpu" in ecr_uri else "docker"
@@ -134,8 +134,6 @@ def execute_ec2_training_performance_test(
     timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
     log_name = f"{model_suite}_results_{os.getenv('CODEBUILD_RESOLVED_SOURCE_VERSION')}_{timestamp}.txt"
     log_location = os.path.join(os.sep, "test", "benchmark", "logs", log_name)
-
-    s3_pth = os.path.join(BENCHMARK_RESULTS_S3_BUCKET_TRCOMP, fw, s3_key)
 
     # Make sure we are logged into ECR so we can pull the image
     connection.run(f"$(aws ecr get-login --no-include-email --region {region})", hide=True)
@@ -146,7 +144,7 @@ def execute_ec2_training_performance_test(
         f"-e LOG_FILE={os.path.join(os.sep, 'test', 'benchmark', 'logs', log_name)} "
         f"-e PR_CONTEXT={1 if is_pr_context() else 0} "
         f"-v {container_test_local_dir}:{os.path.join(os.sep, 'test')} {ecr_uri} "
-        f"{os.path.join(os.sep, 'bin', 'bash')} {test_cmd} {s3_pth}")
+        f"{os.path.join(os.sep, 'bin', 'bash')} {test_cmd}")
 
 
 def read_upload_benchmarking_result_to_cw(metric_name, pth, precision="amp", instance_type="p4d.24xlarge", model_suite="huggingface", namespace="PyTorch/EC2/Benchmarks/TorchDynamo/Inductor"):
