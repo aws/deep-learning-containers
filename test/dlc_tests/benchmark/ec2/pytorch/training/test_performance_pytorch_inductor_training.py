8dc96d2b3fdbdcdd8ec0a6ae893e4544c7239fd4
Ubuntu <ubuntu@ip-172-31-7-132.us-west-2.compute.internal>
2023-04-08 07:31:37 +0000

    generate results for all models

--- test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
+++ test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
@@ -14,7 +14,6 @@ from test.test_utils import (
 )
 
 from test.test_utils.ec2 import (
-    trcomp_perf_data_io,
     read_metric,
     put_metric_data,
 )
@@ -30,17 +29,14 @@ PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_TORCHBENCH_CMD = os.path.join(
 )
 
 PT_EC2_GPU_INDUCTOR_INSTANCE_TYPES = ["p3.2xlarge", "p4d.24xlarge", "g5.4xlarge", "g4dn.4xlarge"]
-PT_EC2_GPU_INDUCTOR_INSTANCE_TYPE_P3 = ["p3.2xlarge"]
-PT_EC2_GPU_INDUCTOR_INSTANCE_TYPE_P4D = ["p4d.24xlarge"]
-PT_EC2_GPU_INDUCTOR_INSTANCE_TYPE_G5 = ["g5.4xlarge"]
-PT_EC2_GPU_INDUCTOR_INSTANCE_TYPE_G4DN = ["g4dn.4xlarge"]
+METRIC_NAMES = ["speedup", "comp_time", "memory", "passrate"]
 
 
 @pytest.mark.integration("inductor")
 @pytest.mark.model("huggingface")
 @pytest.mark.parametrize("ec2_instance_ami", [UBUNTU_18_BASE_DLAMI_US_WEST_2], indirect=True)
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INDUCTOR_INSTANCE_TYPE_P3, indirect=True)
-def test_performance_pytorch_gpu_inductor_huggingface_p3(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
+@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INDUCTOR_INSTANCE_TYPES, indirect=True)
+def test_performance_pytorch_gpu_inductor_huggingface(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
     fw, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     current_timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
     s3_key = os.path.join(PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD, current_timestamp)
@@ -51,54 +47,11 @@ def test_performance_pytorch_gpu_inductor_huggingface_p3(pytorch_training, ec2_c
     execute_ec2_training_performance_test(
         ec2_connection, pytorch_training, test_cmd, "huggingface"
     )
-    #trcomp_perf_data_io(ec2_connection, "~/geomean.csv", s3_key + "/geomean.csv", fw="pytorch", is_upload=False)
-    subprocess.run(f"aws s3 cp {s3_pth}/geomean.csv ~/geomean.csv", shell=True)
-    read_upload_benchmarking_result_to_cw("Speedup", "~")
+    subprocess.run(f"rm -rf huggingface", shell=True)
+    subprocess.run(f"mkdir huggingface", shell=True)
+    subprocess.run(f"aws s3 cp {s3_pth}/ huggingface/ --recursive", shell=True)
+    read_upload_benchmarking_result_to_cw(METRIC_NAMES, "huggingface", instance_type=ec2_instance_type)
 
-@pytest.mark.skip("skip for now")
-@pytest.mark.integration("inductor")
-@pytest.mark.model("huggingface")
-@pytest.mark.parametrize("ec2_instance_ami", [UBUNTU_18_BASE_DLAMI_US_WEST_2], indirect=True)
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INDUCTOR_INSTANCE_TYPE_P4D, indirect=True)
-def test_performance_pytorch_gpu_inductor_huggingfac_p4d(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
-    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
-    s3_key = os.path.join(PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD, time.strftime("%Y-%m-%d-%H-%M-%S"))
-    if Version(image_framework_version) < Version("2.0"):
-        pytest.skip("Torch inductor was introduced in PyTorch 2.0")
-    test_cmd = PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD + " " + ec2_instance_type
-    execute_ec2_training_performance_test(
-        ec2_connection, pytorch_training, test_cmd, "huggingface"
-    )
-    
-@pytest.mark.skip("skip for now")
-@pytest.mark.integration("inductor")
-@pytest.mark.model("huggingface")
-@pytest.mark.parametrize("ec2_instance_ami", [UBUNTU_18_BASE_DLAMI_US_WEST_2], indirect=True)
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INDUCTOR_INSTANCE_TYPE_G5, indirect=True)
-def test_performance_pytorch_gpu_inductor_huggingface_g5(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
-    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
-    s3_key = os.path.join(PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD, time.strftime("%Y-%m-%d-%H-%M-%S"))
-    if Version(image_framework_version) < Version("2.0"):
-        pytest.skip("Torch inductor was introduced in PyTorch 2.0")
-    test_cmd = PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD + " " + ec2_instance_type
-    execute_ec2_training_performance_test(
-        ec2_connection, pytorch_training, test_cmd, "huggingface",
-    )
-
-@pytest.mark.skip("skip for now")
-@pytest.mark.integration("inductor")
-@pytest.mark.model("huggingface")
-@pytest.mark.parametrize("ec2_instance_ami", [UBUNTU_18_BASE_DLAMI_US_WEST_2], indirect=True)
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INDUCTOR_INSTANCE_TYPE_G4DN, indirect=True)
-def test_performance_pytorch_gpu_inductor_huggingface_g4dn(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
-    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
-    s3_key = os.path.join(PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD, time.strftime("%Y-%m-%d-%H-%M-%S"))
-    if Version(image_framework_version) < Version("2.0"):
-        pytest.skip("Torch inductor was introduced in PyTorch 2.0")
-    test_cmd = PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_HUGGINGFACE_CMD + " " + ec2_instance_type
-    execute_ec2_training_performance_test(
-        ec2_connection, pytorch_training, test_cmd, "huggingface"
-    )
     
 @pytest.mark.skip("skip for now")
 @pytest.mark.integration("inductor")
@@ -106,25 +59,41 @@ def test_performance_pytorch_gpu_inductor_huggingface_g4dn(pytorch_training, ec2
 @pytest.mark.parametrize("ec2_instance_ami", [UBUNTU_18_BASE_DLAMI_US_WEST_2], indirect=True)
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INDUCTOR_INSTANCE_TYPES, indirect=True)
 def test_performance_pytorch_gpu_inductor_timm(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
-    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
+    fw, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
+    current_timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+    s3_key = os.path.join(PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_TIMM_CMD, current_timestamp)
+    s3_pth = BENCHMARK_RESULTS_S3_BUCKET_TRCOMP + fw + s3_key
     if Version(image_framework_version) < Version("2.0"):
         pytest.skip("Torch inductor was introduced in PyTorch 2.0")
+    test_cmd = PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_TIMM_CMD + " " + ec2_instance_type + " " + BENCHMARK_RESULTS_S3_BUCKET_TRCOMP + fw + s3_key
     execute_ec2_training_performance_test(
-        ec2_connection, pytorch_training, PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_TIMM_CMD, model_suite="timm"
+        ec2_connection, pytorch_training, test_cmd, "timm_models"
     )
+    subprocess.run(f"rm -rf timm", shell=True)
+    subprocess.run(f"mkdir timm", shell=True)
+    subprocess.run(f"aws s3 cp {s3_pth}/ timm/ --recursive", shell=True)
+    read_upload_benchmarking_result_to_cw(METRIC_NAMES, "timm_models", instance_type=ec2_instance_type, model_suite="timm_models")
 
-@pytest.mark.skip("skip for now")
+
 @pytest.mark.integration("inductor")
 @pytest.mark.model("torchbench")
 @pytest.mark.parametrize("ec2_instance_ami", [UBUNTU_18_BASE_DLAMI_US_WEST_2], indirect=True)
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INDUCTOR_INSTANCE_TYPES, indirect=True)
 def test_performance_pytorch_gpu_inductor_torchbench(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
-    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
+    fw, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
+    current_timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+    s3_key = os.path.join(PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_TORCHBENCH_CMD, current_timestamp)
+    s3_pth = BENCHMARK_RESULTS_S3_BUCKET_TRCOMP + fw + s3_key
     if Version(image_framework_version) < Version("2.0"):
         pytest.skip("Torch inductor was introduced in PyTorch 2.0")
+    test_cmd = PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_TORCHBENCH_CMD + " " + ec2_instance_type + " " + BENCHMARK_RESULTS_S3_BUCKET_TRCOMP + fw + s3_key
     execute_ec2_training_performance_test(
-        ec2_connection, pytorch_training, PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_TORCHBENCH_CMD, model_suite="torchbench"
+        ec2_connection, pytorch_training, test_cmd, "torchbench"
     )
+    subprocess.run(f"rm -rf torchbench")
+    subprocess.run(f"mkdir torchbench")
+    subprocess.run(f"aws s3 cp {s3_pth}/ torchbench/ --recursive", shell=True)
+    read_upload_benchmarking_result_to_cw(METRIC_NAMES, "torchbench", instance_type=ec2_instance_type, model_suite="torchbench")
 
 
 def execute_ec2_training_performance_test(
@@ -150,7 +119,7 @@ def execute_ec2_training_performance_test(
         f"{os.path.join(os.sep, 'bin', 'bash')} {test_cmd}")
 
 
-def read_upload_benchmarking_result_to_cw(metric_name, pth, precision="amp", instance_type="p4d.24xlarge", model_suite="huggingface", namespace="PyTorch/EC2/Benchmarks/TorchDynamo/Inductor"):
+def read_upload_benchmarking_result_to_cw(metric_names, pth, instance_type="p4d.24xlarge", model_suite="huggingface", precision="amp", namespace="PyTorch/EC2/Benchmarks/TorchDynamo/Inductor"):
     
     dimensions = [
              {"Name": "InstanceType", "Value": instance_type},
@@ -158,8 +127,19 @@ def read_upload_benchmarking_result_to_cw(metric_name, pth, precision="amp", ins
              {"Name": "Precision", "Value": precision},
              {"Name": "WorkLoad", "Value": "Training"},
          ]
-    if metric_name == "Speedup":
-        value = read_metric(os.path.join(pth, "geomean.csv"))
-        unit = "None"
-    put_metric_data(metric_name, namespace, unit, value, dimensions)
+    for name in metric_names:
+        if name == "speedup":
+            value = read_metric(os.path.join(pth, "geomean.csv"))
+            unit = "None"
+        if name == "comp_time":
+            value = read_metric(os.path.join(pth, "comp_time.csv"))
+            unit = "Seconds"
+        if name == "memory":
+            value = read_metric(os.path.join(pth, "memory.csv"))
+            unit = "None"
+        if name == "pass_rate":
+            value = read_metric(os.path.join(pth, "pass_rate.csv"))
+            unit = "Percent"
+
+        put_metric_data(name, namespace, unit, value, dimensions)
 
