6472db441d9b3f9a0d9b0f23174eac0ebdecec78
Shibo Xing <shibox@amazon.com>
2023-04-11 17:32:42 +0000

    test: remove native launcher tests

--- test/sagemaker_tests/pytorch/training/integration/sagemaker/test_mnist.py
+++ test/sagemaker_tests/pytorch/training/integration/sagemaker/test_mnist.py
@@ -52,56 +52,6 @@ def test_mnist_distributed_gpu(framework_version, ecr_image, sagemaker_regions, 
 
     invoke_pytorch_helper_function(ecr_image, sagemaker_regions, _test_mnist_distributed, function_args)
 
-@pytest.mark.processor("cpu")
-@pytest.mark.model("mnist")
-@pytest.mark.multinode(2)
-@pytest.mark.integration("smexperiments")
-@pytest.mark.skip_gpu
-def test_mnist_with_native_launcher_distributed_cpu(framework_version, ecr_image, sagemaker_regions, instance_type, dist_cpu_backend):
-    instance_type = instance_type or 'ml.c4.xlarge'
-    function_args = {
-            'framework_version': framework_version,
-            'instance_type': instance_type,
-            'native_launcher': True,
-            'dist_backend': dist_cpu_backend
-        }
-
-    invoke_pytorch_helper_function(ecr_image, sagemaker_regions, _test_mnist_with_native_launcher_distributed, function_args)
-
-
-@pytest.mark.processor("gpu")
-@pytest.mark.model("mnist")
-@pytest.mark.multinode(2)
-@pytest.mark.integration("smexperiments")
-@pytest.mark.skip_cpu
-def test_mnist_with_native_launcher_distributed_gpu(framework_version, ecr_image, sagemaker_regions, instance_type, dist_gpu_backend):
-    instance_type = instance_type or 'ml.g4dn.12xlarge'
-    function_args = {
-            'framework_version': framework_version,
-            'instance_type': instance_type,
-            'native_launcher': True,
-            'dist_backend': dist_gpu_backend
-        }
-
-    invoke_pytorch_helper_function(ecr_image, sagemaker_regions, _test_mnist_with_native_launcher_distributed, function_args)
-
-
-def _test_mnist_with_native_launcher_distributed(ecr_image, sagemaker_session, framework_version, instance_type, dist_backend, native_launcher):
-    with timeout(minutes=DEFAULT_TIMEOUT):
-        pytorch = PyTorch(
-            entry_point=mnist_script,
-            role='SageMakerRole',
-            instance_count=2,
-            instance_type=instance_type,
-            sagemaker_session=sagemaker_session,
-            image_uri=ecr_image,
-            framework_version=framework_version,
-            hyperparameters={'backend': dist_backend, 'epochs': 1, 'sagemaker_toolkit_native_launcher_enabled': native_launcher},
-        )
-        training_input = pytorch.sagemaker_session.upload_data(path=training_dir, key_prefix='pytorch/mnist')
-        pytorch.fit({'training': training_input}, job_name=utils.unique_name_from_base('test-pt-mnist-distributed'))
-
-
 def _test_mnist_distributed(ecr_image, sagemaker_session, framework_version, instance_type, dist_backend):
     with timeout(minutes=DEFAULT_TIMEOUT):
         pytorch = PyTorch(
