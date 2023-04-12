ec856c128225d2cbb3ee555ef79dedf87cdd9acb
Ubuntu <ubuntu@ip-172-31-7-132.us-west-2.compute.internal>
2023-04-12 03:32:06 +0000

    rework the execution function

--- test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
+++ test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
@@ -86,32 +86,23 @@ def test_performance_pytorch_gpu_inductor_timm(pytorch_training, ec2_connection,
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INDUCTOR_INSTANCE_TYPES, indirect=True)
 def test_performance_pytorch_gpu_inductor_torchbench(pytorch_training, ec2_connection, gpu_only, py3_only, ec2_instance_type):
     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
-    current_timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
-    s3_key = os.path.join(os.sep, "torchbench", re.sub("\.", "-", ec2_instance_type), current_timestamp)
-    s3_pth = BENCHMARK_RESULTS_S3_BUCKET_TRCOMP + s3_key
     if Version(image_framework_version) < Version("2.0"):
         pytest.skip("Torch inductor was introduced in PyTorch 2.0")
-    test_cmd = PT_PERFORMANCE_TRAINING_GPU_INDUCTOR_TORCHBENCH_CMD + " " + ec2_instance_type + " " + BENCHMARK_RESULTS_S3_BUCKET_TRCOMP + s3_key
     execute_ec2_training_performance_test(
-        ec2_connection, pytorch_training, test_cmd, "torchbench"
-    )
-    logging.warning("done benchmarking!")
-    output1 = subprocess.check_output(f"rm -rf torchbench", shell=True, timeout=60)
-    output2 = subprocess.check_output(f"mkdir torchbench", shell=True, timeout=60)
-    logging.warning("done making dir!")
-    output3 = subprocess.check_output(f"aws s3 cp {s3_pth}/ torchbench/ --recursive", shell=True, timeout=60)
-    print(output3)
-    read_upload_benchmarking_result_to_cw(METRIC_NAMES, "torchbench", instance_type=ec2_instance_type, model_suite="torchbench")
-    logging.warning("done uploading metrics!")
+        ec2_connection, pytorch_training, "torchbench", ec2_instance_type)
 
 def execute_ec2_training_performance_test(
-    connection, ecr_uri, test_cmd, model_suite, region=DEFAULT_REGION):
+    connection, ecr_uri, model_suite, ec2_instance_type, region=DEFAULT_REGION):
     _, image_framework_version = get_framework_and_version_from_tag(ecr_uri)
+    timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+    s3_key = os.path.join(os.sep, model_suite, re.sub("\.", "-", ec2_instance_type), timestamp)
+    s3_pth = BENCHMARK_RESULTS_S3_BUCKET_TRCOMP + s3_key
+    test_prefix = os.path.join(CONTAINER_TESTS_PREFIX, "benchmark", f"run_pytorch_inductor_training_benchmark_gpu_{model_suite}")   
+    test_cmd = test_prefix + " " + ec2_instance_type + " " + s3_pth
 
     docker_cmd = "nvidia-docker" if "gpu" in ecr_uri else "docker"
     container_test_local_dir = os.path.join("$HOME", "container_tests")
 
-    timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
     log_name = f"{model_suite}_results_{os.getenv('CODEBUILD_RESOLVED_SOURCE_VERSION')}_{timestamp}.txt"
     log_location = os.path.join(os.sep, "test", "benchmark", "logs", log_name)
 
@@ -119,12 +110,19 @@ def execute_ec2_training_performance_test(
     connection.run(f"$(aws ecr get-login --no-include-email --region {region})", hide=True)
     connection.run(f"{docker_cmd} pull {ecr_uri}", hide='out') # this line fixes the time out issue
     # Run training command
-    connection.run(
-        f"{docker_cmd} run --user root "
-        f"-e LOG_FILE={os.path.join(os.sep, 'test', 'benchmark', 'logs', log_name)} "
-        f"-e PR_CONTEXT={1 if is_pr_context() else 0} "
-        f"-v {container_test_local_dir}:{os.path.join(os.sep, 'test')} {ecr_uri} "
-        f"{os.path.join(os.sep, 'bin', 'bash')} {test_cmd}", timeout=60)
+    try:
+        connection.run(
+            f"{docker_cmd} run --user root "
+            f"-e LOG_FILE={os.path.join(os.sep, 'test', 'benchmark', 'logs', log_name)} "
+            f"-e PR_CONTEXT={1 if is_pr_context() else 0} "
+            f"-v {container_test_local_dir}:{os.path.join(os.sep, 'test')} {ecr_uri} "
+            f"{os.path.join(os.sep, 'bin', 'bash')} {test_cmd}", timeout=60)
+    finally:
+        output1 = subprocess.check_output(f"rm -rf {model_suite}", shell=True, timeout=60)
+        output2 = subprocess.check_output(f"mkdir {model_suite}", shell=True, timeout=60)
+        output3 = subprocess.check_output(f"aws s3 cp {s3_pth}/ {model_suite}/ --recursive", shell=True, timeout=60)
+        read_upload_benchmarking_result_to_cw(METRIC_NAMES, model_suite, instance_type=ec2_instance_type, model_suite=model_suite)
+
 
 
 def read_upload_benchmarking_result_to_cw(metric_names, pth, instance_type="p4d.24xlarge", model_suite="huggingface", precision="amp", namespace="PyTorch/EC2/Benchmarks/TorchDynamo/Inductor"):
