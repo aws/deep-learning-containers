347a648545af57765f68e54a5074a4d32c848006
Ubuntu <ubuntu@ip-172-31-7-132.us-west-2.compute.internal>
2023-04-17 05:40:22 +0000

    set timeout in bash scripts

--- test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
+++ test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
@@ -23,7 +23,7 @@ from test.test_utils.ec2 import (
 PT_EC2_GPU_INDUCTOR_INSTANCE_TYPES = ["p3.2xlarge", "p4d.24xlarge", "g5.4xlarge", "g4dn.4xlarge"]
 METRIC_NAMES = ["speedup", "comp_time", "memory", "passrate"]
 
-@pytest.mark.timeout(300)
+
 @pytest.mark.integration("inductor")
 @pytest.mark.model("huggingface")
 @pytest.mark.parametrize("ec2_instance_ami", [UBUNTU_18_BASE_DLAMI_US_WEST_2], indirect=True)
@@ -36,7 +36,6 @@ def test_performance_pytorch_gpu_inductor_huggingface(pytorch_training, ec2_conn
         ec2_connection, pytorch_training, "huggingface", ec2_instance_type)
 
 
-@pytest.mark.timeout(300)
 @pytest.mark.integration("inductor")
 @pytest.mark.model("timm_models")
 @pytest.mark.parametrize("ec2_instance_ami", [UBUNTU_18_BASE_DLAMI_US_WEST_2], indirect=True)
@@ -51,7 +50,6 @@ def test_performance_pytorch_gpu_inductor_timm_models(pytorch_training, ec2_conn
         ec2_connection, pytorch_training, "timm_models", ec2_instance_type)
 
 
-@pytest.mark.timeout(300)
 @pytest.mark.integration("inductor")
 @pytest.mark.model("torchbench")
 @pytest.mark.parametrize("ec2_instance_ami", [UBUNTU_18_BASE_DLAMI_US_WEST_2], indirect=True)
@@ -83,21 +81,22 @@ def execute_ec2_training_performance_test(
     connection.run(f"$(aws ecr get-login --no-include-email --region {region})", hide=True)
     connection.run(f"{docker_cmd} pull {ecr_uri}", hide='out') # this line fixes the time out issue
     # Run training command
+    connection.run(
+        f"{docker_cmd} run --user root "
+        f"-e LOG_FILE={os.path.join(os.sep, 'test', 'benchmark', 'logs', log_name)} "
+        f"-e PR_CONTEXT={1 if is_pr_context() else 0} "
+        f"-v {container_test_local_dir}:{os.path.join(os.sep, 'test')} {ecr_uri} "
+        f"{os.path.join(os.sep, 'bin', 'bash')} {test_cmd}")
+
+    subprocess.check_output(f"rm -rf {model_suite}", shell=True)
+    subprocess.check_output(f"mkdir {model_suite}", shell=True)
     try:
-        connection.run(
-            f"{docker_cmd} run --user root "
-            f"-e LOG_FILE={os.path.join(os.sep, 'test', 'benchmark', 'logs', log_name)} "
-            f"-e PR_CONTEXT={1 if is_pr_context() else 0} "
-            f"-v {container_test_local_dir}:{os.path.join(os.sep, 'test')} {ecr_uri} "
-            f"{os.path.join(os.sep, 'bin', 'bash')} {test_cmd}")
-    finally:
-        subprocess.check_output(f"rm -rf {model_suite}", shell=True)
-        subprocess.check_output(f"mkdir {model_suite}", shell=True)
         subprocess.check_output(f"aws s3 cp {s3_pth}/ {model_suite}/ --recursive", shell=True)
-        try:
-            read_upload_benchmarking_result_to_cw(METRIC_NAMES, model_suite, instance_type=ec2_instance_type, model_suite=model_suite)
-        except:
-            print("Benchmarking was not finished in 5 minutes.")
+        read_upload_benchmarking_result_to_cw(METRIC_NAMES, model_suite, instance_type=ec2_instance_type, model_suite=model_suite)
+    except:
+        print("Benchmarking was not finished in 5 minutes.")
+    else:
+        print("Completed uploading metric data.")
 
 
 def read_upload_benchmarking_result_to_cw(metric_names, pth, instance_type="p4d.24xlarge", model_suite="huggingface", precision="amp", namespace="PyTorch/EC2/Benchmarks/TorchDynamo/Inductor"):
