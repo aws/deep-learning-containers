368bc47eb3d595240da58f6e3f6d7d564da410dd
ShiboXing <shx26@pitt.edu>
2023-04-21 18:53:15 -0400

    [PT-1.13] Upgrade SMModelParallel in Training SM DLC, Fix PT2.0 breaking changes (#2903)
    * Update ZeRO-2D binary for PT1.13 for SMMP 1.15.0 release
    
    * Update SMMP binary to 1.15.0 for PT1.13
    
    * Run Revision A tests + sagemaker_local_tests
    
    * Update test configuration
    
    * Update buildspec.yml to build only SageMaker GPU Docker Image
    
    * Run Revision B tests (SM rc)
    
    * Run Revision C tests (SM efa)
    
    * Update buildspec to run Revision A + sagemaker_local_tests
    Disable build_inference
    
    * Run Revision B test
    
    * Run Revision C tests
    
    * build: do sm gpu build
    
    * Update to run Revision A test
    
    * Update to run Revision B tests
    
    * Try enabling `sagemaker_local_tests`
    
    * Revision B tests
    
    * Re-enable sagemaker_local_tests
    
    * Revision B test
    
    * Test commit Revision A
    
    * Test commit revision B
    
    * Test Revision A
    
    * build: build and run efa
    
    * build: build and run standard sm
    
    * build: run ec2. rc sm tests
    
    * Upgrade flash-attn to 0.2.8 and add triton 2.0.0 to PT1.13 cu117 Dockerfile.gpu
    
    * Re-build container and run standard tests
    
    * fix: correct the scope of fixtures
    
    * build: turn off build, run efa sm tests
    
    * fix: upgrade cryptography for CVE
    
    * fix: remove default mamba installation to get rid of CVE
    
    * fix: add pytorch backend tests backward support
    
    * build: turn off buld
    
    * fix: cryptography dependency
    
    * fix: upgrade smdebug for pandas, model parallel
    
    * fix: horovod with rubik, cryptography version
    
    * test: use correct instance for inductor, run efa sm tests
    
    * revert cu118 changes
    
    * style: newline in buildspec
    
    * fix: autouse fixture usage with pytest_generate_tests, rerun sm standard
    
    * style: format using black
    
    * fix: skip_test fixtures, imports
    
    * build: run rc sm tests
    
    * Revert "style: format using black"
    
    This reverts commit 228f8caff435b23ee067e8d219c53d8b40035a58.
    
    * build: run sm efa
    
    * refactor: parameterize triton, add smmodelparallel comment
    
    * refactor: revert debug prints, patch email info
    
    * refactor: revert email info in patch
    
    * test: remove improper nvidia channel in torch installation
    
    * refactor: parametrize flash attention for rubik
    
    * fix: $HAS_CUDA var in install OSS pytorch
    
    * test: skip msg in skip_test fixtures
    
    * build: turn off build
    
    * refactor: revert necessary changes
    
    * build: revert config
    
    * style: more black formatting
    
    ---------
    
    Co-authored-by: Victor Zhu <viczhu@amazon.com>
    Co-authored-by: Victor Zhu <victorvctr.zh@gmail.com>
    Co-authored-by: Shibo Xing <shibox@amazon.com>

--- test/sagemaker_tests/pytorch/training/integration/sagemaker/test_dgl_inductor.py
+++ test/sagemaker_tests/pytorch/training/integration/sagemaker/test_dgl_inductor.py
@@ -40,8 +40,6 @@ inductor_instance_types = ["ml.p3.8xlarge", "ml.g5.12xlarge", "ml.g4dn.12xlarge"
 @pytest.mark.skip_py2_containers
 @pytest.mark.skip_inductor_test
 def test_dgl_gcn_training_cpu(ecr_image, sagemaker_regions, instance_type):
-    _, image_framework_version = get_framework_and_version_from_tag(ecr_image)
-
     instance_type = instance_type or "ml.c4.xlarge"
     function_args = {
         "instance_type": instance_type,
@@ -57,9 +55,6 @@ def test_dgl_gcn_training_cpu(ecr_image, sagemaker_regions, instance_type):
 @pytest.mark.skip_inductor_test
 @pytest.mark.parametrize("instance_type", inductor_instance_types, indirect=True)
 def test_dgl_gcn_training_gpu(ecr_image, sagemaker_regions, instance_type):
-    _, image_framework_version = get_framework_and_version_from_tag(ecr_image)
-    image_cuda_version = get_cuda_version_from_tag(ecr_image)
-
     instance_type = instance_type or "ml.p3.2xlarge"
     function_args = {
         "instance_type": instance_type,
