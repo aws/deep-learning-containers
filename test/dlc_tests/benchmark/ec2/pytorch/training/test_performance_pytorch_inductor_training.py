c35ad611eab42924752d809d46d21932511300eb
Ubuntu <ubuntu@ip-172-31-7-132.us-west-2.compute.internal>
2023-04-21 21:23:44 +0000

    skip g4dn

--- test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
+++ test/dlc_tests/benchmark/ec2/pytorch/training/test_performance_pytorch_inductor_training.py
@@ -59,6 +59,8 @@ def test_performance_pytorch_gpu_inductor_huggingface(pytorch_training, ec2_conn
     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     if Version(image_framework_version) < Version("2.0"):
         pytest.skip("Torch inductor was introduced in PyTorch 2.0")
+    if ec2_instance_type == "g4dn.4xlarge":
+        pytest.skip("takes more than 4 hours.")
     execute_ec2_training_performance_test(
         ec2_connection, pytorch_training, "huggingface", ec2_instance_type)
 
