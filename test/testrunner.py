3c27e39ec0857160475a8c8b5e7164a120336c21
Ubuntu <ubuntu@ip-172-31-7-132.us-west-2.compute.internal>
2023-04-25 06:57:47 +0000

    benchmarking mode process count increase

--- test/testrunner.py
+++ test/testrunner.py
@@ -396,7 +396,10 @@ def main():
                 raise Exception(f"EKS cluster {eks_cluster_name} is not in active state")
 
         # Execute dlc_tests pytest command
-        pytest_cmd = ["-s", "-rA", test_path, f"--junitxml={report}", "-n=100"]
+        if benchmark_mode:
+            pytest_cmd = ["-s", "-rA", test_path, f"--junitxml={report}", "-n=100"]
+        else:
+            pytest_cmd = ["-s", "-rA", test_path, f"--junitxml={report}", "-n=auto"]
 
         is_habana_image = any("habana" in image_uri for image_uri in all_image_list)
         if specific_test_type == "ec2":
