6b940700afc04fb18b77945b70a2545e047f301d
Mike Schneider <xncqr@amazon.com>
2023-05-11 08:06:58 -0700

    remove x86 pkgs update for benchmark

--- test/dlc_tests/benchmark/ec2/tensorflow/inference/test_performance_tensorflow_inference.py
+++ test/dlc_tests/benchmark/ec2/tensorflow/inference/test_performance_tensorflow_inference.py
@@ -104,7 +104,7 @@ def ec2_performance_tensorflow_inference(
     else:
         ec2_connection.run(f"pip3 install -U pip")
         ec2_connection.run(
-            f"pip3 install --user --upgrade boto3 grpcio 'protobuf<3.21' 'numpy<1.24' 'tensorflow-serving-api<={tf_api_version}'"
+            f"pip3 install --user boto3 grpcio 'tensorflow-serving-api<={tf_api_version}'"
         )
     time_str = time.strftime("%Y-%m-%d-%H-%M-%S")
     commit_info = os.getenv("CODEBUILD_RESOLVED_SOURCE_VERSION")
