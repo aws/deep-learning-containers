0c27501231308a9c1982620c1fcd7997a875a3af
Mike Schneider <104035434+xncqr@users.noreply.github.com>
2023-05-11 08:58:13 -0700

    [tensorflow][build] TFS 2.12.0 graviton (#2945)
    * initial addition of tfs 2.12 Graviton
    
    * setup for test
    
    * Update PIP packages and Python
    
    * downgrave protobuf
    
    * use correct arch binary.
    
    * again.. correct binary
    
    * update SM to use TFS_SHORT 2.12
    
    * adding gunicorn for MME
    
    * update binary to TFS 2.12.1
    
    * running benchmark
    
    * add protobuf update for graviton
    
    * black format
    
    * protobuf for x86_64 pic and method update.
    
    * reverse to /tmp/data/angeldog.jpg
    
    * update buildspec for TFS 2.12.1
    
    * update numpy for tfs benchmark
    
    * revert toml
    
    * revert graviton mode
    
    * update to docker and comments
    
    * protobuf upper pin <3.21
    
    * running benchmark
    
    * show needed fw version for inference 2.12.1
    
    * think versions were reversed
    
    * type and comment on why the Telemetry update
    
    * full test run
    
    * last benchmark
    
    * tests good, revert toml
    
    * remove x86 pkgs update for benchmark

--- test/dlc_tests/container_tests/bin/benchmark/tf2_serving_perf.py
+++ test/dlc_tests/container_tests/bin/benchmark/tf2_serving_perf.py
@@ -699,10 +699,11 @@ def get_perf(results):
 
         if len(mean_results) > 1:
             mean_results = mean_results[1:]  # REmove the first inference
-
-        stats["p50"] = np.percentile(mean_results, 50, interpolation="nearest")
-        stats["p90"] = np.percentile(mean_results, 90, interpolation="nearest")
-        stats["p99"] = np.percentile(mean_results, 99, interpolation="nearest")
+        # "interpolation" is being deprecated and throws warnings when used.
+        # "method" is the replacement.
+        stats["p50"] = np.percentile(mean_results, 50, method="nearest")
+        stats["p90"] = np.percentile(mean_results, 90, method="nearest")
+        stats["p99"] = np.percentile(mean_results, 99, method="nearest")
         stats["mean"] = np.mean(mean_results)
         stats["min"] = np.min(mean_results)
         stats["max"] = np.max(mean_results)
