cc8d06092dc4eb2376bd5c263fdfdb95f7d57443
Shantanu Tripathi <shantanutripathi237@gmail.com>
2023-05-12 18:48:29 -0700

    Run on p3.16xlarge

--- test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
+++ test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
@@ -68,6 +68,7 @@ def test_ec2_tensorflow_inference_gpu(
 def test_ec2_tensorflow_inference_gpu_tensorrt(
     tensorflow_inference, ec2_connection, region, gpu_only, ec2_instance_type
 ):
+    ec2_instance_type = "p3.16xlarge"
     if test_utils.is_image_incompatible_with_instance_type(tensorflow_inference, ec2_instance_type):
         pytest.skip(
             f"Image {tensorflow_inference} is incompatible with instance type {ec2_instance_type}"
