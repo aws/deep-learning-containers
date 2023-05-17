eca5c00df7e1dd75846f09029db7d32e5a1e32cc
arjkesh <33526713+arjkesh@users.noreply.github.com>
2023-05-17 13:40:03 -0700

    update toml

--- test/dlc_tests/sanity/quick_checks/test_generate_coverage_doc.py
+++ test/dlc_tests/sanity/quick_checks/test_generate_coverage_doc.py
@@ -17,9 +17,8 @@ TEST_COVERAGE_REPORT_BUCKET = f"dlc-test-coverage-reports-{ACCOUNT_ID}"
 @pytest.mark.quick_checks
 @pytest.mark.integration("Generating this coverage doc")
 @pytest.mark.model("N/A")
-@pytest.mark.skipif(
-    (is_mainline_context() and is_graviton_architecture()),
-    reason="Skipping the test for Graviton image build in mainline context as ARM image is used as a base",
+@pytest.mark.skip(
+    reason="Test is deprecated in favor of fixture 'require_custom_markers'. Keeping test around as an example of usage.",
 )
 def test_generate_coverage_doc():
     """
