c484d4e3c7a645dfa1d642128431795e963bbafa
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2023-05-26 19:46:57 -0700

    Make correction to build_all_reduce_perf.sh script

--- test/dlc_tests/container_tests/bin/efa/build_all_reduce_perf.sh
+++ test/dlc_tests/container_tests/bin/efa/build_all_reduce_perf.sh
@@ -2,12 +2,19 @@
 
 set -e
 
+# For dockerfiles of PyTorch >= 2.0, CUDA_HOME is already set as an env, and is configured as /opt/conda
+python -c "import torch; from packaging.version import Version; assert Version(torch.__version__) >= Version('2.0')"
+TORCH_VERSION_2x=$?
+if [ $TORCH_VERSION_2x -ne 0 ]; then
+  CUDA_HOME=/usr/local/cuda
+fi
+
 echo "Building all_reduce_perf from nccl-tests"
 cd /tmp/
 rm -rf nccl-tests/
 git clone https://github.com/NVIDIA/nccl-tests.git
 cd nccl-tests/
-make MPI=1 MPI_HOME=/opt/amazon/openmpi NCCL_HOME=/usr/local CUDA_HOME=/usr/local/cuda
+make MPI=1 MPI_HOME=/opt/amazon/openmpi NCCL_HOME=/usr/local CUDA_HOME=${CUDA_HOME}
 cp build/all_reduce_perf /all_reduce_perf
 cd /tmp/
 rm -rf nccl-tests/
