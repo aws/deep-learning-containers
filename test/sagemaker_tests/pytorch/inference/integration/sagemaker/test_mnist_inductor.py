740e8439a4ba6951733693067882533d97f666f9
Shantanu Tripathi <shantanutripathi237@gmail.com>
2023-06-15 16:45:48 -0700

    Add skip_stabilityai to other tests

--- test/sagemaker_tests/pytorch/inference/integration/sagemaker/test_mnist_inductor.py
+++ test/sagemaker_tests/pytorch/inference/integration/sagemaker/test_mnist_inductor.py
@@ -45,6 +45,7 @@ SM_GRAVITON_C7G = ["ml.c7g.4xlarge"]
 @pytest.mark.model("mnist")
 @pytest.mark.processor("cpu")
 @pytest.mark.cpu_test
+@pytest.mark.skip_stabilityai
 def test_mnist_distributed_cpu_inductor(
     framework_version, ecr_image, instance_type, sagemaker_regions
 ):
@@ -65,6 +66,7 @@ def test_mnist_distributed_cpu_inductor(
 @pytest.mark.model("mnist")
 @pytest.mark.processor("cpu")
 @pytest.mark.cpu_test
+@pytest.mark.skip_stabilityai
 @pytest.mark.parametrize("instance_type", SM_GRAVITON_C7G)
 def test_mnist_distributed_graviton_inductor(
     framework_version, ecr_image, instance_type, sagemaker_regions
@@ -87,6 +89,7 @@ def test_mnist_distributed_graviton_inductor(
 @pytest.mark.model("mnist")
 @pytest.mark.processor("gpu")
 @pytest.mark.gpu_test
+@pytest.mark.skip_stabilityai
 @pytest.mark.parametrize("instance_type", SM_SINGLE_GPU_INSTANCE_TYPES)
 def test_mnist_distributed_gpu_inductor(
     framework_version, ecr_image, instance_type, sagemaker_regions
