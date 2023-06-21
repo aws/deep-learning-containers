0c58c9bcd48ad98f011183b2f55638ad15fac333
Shantanu Tripathi <shantanutripathi237@gmail.com>
2023-06-21 11:14:09 -0700

    Change transformer to transformers

--- test/dlc_tests/sanity/test_pre_release.py
+++ test/dlc_tests/sanity/test_pre_release.py
@@ -694,7 +694,7 @@ def test_cuda_paths(gpu):
     python_version = re.search(r"(py\d+)", image).group(1)
     short_python_version = None
     image_tag = re.search(
-        r":(\d+(\.\d+){2}(-(transformer|diffusers)\d+(\.\d+){2})?-(gpu)-(py\d+)(-cu\d+)-(ubuntu\d+\.\d+)((-ec2)?-example|-ec2|-sagemaker-lite|-sagemaker-full|-sagemaker)?)",
+        r":(\d+(\.\d+){2}(-(transformers|diffusers)\d+(\.\d+){2})?-(gpu)-(py\d+)(-cu\d+)-(ubuntu\d+\.\d+)((-ec2)?-example|-ec2|-sagemaker-lite|-sagemaker-full|-sagemaker)?)",
         image,
     ).group(1)
 
