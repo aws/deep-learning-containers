bb8d29859d3496e9e364e5b5bcafd96f2be8d499
Lee Jake <gstu1130@gmail.com>
2023-06-27 14:04:12 -0700

    Address the comment

--- test/sagemaker_tests/pytorch/training/integration/sagemaker/test_gdrcopy.py
+++ test/sagemaker_tests/pytorch/training/integration/sagemaker/test_gdrcopy.py
@@ -24,6 +24,8 @@ from ...integration import (
 )
 from ...integration.sagemaker.timeout import timeout
 from . import invoke_pytorch_estimator
+from ....training import get_efa_test_instance_type
+
 
 RESOURCE_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "resources")
 
