4425660e4e63767dea35094aa733aae128961631
Danny Key <dannykec@amazon.com>
2023-07-18 23:32:57 +0000

    Force all build logs to display in codebuild

--- src/image.py
+++ src/image.py
@@ -210,7 +210,6 @@ class DockerImage:
                 self.summary["status"] = constants.STATUS_MESSAGE[self.build_status]
                 self.summary["end_time"] = datetime.now()
 
-                LOGGER.info(f"Docker Build Logs: \n {self.get_tail_logs_in_pretty_format(100)}")
                 LOGGER.error("ERROR during Docker BUILD")
                 LOGGER.error(
                     f"Error message received for {self.dockerfile} while docker build: {line}"
@@ -227,7 +226,7 @@ class DockerImage:
 
         self.log.append(response)
 
-        LOGGER.info(f"DOCKER BUILD LOGS: \n{self.get_tail_logs_in_pretty_format()}")
+        LOGGER.info(f"DOCKER BUILD LOGS: \n{self.log}")
         LOGGER.info(f"Completed Build for {self.repository}:{self.tag}")
 
         self.build_status = constants.SUCCESS
