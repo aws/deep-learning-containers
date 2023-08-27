12ef665d6919d3186389d8f82656a47bb1b6c3a2
Jeetendra Patil <jspatil@amazon.com>
2023-08-26 18:21:19 -0700

    run efa tests

--- test/dlc_tests/ec2/test_efa.py
+++ test/dlc_tests/ec2/test_efa.py
@@ -77,20 +77,22 @@ def test_pytorch_nccl_tests_using_efa(
         hide=False,
         timeout=300,
     )
-    run_cmd_on_container(
-        WORKER_CONTAINER_NAME,
-        master_connection,
-        "mamba uninstall libhwloc -y",
-        hide=False,
-        timeout=300,
-    )
-    run_cmd_on_container(
-        WORKER_CONTAINER_NAME,
-        master_connection,
-        "mamba list | grep libhwloc",
-        hide=False,
-        timeout=300,
-    )
+    
+    for worker_connection in efa_ec2_connections[1:]:
+        run_cmd_on_container(
+            WORKER_CONTAINER_NAME,
+            worker_connection,
+            "mamba uninstall libhwloc -y",
+            hide=False,
+            timeout=300,
+        )
+        run_cmd_on_container(
+            WORKER_CONTAINER_NAME,
+            worker_connection,
+            "mamba list | grep libhwloc",
+            hide=False,
+            timeout=300,
+        )
     run_cmd_on_container(
         MASTER_CONTAINER_NAME,
         master_connection,
