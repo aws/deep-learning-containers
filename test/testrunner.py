e573398dd2237b5110aac35060561af118185672
Jeetendra Patil <jspatil@amazon.com>
2023-09-06 09:54:13 -0700

    just run ecr scan

--- test/testrunner.py
+++ test/testrunner.py
@@ -402,6 +402,7 @@ def main():
                 raise Exception(f"EKS cluster {eks_cluster_name} is not in active state")
 
         # Execute dlc_tests pytest command
+        test_path = os.path.join(test_path, "test_ecr_scan.py")
         pytest_cmd = ["-s", "-rA", test_path, f"--junitxml={report}", "-n=auto"]
 
         is_habana_image = any("habana" in image_uri for image_uri in all_image_list)
