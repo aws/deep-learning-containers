058b40427a91df89d2fe7b6851a0d37672a05b36
Shantanu Tripathi <shantanutripathi237@gmail.com>
2023-09-22 10:30:45 -0700

    Change NCCL ARGS

--- test/dlc_tests/container_tests/bin/healthcheck_tests/nccl_test.sh
+++ test/dlc_tests/container_tests/bin/healthcheck_tests/nccl_test.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo "Start NCCL all_reduce local test"
-NCCL_ARGS="-b 8 -e 128M -f 2 -g 8"
+NCCL_ARGS="-b 4 -e 128M -f 2 -g 4"
 all_reduce_perf $NCCL_ARGS
 echo "Complete NCCL all_reduce local test"
