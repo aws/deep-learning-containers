8ae87c27e2f60c15ecbbfb8a777b866f0cecb80d
arjkesh <33526713+arjkesh@users.noreply.github.com>
2023-09-25 18:15:22 -0700

    update skip condition

--- test/dlc_tests/ec2/test_transformerengine.py
+++ test/dlc_tests/ec2/test_transformerengine.py
@@ -3,9 +3,7 @@ import os
 import pytest
 
 import test.test_utils.ec2 as ec2_utils
-from test.test_utils import (
-    CONTAINER_TESTS_PREFIX,
-)
+from test.test_utils import CONTAINER_TESTS_PREFIX, is_pr_context, is_efa_dedicated
 from test.test_utils.ec2 import get_efa_ec2_instance_type, filter_efa_instance_type
 
 PT_TE_TESTS_CMD = os.path.join(
@@ -25,10 +23,10 @@ EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION = get_efa_ec2_instance_type(
 @pytest.mark.usefixtures("sagemaker")
 @pytest.mark.allow_p4de_use
 @pytest.mark.parametrize("ec2_instance_type,region", EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION)
-# @pytest.mark.skipif(
-#     is_pr_context() and not is_efa_dedicated(),
-#     reason="Skip EFA test in PR context unless explicitly enabled",
-# )
+@pytest.mark.skipif(
+    is_pr_context() and not is_efa_dedicated(),
+    reason="Skip EFA test in PR context unless explicitly enabled",
+)
 def test_pytorch_transformerengine(
     pytorch_training, ec2_connection, region, ec2_instance_type, gpu_only, py3_only
 ):
