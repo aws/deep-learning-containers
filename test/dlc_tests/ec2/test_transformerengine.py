cc7e5357cd2c8dd56b6969bd7aab7eeed0a8c2f5
arjkesh <33526713+arjkesh@users.noreply.github.com>
2023-09-26 11:56:51 -0700

    Fast follow improvements for TE test experience

--- test/dlc_tests/ec2/test_transformerengine.py
+++ test/dlc_tests/ec2/test_transformerengine.py
@@ -2,8 +2,11 @@ import os
 
 import pytest
 
+from packaging.version import Version
+from packaging.specifiers import SpecifierSet
+
 import test.test_utils.ec2 as ec2_utils
-from test.test_utils import CONTAINER_TESTS_PREFIX, is_pr_context, is_efa_dedicated
+from test.test_utils import CONTAINER_TESTS_PREFIX, is_pr_context, is_efa_dedicated, get_framework_and_version_from_tag, get_cuda_version_from_tag
 from test.test_utils.ec2 import get_efa_ec2_instance_type, filter_efa_instance_type
 
 PT_TE_TESTS_CMD = os.path.join(
@@ -30,4 +33,9 @@ EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION = get_efa_ec2_instance_type(
 def test_pytorch_transformerengine(
     pytorch_training, ec2_connection, region, ec2_instance_type, gpu_only, py3_only
 ):
+    _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
+    image_cuda_version = get_cuda_version_from_tag(pytorch_training)
+    if not (Version(image_framework_version) in SpecifierSet(">=2.0") and image_cuda_version >= "cu121"):
+        pytest.skip("Test requires CUDA12 and PT 2.0 or greater")
+        
     ec2_utils.execute_ec2_training_test(ec2_connection, pytorch_training, PT_TE_TESTS_CMD)
