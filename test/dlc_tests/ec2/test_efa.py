181e478aa5217ca39cef5688c039d40acc7b484b
Xin Wang <xwnamz@amazon.com>
2023-09-26 19:38:48 +0000

    address review comments

--- test/dlc_tests/ec2/test_efa.py
+++ test/dlc_tests/ec2/test_efa.py
@@ -35,92 +35,90 @@ EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION = get_efa_ec2_instance_type(
     filter_function=filter_efa_instance_type,
 )
 
+@pytest.mark.processor("gpu")
+@pytest.mark.model("N/A")
+@pytest.mark.integration("efa")
+@pytest.mark.usefixtures("sagemaker")
+@pytest.mark.allow_p4de_use
+@pytest.mark.multinode(2)
+@pytest.mark.parametrize("ec2_instance_type,region", EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION)
+@pytest.mark.skipif(
+    is_pr_context() and not is_efa_dedicated(),
+    reason="Skip EFA test in PR context unless explicitly enabled",
+)
+def test_pytorch_efa(
+    pytorch_training, efa_ec2_instances, efa_ec2_connections, ec2_instance_type, region, gpu_only
+):
+    """
+    Run EFA Sanity tests on DLC, and then run NCCL Message Transfer and All Reduce tests using EFA
+    on multiple nodes using DLC images. The test scripts are agnostic to the framework and version
+    installed in the DLC image. The test also builds nccl-tests to create the all_reduce_perf
+    binary necessary for multinode tests, on each node.
+    Note: This test must be explicitly enabled on CI, and will only run on EFA-capable instances
+    on pipelines.
+    :param pytorch_training: str PyTorch Training DLC image URI
+    :param efa_ec2_instances: list of tuples of instance-ids and SSH-keys for EFA-enabled instances
+    :param efa_ec2_connections: list of Fabric Connection objects for EFA-enabled instances
+    :param ec2_instance_type: str Instance Type being tested
+    :param region: str Region in which EFA-enabled instances are launched
+    :param gpu_only: pytest fixture to limit test only to GPU DLCs
+    """
+    number_of_nodes = 2
+    _setup_multinode_efa_instances(
+        pytorch_training, efa_ec2_instances, efa_ec2_connections, ec2_instance_type, region
+    )
+    master_connection = efa_ec2_connections[0]
+    run_cmd_on_container(MASTER_CONTAINER_NAME, master_connection, EFA_SANITY_TEST_CMD, hide=False)
 
-# @pytest.mark.processor("gpu")
-# @pytest.mark.model("N/A")
-# @pytest.mark.integration("efa")
-# @pytest.mark.usefixtures("sagemaker")
-# @pytest.mark.allow_p4de_use
-# @pytest.mark.multinode(2)
-# @pytest.mark.parametrize("ec2_instance_type,region", EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION)
-# @pytest.mark.skipif(
-#     is_pr_context() and not is_efa_dedicated(),
-#     reason="Skip EFA test in PR context unless explicitly enabled",
-# )
-# def test_pytorch_efa(
-#     pytorch_training, efa_ec2_instances, efa_ec2_connections, ec2_instance_type, region, gpu_only
-# ):
-#     """
-#     Run EFA Sanity tests on DLC, and then run NCCL Message Transfer and All Reduce tests using EFA
-#     on multiple nodes using DLC images. The test scripts are agnostic to the framework and version
-#     installed in the DLC image. The test also builds nccl-tests to create the all_reduce_perf
-#     binary necessary for multinode tests, on each node.
-#     Note: This test must be explicitly enabled on CI, and will only run on EFA-capable instances
-#     on pipelines.
-#     :param pytorch_training: str PyTorch Training DLC image URI
-#     :param efa_ec2_instances: list of tuples of instance-ids and SSH-keys for EFA-enabled instances
-#     :param efa_ec2_connections: list of Fabric Connection objects for EFA-enabled instances
-#     :param ec2_instance_type: str Instance Type being tested
-#     :param region: str Region in which EFA-enabled instances are launched
-#     :param gpu_only: pytest fixture to limit test only to GPU DLCs
-#     """
-#     number_of_nodes = 2
-#     _setup_multinode_efa_instances(
-#         pytorch_training, efa_ec2_instances, efa_ec2_connections, ec2_instance_type, region
-#     )
-#     master_connection = efa_ec2_connections[0]
-#     run_cmd_on_container(MASTER_CONTAINER_NAME, master_connection, EFA_SANITY_TEST_CMD, hide=False)
+    run_cmd_on_container(
+        MASTER_CONTAINER_NAME,
+        master_connection,
+        f"{EFA_INTEGRATION_TEST_CMD} {HOSTS_FILE_LOCATION} {number_of_nodes}",
+        hide=False,
+        timeout=300,
+    )
 
-#     run_cmd_on_container(
-#         MASTER_CONTAINER_NAME,
-#         master_connection,
-#         f"{EFA_INTEGRATION_TEST_CMD} {HOSTS_FILE_LOCATION} {number_of_nodes}",
-#         hide=False,
-#         timeout=300,
-#     )
-
-
-# @pytest.mark.processor("gpu")
-# @pytest.mark.model("N/A")
-# @pytest.mark.integration("efa")
-# @pytest.mark.usefixtures("sagemaker")
-# @pytest.mark.allow_p4de_use
-# @pytest.mark.multinode(2)
-# @pytest.mark.parametrize("ec2_instance_type,region", EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION)
-# @pytest.mark.skipif(
-#     is_pr_context() and not is_efa_dedicated(),
-#     reason="Skip EFA test in PR context unless explicitly enabled",
-# )
-# def test_efa_tensorflow(
-#     tensorflow_training, efa_ec2_instances, efa_ec2_connections, ec2_instance_type, region, gpu_only
-# ):
-#     """
-#     Run EFA Sanity tests on DLC, and then run NCCL Message Transfer and All Reduce tests using EFA
-#     on multiple nodes using DLC images. The test scripts are agnostic to the framework and version
-#     installed in the DLC image. The test also builds nccl-tests to create the all_reduce_perf
-#     binary necessary for multinode tests, on each node.
-#     Note: This test must be explicitly enabled on CI, and will only run on EFA-capable instances
-#     on pipelines.
-#     :param tensorflow_training: str PyTorch Training DLC image URI
-#     :param efa_ec2_instances: list of tuples of instance-ids and SSH-keys for EFA-enabled instances
-#     :param efa_ec2_connections: list of Fabric Connection objects for EFA-enabled instances
-#     :param ec2_instance_type: str Instance Type being tested
-#     :param region: str Region in which EFA-enabled instances are launched
-#     :param gpu_only: pytest fixture to limit test only to GPU DLCs
-#     """
-#     number_of_nodes = 2
-#     _setup_multinode_efa_instances(
-#         tensorflow_training, efa_ec2_instances, efa_ec2_connections, ec2_instance_type, region
-#     )
-#     master_connection = efa_ec2_connections[0]
-#     run_cmd_on_container(MASTER_CONTAINER_NAME, master_connection, EFA_SANITY_TEST_CMD, hide=False)
-#     run_cmd_on_container(
-#         MASTER_CONTAINER_NAME,
-#         master_connection,
-#         f"{EFA_INTEGRATION_TEST_CMD} {HOSTS_FILE_LOCATION} {number_of_nodes}",
-#         hide=False,
-#         timeout=300,
-#     )
+@pytest.mark.processor("gpu")
+@pytest.mark.model("N/A")
+@pytest.mark.integration("efa")
+@pytest.mark.usefixtures("sagemaker")
+@pytest.mark.allow_p4de_use
+@pytest.mark.multinode(2)
+@pytest.mark.parametrize("ec2_instance_type,region", EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION)
+@pytest.mark.skipif(
+    is_pr_context() and not is_efa_dedicated(),
+    reason="Skip EFA test in PR context unless explicitly enabled",
+)
+def test_efa_tensorflow(
+    tensorflow_training, efa_ec2_instances, efa_ec2_connections, ec2_instance_type, region, gpu_only
+):
+    """
+    Run EFA Sanity tests on DLC, and then run NCCL Message Transfer and All Reduce tests using EFA
+    on multiple nodes using DLC images. The test scripts are agnostic to the framework and version
+    installed in the DLC image. The test also builds nccl-tests to create the all_reduce_perf
+    binary necessary for multinode tests, on each node.
+    Note: This test must be explicitly enabled on CI, and will only run on EFA-capable instances
+    on pipelines.
+    :param tensorflow_training: str PyTorch Training DLC image URI
+    :param efa_ec2_instances: list of tuples of instance-ids and SSH-keys for EFA-enabled instances
+    :param efa_ec2_connections: list of Fabric Connection objects for EFA-enabled instances
+    :param ec2_instance_type: str Instance Type being tested
+    :param region: str Region in which EFA-enabled instances are launched
+    :param gpu_only: pytest fixture to limit test only to GPU DLCs
+    """
+    number_of_nodes = 2
+    _setup_multinode_efa_instances(
+        tensorflow_training, efa_ec2_instances, efa_ec2_connections, ec2_instance_type, region
+    )
+    master_connection = efa_ec2_connections[0]
+    run_cmd_on_container(MASTER_CONTAINER_NAME, master_connection, EFA_SANITY_TEST_CMD, hide=False)
+    run_cmd_on_container(
+        MASTER_CONTAINER_NAME,
+        master_connection,
+        f"{EFA_INTEGRATION_TEST_CMD} {HOSTS_FILE_LOCATION} {number_of_nodes}",
+        hide=False,
+        timeout=300,
+    )
 
 @pytest.mark.processor("gpu")
 @pytest.mark.model("N/A")
--- test/dlc_tests/sanity/test_health_check.py
+++ test/dlc_tests/sanity/test_health_check.py
@@ -13,7 +13,6 @@ from test.test_utils import (
     ec2 as ec2_utils,
 )
 
-DCGM_TEST_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "healthcheck_tests", "dcgm_test.sh")
 EFA_LOCAL_TEST_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "healthcheck_tests", "efa_checker_test.sh")
 NCCL_LOCAL_TEST_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "healthcheck_tests", "nccl_test.sh")
 PT_EC2_MULTI_GPU_INSTANCE_TYPE = ec2_utils.get_ec2_instance_type(
@@ -22,48 +21,6 @@ PT_EC2_MULTI_GPU_INSTANCE_TYPE = ec2_utils.get_ec2_instance_type(
     filter_function=ec2_utils.filter_only_multi_gpu,
 )
 
-
-@pytest.mark.usefixtures("sagemaker_only")
-@pytest.mark.usefixtures("pt201_and_above_only")
-@pytest.mark.processor("gpu")
-@pytest.mark.parametrize("ec2_instance_type", PT_EC2_MULTI_GPU_INSTANCE_TYPE, indirect=True)
-@pytest.mark.model("N/A")
-@pytest.mark.timeout(1500)
-@pytest.mark.integration("health_check")
-def test_health_check_dcgm(gpu, ec2_connection):
-    """
-    Run local DCGM test on Pytorch DLC
-    """
-    docker_cmd = "nvidia-docker"
-    account_id = test_utils.get_account_id_from_image_uri(gpu)
-    image_region = test_utils.get_region_from_image_uri(gpu)
-    dcgm_l1_timeout = 300
-    LOGGER.info(f"test_health_check_dcgm pulling image: {gpu}")
-    test_utils.login_to_ecr_registry(ec2_connection, account_id, image_region)
-    ec2_connection.run(f"{docker_cmd} pull {gpu}", hide="out", timeout=1200)
-
-    image = gpu
-    container_name = test_utils.get_container_name("health_check", image)
-    container_test_local_dir = os.path.join("$HOME", "container_tests")
-    bin_bash_cmd = "--entrypoint /bin/bash "
-    LOGGER.info(f"test_health_check_dcgm starting docker image: {gpu}")
-    ec2_connection.run(
-        f"{docker_cmd} run --name {container_name} "
-        f"-v {container_test_local_dir}:{os.path.join(os.sep, 'test')} -itd {bin_bash_cmd}{gpu}",
-        hide=False,
-    )
-
-    LOGGER.info(f"test_health_check_dcgm run {DCGM_TEST_CMD} on container")
-    executable = os.path.join(os.sep, "bin", "bash")
-    execution_command = (
-        f"{docker_cmd} exec --user root {container_name} {executable} -c '{DCGM_TEST_CMD}'"
-    )
-
-    run_output = ec2_connection.run(execution_command, hide=False, timeout=dcgm_l1_timeout)
-    if not run_output.ok:
-        raise RuntimeError(
-            f"Image {image} DCGM test {DCGM_TEST_CMD} failed: {run_output}"
-        )
 
 # @pytest.mark.usefixtures("sagemaker_only")
 # @pytest.mark.usefixtures("pt201_and_above_only")
