13cb111657048e6860eb381373a4f0589cbd9519
arjkesh <33526713+arjkesh@users.noreply.github.com>
2023-09-27 12:43:13 -0700

    rerun tests

--- test/dlc_tests/ec2/test_transformerengine.py
+++ test/dlc_tests/ec2/test_transformerengine.py
@@ -32,19 +32,17 @@ EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION = get_efa_ec2_instance_type(
 @pytest.mark.usefixtures("sagemaker")
 @pytest.mark.allow_p4de_use
 @pytest.mark.parametrize("ec2_instance_type,region", EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION)
-@pytest.mark.skipif(
-    is_pr_context() and not is_efa_dedicated(),
-    reason="Skip heavy instance test in PR context unless explicitly enabled",
-)
+# @pytest.mark.skipif(
+#     is_pr_context() and not is_efa_dedicated(),
+#     reason="Skip heavy instance test in PR context unless explicitly enabled",
+# )
 def test_pytorch_transformerengine(
     pytorch_training, ec2_connection, region, ec2_instance_type, gpu_only, py3_only
 ):
     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     image_framework_version = Version(image_framework_version)
     image_cuda_version = get_cuda_version_from_tag(pytorch_training)
-    if not (
-        image_framework_version in SpecifierSet(">=2.0") and image_cuda_version >= "cu121"
-    ):
+    if not (image_framework_version in SpecifierSet(">=2.0") and image_cuda_version >= "cu121"):
         pytest.skip("Test requires CUDA12 and PT 2.0 or greater")
 
     ec2_utils.execute_ec2_training_test(ec2_connection, pytorch_training, PT_TE_TESTS_CMD)
