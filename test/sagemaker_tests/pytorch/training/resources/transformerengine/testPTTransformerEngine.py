c187e9461eae3ca58b7338f1dbec4ceba0ca054c
arjkesh <33526713+arjkesh@users.noreply.github.com>
2023-10-12 14:26:48 -0700

    save progress

--- test/sagemaker_tests/pytorch/training/resources/transformerengine/testPTTransformerEngine.py
+++ test/sagemaker_tests/pytorch/training/resources/transformerengine/testPTTransformerEngine.py
@@ -264,6 +264,68 @@ class BasicTransformerLayer(torch.nn.Module):
         x = self.mlp(x)
 
         return x + res
+    
+
+    class BasicTEMLP(torch.nn.Module):
+    def __init__(self,
+                 hidden_size: int,
+                 ffn_hidden_size: int) -> None:
+        super().__init__()
+        self.linear1 = te.Linear(hidden_size, ffn_hidden_size, bias=True)
+        self.linear2 = te.Linear(ffn_hidden_size, hidden_size, bias=True)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = torch.nn.functional.gelu(x, approximate='tanh')
+        x = self.linear2(x)
+        return x
+
+class BasicTETransformerLayer(torch.nn.Module):
+    def __init__(self,
+                 hidden_size: int,
+                 ffn_hidden_size: int,
+                 num_attention_heads: int,
+                 layernorm_eps: int = 1e-5,
+                 attention_dropout: float = 0.1,
+                 hidden_dropout: float = 0.1):
+        super().__init__()
+        self.num_attention_heads = num_attention_heads
+        self.kv_channels = hidden_size // num_attention_heads
+        self.ln1 = te.LayerNorm(hidden_size, eps=layernorm_eps)
+        self.qkv_projection = te.Linear(hidden_size, 3 * hidden_size, bias=True)
+        self.attention = utils.DotProductAttention(
+            num_attention_heads=num_attention_heads,
+            kv_channels=self.kv_channels,
+            attention_dropout=attention_dropout,
+        )
+        self.projection = te.Linear(hidden_size, hidden_size, bias=True)
+        self.dropout = torch.nn.Dropout(hidden_dropout)
+        self.ln2 = te.LayerNorm(hidden_size, eps=layernorm_eps)
+        self.mlp = BasicTEMLP(
+            hidden_size=hidden_size,
+            ffn_hidden_size=ffn_hidden_size,
+        )
+
+    def forward(self,
+                x: torch.Tensor,
+                attention_mask: torch.Tensor):
+        res = x
+        x = self.ln1(x)
+
+        # Fused QKV projection
+        qkv = self.qkv_projection(x)
+        qkv = qkv.view(qkv.size(0), qkv.size(1), self.num_attention_heads, 3 * self.kv_channels)
+        q, k, v = torch.split(qkv, qkv.size(3) // 3, dim=3)
+
+        x = self.attention(q, k, v, attention_mask)
+        x = self.projection(x)
+        x = self.dropout(x)
+        x = res + x
+        res = x
+        x = self.ln2(x)
+        x = self.mlp(x)
+
+        return x + res
 
 
 if __name__ == "__main__":
@@ -276,19 +338,12 @@ if __name__ == "__main__":
 
     # Synthetic data
     x = torch.rand(sequence_length, batch_size, hidden_size).cuda().to(dtype=dtype)
-    dy = torch.rand(sequence_length, batch_size, hidden_size).cuda().to(dtype=dtype)
 
-    basic_transformer = BasicTransformerLayer(
-        hidden_size,
-        ffn_hidden_size,
-        num_attention_heads,
-    )
-    basic_transformer.to(dtype=dtype).cuda()
-    torch.manual_seed(1234)
-    y = basic_transformer(x, attention_mask=None)
-    speedometer(
-        basic_transformer,
-        x,
-        dy,
-        forward_kwargs={"attention_mask": None},
-    )
+basic_te_transformer = BasicTETransformerLayer(
+    hidden_size,
+    ffn_hidden_size,
+    num_attention_heads,
+)
+basic_te_transformer.to(dtype=dtype).cuda()
+utils.share_parameters_with_basic_te_model(basic_te_transformer, basic_transformer)
+
