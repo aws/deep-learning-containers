37047d537a3796b100045062ab32d33ab58bcb86
adimux <adamcherti@gmail.com>
2023-10-19 14:09:38 -0400

    Add smppy binary pt113 tf211 (#3397)
    Co-authored-by: Ting Zhou <ztlevi.work@gmail.com>

---
+++ test/sagemaker_tests/tensorflow/tensorflow2_training/integration/local/test_smppy_local.py
@@ -0,0 +1,58 @@
+# Copyright 2018-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+from __future__ import absolute_import
+
+import os, sys
+import subprocess
+
+import pytest
+from packaging.specifiers import SpecifierSet
+from packaging.version import Version
+from sagemaker.tensorflow import TensorFlow
+
+from test.test_utils import get_framework_and_version_from_tag
+
+RESOURCE_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "resources")
+MNIST_PATH = os.path.join(RESOURCE_PATH, "mnist")
+
+# only the latest version of sagemaker supports profiler
+subprocess.check_call([sys.executable, "-m", "pip", "install", "sagemaker>=2.180.0"])
+
+
+def _skip_if_image_is_not_compatible_with_smppy(image_uri):
+    _, framework_version = get_framework_and_version_from_tag(image_uri)
+    compatible_versions = SpecifierSet("==2.11.*")
+    if Version(framework_version) not in compatible_versions:
+        pytest.skip(f"This test only works for TF versions in {compatible_versions}")
+
+
+@pytest.mark.processor("gpu")
+@pytest.mark.integration("smppy")
+@pytest.mark.model("mnist")
+@pytest.mark.skip_cpu
+@pytest.mark.skip_py2_containers
+def test_smppy_mnist_local(docker_image, sagemaker_local_session, tmpdir):
+    _skip_if_image_is_not_compatible_with_smppy(docker_image)
+
+    script = os.path.join(MNIST_PATH, "mnist_smppy.py")
+    estimator = TensorFlow(
+        entry_point=script,
+        role="SageMakerRole",
+        instance_count=1,
+        instance_type="local_gpu",
+        sagemaker_session=sagemaker_local_session,
+        image_uri=docker_image,
+        output_path="file://{}".format(tmpdir),
+    )
+
+    estimator.fit("file://{}".format(os.path.join(MNIST_PATH, "data")))
---
+++ test/sagemaker_tests/tensorflow/tensorflow2_training/resources/mnist/mnist_smppy.py
@@ -0,0 +1,73 @@
+import argparse
+import glob
+import json
+import os
+
+import numpy as np
+import tensorflow as tf
+
+import smppy
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+
+    # hyperparameters sent by the client are passed as command-line arguments to the script.
+    parser.add_argument("--epochs", type=int, default=1)
+    # Data, model, and output directories
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--train", type=str, default=os.environ["SM_CHANNEL_TRAINING"])
+    parser.add_argument("--hosts", type=list, default=json.loads(os.environ["SM_HOSTS"]))
+    parser.add_argument("--current-host", type=str, default=os.environ["SM_CURRENT_HOST"])
+    parser.add_argument("--save-as-tf", type=bool, default=False)
+
+    return parser.parse_known_args()
+
+
+def _load_training_data(base_dir):
+    x_train = np.load(os.path.join(base_dir, "train", "x_train.npy"))
+    y_train = np.load(os.path.join(base_dir, "train", "y_train.npy"))
+    return x_train, y_train
+
+
+def _load_testing_data(base_dir):
+    x_test = np.load(os.path.join(base_dir, "test", "x_test.npy"))
+    y_test = np.load(os.path.join(base_dir, "test", "y_test.npy"))
+    return x_test, y_test
+
+
+args, unknown = _parse_args()
+
+model = tf.keras.models.Sequential(
+    [
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(512, activation=tf.nn.relu),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax),
+    ]
+)
+
+os.makedirs("/opt/ml/output/profiler/framework", exist_ok=True)
+smp = smppy.SMProfiler.instance()
+config = smppy.Config()
+config.profiler = {
+    "EnableCuda": "1",
+}
+smp.configure(config)
+
+smp.start_profiling()
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+x_train, y_train = _load_training_data(args.train)
+x_test, y_test = _load_testing_data(args.train)
+with smppy.annotate("Training"):
+    model.fit(x_train, y_train, epochs=args.epochs)
+with smppy.annotate("Evaluation"):
+    model.evaluate(x_test, y_test)
+
+smp.stop_profiling()
+
+smp_files = glob.glob("/opt/ml/output/profiler/framework/*.smpraw")
+assert len(smp_files) > 0, f"The local output folder doesn't contain any sagemaker profiler files"
+for f in smp_files:
+    assert os.path.getsize(f) > 0, f"sagemaker profiler file has size 0"
