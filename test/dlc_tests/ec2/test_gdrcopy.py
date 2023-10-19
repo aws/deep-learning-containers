8660fbf3c422407d6cb04219bf56a32419df9104
Sirut Buasai <sirutbuasai27@hotmail.com>
2023-10-18 20:59:10 -0700

    update dev toml

--- test/dlc_tests/ec2/test_gdrcopy.py
+++ test/dlc_tests/ec2/test_gdrcopy.py
@@ -24,10 +24,10 @@ EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION = get_efa_ec2_instance_type(
 @pytest.mark.parametrize(
     "ec2_instance_ami", [test_utils.UBUNTU_20_BASE_DLAMI_US_WEST_2], indirect=True
 )
-@pytest.mark.skipif(
-    is_pr_context() and not is_efa_dedicated(),
-    reason="Skip GDRCopy test in PR context unless explicitly enabled",
-)
+# @pytest.mark.skipif(
+#     is_pr_context() and not is_efa_dedicated(),
+#     reason="Skip GDRCopy test in PR context unless explicitly enabled",
+# )
 def test_gdrcopy(
     pytorch_training, ec2_connection, ec2_instance_type, region, gpu_only, pt113_and_above_only
 ):
