14c82349f86ab323fd44c77a5483d29482f42046
tejaschumbalkar <tejaschumbalkar@gmail.com>
2023-10-26 10:10:28 -0700

    [dgl] Add team marker for dgl tests

--- test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
+++ test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
@@ -226,6 +226,7 @@ def test_pytorch_linear_regression_cpu(pytorch_training, ec2_connection, cpu_onl
 @pytest.mark.model("gcn")
 @pytest.mark.skip_dgl_test
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
+@pytest.mark.team("dgl")
 def test_pytorch_train_dgl_gpu(
     pytorch_training, ec2_connection, ec2_instance_type, gpu_only, py3_only, skip_pt110
 ):
@@ -249,6 +250,7 @@ def test_pytorch_train_dgl_gpu(
 @pytest.mark.integration("dgl")
 @pytest.mark.model("gcn")
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_INSTANCE_TYPE, indirect=True)
+@pytest.mark.team("dgl")
 def test_pytorch_train_dgl_cpu(pytorch_training, ec2_connection, cpu_only, py3_only, skip_pt110):
     # DGL cpu ec2 test doesn't work on PT 1.10 DLC
     execute_ec2_training_test(ec2_connection, pytorch_training, PT_DGL_CMD)
