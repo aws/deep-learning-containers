75f2942c37e5c6b8d67d18380bd6eb719fca3925
Sirut Buasai <sirutbuasai27@hotmail.com>
2023-10-30 11:27:44 -0700

    rerun p5 tests with us-west-2

--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -208,7 +208,8 @@ SAGEMAKER_REMOTE_TEST_TYPE = "sagemaker"
 
 PUBLIC_DLC_REGISTRY = "763104351884"
 
-SAGEMAKER_EXECUTION_REGIONS = ["us-west-2", "us-east-1", "eu-west-1"]
+# SAGEMAKER_EXECUTION_REGIONS = ["us-west-2", "us-east-1", "eu-west-1"]
+SAGEMAKER_EXECUTION_REGIONS = ["us-west-2"]
 # Before SM GA with Trn1, they support launch of ml.trn1 instance only in us-east-1. After SM GA this can be removed
 SAGEMAKER_NEURON_EXECUTION_REGIONS = ["us-west-2"]
 SAGEMAKER_NEURONX_EXECUTION_REGIONS = ["us-east-1"]
