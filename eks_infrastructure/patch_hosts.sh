43054d0a81a2f6ffccdadeb7cb3e7033718e41b1
sirutBuasai <sirutbuasai27@hotmail.com>
2023-11-10 13:45:36 -0800

    maintain new json file for autopatching

--- eks_infrastructure/patch_hosts.sh
+++ eks_infrastructure/patch_hosts.sh
@@ -5,9 +5,9 @@ set -ex
 source ./helper.sh
 
 # Parse parameters from build_param.json config file
-EKS_CLUSTERS=($(jq -r '.eks_clusters[]' build_param.json))
-CONTEXTS=($(jq -r '.contexts[]' build_param.json))
-EKS_VERSION=$(jq -r '.eks_version' build_param.json)
+EKS_CLUSTERS=($(jq -r '.eks_clusters[]' patch_hosts.json))
+CONTEXTS=($(jq -r '.contexts[]' patch_hosts.json))
+EKS_VERSION=$(jq -r '.eks_version' patch_hosts.json)
 
 # Upgrade hosts to latest ami
 upgrade_nodegroup
