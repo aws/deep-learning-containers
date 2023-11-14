7239a1cabbeac81617efde7e06819fd421678d07
G Buasai <sbuasai@amazon.com>
2023-11-14 15:05:35 -0800

    use eks_version from build_param.json file instead

--- eks_infrastructure/patch_hosts.sh
+++ eks_infrastructure/patch_hosts.sh
@@ -7,7 +7,7 @@ source ./helper.sh
 # Parse parameters from build_param.json config file
 EKS_CLUSTERS=($(jq -r '.eks_clusters[]' patch_hosts.json))
 CONTEXTS=($(jq -r '.contexts[]' patch_hosts.json))
-EKS_VERSION=$(jq -r '.eks_version' patch_hosts.json)
+EKS_VERSION=$(jq -r '.eks_version' build_param.json)
 
 # Upgrade hosts to latest ami
 upgrade_nodegroup
