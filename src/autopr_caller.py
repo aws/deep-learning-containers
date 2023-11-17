56d1a164d9f0e4a2568e04c18118da8258b65f7b
Shantanu Tripathi <shantanutripathi237@gmail.com>
2023-11-17 07:20:47 +0000

    Add skips based on context

--- src/autopr_caller.py
+++ src/autopr_caller.py
@@ -201,12 +201,12 @@ def main():
     LOGGER.info(
         f"Message body to be sent to AutoPR Queue: {json.dumps(message_body_to_be_sent_to_autopr_queue)}"
     )
-    ## TODO: only allow pr creation on mainline
-    send_message_to_queue(
-        queue_name=AUTOPR_PROD_QUEUE,
-        queue_region=QUEUE_REGION,
-        message_body_string=json.dumps(message_body_to_be_sent_to_autopr_queue),
-    )
+    if os.getenv("BUILD_CONTEXT") == "MAINLINE":
+        send_message_to_queue(
+            queue_name=AUTOPR_PROD_QUEUE,
+            queue_region=QUEUE_REGION,
+            message_body_string=json.dumps(message_body_to_be_sent_to_autopr_queue),
+        )
 
 
 if __name__ == "__main__":
