c20db1a25bf7836319c0758ebc92c8a8cc8f3d75
Shantanu Tripathi <shantanutripathi237@gmail.com>
2023-11-27 08:11:40 +0000

    Add transfer logic

--- src/image_transfer.py
+++ src/image_transfer.py
@@ -1,6 +1,16 @@
 import os
+import sys
+import json
 import boto3
+import logging
+
 from invoke import run
+from botocore.exceptions import ClientError
+from datetime import datetime, timezone
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.StreamHandler(sys.stdout))
+LOGGER.setLevel(logging.INFO)
 
 
 def pull_image_locally_with_all_its_tags_attached(image_uri):
@@ -34,20 +44,65 @@ def get_benchmark_tested_image_uri_for_beta_image(autopatch_image_uri, benchmark
     return f"{beta_image_repo}:{benchmark_tag_in_beta}"
 
 
+def get_image_transfer_override_flags_from_s3():
+    try:
+        s3_client = boto3.client("s3")
+        sts_client = boto3.client("sts")
+        account_id = sts_client.get_caller_identity().get("Account")
+        result = s3_client.get_object(
+            Bucket=f"dlc-cicd-helper-{account_id}", Key="image_transfer_override_flags.json"
+        )
+        json_content = json.loads(result["Body"].read().decode("utf-8"))
+    except ClientError as e:
+        LOGGER.error("ClientError when performing S3/STS operation. Exception: {}".format(e))
+        json_content = {}
+    return json_content
+
+
+def is_image_transfer_enabled_by_override_flags(image_uri, image_transfer_override_flags):
+    commit_id = os.getenv("CODEBUILD_RESOLVED_SOURCE_VERSION", "default")
+    if commit_id in image_transfer_override_flags:
+        if not image_transfer_override_flags[commit_id] or image_uri in image_transfer_override_flags[commit_id]:
+            # If the list corresponding to the commit ID is empty, then return True. Otherwise, check if image uri
+            # is present in the list and return True if it is present.
+            LOGGER.info(f"[Override Enabled] Transfer override enabled for the image {image_uri}")
+            return True
+    return False
+
+
+def is_image_transferable(autopatch_image_uri, beta_image_uri, image_transfer_override_flags):
+    if is_image_transfer_enabled_by_override_flags(image_uri=autopatch_image_uri, image_transfer_override_flags=image_transfer_override_flags):
+        return True
+    beta_image_push_time = get_push_time_of_image_from_ecr(image_uri=beta_image_uri)
+    current_time = datetime.now(timezone.utc)
+    time_difference = current_time - beta_image_push_time
+    LOGGER.info(f"Beta image was built {time_difference} ago.")
+    total_time_difference_in_seconds = time_difference.total_seconds()
+    if total_time_difference_in_seconds >= 3 * 24 * 60 * 60:
+        # If Beta image was built more than 3 days ago
+        return True
+
+    return False
+
 def main():
     from test import test_utils
 
     dlc_images = test_utils.get_dlc_images()
     image_list = dlc_images.split(" ")
+
+    image_transfer_override_flags = get_image_transfer_override_flags_from_s3()
+
     for autopatch_image in image_list:
         print(autopatch_image)
         autopatch_image_tag_list = pull_image_locally_with_all_its_tags_attached(image_uri=autopatch_image)
         benchmark_tag_in_beta = get_benchmark_tag_attached_to_the_latest_image_in_beta(autopatch_image_tag_list=autopatch_image_tag_list)
         beta_latest_benchmark_image_uri = get_benchmark_tested_image_uri_for_beta_image(autopatch_image_uri=autopatch_image, benchmark_tag_in_beta=benchmark_tag_in_beta)
         beta_image_push_time = get_push_time_of_image_from_ecr(image_uri=beta_latest_benchmark_image_uri)
+        retval = is_image_transferable(autopatch_image_uri=autopatch_image, beta_image_uri=beta_latest_benchmark_image_uri, image_transfer_override_flags=image_transfer_override_flags)
         print(benchmark_tag_in_beta)
         print(beta_image_push_time)
         print(type(beta_image_push_time))
+        print(retval)
 
 
 if __name__ == "__main__":
