3da16aede58a4975c43f454d4d687d47f191ae99
Sirut Buasai <sirutbuasai27@hotmail.com>
2024-01-05 15:02:40 -0800

    use c5.18xlarge

--- test/test_utils/sagemaker.py
+++ test/test_utils/sagemaker.py
@@ -64,7 +64,7 @@ def assign_sagemaker_remote_job_instance_type(image):
     elif "gpu" in image:
         return "ml.p3.8xlarge"
     elif "tensorflow" in image:
-        return "ml.c5.9xlarge"
+        return "ml.c5.18xlarge"
         # return "ml.c4.4xlarge"
     else:
         return "ml.c4.8xlarge"
