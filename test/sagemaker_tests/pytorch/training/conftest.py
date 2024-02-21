06ec6c9e9825a1a3bfdcb6eb75f54d441466989a
Sirut Buasai <sirutbuasai27@hotmail.com>
2024-02-21 13:22:34 -0800

    fix smdebug

--- test/sagemaker_tests/pytorch/training/conftest.py
+++ test/sagemaker_tests/pytorch/training/conftest.py
@@ -505,7 +505,7 @@ def skip_smdebug_test(
         if (
             Version(image_framework_version) in SpecifierSet("==2.0.1")
             and Version(image_cuda_version.strip("cu")) == Version("121")
-        ) or Version(image_framework_version) in SpecifierSet("==2.1"):
+        ) or Version(image_framework_version) in SpecifierSet(">=2.1"):
             pytest.skip(
                 f"SM Profiler v1 is on path for deprecation, skipping this container with tag {image_framework_version}"
             )
