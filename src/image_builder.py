c6f25b2ffd89715d0fd198dacfa42c5e71ad6187
arjkesh <33526713+arjkesh@users.noreply.github.com>
2024-02-28 12:12:28 -0800

    update

--- src/image_builder.py
+++ src/image_builder.py
@@ -141,10 +141,7 @@ def image_builder(buildspec, image_types=[], device_types=[]):
         if is_nightly_build_context():
             additional_image_tags.append(tag_image_with_date(image_tag))
 
-        if build_context != "PR":
-            image_tag = tag_image_with_datetime(image_tag)
-        # If build is not enabled, we don't care about the datetime tag
-        elif is_build_enabled():
+        elif is_build_enabled() or build_context != "PR":
             # Order appears to matter in datetime tagging, so tag with no datetime first, then
             # set image_tag to have datetime
             no_datetime = image_tag
