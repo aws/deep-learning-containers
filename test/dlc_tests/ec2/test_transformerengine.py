429518c918fb3d9a23e2fbe7e3019f2c7063a9fe
Sirut Buasai <sirutbuasai27@hotmail.com>
2024-03-01 11:29:08 -0800

    uncomment efa test

--- test/dlc_tests/ec2/test_transformerengine.py
+++ test/dlc_tests/ec2/test_transformerengine.py
@@ -12,6 +12,7 @@ from test.test_utils import (
     is_efa_dedicated,
     get_framework_and_version_from_tag,
     get_cuda_version_from_tag,
+    are_heavy_instance_ec2_tests_enabled,
 )
 from test.test_utils.ec2 import get_efa_ec2_instance_type, filter_efa_instance_type
 
@@ -33,10 +34,10 @@ EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION = get_efa_ec2_instance_type(
 @pytest.mark.usefixtures("sagemaker")
 @pytest.mark.allow_p4de_use
 @pytest.mark.parametrize("ec2_instance_type,region", EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION)
-# @pytest.mark.skipif(
-#     is_pr_context() and not is_efa_dedicated(),
-#     reason="Skip heavy instance test in PR context unless explicitly enabled",
-# )
+@pytest.mark.skipif(
+    is_pr_context() and not are_heavy_instance_ec2_tests_enabled(),
+    reason="Skip heavy instance test in PR context unless explicitly enabled",
+)
 @pytest.mark.team("conda")
 def test_pytorch_transformerengine(
     pytorch_training, ec2_connection, region, ec2_instance_type, gpu_only, py3_only
