99888663098cd1042a2d11c1f25b2b71a10a1b19
arjkesh <33526713+arjkesh@users.noreply.github.com>
2024-03-27 15:05:11 -0700

    Check ml instance type for inductor test

--- test/sagemaker_tests/pytorch/training/integration/sagemaker/test_pytorchddp_inductor.py
+++ test/sagemaker_tests/pytorch/training/integration/sagemaker/test_pytorchddp_inductor.py
@@ -38,7 +38,7 @@ def can_run_pytorchddp(ecr_image):
 
 
 @pytest.mark.skipif(
-    os.getenv("SM_EFA_TEST_INSTANCE_TYPE") == "p5.48xlarge",
+    os.getenv("SM_EFA_TEST_INSTANCE_TYPE") == "ml.p5.48xlarge",
     reason="Low availability of instance type; Must ensure test works on new instances.",
 )
 @pytest.mark.processor("gpu")
