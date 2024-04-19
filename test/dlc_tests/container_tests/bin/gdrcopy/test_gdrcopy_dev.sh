eb6c38e5d77ea29b1b285f4f59d4ca40d0540ea1
Sirut Buasai <sirutbuasai27@hotmail.com>
2024-04-19 12:18:23 -0700

    refactor gdrcopy ec2 test

--- test/dlc_tests/container_tests/bin/gdrcopy/test_gdrcopy_dev.sh
+++ test/dlc_tests/container_tests/bin/gdrcopy/test_gdrcopy_dev.sh
@@ -12,21 +12,11 @@
 # language governing permissions and limitations under the License.
 
 #!/usr/bin/env bash
-gdrcopy_sanity > tmp_out
+# NOTE: This test script is used for GDRCopy v2.3 and below.
+# For GDRCopy v2.4 and above, use `test_gdrcopy.sh`
+sanity | grep 'Failures: 0, Error: 0' &> /dev/null
 if [ $? != 0 ]; then
     echo "GDRCopy Sanity check failed!"
     exit 1
 fi
-
-# NOTE: A grep guard clause is added because the old GDRCopy test (which is now moved to `test_gdrcopy_dev.sh`)
-# was checking an exit status of grep instead of the sanity test itself.
-# We are now moving towards checking the exit status of the sanity test,
-# but as a safety check we will continue to check the test output as well.
-cat tmp_out | grep 'Failed: 0' &> /dev/null
-if [ $? != 0 ]; then
-    echo "GDRCopy Sanity check passed but failed at grep output check!"
-    echo "Please examine the gdrcopy_sanity output to ensure the tests are passing properly"
-    exit 1
-fi
-
 echo "GDRCopy Sanity check succeed!"
