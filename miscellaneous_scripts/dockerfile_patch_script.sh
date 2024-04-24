7a14f788cd5d8eec54fe38119271c0d24158c8c9
Sirut Buasai <sirutbuasai27@hotmail.com>
2024-04-24 13:40:22 -0700

    install mkl

--- miscellaneous_scripts/dockerfile_patch_script.sh
+++ miscellaneous_scripts/dockerfile_patch_script.sh
@@ -31,21 +31,11 @@ mv $PATCHING_INFO_PATH/patch-details-current $PATCHING_INFO_PATH/patch-details
 chmod +x $PATCHING_INFO_PATH/patch-details/install_script_language.sh && \
 $PATCHING_INFO_PATH/patch-details/install_script_language.sh
 
-if [ $LATEST_RELEASED_IMAGE_URI == "763104351884.dkr.ecr.us-west-2.amazonaws.com/pytorch-training:2.0.1-gpu-py310-cu121-ubuntu20.04-sagemaker" ]; then
-    SMP_URL=https://smppy.s3.amazonaws.com/pytorch/cu121/smprof-0.3.334-cp310-cp310-linux_x86_64.whl
-    pip install --no-cache-dir -U ${SMP_URL}
-    echo "Installed SMP";
-fi
-
-if [ $LATEST_RELEASED_IMAGE_URI == "763104351884.dkr.ecr.us-west-2.amazonaws.com/pytorch-training:2.0.1-gpu-py310-cu118-ubuntu20.04-sagemaker" ]; then
-    SMP_URL=https://smppy.s3.amazonaws.com/pytorch/cu118/smprof-0.3.334-cp310-cp310-linux_x86_64.whl
-    pip install --no-cache-dir -U ${SMP_URL}
-    echo "Installed SMP";
-fi
-
 if [ $LATEST_RELEASED_IMAGE_URI == "763104351884.dkr.ecr.us-west-2.amazonaws.com/pytorch-training:1.13.1-gpu-py39-cu117-ubuntu20.04-ec2" ]; then
     pip install --no-cache-dir -U pybind11
-    echo "Installed pybind11";
+    echo "Installed pybind11"
+    conda install -c conda-forge "mkl<2024.1.0"
+    echo "Installed mkl"
 fi
 
 pip cache purge
