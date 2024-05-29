8fbafb61a94e64c3e8b1a55010db8f1ab21bfef0
Jesus Pequeno <jpeq@amazon.com>
2024-05-29 15:50:07 -0700

    added a job type container 'training' and 'inference'

--- src/prepare_dlc_dev_environment.py
+++ src/prepare_dlc_dev_environment.py
@@ -25,33 +25,69 @@ def get_args():
         "--frameworks",
         nargs="+",
         choices=["pytorch", "tensorflow"],
+        required=True,
         help="ML Framework for which to prepare developer enviornment",
+    )
+    parser.add_argument(
+        "--job_types",
+        nargs="+",
+        choices=["training", "inference"],
+        default=["training", "inference"],
+        help="Training and inference containers to prepare developer environment",
     )
 
     return parser.parse_args()
 
 
-def set_build_frameworks(frameworks):
-    """
-    Write a function that, given a list of frameworks, assembles a dictionary with key/value pairs:
-    {"build_frameworks": ["framework1", "framework2"]}
+class TomlOverrider:
+    def __init__(self):
+        self._overrides = {}
 
-    Make sure there are no repeats
+    def set_build_frameworks(self, frameworks):
+        """
+        Write a function that, given a list of frameworks, assembles a dictionary with key/value pairs:
+        {"build_frameworks": ["framework1", "framework2"]}
 
-    frameworks is a list
-    return a dictionary object
-    """
-    unique_frameworks = list(set(frameworks))
-    return {"build_frameworks": unique_frameworks}
-    #pass
+        Make sure there are no repeats
+
+        frameworks is a list
+        return a dictionary object
+        """
+        unique_frameworks = list(set(frameworks))
+        self._overrides["build_frameworks"] = unique_frameworks
+
+    def set_job_type(self, job_types):
+        """
+        Job type can be one of (or both) "training" or "inference"
+
+        If job_type is training, set build_training to True, and build_inference to False
+        If job type is inference, set build_training to False, and build_inference to True
+        If both are set, set both to true
+        """
+        build_training = "training" in job_types
+        build_inference = "inference" in job_types
+        self._overrides["build_training"] = build_training
+        self._overrides["build_inference"] = build_inference
+
+    @property
+    def overrides(self):
+        return self._overrides
 
 
 def main():
     args = get_args()
     frameworks = args.frameworks
-    LOGGER.info(f"Inferring framework to be {frameworks}...")
-    LOGGER.info(set_build_frameworks(frameworks))
+    job_types = args.job_types
 
+    LOGGER.info(f"Inferring framework to be {frameworks}...")
+
+    overrider = TomlOverrider()
+
+    # Handle frameworks to build
+    overrider.set_build_frameworks(frameworks=frameworks)
+    overrider.set_job_type(job_types=job_types)
+
+    LOGGER.info(overrider.overrides)
 
 if __name__ == "__main__":
     main()
--- test/dlc_tests/sanity/quick_checks/test_prepare_dev_env.py
+++ test/dlc_tests/sanity/quick_checks/test_prepare_dev_env.py
@@ -7,4 +7,17 @@ from src import prepare_dlc_dev_environment
 @pytest.mark.model("N/A")
 @pytest.mark.integration("build_frameworks")
 def test_build_frameworks():
-    assert prepare_dlc_dev_environment.set_build_frameworks(("pytorch", "tensorflow")) == {"build_frameworks": ["pytorch", "tensorflow"]}
+    overrider = prepare_dlc_dev_environment.TomlOverrider()
+    overrider.set_build_frameworks(("pytorch", "tensorflow"))
+    
+    assert overrider.overrides == {"build_frameworks": ["pytorch", "tensorflow"]}
+
+
+@pytest.mark.quick_checks
+@pytest.mark.model("N/A")
+@pytest.mark.integration("job_types")
+def test_build_job_types():
+    overrider = prepare_dlc_dev_environment.TomlOverrider()
+    overrider.set_job_type(("inference", "training"))
+    
+    assert overrider.overrides == {"job_types": ["inference", "training"]}
