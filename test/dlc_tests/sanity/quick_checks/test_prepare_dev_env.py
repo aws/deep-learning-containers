1dc3a1430ac2a1c5246c2ce4513c3955164ade4b
Jesus Pequeno <jpeq@amazon.com>
2024-06-13 16:14:55 -0700

    formated python

--- test/dlc_tests/sanity/quick_checks/test_prepare_dev_env.py
+++ test/dlc_tests/sanity/quick_checks/test_prepare_dev_env.py
@@ -10,7 +10,7 @@ def test_build_frameworks():
     overrider = prepare_dlc_dev_environment.TomlOverrider()
     overrider.set_build_frameworks(("pytorch", "tensorflow"))
 
-    assert overrider.overrides["build"]["build_frameworks"] == ["pytorch", "tensorflow"]  
+    assert overrider.overrides["build"]["build_frameworks"] == ["pytorch", "tensorflow"]
 
 
 @pytest.mark.quick_checks
@@ -19,16 +19,28 @@ def test_build_frameworks():
 def test_build_job_types():
     overrider = prepare_dlc_dev_environment.TomlOverrider()
     overrider.set_job_type(("inference", "training"))
-    assert overrider.overrides["build"]["build_training"] == True and overrider.overrides["build"]["build_inference"] == True
+    assert (
+        overrider.overrides["build"]["build_training"] == True
+        and overrider.overrides["build"]["build_inference"] == True
+    )
 
     overrider.set_job_type(["inference"])
-    assert overrider.overrides["build"]["build_training"] == False and overrider.overrides["build"]["build_inference"] == True
+    assert (
+        overrider.overrides["build"]["build_training"] == False
+        and overrider.overrides["build"]["build_inference"] == True
+    )
 
     overrider.set_job_type(["training"])
-    assert overrider.overrides["build"]["build_training"] == True and overrider.overrides["build"]["build_inference"] == False
+    assert (
+        overrider.overrides["build"]["build_training"] == True
+        and overrider.overrides["build"]["build_inference"] == False
+    )
 
     overrider.set_job_type([])
-    assert overrider.overrides["build"]["build_training"] == False and overrider.overrides["build"]["build_inference"] == False
+    assert (
+        overrider.overrides["build"]["build_training"] == False
+        and overrider.overrides["build"]["build_inference"] == False
+    )
 
 
 @pytest.mark.quick_checks
