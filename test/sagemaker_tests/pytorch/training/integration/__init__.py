e47c734a30e0ccfebd99835026ee3f7691179859
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2024-06-27 15:50:11 -0700

    Deprecate S3 Plugin tests (#4005)
    * Deprecate S3 Plugin tests
    
    * test 1.13 sm
    
    * formatting
    
    * add back s3 skip
    
    * test 2.1 sm
    
    * test 2.2 sm
    
    * retest 1.13 sm
    
    * test 1.13 ec2
    
    * disable 1.13 build
    
    * rebuild retest 1.13 ec2
    
    * retest 1.13 ec2 with build tag override
    
    * test 1.13 sm
    
    * test 2.1 ec2
    
    * retest 2.1 ec2 with logs for eks
    
    * skip test manually
    
    * test 2.1 sm
    
    * test 2.2 ec2
    
    * test 2.2 sm
    
    * test 2.3 ec2
    
    * test 2.3 sm
    
    * Restore ['dlc_developer_config.toml']
    
    dlc_developer_config.toml
    ('Restore to '
     'https://raw.githubusercontent.com/aws/deep-learning-containers/master/dlc_developer_config.toml')
    
    * retest 2.3 sm
    
    * revert toml
    
    * add skip s3 plugin reason

--- test/sagemaker_tests/pytorch/training/integration/__init__.py
+++ test/sagemaker_tests/pytorch/training/integration/__init__.py
@@ -26,7 +26,6 @@ smdataparallel_mnist_script = os.path.join(mnist_path, "smdataparallel_mnist_scr
 fastai_path = os.path.join(resources_path, "fastai")
 fastai_cifar_script = os.path.join(fastai_path, "train_cifar.py")
 fastai_mnist_script = os.path.join(fastai_path, "mnist.py")
-resnet18_path = os.path.join(resources_path, "resnet18")
 smart_sifting_path = os.path.join(resources_path, "smart_sifting")
 
 data_dir = os.path.join(mnist_path, "data")
