66338b8d4efbe9ef7dc2eb4deb5530f342a95429
arjkesh <33526713+arjkesh@users.noreply.github.com>
2024-07-09 00:41:20 -0700

    revert temp change to test

--- test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
+++ test/dlc_tests/ec2/tensorflow/inference/test_tensorflow_inference.py
@@ -384,7 +384,7 @@ def host_setup_for_tensorflow_inference(
         ec2_connection.run(
             (
                 f"pip install --user -qq -U 'tensorflow<={framework_version}' "
-                f" 'tensorflow-serving-api<={framework_version}' "
+                f" 'tensorflow-serving-api<={framework_version}'"
             ),
             hide=True,
         )
