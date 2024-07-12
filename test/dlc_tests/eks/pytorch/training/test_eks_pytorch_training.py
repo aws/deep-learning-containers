cd0722ce6ba937afed4a7f24145e65e9303f48c0
Sirut Buasai <sirutbuasai27@outlook.com>
2024-07-11 19:46:37 -0700

    test conda remove

--- test/dlc_tests/eks/pytorch/training/test_eks_pytorch_training.py
+++ test/dlc_tests/eks/pytorch/training/test_eks_pytorch_training.py
@@ -18,7 +18,6 @@ from packaging.specifiers import SpecifierSet
 LOGGER = eks_utils.LOGGER
 
 
-@pytest.mark.skip("temp skip, unrelated to current changes")
 @pytest.mark.skipif(
     not is_pr_context(),
     reason="Skip this test. It is already tested under PR context and we do not have enough resouces to test it again on mainline pipeline",
@@ -158,7 +157,6 @@ def test_eks_pt_s3_plugin_single_node_training(pytorch_training, outside_version
         run("kubectl delete pods {}".format(pod_name))
 
 
-@pytest.mark.skip("temp skip, unrelated to current changes")
 @pytest.mark.skipif(
     not is_pr_context(), reason="Skip this test. It is already tested under PR context"
 )
