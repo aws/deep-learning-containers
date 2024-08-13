53dd35a2671f641debfc5f5161158828f42ef9a9
Shantanu Tripathi <shantanutripathi237@gmail.com>
2024-08-13 23:56:59 +0000

    Add issue link

--- test/dlc_tests/sanity/test_pre_release.py
+++ test/dlc_tests/sanity/test_pre_release.py
@@ -753,7 +753,8 @@ def test_pip_check(image):
     if framework in ["pytorch"] and Version(framework_version) in SpecifierSet("==2.3.*") and all([substr_to_find in image for substr_to_find in ["gpu", "training"]]):
         exception_strings = []
 
-        for ex_ver in ["1.11.1.1", "5.7.8"]:
+        # Adding due to the latest pip check platform feature: https://github.com/pypa/pip/issues/12884
+        for ex_ver in ["1.11.1.1"]:
             exception_strings += [f"ninja {ex_ver}".replace(".", r"\.")]
 
         allowed_exceptions.append(
