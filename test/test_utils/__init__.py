b7a2d86c8141ed55350278e120911a684e47d54e
Sally Seok <sallyseo@amazon.com>
2024-08-30 13:31:15 -0700

    replace underscores with hyphen for all package names

--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -2352,10 +2352,7 @@ def get_installed_python_packages_with_version(docker_exec_command: str):
     list_of_package_data_dicts = json.loads(run_output.stdout)
 
     for package_data_dict in list_of_package_data_dicts:
-        package_name = package_data_dict["name"].lower()
-        # account for "sagemaker_training" vs "sagemaker-training" package name difference
-        if package_name in ["sagemaker_training"]: # potentially add more packages here in the future if they have the same issue
-            package_name = package_name.replace("_", "-")
+        package_name = package_data_dict["name"].lower().replace("_", "-")
         if package_name in package_version_dict:
             raise Exception(
                 f""" Package {package_name} existing multiple times in {list_of_package_data_dicts}"""
