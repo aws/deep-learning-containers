813650002924b2cbba651599b96734f82c44f25a
Yadan Wei <weiyadan@amazon.com>
2024-09-11 17:52:09 +0000

    skip some sm tests

--- test/dlc_tests/conftest.py
+++ test/dlc_tests/conftest.py
@@ -894,7 +894,7 @@ def skip_torchdata_test(request):
     if not image_uri:
         return
 
-    skip_dict = {">2.1.1": ["cpu", "cu121"]}
+    skip_dict = {">2.1.1": ["cpu", "cu121"], ">=2.4": ["cpu", "cu124"]}
     if _validate_pytorch_framework_version(request, image_uri, "skip_torchdata_test", skip_dict):
         pytest.skip(
             f"Torchdata has paused development as of July 2023 and the latest compatible PyTorch version is 2.1.1."
@@ -913,7 +913,7 @@ def skip_smdebug_v1_test(request):
     else:
         return
 
-    skip_dict = {"==2.0.*": ["cu121"], ">=2.1": ["cpu", "cu121"]}
+    skip_dict = {"==2.0.*": ["cu121"], ">=2.1,<2.4": ["cpu", "cu121"], ">=2.4": ["cpu", "cu124"]}
     if _validate_pytorch_framework_version(request, image_uri, "skip_smdebug_v1_test", skip_dict):
         pytest.skip(f"SM Profiler v1 is on path for deprecation, skipping test")
 
@@ -931,7 +931,7 @@ def skip_dgl_test(request):
     else:
         return
 
-    skip_dict = {"==2.0.*": ["cu121"], ">=2.1": ["cpu", "cu121"]}
+    skip_dict = {"==2.0.*": ["cu121"], ">=2.1, <2.4": ["cpu", "cu121"],">=2.4": ["cpu", "cu124"]}
     if _validate_pytorch_framework_version(request, image_uri, "skip_dgl_test", skip_dict):
         pytest.skip(f"DGL binaries are removed, skipping test")
 
@@ -993,6 +993,7 @@ def skip_serialized_release_pt_test(request):
     skip_dict = {
         "==1.13.*": ["cpu", "cu117"],
         ">=2.1,<2.4": ["cpu", "cu121"],
+        ">=2.4,<2.5": ["cpu", "cu124"],
     }
     if _validate_pytorch_framework_version(
         request, image_uri, "skip_serialized_release_pt_test", skip_dict
