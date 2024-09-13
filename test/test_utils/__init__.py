1847736cb44d8a22904af9f720ccf4c4a2e80f00
Sally Seok <sallyseo@amazon.com>
2024-09-12 18:16:20 -0700

    fix typo

--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -141,7 +141,7 @@ UL22_BASE_ARM64_DLAMI_US_EAST_1 = get_ami_id_boto3(
     ami_name_pattern="Deep Learning ARM64 Base OSS Nvidia Driver GPU AMI (Ubuntu 22.04) ????????",
 )
 AML2_BASE_ARM64_DLAMI_US_WEST_2 = get_ami_id_boto3( ### sally temp
-    region_name="us-east-1",
+    region_name="us-west-1",
     ami_name_pattern="Deep Learning ARM64 Base OSS Nvidia Driver GPU AMI (Amazon Linux 2) ????????"
 )
 
