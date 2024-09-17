54de928a11494416e0bb9a04f48f4ce92a4584c8
Yadan Wei <weiyadan@amazon.com>
2024-09-17 20:25:54 +0000

    test skip tests on smpy

--- test/sagemaker_tests/pytorch/training/conftest.py
+++ test/sagemaker_tests/pytorch/training/conftest.py
@@ -548,6 +548,21 @@ def skip_smddataparallel_test(
     ):
         pytest.skip(f"SM Data Parallel binaries do not exist in this image, skipping test")
 
+@pytest.fixture(autouse=True)
+def skip_smppy_test(
+    request,
+    processor,
+    ecr_image,
+):
+    """ For each currency release, we can skip smppy tests if the Profiler binary does not exist.
+    However, when the Profiler binaries are added, be sure to fix the test logic such that the tests are not skipped.
+    """
+    skip_dict = {">=2.4": ["cu124"]}
+    if _validate_pytorch_framework_version(
+        request, processor, ecr_image, "skip_smppy_test", skip_dict
+    ):
+        pytest.skip(f"Profiler binaries do not exist in this image, skipping test")
+
 
 @pytest.fixture(autouse=True)
 def skip_p5_tests(request, processor, ecr_image):
