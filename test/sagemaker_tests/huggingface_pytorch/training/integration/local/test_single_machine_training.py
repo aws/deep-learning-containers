3131721f020f225b1156e88e882a25968a4eab87
malavhs <malavhs@amazon.com>
2024-10-24 06:02:54 +0000

    increase max steps

--- test/sagemaker_tests/huggingface_pytorch/training/integration/local/test_single_machine_training.py
+++ test/sagemaker_tests/huggingface_pytorch/training/integration/local/test_single_machine_training.py
@@ -30,7 +30,7 @@ def test_distilbert_base(
 ):
     # hyperparameters, which are passed into the training job
     hyperparameters = {
-        "max_steps": 5,
+        "max_steps": 100,
         "train_batch_size": 4,
         "model_name": "distilbert-base-uncased",
     }
