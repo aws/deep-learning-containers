38c97e0f7f0c6e2e158e45ffbce2a09141b2ae28
JingyaHuang <huang_jingya@outlook.com>
2024-10-29 14:57:14 +0000

    which test bugs?

--- test/sagemaker_tests/huggingface_pytorch/training/integration/local/test_single_machine_training.py
+++ test/sagemaker_tests/huggingface_pytorch/training/integration/local/test_single_machine_training.py
@@ -17,7 +17,6 @@ from packaging.version import Version
 from sagemaker.huggingface import HuggingFace
 
 from ...integration import ROLE, distilbert_script, distilbert_torch_compiled_script
-from ...integration.local.timeout import timeout
 
 
 @pytest.mark.model("hf_bert")
@@ -37,53 +36,50 @@ def test_distilbert_base(
         "max_steps": 5,
         "train_batch_size": 4,
         "model_name": "distilbert/distilbert-base-uncased",
-        # "model_dir": "/opt/ml/model",
     }
 
-    with timeout(minutes=30):
-        estimator = HuggingFace(
-            entry_point=distilbert_script,
-            instance_type="local_gpu",
-            sagemaker_session=sagemaker_local_session,
-            image_uri=docker_image,
-            instance_count=1,
-            role=ROLE,
-            py_version=py_version,
-            hyperparameters=hyperparameters,
-        )
+    estimator = HuggingFace(
+        entry_point=distilbert_script,
+        instance_type="local_gpu",
+        sagemaker_session=sagemaker_local_session,
+        image_uri=docker_image,
+        instance_count=1,
+        role=ROLE,
+        py_version=py_version,
+        hyperparameters=hyperparameters,
+    )
 
-        estimator.fit()
+    estimator.fit()
 
 
-@pytest.mark.model("hf_bert")
-@pytest.mark.integration("hf_local")
-@pytest.mark.skip_cpu
-@pytest.mark.skip_py2_containers
-@pytest.mark.skip_trcomp_containers
-@pytest.mark.team("sagemaker-1p-algorithms")
-def test_distilbert_base_torch_compiled(
-    docker_image, processor, instance_type, sagemaker_local_session, py_version, framework_version
-):
-    if "pytorch" in docker_image and Version(framework_version) < Version("2.2"):
-        pytest.skip("Skipping torch compile tests for PT")
+# @pytest.mark.model("hf_bert")
+# @pytest.mark.integration("hf_local")
+# @pytest.mark.skip_cpu
+# @pytest.mark.skip_py2_containers
+# @pytest.mark.skip_trcomp_containers
+# @pytest.mark.team("sagemaker-1p-algorithms")
+# def test_distilbert_base_torch_compiled(
+#     docker_image, processor, instance_type, sagemaker_local_session, py_version, framework_version
+# ):
+#     if "pytorch" in docker_image and Version(framework_version) < Version("2.2"):
+#         pytest.skip("Skipping torch compile tests for PT")
 
-    # hyperparameters, which are passed into the training job
-    hyperparameters = {
-        "max_steps": 5,
-        "train_batch_size": 4,
-        "model_name": "distilbert/distilbert-base-uncased",
-    }
+#     # hyperparameters, which are passed into the training job
+#     hyperparameters = {
+#         "max_steps": 5,
+#         "train_batch_size": 4,
+#         "model_name": "distilbert/distilbert-base-uncased",
+#     }
 
-    with timeout(minutes=30):
-        estimator = HuggingFace(
-            entry_point=distilbert_torch_compiled_script,
-            instance_type="local_gpu",
-            sagemaker_session=sagemaker_local_session,
-            image_uri=docker_image,
-            instance_count=1,
-            role=ROLE,
-            py_version=py_version,
-            hyperparameters=hyperparameters,
-        )
+#     estimator = HuggingFace(
+#         entry_point=distilbert_torch_compiled_script,
+#         instance_type="local_gpu",
+#         sagemaker_session=sagemaker_local_session,
+#         image_uri=docker_image,
+#         instance_count=1,
+#         role=ROLE,
+#         py_version=py_version,
+#         hyperparameters=hyperparameters,
+#     )
 
-        estimator.fit()
+#     estimator.fit()
