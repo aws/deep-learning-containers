00e66da7533e309153c8e64aa805ca356093ef9c
Sally Seok <sallyseo@amazon.com>
2024-10-29 17:17:53 -0700

    move gevent monkey patching to python_service.py

--- tensorflow/inference/docker/build_artifacts/sagemaker/serve.py
+++ tensorflow/inference/docker/build_artifacts/sagemaker/serve.py
@@ -11,11 +11,6 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-# monkey patching to ensure that all I/O operations are properly made asynchronous.
-import gevent.monkey
-
-gevent.monkey.patch_all()
-
 import boto3
 import logging
 import os
