3feea566fe93bdf089cdbf1d8992e1784e5c868e
Sally Seok <sallyseo@amazon.com>
2024-12-05 14:14:45 -0800

    Add Graviton/ARM64 GPU deep canary tests

--- test/dlc_tests/ec2/pytorch/inference/test_pytorch_inference.py
+++ test/dlc_tests/ec2/pytorch/inference/test_pytorch_inference.py
@@ -109,6 +109,36 @@ def test_ec2_pytorch_inference_cpu_deep_canary(pytorch_inference, ec2_connection
 )
 @pytest.mark.deep_canary("Reason: This test is a simple pytorch inference test")
 @pytest.mark.model("densenet")
+@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_GRAVITON_INSTANCE_TYPE, indirect=True)
+@pytest.mark.parametrize("ec2_instance_ami", [test_utils.UL20_CPU_ARM64_US_WEST_2], indirect=True)
+def test_ec2_pytorch_inference_graviton_gpu_deep_canary(
+    pytorch_inference_graviton, ec2_connection, region, gpu_only
+):
+    ec2_pytorch_inference(pytorch_inference_graviton, "gpu", ec2_connection, region)
+
+
+@pytest.mark.usefixtures("sagemaker")
+@pytest.mark.skipif(
+    not test_utils.is_deep_canary_context() or not os.getenv("REGION") == "us-west-2",
+    reason="This test only needs to run in deep-canary context in us-west-2",
+)
+@pytest.mark.deep_canary("Reason: This test is a simple pytorch inference test")
+@pytest.mark.model("densenet")
+@pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_ARM64_INSTANCE_TYPE, indirect=True)
+@pytest.mark.parametrize("ec2_instance_ami", [test_utils.UL20_CPU_ARM64_US_WEST_2], indirect=True)
+def test_ec2_pytorch_inference_arm64_gpu_deep_canary(
+    pytorch_inference_arm64, ec2_connection, region, gpu_only
+):
+    ec2_pytorch_inference(pytorch_inference_arm64, "gpu", ec2_connection, region)
+
+
+@pytest.mark.usefixtures("sagemaker")
+@pytest.mark.skipif(
+    not test_utils.is_deep_canary_context() or not os.getenv("REGION") == "us-west-2",
+    reason="This test only needs to run in deep-canary context in us-west-2",
+)
+@pytest.mark.deep_canary("Reason: This test is a simple pytorch inference test")
+@pytest.mark.model("densenet")
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_CPU_GRAVITON_INSTANCE_TYPE, indirect=True)
 @pytest.mark.parametrize("ec2_instance_ami", [test_utils.UL20_CPU_ARM64_US_WEST_2], indirect=True)
 def test_ec2_pytorch_inference_graviton_cpu_deep_canary(
