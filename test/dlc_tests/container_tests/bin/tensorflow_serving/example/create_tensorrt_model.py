8b8e50dfa40ebc1a49e1a03047f62ffedcf84b87
Yadan Wei <yadanwei@amazon.com>
2025-01-16 13:52:17 -0800

    change model save path

--- test/dlc_tests/container_tests/bin/tensorflow_serving/example/create_tensorrt_model.py
+++ test/dlc_tests/container_tests/bin/tensorflow_serving/example/create_tensorrt_model.py
@@ -43,13 +43,15 @@ def run_training(model_save_folder_path=os.path.join("script_folder", "models"))
     ## Versions of TF >= 2.16 come along with Keras 3. Model.save() API of keras 3 has significant differences with that of keras 2.
     ## Consequently, we continue to use save() for keras < 3 and shift to export for keras >= 3.
     ## Please refer to the following link for more details: https://keras.io/guides/migrating_to_keras_3/
+    ## https://keras.io/api/models/model_saving_apis/model_saving_and_loading/#save-method
     try:
         model.export(SAVED_MODEL_DIR) if Version(keras.version()) in SpecifierSet(
             ">=3"
         ) else model.save(SAVED_MODEL_DIR)
     except AttributeError as e:
         if "module 'keras.api._v2.keras' has no attribute 'version'" == f"{e}":
-            model.save(SAVED_MODEL_DIR)
+            # model.save(SAVED_MODEL_DIR)
+            model.save(os.path.join(model_save_folder_path, "native_saved_model.keras"))
         else:
             raise
 
