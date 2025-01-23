e46b488ea6f34f5e3546bf6b64aee5c864c9a2af
Yadan Wei <yadanwei@amazon.com>
2025-01-23 15:44:16 -0800

    match EFA log change for p4d and p5

--- test/dlc_tests/ec2/test_efa.py
+++ test/dlc_tests/ec2/test_efa.py
@@ -102,10 +102,10 @@ def test_pytorch_efa(
 @pytest.mark.allow_p4de_use
 @pytest.mark.multinode(2)
 @pytest.mark.parametrize("ec2_instance_type,region", EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION)
-# @pytest.mark.skipif(
-#     is_pr_context() and not is_efa_dedicated(),
-#     reason="Skip EFA test in PR context unless explicitly enabled",
-# )
+@pytest.mark.skipif(
+    is_pr_context() and not is_efa_dedicated() and not are_heavy_instance_ec2_tests_enabled(),
+    reason="Skip EFA test in PR context unless explicitly enabled",
+)
 @pytest.mark.team("frameworks")
 def test_efa_tensorflow(
     tensorflow_training, efa_ec2_instances, efa_ec2_connections, ec2_instance_type, region, gpu_only
