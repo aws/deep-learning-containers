6ac1b7e21845287301a003b8b4787444313f5afe
Yadan-Wei <84998528+Yadan-Wei@users.noreply.github.com>
2025-01-24 11:03:20 -0800

    Check tf218 training efa test (#4504)
    * check tf218 efa test
    
    * enable efa tests
    
    * change AWS Libfabric grep and exit message if error exists
    
    * match EFA log change for p4d and p5
    
    * add --fix-missing to fix cpu test
    
    * revert toml
    
    * revert toml
    
    ---------
    
    Co-authored-by: Yadan Wei <yadanwei@amazon.com>

--- test/dlc_tests/ec2/test_efa.py
+++ test/dlc_tests/ec2/test_efa.py
@@ -103,7 +103,7 @@ def test_pytorch_efa(
 @pytest.mark.multinode(2)
 @pytest.mark.parametrize("ec2_instance_type,region", EC2_EFA_GPU_INSTANCE_TYPE_AND_REGION)
 @pytest.mark.skipif(
-    is_pr_context() and not is_efa_dedicated(),
+    is_pr_context() and not is_efa_dedicated() and not are_heavy_instance_ec2_tests_enabled(),
     reason="Skip EFA test in PR context unless explicitly enabled",
 )
 @pytest.mark.team("frameworks")
