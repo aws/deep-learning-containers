bd6e4bb2a0c175f1c1944136ff6c4c417ee36f23
Yadan Wei <yadanwei@amazon.com>
2025-02-10 15:17:49 -0800

    fix torch_data url and add back torch data tests

--- test/dlc_tests/conftest.py
+++ test/dlc_tests/conftest.py
@@ -911,6 +911,7 @@ def skip_torchdata_test(request):
             f"Torchdata has paused development as of July 2023 and the latest compatible PyTorch version is 2.1.1."
             f"For more information, see https://github.com/pytorch/data/issues/1196."
             f"Skipping test"
+            f"Start from PyTorch 2.6, Torchdata is added back to the container."
         )
 
 
@@ -924,7 +925,7 @@ def skip_smdebug_v1_test(request):
     else:
         return
 
-    skip_dict = {"==2.0.*": ["cu121"], ">=2.1,<2.4": ["cpu", "cu121"], ">=2.4": ["cpu", "cu124"]}
+    skip_dict = {"==2.0.*": ["cu121"], ">=2.1,<2.4": ["cpu", "cu121"], ">=2.4,<2.6": ["cpu", "cu124"]}
     if _validate_pytorch_framework_version(request, image_uri, "skip_smdebug_v1_test", skip_dict):
         pytest.skip(f"SM Profiler v1 is on path for deprecation, skipping test")
 
