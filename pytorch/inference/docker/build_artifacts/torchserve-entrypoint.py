be4c5cf54683144c8f9a04ac2f24764d19656a2e
sirutBuasai <sirutbuasai27@outlook.com>
2025-02-14 14:39:51 -0800

    Patch cuda compat mounting on PT 2.6 Inference SM

--- pytorch/inference/docker/build_artifacts/torchserve-entrypoint.py
+++ pytorch/inference/docker/build_artifacts/torchserve-entrypoint.py
@@ -18,6 +18,12 @@ import sys
 import os
 import signal
 
+# run compat mounting by default
+try:
+    subprocess.run(["bash", "-m", "/usr/local/bin/start_cuda_compat.sh"])
+except Exception as e:
+    print(f"Error running script: {e}")
+
 
 def reap_zombies(signum, frame):
     """
