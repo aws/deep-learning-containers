ac351832abe0ad690ce3e631465a0d5009a24974
sirutBuasai <sirutbuasai27@outlook.com>
2025-02-14 02:01:46 -0800

    fix start_cuda_compat file

--- miscellaneous_scripts/start_cuda_compat.sh
+++ miscellaneous_scripts/start_cuda_compat.sh
@@ -4,18 +4,18 @@ verlte() {
   [ "$1" = "$2" ] && return 1 || [ "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ]
 }
 
-if [ -f ${CUDA_HOME}/compat/libcuda.so.1 ]; then
-  CUDA_COMPAT_MAX_DRIVER_VERSION=$(readlink ${CUDA_HOME}/compat/libcuda.so.1 | cut -d'.' -f 3-)
-  echo "CUDA compat package requires Nvidia driver ⩽${CUDA_COMPAT_MAX_DRIVER_VERSION}"
-  NVIDIA_DRIVER_VERSION=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader --id=0 2>/dev/null || true)
-  echo "Current installed Nvidia driver version is ${NVIDIA_DRIVER_VERSION}"
-  if verlte $NVIDIA_DRIVER_VERSION $CUDA_COMPAT_MAX_DRIVER_VERSION; then
-    echo "Setup CUDA compatibility libs path to LD_LIBRARY_PATH"
+if [ -f /usr/local/cuda/compat/libcuda.so.1 ]; then
+  CUDA_COMPAT_MAX_DRIVER_VERSION=$(readlink /usr/local/cuda/compat/libcuda.so.1 | cut -d'.' -f 3-)
+  echo "CUDA compat package should be installed for NVIDIA driver smaller than ${CUDA_COMPAT_MAX_DRIVER_VERSION}"
+  NVIDIA_DRIVER_VERSION=$(sed -n 's/^NVRM.*Kernel Module *\([0-9.]*\).*$/\1/p' /proc/driver/nvidia/version 2>/dev/null || true)
+  echo "Current installed NVIDIA driver version is ${NVIDIA_DRIVER_VERSION}"
+  if verlt $NVIDIA_DRIVER_VERSION $CUDA_COMPAT_MAX_DRIVER_VERSION; then
+    echo "Adding CUDA compat to LD_LIBRARY_PATH"
     export LD_LIBRARY_PATH=/usr/local/cuda/compat:$LD_LIBRARY_PATH
     echo $LD_LIBRARY_PATH
   else
-    echo "Skip CUDA compat libs setup as newer Nvidia driver is installed"
+    echo "Skipping CUDA compat setup as newer NVIDIA driver is installed"
   fi
 else
-  echo "Skip CUDA compat libs setup as package not found"
+  echo "Skipping CUDA compat setup as package not found"
 fi
