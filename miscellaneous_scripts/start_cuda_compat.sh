ca228e95411aad2bb31e92f2f830ba2370bc1cd7
sirutBuasai <sirutbuasai27@outlook.com>
2025-02-14 10:07:43 -0800

    fix vertle

--- miscellaneous_scripts/start_cuda_compat.sh
+++ miscellaneous_scripts/start_cuda_compat.sh
@@ -9,7 +9,7 @@ if [ -f /usr/local/cuda/compat/libcuda.so.1 ]; then
   echo "CUDA compat package should be installed for NVIDIA driver smaller than ${CUDA_COMPAT_MAX_DRIVER_VERSION}"
   NVIDIA_DRIVER_VERSION=$(sed -n 's/^NVRM.*Kernel Module *\([0-9.]*\).*$/\1/p' /proc/driver/nvidia/version 2>/dev/null || true)
   echo "Current installed NVIDIA driver version is ${NVIDIA_DRIVER_VERSION}"
-  if verlt $NVIDIA_DRIVER_VERSION $CUDA_COMPAT_MAX_DRIVER_VERSION; then
+  if verlte $NVIDIA_DRIVER_VERSION $CUDA_COMPAT_MAX_DRIVER_VERSION; then
     echo "Adding CUDA compat to LD_LIBRARY_PATH"
     export LD_LIBRARY_PATH=/usr/local/cuda/compat:$LD_LIBRARY_PATH
     echo $LD_LIBRARY_PATH
