68639744b2ee0e79a66477d6437570c3f69a28b3
ruhanprasad <prasadru@amazon.com>
2025-03-10 21:38:53 +0000

    trigger tests

--- test/sagemaker_tests/pytorch/training/conftest.py
+++ test/sagemaker_tests/pytorch/training/conftest.py
@@ -558,7 +558,7 @@ def skip_smddataparallel_test(
     For each currency release, we can skip SMDDP tests if the binary does not exist.
     However, when the SMDDP binaries are added, be sure to fix the test logic such that the tests are not skipped.
     """
-    skip_dict = {"==2.0.*": ["cu121"], ">=2.5,<2.6": ["cu124"], ">=2.6": ["cu126"]}
+    skip_dict = {"==2.0.*": ["cu121"], ">=2.6": ["cu126"]}
     if _validate_pytorch_framework_version(
         request, processor, ecr_image, "skip_smddataparallel_test", skip_dict
     ):
