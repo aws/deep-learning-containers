0d7b6bef75b0c50c21d5f2a54ab22249624c5fd2
Michael Benayoun <mickbenayoun@gmail.com>
2025-04-09 22:05:31 +0200

    Fix

--- test/sagemaker_tests/huggingface_pytorch/training/integration/sagemaker/test_neuronx.py
+++ test/sagemaker_tests/huggingface_pytorch/training/integration/sagemaker/test_neuronx.py
@@ -35,10 +35,11 @@ hyperparameters = {
     "bf16": True,
     "max_seq_length": 128,
     "max_train_samples": 10,
-    "per_device_train_batch_size": 4,
+    "per_device_train_batch_size": 1,
     "num_train_epochs": 1,
     "logging_steps": 1,
     "output_dir": "/opt/ml/model",
+    "report_to": "none",
 }
 
 
@@ -150,7 +151,7 @@ def _test_neuronx_text_classification_function(
             instance_type=instance_type,
             sagemaker_session=sagemaker_session,
             py_version=py_version,
-            # distribution=distribution,  # Uncomment when it is enabled by HuggingFace Estimator
+            distribution=distribution,
             hyperparameters=hyperparameters,
         )
         estimator.fit(job_name=sagemaker.utils.unique_name_from_base("test-hf-pt-glue-neuronx"))
