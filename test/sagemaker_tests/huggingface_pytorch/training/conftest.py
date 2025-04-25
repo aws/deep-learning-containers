6b9fbdb45f3b9a7f5f100464b6dc122e3098d05c
EC2 Default User <ec2-user@ip-10-90-0-235.ec2.internal>
2025-04-25 14:22:41 +0000

    absolute import for issue in loading variables

--- test/sagemaker_tests/huggingface_pytorch/training/conftest.py
+++ test/sagemaker_tests/huggingface_pytorch/training/conftest.py
@@ -37,9 +37,11 @@ from .integration import (
     get_transformers_version_from_image_uri,
 )
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../../"))
-sys.path.append(project_root)
-from test.sagemaker_tests import NO_P4_REGIONS, NO_G5_REGIONS
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sagemaker_tests_dir = os.path.abspath(os.path.join(current_dir, "../../../"))
+sys.path.append(sagemaker_tests_dir)
+
+from sagemaker_tests import NO_P4_REGIONS, NO_G5_REGIONS
 
 logger = logging.getLogger(__name__)
 logging.getLogger("boto").setLevel(logging.INFO)
