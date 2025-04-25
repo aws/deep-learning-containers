34d90f01109d2258fac8270934f2370d89061131
Yadan Wei <yadanwei@amazon.com>
2025-04-25 14:55:58 -0700

    skip test for arm64 and graviton

--- test/dlc_tests/sanity/test_telemetry.py
+++ test/dlc_tests/sanity/test_telemetry.py
@@ -26,6 +26,8 @@ def test_pytorch_inference_job_type_env_var(pytorch_inference):
 @pytest.mark.usefixtures("sagemaker", "functionality_sanity")
 @pytest.mark.model("N/A")
 def test_pytorch_inference_job_type_env_var_arm64(pytorch_inference_arm64):
+    if Version(pytorch_inference_arm64) < Version("2.7"):
+        pytest.skip("This env variable was added after PT 2.7 release. Skipping test.")
     _test_pytorch_job_type_env_var(pytorch_inference_arm64, "inference")
 
 
@@ -33,6 +35,8 @@ def _test_pytorch_job_type_env_var(image, job_type):
     _, image_framework_version = get_framework_and_version_from_tag(image)
     if Version(image_framework_version) < Version("1.10"):
         pytest.skip("This env variable was added after PT 1.10 release. Skipping test.")
+    if Version(image_framework_version) < Version("2.7") and ("graviton" in image or "arm64" in image):
+        pytest.skip("This env variable was added for arm64 or graviton image after PT 2.7 release. Skipping test.")
     env_vars = {"DLC_CONTAINER_TYPE": job_type}
     container_name_prefix = (
         "pt_train_job_type_env_var" if job_type == "training" else "pt_inference_job_type_env_var"
