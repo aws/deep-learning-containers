ff5b1a67c2a2169e6b8d9455a0001d94f4fbd0af
zhuofuAMZ <bzhuofu@amazon.com>
2025-04-28 10:30:14 -0700

    test TF 2.18 sm

--- test/dlc_tests/ec2/test_telemetry.py
+++ test/dlc_tests/ec2/test_telemetry.py
@@ -437,7 +437,6 @@ def _run_tag_failure_IMDSv1_disabled(image_uri, ec2_client, ec2_instance, ec2_co
         ec2_client.delete_tags(Resources=[ec2_instance_id], Tags=[{"Key": expected_tag_key}])
 
     # Disable access to EC2 instance metadata
-    # ec2_utils.kill_background_processes_and_run_apt_get_update(ec2_connection)
     ec2_connection.run(f"sudo dnf install -y net-tools")
     ec2_connection.run(f"sudo route add -host 169.254.169.254 reject")
 
