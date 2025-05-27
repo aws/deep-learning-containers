29ab1d87f3737cd2dbe5d55d1f03ceee7c87d6fb
jkottu <jkottu@amazon.com>
2025-05-26 18:26:32 -0700

    rebuild ec2

--- test/dlc_tests/ec2/pytorch/training/test_pytorch_training_2_7.py
+++ test/dlc_tests/ec2/pytorch/training/test_pytorch_training_2_7.py
@@ -35,7 +35,6 @@ def test_pytorch_2_7_gpu(
         (common_cases.pytorch_training_torchdata, (pytorch_training, ec2_connection)),
         (common_cases.pytorch_cudnn_match_gpu, (pytorch_training, ec2_connection, region)),
         (common_cases.pytorch_curand_gpu, (pytorch_training, ec2_connection)),
-        (common_cases.pytorch_telemetry_framework_gpu, (pytorch_training, ec2_connection)),
         (common_cases.pytorch_telemetry_bashrc_gpu, (pytorch_training, ec2_connection)),
         (common_cases.pytorch_telemetry_entrypoint_gpu, (pytorch_training, ec2_connection)),
     ]
@@ -126,7 +125,6 @@ def test_pytorch_2_7_cpu(pytorch_training___2__7, ec2_connection, cpu_only):
         (common_cases.pytorch_mpi, (pytorch_training, ec2_connection)),
         (common_cases.pytorch_training_torchaudio, (pytorch_training, ec2_connection)),
         (common_cases.pytorch_training_torchdata, (pytorch_training, ec2_connection)),
-        (common_cases.pytorch_telemetry_framework_cpu, (pytorch_training, ec2_connection)),
         (common_cases.pytorch_telemetry_bashrc_cpu, (pytorch_training, ec2_connection)),
         (common_cases.pytorch_telemetry_entrypoint_cpu, (pytorch_training, ec2_connection)),
     ]
