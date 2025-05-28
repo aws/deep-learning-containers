03f34aee8f9396a1be9f3d359ed5a235954a4f12
Atharva Kale <athrajan@amazon.com>
2025-05-28 11:58:21 -0700

    build changes

---
+++ autogluon/training/out.py
@@ -0,0 +1,14 @@
+import os
+
+try:
+    if os.path.exists("/usr/local/bin/deep_learning_container.py") and (
+        os.getenv("OPT_OUT_TRACKING") is None or os.getenv("OPT_OUT_TRACKING", "").lower() != "true"
+    ):
+        import threading
+
+        cmd = "python /usr/local/bin/deep_learning_container.py --framework autogluon --framework-version 1.3.1 --container-type training &>/dev/null"
+        x = threading.Thread(target=lambda: os.system(cmd))
+        x.setDaemon(True)
+        x.start()
+except Exception:
+    pass
