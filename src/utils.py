4efd574f6d92c4563f0aa3f4c16358eec23f3976
Atharva Kale <97895612+Atharva-Rajan-Kale@users.noreply.github.com>
2025-05-29 12:34:22 -0700

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -20,6 +20,7 @@ import logging
 import sys
 import boto3
 import constants
+
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
