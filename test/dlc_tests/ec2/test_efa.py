11ab9325ef9369fdf84f431923f56a4303dbda2e
Jinyan Li <jinyali@amazon.com>
2025-06-02 21:30:00 -0700

    change to printf for writing ipv6 addr to /etc/hosts

--- test/dlc_tests/ec2/test_efa.py
+++ test/dlc_tests/ec2/test_efa.py
@@ -357,19 +357,26 @@ def _create_master_mpi_hosts_file(efa_ec2_connections, worker_instance_ids, inst
             raise RuntimeError("IPv6 testing enabled but not all workers have IPv6 addresses")
         
         hosts_string = f"compute1 slots={slots} "
-        etc_string = f'"{master_ip}" compute1'
+        etc_string = f'{master_ip} compute1'
         compute_counter = 2
         
         for worker_ip in worker_ips:
             compute_name = f"compute{compute_counter}"
             hosts_string += f"\n{compute_name} slots={slots} "
-            etc_string += f'\n"{worker_ip}" {compute_name}'
+            etc_string += f'\\n{worker_ip} {compute_name}'
             compute_counter += 1
         
         run_cmd_on_container(
             MASTER_CONTAINER_NAME,
             master_connection,
-            f"echo '{etc_string}' > /etc/hosts"
+            f'printf "{etc_string}" > /etc/hosts'
+        )
+
+        # TODO: remove cat command after making sure it works
+        run_cmd_on_container(
+            MASTER_CONTAINER_NAME,
+            master_connection,
+            "cat /etc/hosts"
         )
 
         run_cmd_on_container(
