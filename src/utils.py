701156d05527e6830cde90fc544893ddae2150a4
Atharva Kale <97895612+Atharva-Rajan-Kale@users.noreply.github.com>
2025-06-16 10:24:55 -0700

    AutoGluon 1.3.1: Format code with black

--- src/utils.py
+++ src/utils.py
@@ -18,7 +18,8 @@ import re
 import json
 import logging
 import sys
-_project_root=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+_project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if _project_root not in sys.path:
     sys.path.insert(0, _project_root)
 import boto3
