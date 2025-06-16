a68d0f0fa04a28131f46e99a05e2cb95fadf6349
Atharva Kale <97895612+Atharva-Rajan-Kale@users.noreply.github.com>
2025-06-16 09:52:25 -0700

    pr-test-1

--- src/utils.py
+++ src/utils.py
@@ -18,6 +18,9 @@ import re
 import json
 import logging
 import sys
+_project_root=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if _project_root not in sys.path:
+    sys.path.insert(0, _project_root)
 import boto3
 import constants
 
