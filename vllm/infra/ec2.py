df0751ecfc473b9e2113e9775944e33e9be6ef26
jkottu <jkottu@amazon.com>
2025-07-09 11:32:03 -0700

    added new dir for vllm test and infra

---
+++ vllm/infra/ec2.py
@@ -0,0 +1,6 @@
+def setup():
+    pass
+
+
+if __name__ == "__main__":
+    setup()
--- vllm/trigger_test.py
+++ vllm/trigger_test.py
@@ -2,6 +2,8 @@ import os, sys
 import logging
 
 from test.test_utils import get_dlc_images
+from infra.ec2 import setup
+from test.ec2 import run_test
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
@@ -10,10 +12,10 @@ LOGGER.addHandler(logging.StreamHandler(sys.stdout))
 
 def main():
 
-    print("Triggering test from vllm")
+    LOGGER.info("Triggering test from vllm")
     test_type = os.getenv("TEST_TYPE")
 
-    print("TEST_TYPE", test_type)
+    LOGGER.info(f"TEST_TYPE {test_type}")
     executor_mode = os.getenv("EXECUTOR_MODE", "False").lower() == "true"
     dlc_images = os.getenv("DLC_IMAGE") if executor_mode else get_dlc_images()
     # Enable IPv6 testing from environment variable
@@ -22,9 +24,19 @@ def main():
     # Executing locally ona can provide commit_id or may ommit it. Assigning default value for local executions:
     commit_id = os.getenv("CODEBUILD_RESOLVED_SOURCE_VERSION", default="unrecognised_commit_id")
 
-    print("Commit ID", commit_id)
+    LOGGER.info(f"Commit ID {commit_id}")
 
     LOGGER.info(f"Images tested: {dlc_images}")
     all_image_list = dlc_images.split(" ")
     standard_images_list = [image_uri for image_uri in all_image_list if "example" not in image_uri]
-    print("\n Images URIs \n", standard_images_list)
+    LOGGER.info(f"\n Images URIs \n {standard_images_list}")
+
+    # setup
+    setup()
+
+    # test
+    run_test()
+
+
+if __name__ == "__main__":
+    main()
