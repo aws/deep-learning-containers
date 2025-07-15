0192682e6d148bcc3f9f0ff393b2632ca35bcea5
Yadan Wei <yadanwei@amazon.com>
2025-07-15 09:46:15 -0700

    build PT2.5 TR EC2

--- test/dlc_tests/ec2/test_nvjpeg.py
+++ test/dlc_tests/ec2/test_nvjpeg.py
@@ -40,6 +40,7 @@ def _run_nvjpeg_test(image_uri, ec2_connection):
     account_id = test_utils.get_account_id_from_image_uri(image_uri)
     image_region = test_utils.get_region_from_image_uri(image_uri)
     repo_name, image_tag = test_utils.get_repository_and_tag_from_image_uri(image_uri)
+    cuda_version = test_utils.get_cuda_version_from_tag(image_uri)
 
     container_name = f"{repo_name}-test-nvjpeg"
 
@@ -52,18 +53,36 @@ def _run_nvjpeg_test(image_uri, ec2_connection):
     ec2_connection.run(
         f"docker run --runtime=nvidia --gpus all --name {container_name} -id {image_uri}"
     )
-
+    cuda_version_numeric = cuda_version.strip("cu")
+    if cuda_version_numeric < Version("126"):
+        # 12.4.1 has a different branch tag in cuda-samples
+        if cuda_version_numeric == Version("124"):
+            git_branch_tag = "12.4.1"
+        else:
+            git_branch_tag = f"{cuda_version_numeric[:-1]}.{cuda_version_numeric[-1]}"
+        test_command = (
+            f"git clone -b v{git_branch_tag} https://github.com/NVIDIA/cuda-samples.git && "
+            "cd cuda-samples/Samples/4_CUDA_Libraries/nvJPEG && "
+            "make -j$(nproc) && "
+            "./nvJPEG"
+        )
+    else:
+        # For CUDA 12.6 and above, we use the v12.8 branch of cuda-samples
+        # This is a workaround for the issue where the nvJPEG sample in the
+        # cuda-samples repository does not support compute_100 architecture.
+        # The v12.8 branch is used to avoid the issue with compute_100 architecture.
+        # See
     # sample 12.9 or master branch has compute_100 arch support issue
     # https://github.com/NVIDIA/cuda-samples/issues/367
-    test_command = (
-        f"git clone -b v12.8 https://github.com/NVIDIA/cuda-samples.git && "
-        "cd cuda-samples && "
-        "mkdir build && cd build && "
-        "cmake .. && "
-        "cd Samples/4_CUDA_Libraries/nvJPEG && "
-        "make -j$(nproc) && "
-        "./nvJPEG"
-    )
+        test_command = (
+            f"git clone -b v12.8 https://github.com/NVIDIA/cuda-samples.git && "
+            "cd cuda-samples && "
+            "mkdir build && cd build && "
+            "cmake .. && "
+            "cd Samples/4_CUDA_Libraries/nvJPEG && "
+            "make -j$(nproc) && "
+            "./nvJPEG"
+        )
 
     output = ec2_connection.run(
         f"docker exec {container_name} /bin/bash -c '{test_command}'"
