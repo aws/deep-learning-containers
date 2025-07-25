fa3d5147a963b4cc914068efbb2a5fad49b976a0
Jinyan Li <jinyali@amazon.com>
2025-07-25 10:40:28 -0700

    modify asg tags

--- eks_infrastructure/create_cluster.sh
+++ eks_infrastructure/create_cluster.sh
@@ -487,7 +487,9 @@ function add_tags_asg() {
 
     if [[ ${nodegroup_name} == *"vllm"* ]]; then
       aws autoscaling create-or-update-tags \
-        --tags ResourceId=${asg_name},ResourceType=auto-scaling-group,Key=k8s.io/cluster-autoscaler/node-template/label/role,Value=large-model-worker,PropagateAtLaunch=true
+        --tags ResourceId=${asg_name},ResourceType=auto-scaling-group,Key=k8s.io/cluster-autoscaler/node-template/label/role,Value=large-model-worker,PropagateAtLaunch=true \
+        ResourceId=${asg_name},ResourceType=auto-scaling-group,Key=k8s.io/cluster-autoscaler/node-template/label/test_type,Value=vllm,PropagateAtLaunch=true \
+        ResourceId=${asg_name},ResourceType=auto-scaling-group,Key=k8s.io/cluster-autoscaler/node-template/resources/vpc.amazonaws.com/efa,Value=4,PropagateAtLaunch=true
     fi
   done
 
