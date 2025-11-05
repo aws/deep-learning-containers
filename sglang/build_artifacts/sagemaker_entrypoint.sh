a8b06f4363402faf23bae2b72cfe9c6e96ff50b3
sirutBuasai <sirutbuasai27@outlook.com>
2025-11-05 14:47:54 -0800

    inital commit

---
+++ sglang/build_artifacts/sagemaker_entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Check if telemetry file exists before executing
+# Execute telemetry script if it exists, suppress errors
+bash /usr/local/bin/bash_telemetry.sh >/dev/null 2>&1 || true
+
+eval "$@"
