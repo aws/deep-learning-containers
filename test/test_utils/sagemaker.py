34bb10c8867435870dede072b001a933f06cda0c
Yadan Wei <yadanwei@amazon.com>
2025-11-06 00:25:31 -0800

    check docker compose

--- test/test_utils/sagemaker.py
+++ test/test_utils/sagemaker.py
@@ -345,6 +345,12 @@ def execute_local_tests(image, pytest_cache_params):
             "sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose"
         )
         ec2_conn.run("sudo chmod +x /usr/local/bin/docker-compose")
+        # debug info
+        result = ec2_conn.run("docker-compose --version")
+        print(f"Docker Compose installed successfully: {result.stdout.strip()}")
+        if result.failed:
+            raise DLCSageMakerLocalTestFailure(f"Docker Compose installation failed: {result.stderr}")
+        
         with ec2_conn.cd(path):
             ec2_conn.run(f"pip install -r requirements.txt")
             pytest_cache_util.download_pytest_cache_from_s3_to_ec2(
