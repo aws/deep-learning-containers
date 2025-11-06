d81a34360691238f36b63b62913916c56d901634
sirutBuasai <sirutbuasai27@outlook.com>
2025-11-06 00:08:49 +0000

    tmp test qwen

--- sglang/build_artifacts/sagemaker_entrypoint.sh
+++ sglang/build_artifacts/sagemaker_entrypoint.sh
@@ -31,4 +31,5 @@ if [ -n "$CHUNKED_PREFILL_SIZE" ]; then
     SERVER_ARGS="${SERVER_ARGS} --chunked-prefill-size ${CHUNKED_PREFILL_SIZE}"
 fi
 
-python3 -m sglang.launch_server --model-path /opt/ml/model $SERVER_ARGS
+# python3 -m sglang.launch_server --model-path /opt/ml/model $SERVER_ARGS
+python3 -m sgland.launch_server --model-path Qwen/Qwen3-0.6B --reasoning-parser qwen3
