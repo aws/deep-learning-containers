b6716a2003ff154e37e7219e6a6fab69dd269ee0
sirutBuasai <sirutbuasai27@outlook.com>
2025-11-10 23:35:11 +0000

    add sglang

--- src/image_builder.py
+++ src/image_builder.py
@@ -685,7 +685,7 @@ def get_job_type(image_repo_uri):
         "inference": "inference",
         "base": "general",
         "vllm": "general",
-        # "sglang": "general",
+        "sglang": "general",
     }
 
     for key, job_type in job_type_mapping.items():
