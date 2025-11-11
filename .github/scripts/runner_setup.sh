e5ad9e6acccc9f93cd929b4a4ad630f3ad0d3e7f
Junpu Fan <junpu@amazon.com>
2025-11-11 07:54:21 -0800

    update
    Signed-off-by: Junpu Fan <junpu@amazon.com>

--- .github/scripts/runner_setup.sh
+++ .github/scripts/runner_setup.sh
@@ -5,4 +5,6 @@ if ! command -v uv &> /dev/null; then
     curl -LsSf https://astral.sh/uv/install.sh | UV_INSTALL_DIR="/usr/local/bin" sh
     uv self update
 fi
+uv python install 3.12
+uv python list
 docker --version
