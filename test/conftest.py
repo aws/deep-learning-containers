8cc6078bed3c907c31613e9b257c8ebd80e00def
sirutBuasai <sirutbuasai27@outlook.com>
2025-11-20 14:05:29 -0500

    move aws_session to global conftest
    Signed-off-by: sirutBuasai <sirutbuasai27@outlook.com>

--- test/conftest.py
+++ test/conftest.py
@@ -13,12 +13,27 @@
 """Common pytest fixtures for all tests under module test/"""
 
 import pytest
+from test_utils.aws import AWSSessionManager
+from test_utils.constants import DEFAULT_REGION
 
 
 def pytest_addoption(parser):
     parser.addoption("--image-uri", action="store", help="Image URI to be tested")
+    parser.addoption(
+        "--region", action="store", default=DEFAULT_REGION, help="AWS Region to test image on AWS"
+    )
 
 
 @pytest.fixture(scope="session")
 def image_uri(request):
     return request.config.getoption("--image-uri")
+
+
+@pytest.fixture(scope="session")
+def region(request):
+    return request.config.getoption("--region")
+
+
+@pytest.fixture(scope="session")
+def aws_session(region):
+    return AWSSessionManager(region)
