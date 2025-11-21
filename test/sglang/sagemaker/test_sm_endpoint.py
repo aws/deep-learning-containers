82bdb21fa52d4007da8b1be8165b966aee41e687
sirutBuasai <sirutbuasai27@outlook.com>
2025-11-21 18:16:45 -0500

    trigger tests
    Signed-off-by: sirutBuasai <sirutbuasai27@outlook.com>

--- test/sglang/sagemaker/test_sm_endpoint.py
+++ test/sglang/sagemaker/test_sm_endpoint.py
@@ -12,6 +12,7 @@
 # language governing permissions and limitations under the License.
 """Integration test for serving endpoint with SGLang DLC"""
 
+# TEST TRIGGER
 import json
 import logging
 from pprint import pformat
