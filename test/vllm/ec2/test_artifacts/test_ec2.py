b6fd5b0e86545a7c471d3dd18f12dbd5f3eefce5
jkottu <jkottu@amazon.com>
2025-12-09 15:50:41 -0800

    retest

--- test/vllm/ec2/test_artifacts/test_ec2.py
+++ test/vllm/ec2/test_artifacts/test_ec2.py
@@ -165,7 +165,6 @@ def test_vllm_benchmark_on_multi_node(head_connection, worker_connection, image_
 
         print("Running benchmark...")
         benchmark_cmd = "source vllm_env/bin/activate &&" + create_benchmark_command()
-        benchmark_cmd = create_benchmark_command()
         benchmark_result = head_connection.run(benchmark_cmd, timeout=7200)
 
         return benchmark_result
