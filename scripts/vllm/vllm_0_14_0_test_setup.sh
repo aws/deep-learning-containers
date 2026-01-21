ce941eb445caab8953387e18446d9b072d2f6eb0
Junpu Fan <junpufan@gmail.com>
2026-01-20 18:43:24 -0800

    fix test dep
    Signed-off-by: Junpu Fan <junpufan@gmail.com>

--- scripts/vllm/vllm_0_14_0_test_setup.sh
+++ scripts/vllm/vllm_0_14_0_test_setup.sh
@@ -2,8 +2,8 @@
 set -eux
 
 # delete old test dependencies file and regen
-# rm vllm_source/requirements/test.txt
-# uv pip compile vllm_source/requirements/test.in -o vllm_source/requirements/test.txt --index-strategy unsafe-best-match --torch-backend cu129 --python-platform x86_64-manylinux_2_28 --python-version 3.12
+rm vllm_source/requirements/test.txt
+uv pip compile vllm_source/requirements/test.in -o vllm_source/requirements/test.txt --index-strategy unsafe-best-match --torch-backend cu129 --python-platform x86_64-manylinux_2_28 --python-version 3.12
 uv pip install --system -r vllm_source/requirements/common.txt -r vllm_source/requirements/dev.txt --torch-backend=auto
 uv pip install --system pytest pytest-asyncio
 uv pip install --system -e vllm_source/tests/vllm_test_utils
