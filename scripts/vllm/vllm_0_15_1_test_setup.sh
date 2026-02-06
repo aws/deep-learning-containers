a226b9bf0d78a840be985e4775af785b7209b72e
Junpu Fan <junpu@amazon.com>
2026-02-06 15:49:11 -0800

    feat(vllm): Update vLLM from 0.15.0 to 0.15.1 (#5634)
    * feat(vllm): Update vLLM from 0.15.0 to 0.15.1
    
    Update vLLM base image and test configurations to use version 0.15.1.
    This patch version update includes bug fixes and improvements from the
    upstream vLLM project.
    
    Changes:
    - Update base Docker image to vllm/vllm-openai:v0.15.1
    - Update VLLM_VERSION environment variable to 0.15.1
    - Rename test setup script from vllm_0_15_0_test_setup.sh to vllm_0_15_1_test_setup.sh
    - Update all test workflow references to use new test setup script
    
    Signed-off-by: Junpu Fan <junpufan@gmail.com>
    
    * fix deps
    
    Signed-off-by: Junpu Fan <junpufan@gmail.com>
    
    ---------
    
    Signed-off-by: Junpu Fan <junpufan@gmail.com>

--- scripts/vllm/vllm_0_15_1_test_setup.sh
+++ scripts/vllm/vllm_0_15_1_test_setup.sh
@@ -4,7 +4,8 @@ set -eux
 # delete old test dependencies file and regen
 rm vllm_source/requirements/test.txt
 uv pip compile vllm_source/requirements/test.in -o vllm_source/requirements/test.txt --index-strategy unsafe-best-match --torch-backend cu129 --python-platform x86_64-manylinux_2_28 --python-version 3.12
-uv pip install --system -r vllm_source/requirements/common.txt -r vllm_source/requirements/dev.txt --torch-backend=auto
+# uv pip install --system -r vllm_source/requirements/common.txt --torch-backend=auto
+uv pip install --system -r vllm_source/requirements/dev.txt
 uv pip install --system pytest pytest-asyncio
 uv pip install --system -e vllm_source/tests/vllm_test_utils
 uv pip install --system hf_transfer
