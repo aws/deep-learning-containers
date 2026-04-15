45db4d497d0a3cc7dde4eccaaa77f074888c8cea
Yadan Wei <yadanwei@amazon.com>
2026-04-15 15:40:53 -0700

    chore: remove test build-cache files (tests passed)

--- docker/vllm/build-cache-test/hello.c
+++
@@ -1,5 +0,0 @@
-#include <Python.h>
-static PyObject* hello(PyObject* self, PyObject* args) { return PyUnicode_FromString("hello from cached wheel"); }
-static PyMethodDef methods[] = {{"hello", hello, METH_NOARGS, NULL}, {NULL, NULL, 0, NULL}};
-static struct PyModuleDef module = {PyModuleDef_HEAD_INIT, "hello_ext", NULL, -1, methods};
-PyMODINIT_FUNC PyInit_hello_ext(void) { return PyModule_Create(&module); }
--- docker/vllm/build-cache-test/setup.py
+++
@@ -1,3 +0,0 @@
-from setuptools import Extension, setup
-
-setup(name="hello-ext", version="0.1.0", ext_modules=[Extension("hello_ext", ["hello.c"])])
