9b2726c99c9864af8f84521df99415849a10567b
Enrique Hernández Calabrés <ehcalabres@gmail.com>
2026-05-20 12:50:20 +0200

    Update vLLM to version 0.21.0 and transformers to version 5.8.1

--- huggingface/vllm/telemetry.sh
+++ huggingface/vllm/telemetry.sh
@@ -4,8 +4,9 @@ if [ -f /usr/local/bin/deep_learning_container.py ] && [[ -z "${OPT_OUT_TRACKING
     (
         python /usr/local/bin/deep_learning_container.py \
             --framework "huggingface_vllm" \
-            --framework-version "0.19.1" \
+            --framework-version "0.21.0" \
             --container-type "general" \
             &>/dev/null &
     )
 fi
+
