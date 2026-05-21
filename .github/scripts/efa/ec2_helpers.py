0fc5bcfde6b5812dad8654343b641bf1f4351735
Kevin Wang <kwanggg@amazon.com>
2026-05-21 04:02:36 +0000

    fix: use before_date=2026-05-12 to get AMI with Docker + pre-embargo driver

--- .github/scripts/efa/ec2_helpers.py
+++ .github/scripts/efa/ec2_helpers.py
@@ -395,7 +395,7 @@ def efa_instances(image_uri, instance_type="p4d.24xlarge", region=DEFAULT_REGION
     aws_session = AWSSessionManager(region=region)
     # TODO: Remove before_date once DLAMI publishes driver >= 580.159 (post-embargo).
     # The 20260519 AMI has embargo driver 580.150 which is incompatible with CUDA 13.0.2 on A100.
-    ami_id = aws_session.get_latest_ami(before_date="2026-05-05")
+    ami_id = aws_session.get_latest_ami(before_date="2026-05-12")
     sg_id = get_efa_security_group_id(aws_session)
 
     key_name = None
