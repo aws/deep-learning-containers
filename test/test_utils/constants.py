d42759c51ca1898c52ea2ef612d58f0d7f657bfa
Jinyan Li <jinyali@amazon.com>
2026-06-04 22:50:25 -0700

    fix: pin ray sagemaker test to cu12 ami

--- test/test_utils/constants.py
+++ test/test_utils/constants.py
@@ -2,4 +2,6 @@ DEFAULT_REGION = "us-west-2"
 EC2_INSTANCE_ROLE_NAME = "ec2TestInstanceRole"
 SAGEMAKER_ROLE = "SageMakerRole"
 # https://docs.aws.amazon.com/sagemaker/latest/APIReference/API_ProductionVariant.html
-INFERENCE_AMI_VERSION = "al2023-ami-sagemaker-inference-gpu-4-1"
+INFERENCE_AMI_VERSION_CU12 = "al2-ami-sagemaker-inference-gpu-3-1"
+INFERENCE_AMI_VERSION_CU13 = "al2023-ami-sagemaker-inference-gpu-4-1"
+INFERENCE_AMI_VERSION = INFERENCE_AMI_VERSION_CU13
