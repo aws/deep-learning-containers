e4a208fa3a59a61e881d36db582323f3b9f46e09
Jyothirmai Kottu <jyothirmaikottu@gmail.com>
2026-07-01 11:15:58 -0700

    Upgrade DLC EKS clusters to Kubernetes 1.36 (#6330)
    Set build_param.json operation to upgrade_cluster and eks_version to 1.36
    for the PR and MAINLINE contexts (dlc-pytorch, dlc-tensorflow). Bump the
    kubectl client pin in env_setup.sh to 1.35.3, which is version-skew
    compatible with a 1.36 control plane. cluster-autoscalar image stays at
    v1.35.0 until the 1.36 image is published.

--- eks_infrastructure/env_setup.sh
+++ eks_infrastructure/env_setup.sh
@@ -5,7 +5,7 @@ set -ex
 
 # The below url/version is based on EKS v1.32.0. The same needs to be updated for EKS version upgrade.
 # https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html find the link
-KUBECTL_CLIENT="https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.0/2026-01-29/bin/linux/amd64/kubectl"
+KUBECTL_CLIENT="https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.3/2026-04-08/bin/linux/amd64/kubectl"
 EKSCTL_CLIENT="https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz"
 # https://github.com/kubernetes-sigs/aws-iam-authenticator/releases?page=1 to find the version support
 AWS_IAM_AUTHENTICATOR="https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.7.10/aws-iam-authenticator_0.7.10_linux_amd64"
