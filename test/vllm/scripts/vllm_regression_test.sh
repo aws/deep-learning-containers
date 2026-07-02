6a51f6437a0a90067eaafa94fcea12cc7c8a289e
sirutBuasai <sirutbuasai27@outlook.com>
2026-07-01 23:03:39 -0700

    add modelscope comment
    Signed-off-by: sirutBuasai <sirutbuasai27@outlook.com>

--- test/vllm/scripts/vllm_regression_test.sh
+++ test/vllm/scripts/vllm_regression_test.sh
@@ -11,5 +11,6 @@ fi
 cd vllm_source/tests
 # modelscope<1.38: 1.38+ removed `revision` kwarg from LegacyHubApi.get_model_files(),
 # breaking vLLM's repo_utils.py. Unpin after upstream vLLM adapts to the new API.
+# unpin after https://github.com/vllm-project/vllm/pull/47325 is merged
 uv pip install $UV_FLAGS "modelscope<1.38"
 pytest -v -s test_regression.py
