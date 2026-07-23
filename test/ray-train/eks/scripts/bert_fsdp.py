0986fd146c99b426f5db0be3fb480bf883556851
Kevin Wang <kwanggg@amazon.com>
2026-07-23 00:01:44 +0000

    ray-train: default run-efa-test to true (remove explicit autorelease override)

--- test/ray-train/eks/scripts/bert_fsdp.py
+++ test/ray-train/eks/scripts/bert_fsdp.py
@@ -5,28 +5,29 @@ integration, HF transformers/datasets, checkpoint round-trip, FSx read path.
 
 Data is pre-staged to /fsx/hf_cache (HF_HUB_OFFLINE=1, no runtime downloads).
 """
+
 import os
+
 os.environ["HF_HOME"] = "/fsx/hf_cache"
 os.environ["HF_HUB_OFFLINE"] = "1"
 
-import torch
-import torch.nn.functional as F
 import pytorch_lightning as pl
-from torch.distributed.fsdp import ShardingStrategy, BackwardPrefetch
-
 import ray
 import ray.train
-from ray.train import RunConfig, ScalingConfig, CheckpointConfig
-from ray.train.torch import TorchTrainer
+import torch
+import torch.nn.functional as F
+from datasets import load_dataset
+from evaluate import load as load_metric
+from ray.train import CheckpointConfig, RunConfig, ScalingConfig
 from ray.train.lightning import (
     RayFSDPStrategy,
     RayLightningEnvironment,
     RayTrainReportCallback,
     prepare_trainer,
 )
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from datasets import load_dataset
-from evaluate import load as load_metric
+from ray.train.torch import TorchTrainer
+from torch.distributed.fsdp import BackwardPrefetch, ShardingStrategy
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 MODEL_NAME = "bert-base-cased"
 NUM_WORKERS = 8
@@ -53,9 +54,7 @@ class SentimentModel(pl.LightningModule):
         super().__init__()
         self.lr = lr
         self.eps = eps
-        self.model = AutoModelForSequenceClassification.from_pretrained(
-            MODEL_NAME, num_labels=2
-        )
+        self.model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=2)
         self.metric = load_metric("glue", "cola")
         self.predictions = []
         self.references = []
